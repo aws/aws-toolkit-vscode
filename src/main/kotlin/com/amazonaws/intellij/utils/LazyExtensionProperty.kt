d78a0f8223db5305a87e84283fee2737ffa05403
Kyle Thomson <kylthoms@amazon.com>
2017-01-13 11:52:26 -0800

    Adding dokka Kotlin documentation generator

---
+++ src/main/kotlin/com/amazonaws/intellij/utils/LazyExtensionProperty.kt
@@ -0,0 +1,34 @@
+package com.amazonaws.intellij.utils
+
+import java.util.*
+import kotlin.reflect.KProperty
+
+/**
+ * A helper that allows extension properties to be lazily evaluated and the result cached
+ * Useful for extending types with expensive lookup functions.
+ *
+ * @sample LazyExtensionProperty.sample
+ */
+class LazyExtensionProperty<TypeBeingExtended, Return>(val initializer: TypeBeingExtended.() -> Return) {
+    private val values = WeakHashMap<TypeBeingExtended, Return>()
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Return = synchronized(values)
+    {
+        thisRef as TypeBeingExtended
+        return values.getOrPut(thisRef) { thisRef.initializer() }
+    }
+}
+
+/**
+ * Documentation only
+ */
+@Suppress("UNUSED")
+private class LazyExtensionPropertyExample {
+    data class SomeClass(val property: String)
+
+    val SomeClass.lazilyEvaluatedExtensionProperty: String by LazyExtensionProperty<SomeClass, String> {
+        //some expensive function that determines value of lazilyEvaluatedExtensionProperty
+        this.property
+    }
+}
