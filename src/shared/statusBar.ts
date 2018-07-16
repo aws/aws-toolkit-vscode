41ac8faf584924dbef2c95e99307b29089bb9b25
Steve Roberts <strobe@amazon.com>
2018-07-16 15:54:19 -0700

    Picked up some new icons for demo purposes

--- src/shared/statusBar.ts
+++ src/shared/statusBar.ts
@@ -33,7 +33,7 @@ export class AWSStatusBar {
         }
 
         if (profileName && region) {
-            this.credentialAndRegionContext.text = 'AWS: ' + profileName + '@' + region;
+            this.credentialAndRegionContext.text = 'AWS: ' + profileName + '/' + region;
             this.credentialAndRegionContext.show();
         } else {
             this.credentialAndRegionContext.hide();
