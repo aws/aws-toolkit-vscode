06b8d1d7530fe360f5ea1e8b52a85ad070654f4e
Austin Brooks <alfredbr@amazon.com>
2018-07-19 10:07:37 -0700

    Remove flags from Icons

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/Icons.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/Icons.kt
@@ -24,14 +24,4 @@ object Icons {
         @JvmField val EC2_SERVICE_ICON = IconLoader.getIcon("/icons/rds-service.png")
         @JvmField val EC2_INSTANCE_ICON = IconLoader.getIcon("/icons/index.png")
     }
-
-    object Flags {
-        @JvmField val EU_ICON = IconLoader.getIcon("/icons/flags/eu.png")
-        @JvmField val USA_ICON = IconLoader.getIcon("/icons/flags/us.png")
-        @JvmField val SINGAPORE_ICON = IconLoader.getIcon("/icons/flags/singapore.png")
-        @JvmField val JAPAN_ICON = IconLoader.getIcon("/icons/flags/japan.png")
-        @JvmField val AUSTRALIA_ICON = IconLoader.getIcon("/icons/flags/australia.png")
-        @JvmField val BRAZIL_ICON = IconLoader.getIcon("/icons/flags/brazil.png")
-        @JvmField val IRELAND_ICON = IconLoader.getIcon("/icons/flags/ireland.png")
-    }
 }
