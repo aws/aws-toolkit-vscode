d4cd868d058d14261e2dfb9d10bcaab387024aef
Austin Brooks <alfredbr@amazon.com>
2018-07-19 10:11:56 -0700

    Disable S3 (#89)
    * Disable S3 bucket browser
    * Disable S3 explorer node

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
@@ -3,16 +3,9 @@ package software.aws.toolkits.jetbrains.core.explorer
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import software.amazon.awssdk.services.lambda.LambdaClient
-import software.amazon.awssdk.services.s3.S3Client
 import software.aws.toolkits.jetbrains.services.lambda.LambdaServiceNode
-import software.aws.toolkits.jetbrains.services.s3.S3ServiceNode
 
 enum class AwsExplorerService(val serviceId: String) {
-    S3(S3Client.SERVICE_NAME) {
-        override fun buildServiceRootNode(project: Project): S3ServiceNode {
-            return S3ServiceNode(project)
-        }
-    },
     LAMBDA(LambdaClient.SERVICE_NAME) {
         override fun buildServiceRootNode(project: Project): LambdaServiceNode {
             return LambdaServiceNode(project)
