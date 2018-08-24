0d5010f1f8ff3d9221148e5edf34a2733117661c
Austin Brooks <alfredbr@amazon.com>
2018-08-24 13:56:02 -0700

    Add support for using templates or files in Lambda run configurations
    * Add the Lambda input templates to run configuration editor
    * Add the ability to load template from a file
    * Add the ability to use a static input file
    
    Fixes #150, #151

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/local/LocalLambdaRunSettingsEditorPanel.java
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/local/LocalLambdaRunSettingsEditorPanel.java
@@ -4,32 +4,77 @@ import com.intellij.json.JsonLanguage;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComboBox;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.ui.ComboboxWithBrowseButton;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.EditorTextFieldProvider;
+import com.intellij.ui.SortedComboBoxModel;
 import com.intellij.util.textCompletion.TextCompletionProvider;
 import com.intellij.util.textCompletion.TextFieldWithCompletion;
+import com.intellij.util.ui.UIUtil;
 import java.util.Collections;
+import java.util.Comparator;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import software.amazon.awssdk.services.lambda.model.Runtime;
+import software.aws.toolkits.core.lambda.LambdaSampleEvent;
 import software.aws.toolkits.jetbrains.ui.EnvironmentVariablesTextField;
 
 public final class LocalLambdaRunSettingsEditorPanel {
     JPanel panel;
     EditorTextField handler;
-    EditorTextField input;
+    EditorTextField inputText;
     EnvironmentVariablesTextField environmentVariables;
     ComboBox<Runtime> runtime;
+    ComboboxWithBrowseButton inputTemplates;
+    JRadioButton useInputFile;
+    JRadioButton useInputText;
+    TextFieldWithBrowseButton inputFile;
+    ComboBox<LambdaSampleEvent> eventComboBox;
+    SortedComboBoxModel<LambdaSampleEvent> eventComboBoxModel;
     private final Project project;
     private final TextCompletionProvider handlerCompletionProvider;
 
     public LocalLambdaRunSettingsEditorPanel(Project project, TextCompletionProvider handlerCompletionProvider) {
         this.project = project;
         this.handlerCompletionProvider = handlerCompletionProvider;
+
+        useInputText.addActionListener(e -> updateComponents());
+        useInputFile.addActionListener(e -> updateComponents());
+
+        updateComponents();
     }
 
     private void createUIComponents() {
         EditorTextFieldProvider textFieldProvider = ServiceManager.getService(project, EditorTextFieldProvider.class);
-        input = textFieldProvider.getEditorField(JsonLanguage.INSTANCE, project, Collections.emptyList());
+        inputText = textFieldProvider.getEditorField(JsonLanguage.INSTANCE, project, Collections.emptyList());
         handler = new TextFieldWithCompletion(project, handlerCompletionProvider, "", true, true, true, true);
+
+        eventComboBoxModel = new SortedComboBoxModel<>(Comparator.comparing(LambdaSampleEvent::getName));
+        eventComboBox = new ComboBox<>(eventComboBoxModel);
+        inputTemplates = new ComboboxWithBrowseButton(eventComboBox);
+    }
+
+    private void updateComponents() {
+        inputTemplates.setEnabled(useInputText.isSelected());
+        inputText.setEnabled(useInputText.isSelected());
+        if(inputText.isEnabled()) {
+            inputText.setBackground(UIUtil.getTextFieldBackground());
+        } else {
+            inputText.setBackground(panel.getBackground());
+        }
+
+        inputFile.setEnabled(useInputFile.isSelected());
+    }
+
+    public boolean isUsingInputFile() {
+        return useInputFile.isSelected();
+    }
+
+    public void setUsingInputFile(boolean value) {
+        useInputFile.setSelected(value);
+        useInputText.setSelected(!value);
+
+        updateComponents();
     }
 }
--- jetbrains-core/src/software/aws/toolkits/jetbrains/utils/ui/UiUtils.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/utils/ui/UiUtils.kt
@@ -1,8 +1,14 @@
 package software.aws.toolkits.jetbrains.utils.ui
 
+import com.intellij.lang.Language
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.ui.ComboBox
+import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
+import com.intellij.ui.EditorTextField
 import javax.swing.DefaultComboBoxModel
 import javax.swing.JComboBox
 import javax.swing.JTextField
@@ -39,3 +45,23 @@ fun JTextField?.blankAsNull(): String? = if (this?.text?.isNotBlank() == true) {
 
 @Suppress("UNCHECKED_CAST")
 fun <T> JComboBox<T>?.selected(): T? = this?.selectedItem as? T
+
+fun EditorTextField.formatAndSet(content: String, language: Language) {
+    // Initial docs can't be undo'ed
+    CommandProcessor.getInstance().runUndoTransparentAction {
+        runWriteAction {
+            val formatted =
+                LanguageCodeStyleSettingsProvider.createFileFromText(
+                    language,
+                    project,
+                    content
+                )?.let {
+                    CodeStyleManager.getInstance(project).reformat(it)
+                    it.text
+                } ?: content
+
+            val document = this.document
+            document.replaceString(0, document.textLength, formatted)
+        }
+    }
+}
