05430ab2fc2bcb58d08886d991e6949111f23785
Kyle Thomson <kylthoms@amazon.com>
2018-08-28 13:09:28 -0700

    Adding changelog to the JetBrains plugin xml.

--- buildSrc/src/toolkits/gradle/ChangeLog.kt
+++ buildSrc/src/toolkits/gradle/ChangeLog.kt
@@ -10,11 +10,9 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import java.io.BufferedWriter
 import java.io.File
 import java.nio.file.Path
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import kotlin.streams.toList
 
 val MAPPER: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()).enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
@@ -23,42 +21,6 @@ val MAPPER: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()
 interface ChangeLogWriter {
     fun write(entry: ReleaseEntry)
     fun flush()
-}
-
-class GithubWriter(file: Path) : ChangeLogWriter {
-
-    private val writer = file.toFile().bufferedWriter()
-
-    override fun write(entry: ReleaseEntry) {
-        writer.writeLine("# _${entry.version}_ (${DateTimeFormatter.ISO_DATE.format(entry.date)})")
-
-        val entries = entry.entries.groupBy { it.type }
-
-        ChangeType.values().forEach { type ->
-            entries[type]?.run { writer.writeSection(type.sectionTitle, this.map { it.description }) }
-        }
-        writer.newLine()
-    }
-
-    override fun flush() {
-        writer.flush()
-        writer.close()
-    }
-
-    private companion object {
-        fun BufferedWriter.writeLine(text: String) {
-            write(text)
-            newLine()
-        }
-
-        fun BufferedWriter.writeSection(section: String, entries: List<String>) {
-            entries.forEach {
-                val lines = it.lines()
-                writeLine("  - **($section)** ${lines.first().trim()}")
-                lines.takeLast(lines.size - 1).map { "    ${it.trim()}" }.forEach { writeLine(it) }
-            }
-        }
-    }
 }
 
 class ChangeLogGenerator(private vararg val writers: ChangeLogWriter) {
@@ -103,7 +65,8 @@ class ReleaseCreator(private val unreleasedFiles: List<File>, private val nextRe
     }
 
     fun create(version: String, date: LocalDate = LocalDate.now()) {
-        val entries = unreleasedFiles.map { readFile<Entry>(it) }
+        val entriesByType = unreleasedFiles.map { readFile<Entry>(it) }.groupBy { it.type }
+        val entries = ChangeType.values().flatMap { entriesByType.getOrDefault(it, emptyList()) }
         val release = ReleaseEntry(date, version, entries)
 
         MAPPER.writerWithDefaultPrettyPrinter().writeValue(nextReleaseFile, release)
---
+++ buildSrc/src/toolkits/gradle/GithubWriter.kt
@@ -0,0 +1,34 @@
+package toolkits.gradle
+
+import java.io.BufferedWriter
+import java.nio.file.Path
+import java.time.format.DateTimeFormatter
+
+class GithubWriter(file: Path) : ChangeLogWriter {
+
+    private val writer = file.toFile().bufferedWriter()
+
+    override fun write(entry: ReleaseEntry) {
+        writer.writeLine("# _${entry.version}_ (${DateTimeFormatter.ISO_DATE.format(entry.date)})")
+        entry.entries.forEach { writer.writeEntry(it) }
+        writer.newLine()
+    }
+
+    override fun flush() {
+        writer.flush()
+        writer.close()
+    }
+
+    private companion object {
+        fun BufferedWriter.writeLine(text: String) {
+            write(text)
+            newLine()
+        }
+
+        fun BufferedWriter.writeEntry(entry: Entry) {
+            val lines = entry.description.lines()
+            writeLine("  - **(${entry.type.sectionTitle})** ${lines.first().trim()}")
+            lines.takeLast(lines.size - 1).map { "    ${it.trim()}" }.forEach { writeLine(it) }
+        }
+    }
+}
