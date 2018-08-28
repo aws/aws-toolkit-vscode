750db9759eb3510dd50dab5ec93e84ead34e8515
Austin Brooks <alfredbr@amazon.com>
2018-08-28 14:37:19 -0700

    Create a custom ktlint rule for copyright header

---
+++ ktlint-rules/src/software/aws/toolkits/ktlint/rules/CopyrightHeaderRule.kt
@@ -0,0 +1,26 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.ktlint.rules
+
+import com.github.shyiko.ktlint.core.Rule
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import java.time.Clock
+import java.time.LocalDate
+
+class CopyrightHeaderRule(clock: Clock = Clock.systemUTC()) : Rule("copyright-header"), Rule.Modifier.RestrictToRoot {
+    private val header = """
+            // Copyright ${LocalDate.now(clock).year} Amazon.com, Inc. or its affiliates. All Rights Reserved.
+            // SPDX-License-Identifier: Apache-2.0
+        """.trimIndent()
+
+    override fun visit(
+        node: ASTNode,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        if (!node.text.startsWith(header)) {
+            emit(node.startOffset, "Missing or incorrect file header", false)
+        }
+    }
+}
