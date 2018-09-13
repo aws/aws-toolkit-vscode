ff731341d27f96c64159d9822d7bcdc0a1cf157d
Austin Brooks <alfredbr@amazon.com>
2018-09-13 13:27:45 -0700

    Fix tool window name

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerFactory.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerFactory.kt
@@ -7,10 +7,15 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import software.aws.toolkits.resources.message
 
 @Suppress("unused")
 class AwsExplorerFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         toolWindow.component.parent.add(ExplorerToolWindow(project))
+    }
+
+    override fun init(toolWindow: ToolWindow) {
+        toolWindow.stripeTitle = message("explorer.label")
     }
 }
