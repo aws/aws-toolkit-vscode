1eb13244db0a91e6570ec8812e6764235326e16b
Christopher Christou <chrstou@amazon.com>
2018-09-17 14:07:56 -0700

    When signing into AWS, and the profile's default region is hidden in the Explorer, the Toolkit now provides opportunity to automatically show it in the Explorer. Also: - VS Code shows credential profiles from `credentials` and `config` files now

--- src/shared/credentials/defaultCredentialsFileReaderWriter.ts
+++ src/shared/credentials/defaultCredentialsFileReaderWriter.ts
@@ -6,22 +6,58 @@
 'use strict'
 
 import { CredentialsFileReaderWriter } from "./credentialsFileReaderWriter"
-import { loadSharedConfigFiles, saveProfile } from "./credentialsFile"
+import { loadSharedConfigFiles, saveProfile, Profile } from "./credentialsFile"
 
 export class DefaultCredentialsFileReaderWriter implements CredentialsFileReaderWriter {
     async getProfileNames(): Promise<string[]> {
-        let profileNames: string[] = []
-
         // TODO: cache the file and attach a watcher to it
         const credentialFiles = await loadSharedConfigFiles()
-        profileNames = Object.keys(credentialFiles.credentialsFile)
 
-        return new Promise<string[]>(resolve => {
-            resolve(profileNames)
-        })
+        const credentialsProfiles = Object.keys(credentialFiles.credentialsFile)
+        const configProfiles = Object.keys(credentialFiles.configFile)
+
+        const profileNames = new Set(credentialsProfiles.concat(configProfiles))
+
+        return Promise.resolve(Array.from(profileNames))
     }
 
     async addProfileToFile(profileName: string, accessKey: string, secretKey: string): Promise<void> {
         await saveProfile(profileName, accessKey, secretKey)
+    }
+
+    /**
+     * Gets the default region for a credentials profile
+     * 
+     * @param profileName Profile to get the default region from
+     * @returns Default region, undefined if region is not set
+     */
+    async getDefaultRegion(profileName: string): Promise<string | undefined> {
+        const profile = await this.getProfile(profileName)
+        return Promise.resolve(profile.region)
+    }
+
+    /**
+     * Returns a credentials profile, combined from the config and credentials file where applicable.
+     * 
+     * @param profileName Credentials Profile to load
+     * @returns Profile data. Nonexistent Profiles will return an empty mapping.
+     */
+    private async getProfile(profileName: string): Promise<Profile> {
+        const credentialFiles = await loadSharedConfigFiles()
+
+        let profile: Profile = {}
+
+        if (credentialFiles.configFile && credentialFiles.configFile[profileName]) {
+            profile = credentialFiles.configFile[profileName]
+        }
+
+        if (credentialFiles.credentialsFile && credentialFiles.credentialsFile[profileName]) {
+            const credentialsProfile = credentialFiles.credentialsFile[profileName]
+            for (const index in credentialsProfile) {
+                profile[index] = credentialsProfile[index]
+            }
+        }
+
+        return Promise.resolve(profile)
     }
 }
