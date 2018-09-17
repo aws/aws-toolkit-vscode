8c7a0ab66d102f75209879d3436821318d2ba35d
Matthew Pirocchi <pirocchi@amazon.com>
2018-09-17 14:11:56 -0700

    Don't hard-code expected version numbers in unit tests.

--- src/test/awsClientBuilder.test.ts
+++ src/test/awsClientBuilder.test.ts
@@ -32,18 +32,25 @@ suite('AwsClientBuilder Tests', () => {
         }
     }
 
+    // We don't want to test against the versions reported by package.json and vscode.version--this is
+    // what the product code does, so these tests would still pass with bad versions. Instead, we
+    // verify that a valid semver is used. This protects us against things like `null`, `undefined`,
+    // or other unexpected values.
+    const semverRegex = require('semver-regex')() as RegExp;
+    const userAgentRegex = new RegExp("^AWS-Toolkit-For-VisualStudio\\/" + semverRegex.source + " Visual-Studio-Code\\/" + semverRegex.source);
+
     test('createAndConfigureSdkClient includes custom user-agent if no options are specified', async () => {
         const builder = new AWSClientBuilder(new FakeAwsContext());
         const service = await builder.createAndConfigureSdkClient(FakeService);
-            
-        assert.equal(service.config.customUserAgent, 'AWS-Toolkit-For-VisualStudio/0.0.1 Visual-Studio-Code/1.27.2');
+        
+        assert.equal(userAgentRegex.test(service.config.customUserAgent), true);
     });
 
     test('createAndConfigureSdkClient includes custom user-agent if not specified in options', async () => {
         const builder = new AWSClientBuilder(new FakeAwsContext());
         const service = await builder.createAndConfigureSdkClient(FakeService, {});
 
-        assert.equal(service.config.customUserAgent, 'AWS-Toolkit-For-VisualStudio/0.0.1 Visual-Studio-Code/1.27.2');
+        assert.equal(userAgentRegex.test(service.config.customUserAgent), true);
     });
 
     test('createAndConfigureSdkClient does not override custom user-agent if specified in options', async () => {
