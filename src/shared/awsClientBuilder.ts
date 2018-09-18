8f32c132081398867f2b43004d480d5428548df4
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2018-09-18 13:08:21 -0400

    Include custom user-agent with requests. (#52)

--- src/shared/awsClientBuilder.ts
+++ src/shared/awsClientBuilder.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import { AwsContext } from './awsContext';
+import { env, version } from 'vscode';
 
 export class AWSClientBuilder {
 
@@ -13,22 +14,27 @@ export class AWSClientBuilder {
 
     // centralized construction of transient AWS service clients, allowing us
     // to customize requests and/or user agent
-    public async createAndConfigureSdkClient(awsService: any, awsServiceOpts: any | undefined, region: string | undefined): Promise<any> {
+    public async createAndConfigureSdkClient(
+        awsService: any,
+        awsServiceOpts: any | undefined = {},
+        region: string | undefined = undefined
+    ): Promise<any> {
+        
 
-        if (awsServiceOpts) {
-            if (!awsServiceOpts.credentials) {
-                awsServiceOpts.credentials = await this._awsContext.getCredentials();
-            }
-            if (!awsServiceOpts.region && region) {
-                awsServiceOpts.region = region;
-            }
-
-            return new awsService(awsServiceOpts);
+        if (!awsServiceOpts.credentials) {
+            awsServiceOpts.credentials = await this._awsContext.getCredentials();
         }
 
-        return new awsService({
-            credentials: await this._awsContext.getCredentials(),
-            region: region
-        });
+        if (!awsServiceOpts.region && region) {
+            awsServiceOpts.region = region;
+        }
+
+        if (!awsServiceOpts.customUserAgent) {
+            const pluginVersion: string = require('../../package.json').version;
+            const platformName = env.appName.replace(/\s/g, '-');
+            awsServiceOpts.customUserAgent = `AWS-Toolkit-For-VisualStudio/${pluginVersion} ${platformName}/${version}`;
+        }
+
+        return new awsService(awsServiceOpts);
     }
 }
---
+++ src/test/awsClientBuilder.test.ts
@@ -0,0 +1,62 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import { ContextChangeEventsArgs } from '../shared/defaultAwsContext';
+import { AwsContext } from '../shared/awsContext';
+import { AWSClientBuilder } from '../shared/awsClientBuilder';
+
+suite('AwsClientBuilder Tests', () => {
+    class FakeAwsContext implements AwsContext {
+        onDidChangeContext: vscode.Event<ContextChangeEventsArgs> = new vscode.EventEmitter<ContextChangeEventsArgs>().event;
+        getCredentials(): Promise<AWS.Credentials | undefined> {
+            return Promise.resolve(undefined);
+        }
+        getCredentialProfileName(): string | undefined {
+            throw new Error('Method not implemented.');
+        }
+        setCredentialProfileName(profileName?: string | undefined): Promise<void> {
+            throw new Error('Method not implemented.');
+        }
+        getExplorerRegions(): Promise<string[]> {
+            throw new Error('Method not implemented.');
+        }
+        addExplorerRegion(region: string | string[]): Promise<void> {
+            throw new Error('Method not implemented.');
+        }
+        removeExplorerRegion(region: string | string[]): Promise<void> {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    class FakeService {
+        constructor(public config: any) {
+        }
+    }
+
+    // We don't want to test against the versions reported by package.json and vscode.version--this is
+    // what the product code does, so these tests would still pass with bad versions. Instead, we
+    // verify that a valid semver is used. This protects us against things like `null`, `undefined`,
+    // or other unexpected values.
+    const semverRegex = require('semver-regex')() as RegExp;
+    const userAgentRegex = new RegExp("^AWS-Toolkit-For-VisualStudio\\/" + semverRegex.source + " Visual-Studio-Code\\/" + semverRegex.source);
+
+    test('createAndConfigureSdkClient includes custom user-agent if no options are specified', async () => {
+        const builder = new AWSClientBuilder(new FakeAwsContext());
+        const service = await builder.createAndConfigureSdkClient(FakeService);
+        
+        assert.equal(userAgentRegex.test(service.config.customUserAgent), true);
+    });
+
+    test('createAndConfigureSdkClient includes custom user-agent if not specified in options', async () => {
+        const builder = new AWSClientBuilder(new FakeAwsContext());
+        const service = await builder.createAndConfigureSdkClient(FakeService, {});
+
+        assert.equal(userAgentRegex.test(service.config.customUserAgent), true);
+    });
+
+    test('createAndConfigureSdkClient does not override custom user-agent if specified in options', async () => {
+        const builder = new AWSClientBuilder(new FakeAwsContext());
+        const service = await builder.createAndConfigureSdkClient(FakeService, { customUserAgent: 'CUSTOM USER AGENT' });
+
+        assert.equal(service.config.customUserAgent, 'CUSTOM USER AGENT');
+    });
+});
