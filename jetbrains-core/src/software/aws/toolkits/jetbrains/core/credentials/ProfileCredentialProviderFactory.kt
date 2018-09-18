1d8d3ddb822bf649132633cebfb0953ac411ad0e
Austin Brooks <alfredbr@amazon.com>
2018-09-18 15:16:34 -0700

    Remove the Env Var and Sys Prop credential providers

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/ProfileCredentialProviderFactory.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/ProfileCredentialProviderFactory.kt
@@ -3,9 +3,7 @@
 
 package software.aws.toolkits.jetbrains.core.credentials
 
-import software.aws.toolkits.core.credentials.EnvironmentVariableToolkitCredentialsProviderFactory
 import software.aws.toolkits.core.credentials.ProfileToolkitCredentialsProviderFactory
-import software.aws.toolkits.core.credentials.SystemPropertyToolkitCredentialsProviderFactory
 import software.aws.toolkits.core.credentials.ToolkitCredentialsProviderFactory
 import software.aws.toolkits.jetbrains.core.AwsSdkClient
 import software.aws.toolkits.jetbrains.core.region.AwsRegionProvider
@@ -16,17 +14,5 @@ class ProfileCredentialProviderFactory : CredentialProviderFactory {
             AwsSdkClient.getInstance().sdkHttpClient,
             AwsRegionProvider.getInstance()
         )
-    }
-}
-
-class EnvironmentCredentialProviderFactory : CredentialProviderFactory {
-    override fun createToolkitCredentialProviderFactory(): ToolkitCredentialsProviderFactory {
-        return EnvironmentVariableToolkitCredentialsProviderFactory()
-    }
-}
-
-class SystemPropertyCredentialProviderFactory : CredentialProviderFactory {
-    override fun createToolkitCredentialProviderFactory(): ToolkitCredentialsProviderFactory {
-        return SystemPropertyToolkitCredentialsProviderFactory()
     }
 }
