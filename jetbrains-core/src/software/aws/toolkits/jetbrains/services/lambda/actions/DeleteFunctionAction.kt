5571232881654dca2fa5ae3f5ccb0187b289e6af
Austin Brooks <alfredbr@amazon.com>
2018-09-18 14:50:11 -0700

    Make the Delete Function action dumb aware
    * Also move it into the actions sub package
    
    Fixes #227

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/actions/DeleteFunctionAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/actions/DeleteFunctionAction.kt
@@ -1,9 +1,10 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package software.aws.toolkits.jetbrains.services.lambda
+package software.aws.toolkits.jetbrains.services.lambda.actions
 
 import software.aws.toolkits.jetbrains.core.DeleteResourceAction
+import software.aws.toolkits.jetbrains.services.lambda.LambdaFunctionNode
 
 class DeleteFunctionAction : DeleteResourceAction<LambdaFunctionNode>() {
     override fun performDelete(selected: LambdaFunctionNode) {
