7326e06f43c92c34a4f71818e3ffde163a7faaf7
Austin Brooks <alfredbr@amazon.com>
2018-10-03 13:27:22 -0700

    Add SAM CLI Java Debug integ test

--- jetbrains-core/it/software/aws/toolkits/jetbrains/services/lambda/execution/sam/SamRunConfigurationIntegrationTest.kt
+++ jetbrains-core/it/software/aws/toolkits/jetbrains/services/lambda/execution/sam/SamRunConfigurationIntegrationTest.kt
@@ -4,9 +4,16 @@
 package software.aws.toolkits.jetbrains.services.lambda.execution.sam
 
 import com.intellij.compiler.CompilerTestUtil
+import com.intellij.debugger.DebuggerManagerEx
+import com.intellij.debugger.engine.DebugProcessAdapterImpl
+import com.intellij.debugger.engine.SuspendContextImpl
+import com.intellij.debugger.impl.DebuggerManagerListener
+import com.intellij.debugger.impl.DebuggerSession
+import com.intellij.debugger.impl.PrioritizedTask
 import com.intellij.execution.ExecutorRegistry
 import com.intellij.execution.Output
 import com.intellij.execution.OutputListener
+import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.process.ProcessEvent
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
@@ -16,6 +23,8 @@ import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.projectRoots.impl.JavaAwareProjectJdkTableImpl
 import com.intellij.openapi.roots.CompilerProjectExtension
 import com.intellij.openapi.roots.ModuleRootModificationUtil
+import com.intellij.openapi.util.Key
+import com.intellij.psi.PsiJavaFile
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.runInEdtAndWait
 import org.assertj.core.api.Assertions.assertThat
@@ -41,7 +50,7 @@ class SamRunConfigurationIntegrationTest {
 
         val fixture = projectRule.fixture
         val module = fixture.addModule("main")
-        fixture.addClass(
+        val psiClass = fixture.addClass(
             module,
             """
             package com.example;
@@ -53,6 +62,9 @@ class SamRunConfigurationIntegrationTest {
             }
             """
         )
+        runInEdtAndWait {
+            fixture.openFileInEditor(psiClass.containingFile.virtualFile)
+        }
 
         setUpCompiler()
     }
@@ -89,18 +101,70 @@ class SamRunConfigurationIntegrationTest {
         assertThat(runConfiguration).isNotNull
 
         val executeLambda = executeLambda(runConfiguration)
-        println(executeLambda.stderr)
         assertThat(executeLambda.exitCode).isEqualTo(0)
         assertThat(executeLambda.stdout).contains("HELLO WORLD")
     }
 
-    private fun executeLambda(runConfiguration: SamRunConfiguration): Output {
-        val executor = ExecutorRegistry.getInstance().getExecutorById(DefaultRunExecutor.EXECUTOR_ID)
+    @Test
+    fun samIsExecutedWithDebugger() {
+        val debugManager = DebuggerManagerEx.getInstanceEx(projectRule.project)
+        runInEdtAndWait {
+            val breakpointManager = debugManager.breakpointManager
+
+            val document = projectRule.fixture.editor.document
+            val lambdaClass = projectRule.fixture.file as PsiJavaFile
+            val lambdaBody = lambdaClass.classes[0].allMethods[0].body!!.statements[0]
+            val lineNumber = document.getLineNumber(lambdaBody.textOffset)
+            assertThat(breakpointManager.addLineBreakpoint(document, lineNumber)).isNotNull
+        }
+
+        val runConfiguration = createRunConfiguration(project = projectRule.project, input = "\"Hello World\"")
+        assertThat(runConfiguration).isNotNull
+
+        var breakpointHit = false
+
+        debugManager.addDebuggerManagerListener(object : DebuggerManagerListener {
+            override fun sessionAttached(session: DebuggerSession) {
+                println("Debugger attached")
+                val debugProcess = session.process
+                debugProcess.addDebugProcessListener(object : DebugProcessAdapterImpl() {
+                    override fun paused(suspendContext: SuspendContextImpl) {
+                        println("Resuming: $suspendContext")
+                        breakpointHit = true
+                        debugProcess.managerThread.schedule(
+                            debugProcess.createResumeCommand(
+                                suspendContext,
+                                PrioritizedTask.Priority.LOWEST
+                            )
+                        )
+                    }
+                })
+            }
+        })
+
+        val executeLambda = executeLambda(runConfiguration, DefaultDebugExecutor.EXECUTOR_ID)
+
+        assertThat(executeLambda.exitCode).isEqualTo(0)
+        assertThat(executeLambda.stdout).contains("HELLO WORLD")
+
+        assertThat(breakpointHit).isTrue()
+    }
+
+    private fun executeLambda(
+        runConfiguration: SamRunConfiguration,
+        executorId: String = DefaultRunExecutor.EXECUTOR_ID
+    ): Output {
+        val executor = ExecutorRegistry.getInstance().getExecutorById(executorId)
         val executionEnvironment = ExecutionEnvironmentBuilder.create(executor, runConfiguration).build()
         val executionFuture = CompletableFuture<Output>()
         runInEdt {
             executionEnvironment.runner.execute(executionEnvironment) {
                 it.processHandler?.addProcessListener(object : OutputListener() {
+                    override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
+                        super.onTextAvailable(event, outputType)
+                        println("SAM CLI: ${event.text}")
+                    }
+
                     override fun processTerminated(event: ProcessEvent) {
                         super.processTerminated(event)
                         executionFuture.complete(this.output)
