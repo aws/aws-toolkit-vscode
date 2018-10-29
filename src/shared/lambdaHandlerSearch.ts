ce746c62258616f4ccded731e46d1adf735bcd09
awschristou <39839589+awschristou@users.noreply.github.com>
2018-10-29 13:11:41 -0700

    Utility class to detect Typescript functions that can possibly be used as Lambda Function Handlers. (#142)
    * Utility class to detect Typescript functions that can possibly be used as Lambda Function Handlers.

---
+++ src/shared/lambdaHandlerSearch.ts
@@ -0,0 +1,23 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+export interface LambdaHandlerCandidate {
+    handlerName: string,
+    filename: string,
+    positionStart: number,
+    positionEnd: number,
+}
+
+export interface LambdaHandlerSearch {
+
+    /**
+     * @description Looks for functions that appear to be valid Lambda Function Handlers.
+     * @returns A collection of information for each detected candidate.
+     */
+    findCandidateLambdaHandlers(): Promise<LambdaHandlerCandidate[]>
+
+}
---
+++ src/test/samples/typescript/sampleFunctions.ts
@@ -0,0 +1,49 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+/* tslint:disable:no-unused-variable */
+
+// @ts-ignore
+function functionWithNoArgs(): void { }
+
+export function exportedFunctionWithNoArgs(): void { }
+
+// @ts-ignore
+function functionWithOneArg(arg1: string): void { }
+
+export function exportedFunctionWithOneArg(arg1: string): void { }
+
+// @ts-ignore
+function functionWithTwoArgs(arg1: string, arg2: string): void { }
+
+export function exportedFunctionWithTwoArgs(arg1: string, arg2: string): void { }
+
+// @ts-ignore
+function functionWithThreeArgs(arg1: string, arg2: string, arg3: string): void { }
+
+export function exportedFunctionWithThreeArgs(arg1: string, arg2: string, arg3: string): void { }
+
+// @ts-ignore
+function functionWithFourArgs(arg1: string, arg2: string, arg3: string, arg4: string): void { }
+
+export function exportedFunctionWithFourArgs(arg1: string, arg2: string, arg3: string, arg4: string): void { }
+
+export const exportedArrowFunction = (arg1: string) => { }
+
+export const exportedArrowFunctionWithFourArgs = (arg1: string, arg2: string, arg3: string, arg4: string) => { }
+
+function exportedViaDeclaration(arg1: string) { }
+
+const exportedArrowViaDeclaration = (arg1: string) => { }
+const exportedArrowViaDeclarationAlt: (x: string) => void = (arg1) => { }
+
+const exportedArrowViaDeclarationWithFourArgs = (arg1: string, arg2: string, arg3: string, arg4: string) => { }
+const exportedArrowViaDeclarationWithFourArgsAlt:
+    (arg1: string, arg2: string, arg3: string, arg4: string) => void = (arg1, arg2, arg3, arg4) => { }
+
+export { exportedViaDeclaration, exportedArrowViaDeclaration, exportedArrowViaDeclarationAlt }
+export { functionWithFourArgs, exportedArrowViaDeclarationWithFourArgs, exportedArrowViaDeclarationWithFourArgsAlt }
