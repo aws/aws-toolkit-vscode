ae88f3ceb752c0a8802944e8e8aedae1ada7113f
Kyle Thomson <kylthoms@amazon.com>
2018-11-21 14:59:08 -0800

    Making the telemetry notification sticky so that a customer has to explicitly dismiss it.
    Fixes #586

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/AwsTelemetryPrompter.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/AwsTelemetryPrompter.kt
@@ -3,26 +3,34 @@
 
 package software.aws.toolkits.jetbrains.core
 
-import com.intellij.notification.NotificationListener
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import software.aws.toolkits.jetbrains.settings.AwsSettings
 import software.aws.toolkits.jetbrains.settings.AwsSettingsConfigurable
-import software.aws.toolkits.jetbrains.utils.notifyInfo
 import software.aws.toolkits.resources.message
+
+internal const val GROUP_DISPLAY_ID = "AWS Telemetry"
 
 class AwsTelemetryPrompter : StartupActivity {
 
     override fun runActivity(project: Project) {
         if (!AwsSettings.getInstance().promptedForTelemetry) {
-            notifyInfo(title = message("aws.settings.telemetry.prompt.title"),
-                    content = message("aws.settings.telemetry.prompt.message"),
-                    project = project,
-                    listener = NotificationListener { notification, _ ->
-                        ShowSettingsUtil.getInstance().showSettingsDialog(project, AwsSettingsConfigurable::class.java)
-                        notification.expire()
-                    })
+            val group = NotificationGroup(GROUP_DISPLAY_ID, NotificationDisplayType.STICKY_BALLOON, true)
+
+            val notification = group.createNotification(
+                message("aws.settings.telemetry.prompt.title"), message("aws.settings.telemetry.prompt.message"), NotificationType.INFORMATION
+            ) { notification, _ ->
+                ShowSettingsUtil.getInstance().showSettingsDialog(project, AwsSettingsConfigurable::class.java)
+                notification.expire()
+            }
+
+            Notifications.Bus.notify(notification, project)
+
             AwsSettings.getInstance().promptedForTelemetry = true
         }
     }
