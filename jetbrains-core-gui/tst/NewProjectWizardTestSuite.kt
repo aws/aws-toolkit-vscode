c41bb171102d13e7061a27a58e6b36053d9346b7
Richard Li <richali@amazon.com>
2019-01-10 11:34:58 -0800

    GUI testing using JetBrains' Test GUI Framework

---
+++ jetbrains-core-gui/tst/NewProjectWizardTestSuite.kt
@@ -0,0 +1,15 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import com.intellij.testGuiFramework.framework.GuiTestSuite
+import com.intellij.testGuiFramework.framework.GuiTestSuiteRunner
+import com.intellij.testGuiFramework.framework.RunWithIde
+import com.intellij.testGuiFramework.launcher.ide.CommunityIde
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import software.aws.toolkits.jetbrains.ui.wizard.java.SamInitProjectBuilderIntelliJTest
+
+@RunWith(GuiTestSuiteRunner::class)
+@RunWithIde(CommunityIde::class)
+@Suite.SuiteClasses(SamInitProjectBuilderIntelliJTest::class)
+class NewProjectWizardTestSuite : GuiTestSuite()
