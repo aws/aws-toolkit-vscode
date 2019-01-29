e4b9a911390f1c75a1e03513f23cb646ec2a6e95
awschristou <39839589+awschristou@users.noreply.github.com>
2019-01-29 14:18:31 -0800

    Icon removal and UI Cleanup (#220)
    * Removed stub "New Lambda" Command and icons
    * Removed stub Deploy Lambda command and icons
    * Removed Invoke Lambda icons, and button from the AWS Explorer bar
    * Removed placeholder Lambda Function icons
    * Removed placeholder svgs not currently referenced
    * Added separators to the Lambda Function context menu
    * Fixed issue where "Hide Region" command was not properly associated with the Region nodes
    * Fixed Explorer to say "AWS: EXPLORER" instead of "AWS: LAMBDA"

--- src/lambda/lambdaTreeDataProvider.ts
+++ src/lambda/lambdaTreeDataProvider.ts
@@ -19,10 +19,8 @@ import { RefreshableAwsTreeProvider } from '../shared/treeview/awsTreeProvider'
 import { intersection, toMap, updateInPlace } from '../shared/utilities/collectionUtils'
 import { deleteCloudFormation } from './commands/deleteCloudFormation'
 import { deleteLambda } from './commands/deleteLambda'
-import { deployLambda } from './commands/deployLambda'
 import { getLambdaConfig } from './commands/getLambdaConfig'
 import { invokeLambda } from './commands/invokeLambda'
-import { newLambda } from './commands/newLambda'
 import { showErrorDetails } from './commands/showErrorDetails'
 import { CloudFormationStackNode } from './explorer/cloudFormationNodes'
 import { DefaultRegionNode } from './explorer/defaultRegionNode'
@@ -53,11 +51,6 @@ export class LambdaTreeDataProvider implements vscode.TreeDataProvider<AWSTreeNo
 
     public initialize(): void {
         vscode.commands.registerCommand('aws.refreshAwsExplorer', async () => this.refresh())
-        vscode.commands.registerCommand('aws.newLambda', async () => await newLambda())
-        vscode.commands.registerCommand(
-            'aws.deployLambda',
-            async (node: FunctionNodeBase) => await deployLambda(node)
-        )
         vscode.commands.registerCommand(
             'aws.deleteLambda',
             async (node: StandaloneFunctionNode) => await deleteLambda(
