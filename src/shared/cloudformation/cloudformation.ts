5a5b8bec320b8f8577f6192dc49c2c9fe6288a06
Matthew Pirocchi <pirocchi@amazon.com>
2019-02-06 13:19:14 -0800

    Fix version mismatch between js-yaml and @types/js-yaml.

--- src/shared/cloudformation/cloudformation.ts
+++ src/shared/cloudformation/cloudformation.ts
@@ -34,7 +34,7 @@ export namespace CloudFormation {
         return yaml.safeLoad(
             templateAsYaml,
             {
-                schema
+                schema: schema as yaml.SchemaDefinition
             }
         ) as Template
     }
