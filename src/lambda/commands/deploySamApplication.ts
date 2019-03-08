f74a7e8f53c5a56accdfbf7d767050debe1718cb
Christopher Christou <chrstou@amazon.com>
2019-03-08 08:40:04 -0800

    Show status bar message while application is deploying

--- src/lambda/commands/deploySamApplication.ts
+++ src/lambda/commands/deploySamApplication.ts
@@ -30,40 +30,57 @@ export async function deploySamApplication({
     }
 
     const { template, s3Bucket, stackName } = args
+    const deployApplicationPromise = new Promise(async (resolve, reject) => {
 
-    const tempFolder = await mkdtemp('samDeploy')
-    const outputTemplatePath = path.join(tempFolder, 'template.yaml')
-    let stage = 'packaging'
-    try {
-        const packageInvocation = new SamCliPackageInvocation(template.fsPath, outputTemplatePath, s3Bucket, invoker)
-        restParams.outputChannel.show(true)
-        // TODO: Add nls support
-        restParams.outputChannel.appendLine(`Packaging SAM Application to S3 Bucket: ${s3Bucket}`)
-        await packageInvocation.execute()
+        const tempFolder = await mkdtemp('samDeploy')
+        const outputTemplatePath = path.join(tempFolder, 'template.yaml')
+        let stage = 'packaging'
+        try {
+            const packageInvocation = new SamCliPackageInvocation(
+                template.fsPath,
+                outputTemplatePath,
+                s3Bucket,
+                invoker
+            )
+            restParams.outputChannel.show(true)
+            // TODO: Add nls support
+            restParams.outputChannel.appendLine(`Packaging SAM Application to S3 Bucket: ${s3Bucket}`)
+            await packageInvocation.execute()
 
-        stage = 'deploying'
-        const deployInvocation = new SamCliDeployInvocation(outputTemplatePath, stackName, invoker)
-        // Deploying can take a very long time for Python Lambda's with native dependencies so user needs feedback
-        restParams.outputChannel.appendLine(localize(
-          'AWS.samcli.deploy.stackName.initiated',
-          'Deploying {0} stack...',
-          stackName
-        ))
-        await deployInvocation.execute()
-        // TODO: Add nls support
-        const msg = `Successfully deployed SAM Application to CloudFormation Stack: ${stackName}`
-        restParams.outputChannel.appendLine(msg)
-        // TODO: Is this the right way to provide this feedback?
-        vscode.window.showInformationMessage(msg)
-    } catch (err) {
-        // TODO: Add nls support
-        const msg = `Failed to deploy SAM application. Error while ${stage}: ${String(err)}`
-        restParams.outputChannel.appendLine(msg)
-        // TODO: Is this the right way to provide this feedback?
-        vscode.window.showWarningMessage(msg)
-    } finally {
-        await del(tempFolder, {
-            force: true
-        })
-    }
+            stage = 'deploying'
+            const deployInvocation = new SamCliDeployInvocation(outputTemplatePath, stackName, invoker)
+            // Deploying can take a very long time for Python Lambda's with native dependencies so user needs feedback
+            restParams.outputChannel.appendLine(localize(
+                'AWS.samcli.deploy.stackName.initiated',
+                'Deploying {0} stack...',
+                stackName
+            ))
+            await deployInvocation.execute()
+            // TODO: Add nls support
+            const msg = `Successfully deployed SAM Application to CloudFormation Stack: ${stackName}`
+            restParams.outputChannel.appendLine(msg)
+            // TODO: Is this the right way to provide this feedback?
+            vscode.window.showInformationMessage(msg)
+        } catch (err) {
+            // TODO: Add nls support
+            const msg = `Failed to deploy SAM application. Error while ${stage}: ${String(err)}`
+            restParams.outputChannel.appendLine(msg)
+            // TODO: Is this the right way to provide this feedback?
+            vscode.window.showWarningMessage(msg)
+        } finally {
+            await del(tempFolder, {
+                force: true
+            })
+            resolve()
+        }
+    })
+
+    vscode.window.setStatusBarMessage(
+        localize(
+            'AWS.samcli.deploy.statusbar.message',
+            '$(cloud-upload) Deploying SAM Application to {0}...',
+            stackName,
+        ),
+        deployApplicationPromise
+    )
 }
