973991b3a7e5220ae3974fa0d0c89978100ceab1
Bryce Ito <bryceito@amazon.com>
2019-03-12 14:25:26 -0700

    Adding back button flow to wizard

--- src/lambda/wizards/samInitWizard.ts
+++ src/lambda/wizards/samInitWizard.ts
@@ -13,12 +13,22 @@ import * as os from 'os'
 import * as path from 'path'
 import * as vscode from 'vscode'
 import { SamCliInitArgs } from '../../shared/sam/cli/samCliInit'
+import * as input from '../../shared/ui/input'
+import * as picker from '../../shared/ui/picker'
 import * as lambdaRuntime from '../models/samLambdaRuntime'
 import { MultiStepWizard, WizardStep } from '../wizards/multiStepWizard'
 
 export interface CreateNewSamAppWizardContext {
     readonly lambdaRuntimes: immutable.Set<lambdaRuntime.SamLambdaRuntime>
     readonly workspaceFolders: vscode.WorkspaceFolder[] | undefined
+
+    promptUserForRuntime(
+        currRuntime?: lambdaRuntime.SamLambdaRuntime
+    ): Promise<lambdaRuntime.SamLambdaRuntime | undefined>
+
+    promptUserForLocation(): Promise<vscode.Uri | undefined>
+
+    promptUserForName(): Promise<string | undefined>
 
     showInputBox(
         options?: vscode.InputBoxOptions,
@@ -60,6 +70,141 @@ class DefaultCreateNewSamAppWizardContext implements CreateNewSamAppWizardContex
     public get workspaceFolders(): vscode.WorkspaceFolder[] | undefined {
         return vscode.workspace.workspaceFolders
     }
+
+    public async promptUserForRuntime(
+        currRuntime?: lambdaRuntime.SamLambdaRuntime
+    ): Promise<lambdaRuntime.SamLambdaRuntime | undefined> {
+
+        const quickPick = await picker.createQuickPick<vscode.QuickPickItem>({
+            options: {
+                ignoreFocusOut: true,
+                placeHolder: localize(
+                    'AWS.samcli.initWizard.runtime.prompt',
+                    'Select a SAM Application Runtime'
+                ),
+                value: String(currRuntime) || ''
+            },
+            items: lambdaRuntime.samLambdaRuntimes
+                .toArray()
+                .sort()
+                .map(runtime => ({
+                    label: runtime,
+                    alwaysShow: runtime === currRuntime,
+                    description: runtime === currRuntime ?
+                        localize('AWS.samcli.deploy.region.previousRegion', 'Selected Previously') : ''
+                }))
+        })
+
+        const choices = await picker.promptUser({
+            picker: quickPick
+        })
+
+        if (!choices || choices.length === 0) {
+            return undefined
+        }
+
+        if (choices.length > 1) {
+            console.error(
+                `Received ${choices.length} responses from user, expected 1.` +
+                ' Cancelling to prevent deployment of unexpected template.'
+            )
+
+            return undefined
+        }
+
+        return choices[0].label as lambdaRuntime.SamLambdaRuntime
+    }
+
+    public async promptUserForLocation(): Promise<vscode.Uri | undefined> {
+        const items: FolderQuickPickItem[] = (this.workspaceFolders || [])
+            .map<FolderQuickPickItem>(f => new WorkspaceFolderQuickPickItem(f))
+            .concat([new BrowseFolderQuickPickItem(this)])
+
+        const quickPick = await picker.createQuickPick({
+            options: {
+                ignoreFocusOut: true,
+                placeHolder: localize(
+                    'AWS.samcli.initWizard.location.prompt',
+                    'Select a location for your new project'
+                )
+            },
+            items: items,
+            buttons: [
+                vscode.QuickInputButtons.Back
+            ]
+        })
+
+        const choices = await picker.promptUser({
+            picker: quickPick,
+            onDidTriggerButton: (button, resolve, reject) => {
+                if (button === vscode.QuickInputButtons.Back) {
+                    resolve(undefined)
+                }
+            }
+        })
+
+        if (!choices || choices.length === 0) {
+            return undefined
+        }
+
+        if (choices.length > 1) {
+            console.error(
+                `Received ${choices.length} responses from user, expected 1.` +
+                ' Cancelling to prevent deployment of unexpected template.'
+            )
+
+            return undefined
+        }
+
+        return choices[0].getUri()
+    }
+
+    public async promptUserForName(): Promise<string | undefined> {
+        const inputBox = await input.createInputBox({
+            options: {
+                title: '',
+                prompt: localize(
+                    'AWS.samcli.initWizard.name.prompt',
+                    'Choose a name for your new application'
+                ),
+                placeHolder: localize(
+                    'AWS.samcli.initWizard.name.placeholder',
+                    'application name'
+                ),
+                ignoreFocusOut: true,
+            },
+            buttons: [
+                vscode.QuickInputButtons.Back
+            ]
+        })
+
+        return await input.promptUser({
+            inputBox: inputBox,
+            onValidateInput: (value: string) => {
+                if (!value) {
+                    return localize(
+                        'AWS.samcli.initWizard.name.error.empty',
+                        'Application name cannot be empty'
+                    )
+                }
+
+                if (value.includes(path.sep)) {
+                    return localize(
+                        'AWS.samcli.initWizard.name.error.pathSep',
+                        'The path separator ({0}) is not allowed in application names',
+                        path.sep
+                    )
+                }
+
+                return undefined
+            },
+            onDidTriggerButton: (button, resolve, reject) => {
+                if (button === vscode.QuickInputButtons.Back) {
+                    resolve(undefined)
+                }
+            }
+        })
+    }
 }
 
 export class CreateNewSamAppWizard extends MultiStepWizard<SamCliInitArgs> {
@@ -90,80 +235,19 @@ export class CreateNewSamAppWizard extends MultiStepWizard<SamCliInitArgs> {
     }
 
     private readonly RUNTIME: WizardStep = async () => {
-        const runtimeItems = this.context.lambdaRuntimes
-            .toArray()
-            .sort()
-            .map(runtime => ({ label: runtime }))
+        this.runtime = await this.context.promptUserForRuntime(this.runtime)
 
-        const result = await this.context.showQuickPick<vscode.QuickPickItem>(runtimeItems, {
-            ignoreFocusOut: true,
-            placeHolder: localize(
-                'AWS.samcli.initWizard.runtime.prompt',
-                'Select a SAM Application Runtime'
-            )
-        })
-
-        if (!result) {
-            return undefined
-        }
-
-        this.runtime = result.label as lambdaRuntime.SamLambdaRuntime
-
-        return this.LOCATION
+        return this.runtime ? this.LOCATION : undefined
     }
 
     private readonly LOCATION: WizardStep = async () => {
-        const choices: FolderQuickPickItem[] = (this.context.workspaceFolders || [])
-            .map<FolderQuickPickItem>(f => new WorkspaceFolderQuickPickItem(f))
-            .concat([new BrowseFolderQuickPickItem(this.context)])
-
-        const selection = await this.context.showQuickPick(choices, {
-            ignoreFocusOut: true,
-            placeHolder: localize(
-                'AWS.samcli.initWizard.location.prompt',
-                'Select a location for your new project'
-            )
-        })
-        if (!selection) {
-            return this.RUNTIME
-        }
-        this.location = await selection.getUri()
+        this.location = await this.context.promptUserForLocation()
 
         return this.location ? this.NAME : this.RUNTIME
     }
 
     private readonly NAME: WizardStep = async () => {
-        this.name = await this.context.showInputBox({
-            value: 'my-sam-app',
-            prompt: localize(
-                'AWS.samcli.initWizard.name.prompt',
-                'Choose a name for your new application'
-            ),
-            placeHolder: localize(
-                'AWS.samcli.initWizard.name.placeholder',
-                'application name'
-            ),
-            ignoreFocusOut: true,
-
-            validateInput(value: string): string | undefined | null | Thenable<string | undefined | null> {
-                if (!value) {
-                    return localize(
-                        'AWS.samcli.initWizard.name.error.empty',
-                        'Application name cannot be empty'
-                    )
-                }
-
-                if (value.includes(path.sep)) {
-                    return localize(
-                        'AWS.samcli.initWizard.name.error.pathSep',
-                        'The path separator ({0}) is not allowed in application names',
-                        path.sep
-                    )
-                }
-
-                return undefined
-            }
-        })
+        this.name = await this.context.promptUserForName()
 
         return this.name ? undefined : this.LOCATION
     }
