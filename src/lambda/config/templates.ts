e2505a8ddc6e7cff79a0d46e5ad09b45f1202110
Matthew Pirocchi <pirocchi@amazon.com>
2019-03-12 16:55:51 -0700

    First draft.

---
+++ src/lambda/config/templates.ts
@@ -0,0 +1,133 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import { parse, ParseError, ParseErrorCode } from 'jsonc-parser'
+import * as os from 'os'
+import * as vscode from 'vscode'
+import * as fsUtils from '../../shared/filesystemUtilities'
+import { getLogger, Logger } from '../../shared/logger'
+
+export interface TemplatesConfig {
+    templates: {
+        [relativePath: string]: TemplateConfig
+    }
+}
+
+export interface TemplateConfig {
+    parameterOverrides?: {
+        [key: string]: string
+    }
+}
+
+export interface LoadTemplatesConfigContext {
+    logger: Pick<Logger, 'warn'>
+    readFile(path: string): Promise<string>
+}
+
+export function load(
+    workspaceFolder: Pick<vscode.WorkspaceFolder, 'uri'>,
+    context?: LoadTemplatesConfigContext
+): Promise<TemplatesConfig>
+export function load(
+    workspaceUri: vscode.Uri,
+    context?: LoadTemplatesConfigContext
+): Promise<TemplatesConfig>
+export function load(
+    workspacePath: string,
+    context?: LoadTemplatesConfigContext
+): Promise<TemplatesConfig>
+export async function load(
+    workspaceFolderOrUriOrPath: Pick<vscode.WorkspaceFolder, 'uri'> | vscode.Uri | string,
+    context: LoadTemplatesConfigContext = {
+        logger: getLogger(),
+        readFile: async path => await fsUtils.readFileAsString(path)
+    }
+): Promise<TemplatesConfig> {
+    const getWorkspaceFolderPath = (input: Pick<vscode.WorkspaceFolder, 'uri'> | vscode.Uri | string): string => {
+        if (typeof input === 'string') {
+            return input
+        }
+
+        const workspaceFolder = input as Pick<vscode.WorkspaceFolder, 'uri'>
+        if (workspaceFolder.uri) {
+            return workspaceFolder.uri.fsPath
+        }
+
+        return (input as vscode.Uri).fsPath
+    }
+    const workspaceFolderPath = getWorkspaceFolderPath(workspaceFolderOrUriOrPath)
+
+    try {
+        const raw = await context.readFile(workspaceFolderPath)
+        const errors: ParseError[] = []
+        const config = parse(raw, errors) as TemplatesConfig
+        if (errors.length > 0) {
+            const message = errors.length === 1 ?
+                ` ${formatParseError(errors[0])}`  :
+                `${os.EOL}${errors.map(formatParseError).join(os.EOL)}`
+
+            throw new Error(`Could not parse .aws/templates.json:${message}`)
+        }
+
+        return config
+    } catch (err) {
+        if (Array.isArray(err) && (err as any[]).length === 1) {
+            err = (err as any[])[0]
+        }
+
+        context.logger.warn(`Could not load .aws/templates.json: ${err}`)
+
+        return {
+            templates: {}
+        }
+    }
+}
+
+function formatParseError(error: ParseError) {
+    return `${getParseErrorDescription(error.error)} at offset ${error.offset}, length ${error.length}`
+}
+
+// Reverse enum mappings are only generated for non-const numerical enums,
+// but ParseErrorCode is a const enum. So we have to reverse-map manually.
+function getParseErrorDescription(code: ParseErrorCode) {
+    switch (code) {
+        case ParseErrorCode.CloseBraceExpected:
+            return 'close brace expected'
+        case ParseErrorCode.CloseBracketExpected:
+            return 'close bracket expected'
+        case ParseErrorCode.ColonExpected:
+            return 'colon expected'
+        case ParseErrorCode.CommaExpected:
+            return 'command expected'
+        case ParseErrorCode.EndOfFileExpected:
+            return 'end of file expected'
+        case ParseErrorCode.InvalidCharacter:
+            return 'invalid character'
+        case ParseErrorCode.InvalidCommentToken:
+            return 'invalid comment token'
+        case ParseErrorCode.InvalidEscapeCharacter:
+            return 'invalid escape character'
+        case ParseErrorCode.InvalidNumberFormat:
+            return 'invalid number format'
+        case ParseErrorCode.InvalidSymbol:
+            return 'invalid symbol'
+        case ParseErrorCode.InvalidUnicode:
+            return 'invalid unicode'
+        case ParseErrorCode.PropertyNameExpected:
+            return 'property name expected'
+        case ParseErrorCode.UnexpectedEndOfComment:
+            return 'unexpected end of comment'
+        case ParseErrorCode.UnexpectedEndOfNumber:
+            return 'unexpected end of number'
+        case ParseErrorCode.UnexpectedEndOfString:
+            return 'unexpected end of string'
+        case ParseErrorCode.ValueExpected:
+            return 'value expected'
+        // By omitting the default case, we force the compiler to yell at us
+        // if any enum members are added/removed/changed.
+    }
+}
---
+++ src/test/lambda/config/templates.test.ts
@@ -0,0 +1,156 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import * as assert from 'assert'
+import * as path from 'path'
+import * as vscode from 'vscode'
+import { load, LoadTemplatesConfigContext } from '../../../lambda/config/templates'
+
+describe('templates', async () => {
+    describe('load', async () => {
+        it('logs a message on error loading file', async () => {
+            let warnCount: number = 0
+            const context: LoadTemplatesConfigContext = {
+                logger: {
+                    warn(...message: (Error | string)[]) {
+                        assert.strictEqual(message.length, 1)
+                        assert.strictEqual(message[0], 'Could not load .aws/templates.json: Error: oh no')
+                        warnCount++
+                    }
+                },
+                readFile: async pathLike => { throw new Error('oh no') }
+            }
+
+            await load('', context)
+
+            assert.strictEqual(warnCount, 1)
+        })
+
+        it('returns a minimal config on error loading file', async () => {
+            const context: LoadTemplatesConfigContext = {
+                logger: { warn(...message: (Error | string)[]) {} },
+                readFile: async pathLike => { throw new Error('oh no') }
+            }
+
+            const config = await load('', context)
+
+            assert.ok(config)
+            assert.ok(config.templates)
+            assert.strictEqual(Object.getOwnPropertyNames(config.templates).length, 0)
+        })
+
+        it('gracefully handles invalid JSON', async () => {
+            let warnCount: number = 0
+            const context: LoadTemplatesConfigContext = {
+                logger: {
+                    warn(...message: (Error | string)[]) {
+                        assert.strictEqual(message.length, 1)
+                        assert.strictEqual(
+                            message[0],
+                            // tslint:disable-next-line:max-line-length
+                            'Could not load .aws/templates.json: Error: Could not parse .aws/templates.json: close brace expected at offset 1, length 0'
+                        )
+                        warnCount++
+                    }
+                },
+                readFile: async pathLike => '{'
+            }
+
+            await load('', context)
+
+            assert.strictEqual(warnCount, 1)
+        })
+
+        it('supports JSON comments', async () => {
+            const rawJson = `{
+                "templates": {
+                    // A single-comment.
+                    /*
+                      A multi-line comment.
+                    */
+                    "relative/path/to/template.yaml": {
+                    }
+                }
+            }`
+
+            const context: LoadTemplatesConfigContext = {
+                logger: { warn(...message: (Error | string)[]) {} },
+                readFile: async pathLike => rawJson
+            }
+
+            const config = await load('', context)
+
+            assert.ok(config)
+            assert.ok(config.templates)
+            assert.strictEqual(Object.getOwnPropertyNames(config.templates).length, 1)
+
+            const template = config.templates['relative/path/to/template.yaml']
+            assert.ok(template)
+            assert.strictEqual(Object.getOwnPropertyNames(template).length, 0)
+        })
+
+        it('reads from the correct path when a workspaceFolder is provided', async () => {
+            const readArgs: string[] = []
+            const context: LoadTemplatesConfigContext = {
+                logger: { warn(...message: (Error | string)[]) {} },
+                readFile: async pathLike => {
+                    readArgs.push(pathLike)
+
+                    return '{}'
+                }
+            }
+
+            await load(
+                { uri: vscode.Uri.file(path.join('my', 'path')) },
+                context
+            )
+
+            assert.strictEqual(readArgs.length, 1)
+            assert.strictEqual(readArgs[0], path.sep + path.join('my', 'path'))
+        })
+
+        it('reads from the correct path when a uri is provided', async () => {
+            const readArgs: string[] = []
+            const context: LoadTemplatesConfigContext = {
+                logger: { warn(...message: (Error | string)[]) {} },
+                readFile: async pathLike => {
+                    readArgs.push(pathLike)
+
+                    return '{}'
+                }
+            }
+
+            await load(
+                vscode.Uri.file(path.join('my', 'path')),
+                context
+            )
+
+            assert.strictEqual(readArgs.length, 1)
+            assert.strictEqual(readArgs[0], path.sep + path.join('my', 'path'))
+        })
+
+        it('reads the correct path when a string is provided', async () => {
+            const readArgs: string[] = []
+            const context: LoadTemplatesConfigContext = {
+                logger: { warn(...message: (Error | string)[]) {} },
+                readFile: async pathLike => {
+                    readArgs.push(pathLike)
+
+                    return '{}'
+                }
+            }
+
+            await load(
+                path.join('my', 'path'),
+                context
+            )
+
+            assert.strictEqual(readArgs.length, 1)
+            assert.strictEqual(readArgs[0], path.join('my', 'path'))
+        })
+    })
+})
