67fff2bedf2dabf238de2274e1384ac57927eb17
Austin Brooks <abrooksv@users.noreply.github.com>
2019-03-13 14:57:09 -0700

    Change CloudFormation icon (#831)

--- jetbrains-core/src/icons/AwsIcons.kt
+++ jetbrains-core/src/icons/AwsIcons.kt
@@ -20,6 +20,7 @@ object AwsIcons {
     }
 
     object Resources {
+        @JvmField val CLOUDFORMATION_STACK = IconLoader.getIcon("/icons/resources/CloudFormationStack.svg") // 16x16
         @JvmField val LAMBDA_FUNCTION = IconLoader.getIcon("/icons/resources/LambdaFunction.svg") // 16x16
         @JvmField val SERVERLESS_APP = IconLoader.getIcon("/icons/resources/ServerlessApp.svg") // 16x16
     }
