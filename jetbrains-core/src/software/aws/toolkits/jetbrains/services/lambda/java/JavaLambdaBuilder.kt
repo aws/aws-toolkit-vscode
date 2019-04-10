7006c444a6c276f11c8e7b73a538d9e50af5f162
Austin Brooks <abrooksv@users.noreply.github.com>
2019-04-10 10:00:15 -0700

    Query project root from Maven/Gradle to determine the base directory for Java (#900)
    * When using Java handler based build, query Maven/Gradle for the project root.

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/java/JavaLambdaBuilder.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/java/JavaLambdaBuilder.kt
@@ -4,11 +4,14 @@
 package software.aws.toolkits.jetbrains.services.lambda.java
 
 import com.intellij.execution.process.ProcessHandler
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
+import org.jetbrains.idea.maven.project.MavenProjectsManager
 import software.amazon.awssdk.services.lambda.model.Runtime
 import software.aws.toolkits.jetbrains.services.lambda.BuiltLambda
 import software.aws.toolkits.jetbrains.services.lambda.LambdaBuilder
@@ -27,7 +30,11 @@ class JavaLambdaBuilder : LambdaBuilder() {
         samOptions: SamOptions,
         onStart: (ProcessHandler) -> Unit
     ): CompletionStage<BuiltLambda> {
-        val baseDir = getBaseDirectory(module).path
+        val baseDir = when {
+            isGradle(module) -> getGradleProjectLocation(module)
+            isMaven(module) -> getPomLocation(module)
+            else -> throw IllegalStateException(message("lambda.build.java.unsupported_build_system", module))
+        }
         val customTemplate = FileUtil.createTempFile("template", ".yaml", true)
         val logicalId = "Function"
         SamTemplateUtils.writeDummySamTemplate(customTemplate, logicalId, runtime, baseDir, handler, envVars)
@@ -35,12 +42,23 @@ class JavaLambdaBuilder : LambdaBuilder() {
         return buildLambdaFromTemplate(module, customTemplate.toPath(), logicalId, samOptions, onStart)
     }
 
-    private fun getBaseDirectory(module: Module): VirtualFile {
-        val rootManager = ModuleRootManager.getInstance(module)
-        val contentRoots = rootManager.contentRoots
-        return when (contentRoots.size) {
-            1 -> contentRoots[0]
-            else -> throw IllegalArgumentException(message("lambda.build.too_many_content_roots"))
+    private fun isGradle(module: Module): Boolean = ExternalSystemModulePropertyManager.getInstance(module)
+        .getExternalSystemId() == "GRADLE"
+
+    private fun getGradleProjectLocation(module: Module): String =
+        ExternalSystemApiUtil.getExternalProjectPath(module)
+            ?: throw IllegalStateException(message("lambda.build.unable_to_locate_project_root", module))
+
+    private fun isMaven(module: Module): Boolean {
+        if (PluginManager.getPlugin(PluginId.getId("org.jetbrains.idea.maven"))?.isEnabled == true) {
+            return MavenProjectsManager.getInstance(module.project).isMavenizedModule(module)
         }
+
+        return false
     }
+
+    private fun getPomLocation(module: Module): String =
+        MavenProjectsManager.getInstance(module.project).findProject(module)?.directory ?: throw IllegalStateException(
+            message("lambda.build.unable_to_locate_project_root", module)
+        )
 }
