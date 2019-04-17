55fda65a8faaa7ab71260bc9eca7e258c9061812
Bryce Ito <bryceito@amazon.com>
2019-04-17 08:56:12 -0700

    Updating runtimes to match versions currently supported by SAM CLI

--- src/lambda/models/samLambdaRuntime.ts
+++ src/lambda/models/samLambdaRuntime.ts
@@ -16,7 +16,6 @@ export type SamLambdaRuntime =
     'python' |
     'nodejs6.10' |
     'nodejs8.10' |
-    'nodejs4.3' |
     'nodejs' |
     'dotnetcore2.1' |
     'dotnetcore2.0' |
@@ -27,6 +26,7 @@ export type SamLambdaRuntime =
     'go' |
     'java8' |
     'java' |
+    'ruby' |
     'ruby2.5'
 
 export const samLambdaRuntimes: immutable.Set<SamLambdaRuntime> = immutable.Set([
@@ -36,7 +36,6 @@ export const samLambdaRuntimes: immutable.Set<SamLambdaRuntime> = immutable.Set(
     'python',
     'nodejs6.10',
     'nodejs8.10',
-    'nodejs4.3',
     'nodejs',
     'dotnetcore2.1',
     'dotnetcore2.0',
@@ -47,6 +46,7 @@ export const samLambdaRuntimes: immutable.Set<SamLambdaRuntime> = immutable.Set(
     'go',
     'java8',
     'java',
+    'ruby',
     'ruby2.5'
 ] as SamLambdaRuntime[])
 
