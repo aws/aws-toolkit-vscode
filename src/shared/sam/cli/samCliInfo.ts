99dc03e137a6691a05a619789bb01c51899b0771
Christopher Christou <chrstou@amazon.com>
2019-05-01 21:04:12 -0700

    Resurrect samCliInfo

---
+++ src/shared/sam/cli/samCliInfo.ts
@@ -0,0 +1,64 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import { getLogger, Logger } from '../../logger'
+import { ChildProcessResult } from '../../utilities/childProcess'
+import { DefaultSamCliProcessInvoker } from './samCliInvoker'
+// TODO : move SamCliInfoResponse back to this file
+import { SamCliInfoResponse, SamCliProcessInvoker } from './samCliInvokerUtils'
+
+export class SamCliInfoInvocation {
+    public constructor(
+        private readonly invoker: SamCliProcessInvoker = new DefaultSamCliProcessInvoker()
+    ) {
+    }
+
+    public async execute(): Promise<SamCliInfoResponse> {
+        const logger: Logger = getLogger()
+        const { error, exitCode, stderr, stdout }: ChildProcessResult = await this.invoker.invoke('--info')
+
+        if (exitCode === 0) {
+            const response = this.convertOutput(stdout)
+
+            if (!!response) {
+                return response
+            }
+
+            throw new Error('SAM CLI did not return expected data')
+        }
+
+        console.error('SAM CLI error')
+        console.error(`Exit code: ${exitCode}`)
+        console.error(`Error: ${error}`)
+        console.error(`stderr: ${stderr}`)
+        console.error(`stdout: ${stdout}`)
+
+        const err = new Error(
+            `sam --info encountered an error: ${error}
+    ${error && error.message ? 'message: ' + error.message : ''}
+    stderr : ${stderr}
+    stdout : ${stdout}`
+        )
+        logger.error(err)
+        throw err
+    }
+
+    /**
+     * Parses the output into a typed object with expected data
+     * @param text output from a `sam --info` call
+     */
+    protected convertOutput(text: string): SamCliInfoResponse | undefined {
+        const logger: Logger = getLogger()
+        try {
+            return JSON.parse(text) as SamCliInfoResponse
+        } catch (err) {
+            logger.error(err as Error)
+
+            return undefined
+        }
+    }
+}
---
+++ src/test/shared/sam/cli/samCliInfo.test.ts
@@ -0,0 +1,58 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import * as assert from 'assert'
+import { TestLogger } from '../../../../shared/loggerUtils'
+import { SamCliInfoInvocation } from '../../../../shared/sam/cli/samCliInfo'
+import { SamCliInfoResponse } from '../../../../shared/sam/cli/samCliInvokerUtils'
+
+describe('SamInfoCliCommand', async () => {
+
+    class TestSamCliInfoCommand extends SamCliInfoInvocation {
+        public convertOutput(text: string): SamCliInfoResponse | undefined {
+            return super.convertOutput(text)
+        }
+    }
+
+    let logger: TestLogger
+
+    before(async () => {
+        logger = await TestLogger.createTestLogger()
+    })
+
+    after(async () => {
+        await logger.cleanupLogger()
+    })
+
+    it('converts sam info response to SamCliInfoResponse', async () => {
+        const response: SamCliInfoResponse | undefined = new TestSamCliInfoCommand()
+            .convertOutput('{"version": "1.2.3"}')
+
+        assert.ok(response)
+        assert.strictEqual(response!.version, '1.2.3')
+    })
+
+    it('converts sam info response without version to SamCliInfoResponse', async () => {
+        const response: SamCliInfoResponse | undefined = new TestSamCliInfoCommand()
+            .convertOutput('{}')
+
+        assert.ok(response)
+        assert.strictEqual(response!.version, undefined)
+    })
+
+    it('converts non-response to undefined', async () => {
+        [
+            'qwerty',
+            '{"version": "1.2.3"} you have no new email messages'
+        ].forEach(output => {
+            const response: SamCliInfoResponse | undefined = new TestSamCliInfoCommand()
+                .convertOutput(output)
+
+            assert.strictEqual(response, undefined, `Expected text to not parse: ${output}`)
+        })
+    })
+})
