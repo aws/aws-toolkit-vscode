0d388b4e69aeb08b5abd7254128efb72d1d408f3
Bryce Ito <bryceito@amazon.com>
2019-05-08 15:27:25 -0700

    Adding tests for buttons.ts file

---
+++ src/test/shared/ui/buttons.test.ts
@@ -0,0 +1,28 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import * as assert from 'assert'
+import * as vscode from 'vscode'
+import * as buttons from '../../../shared/ui/buttons'
+
+describe('UI buttons', () => {
+    it('creates a help button without a tooltip or icons', () => {
+        const help = buttons.createHelpButton()
+        const paths = help.iconPath as {light: vscode.Uri, dark: vscode.Uri}
+
+        assert.strictEqual(help.tooltip, undefined)
+        assert.ok(paths.light)
+        assert.ok(paths.dark)
+    })
+
+    it('creates a help button with a tooltip', () => {
+        const tooltip = 'you must be truly desperate to come to me for help'
+        const help = buttons.createHelpButton(tooltip)
+
+        assert.strictEqual(help.tooltip, tooltip)
+    })
+})
