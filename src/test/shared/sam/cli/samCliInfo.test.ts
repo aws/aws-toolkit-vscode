ae87cc72affb81bb70c7c2ef6b102cd2f26ee9d0
Christopher Christou <chrstou@amazon.com>
2019-05-14 18:09:23 -0700

    Added SamCliInfoInvocation test coverage

--- src/test/shared/sam/cli/samCliInfo.test.ts
+++ src/test/shared/sam/cli/samCliInfo.test.ts
@@ -8,8 +8,37 @@
 import * as assert from 'assert'
 import { TestLogger } from '../../../../shared/loggerUtils'
 import { SamCliInfoInvocation, SamCliInfoResponse } from '../../../../shared/sam/cli/samCliInfo'
+import { ChildProcessResult } from '../../../../shared/utilities/childProcess'
+import { assertThrowsError } from '../../utilities/assertUtils'
+import { TestSamCliProcessInvoker } from './testSamCliProcessInvoker'
 
-describe('SamInfoCliCommand', async () => {
+describe('SamCliInfoInvocation', async () => {
+
+    class FakeChildProcessResult implements ChildProcessResult {
+        public exitCode: number
+        public error: Error | undefined
+        public stdout: string
+        public stderr: string
+
+        public constructor(
+            {
+                exitCode = 0,
+                stdout = '',
+                stderr = '',
+                ...params
+            }: {
+                exitCode?: number
+                error?: Error | undefined
+                stdout?: string
+                stderr?: string
+            }
+        ) {
+            this.exitCode = exitCode
+            this.error = params.error
+            this.stdout = stdout
+            this.stderr = stderr
+        }
+    }
 
     class TestSamCliInfoCommand extends SamCliInfoInvocation {
         public convertOutput(text: string): SamCliInfoResponse | undefined {
@@ -53,5 +82,69 @@ describe('SamInfoCliCommand', async () => {
 
             assert.strictEqual(response, undefined, `Expected text to not parse: ${output}`)
         })
+    })
+
+    it('handles successful errorcode and output', async () => {
+        const invoker: TestSamCliProcessInvoker = new TestSamCliProcessInvoker(
+            (args: any[]): ChildProcessResult => {
+                return new FakeChildProcessResult(
+                    {
+                        stdout: '{"version": "1.2.3"}'
+                    }
+                )
+            }
+        )
+        const samInfo: SamCliInfoInvocation = new SamCliInfoInvocation(invoker)
+
+        const response = await samInfo.execute()
+        assert.ok(response)
+        assert.strictEqual(response.version, '1.2.3', 'unexpected sam info version')
+    })
+
+    it('handles successful errorcode with strange output', async () => {
+        const invoker: TestSamCliProcessInvoker = new TestSamCliProcessInvoker(
+            (args: any[]): ChildProcessResult => {
+                return new FakeChildProcessResult(
+                    {
+                        stdout: 'unexpected output'
+                    }
+                )
+            }
+        )
+        const samInfo: SamCliInfoInvocation = new SamCliInfoInvocation(invoker)
+
+        await assertThrowsError(
+            async () => {
+                await samInfo.execute()
+            },
+            'Expected a error to be thrown'
+        )
+    })
+
+    it('throws when unsuccessful errorcode is encountered', async () => {
+        const childProcessResult: FakeChildProcessResult = new FakeChildProcessResult(
+            {
+                exitCode: 1,
+                error: new Error('some-error-message'),
+                stderr: 'somestderr',
+                stdout: 'somestdout',
+            }
+        )
+
+        const invoker: TestSamCliProcessInvoker = new TestSamCliProcessInvoker(
+            (args: any[]): ChildProcessResult => childProcessResult
+        )
+        const samInfo: SamCliInfoInvocation = new SamCliInfoInvocation(invoker)
+
+        const error: Error = await assertThrowsError(
+            async () => {
+                await samInfo.execute()
+            },
+            'Expected a error to be thrown'
+        )
+
+        assert.ok(error.message.indexOf(childProcessResult.error!.message) !== -1, 'error message was not in error')
+        assert.ok(error.message.indexOf(childProcessResult.stderr) !== -1, 'stderr was not in error')
+        assert.ok(error.message.indexOf(childProcessResult.stdout) !== -1, 'stdout was not in error')
     })
 })
---
+++ src/test/shared/sam/cli/testSamCliProcessInvoker.ts
@@ -0,0 +1,26 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+import { SpawnOptions } from 'child_process'
+
+import { SamCliProcessInvoker } from '../../../../shared/sam/cli/samCliInvokerUtils'
+import { ChildProcessResult } from '../../../../shared/utilities/childProcess'
+
+export class TestSamCliProcessInvoker implements SamCliProcessInvoker {
+    public constructor(
+        private readonly onInvoke: (...args: any[]) => ChildProcessResult
+    ) {
+    }
+
+    public invoke(options: SpawnOptions, ...args: string[]): Promise<ChildProcessResult>
+    public invoke(...args: string[]): Promise<ChildProcessResult>
+    public async invoke(first: SpawnOptions | string, ...rest: string[]): Promise<ChildProcessResult> {
+        const args = typeof first === 'string' ? [first, ...rest] : rest
+
+        return this.onInvoke(args)
+    }
+}
