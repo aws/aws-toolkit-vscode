cb56d5fef6a47ef64f384755c3429c6ac352dc4b
Christopher Christou <chrstou@amazon.com>
2019-05-23 16:33:34 -0700

    TestLogger now handles log path automatically

--- src/shared/loggerUtils.ts
+++ src/shared/loggerUtils.ts
@@ -11,12 +11,16 @@ import * as filesystemUtilities from './filesystemUtilities'
 import * as l from './logger'
 
 export class TestLogger {
-    // initializes a default logger. This persists through all tests.
-    // initializing a default logger means that any tested files with logger statements will work.
-    // as a best practice, please initialize a TestLogger before running tests on a file with logger statements.
+    /**
+     * initializes a default logger. This persists through all tests.
+     * initializing a default logger means that any tested files with logger statements will work.
+     * as a best practice, please initialize a TestLogger before running tests on a file with logger statements.
+     *
+     * @param logFolder - Folder to be managed by this object. Will be deleted on cleanup
+     * @param logger - Logger to work with
+     */
     private constructor(
         private readonly logFolder: string,
-        private readonly logfilePath: string,
         private readonly logger: l.Logger
     ) { }
 
@@ -30,19 +34,22 @@ export class TestLogger {
     }
 
     public async logContainsText(str: string): Promise<boolean> {
-        const logText = await filesystemUtilities.readFileAsString(this.logfilePath)
+        const logText = await filesystemUtilities.readFileAsString(TestLogger.getLogPath(this.logFolder))
 
-        return (logText.includes(str))
+        return logText.includes(str)
     }
 
     public static async createTestLogger(): Promise<TestLogger> {
         const logFolder = await filesystemUtilities.makeTemporaryToolkitFolder()
-        const logFilePath = path.join(logFolder, 'temp.log')
         const logger = await l.initialize({
-            logPath: logFilePath,
+            logPath: TestLogger.getLogPath(logFolder),
             logLevel: 'debug'
         })
 
-        return new TestLogger(logFolder, logFilePath, logger)
+        return new TestLogger(logFolder, logger)
+    }
+
+    private static getLogPath(logFolder: string): string {
+        return path.join(logFolder, 'temp.log')
     }
 }
