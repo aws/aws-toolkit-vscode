ae8ee26a3f5892c51c30c915197a61f2c27e7325
Matthew Pirocchi <pirocchi@amazon.com>
2019-06-10 18:02:40 -0400

    Fix rendering of Python code lenses when JEDI is enabled. (#605)

--- src/shared/codelens/pythonCodeLensProvider.ts
+++ src/shared/codelens/pythonCodeLensProvider.ts
@@ -16,6 +16,7 @@ import { LambdaHandlerCandidate } from '../lambdaHandlerSearch'
 import { getLogger } from '../logger'
 import { DefaultValidatingSamCliProcessInvoker } from '../sam/cli/defaultValidatingSamCliProcessInvoker'
 import { DefaultSamLocalInvokeCommand, WAIT_FOR_DEBUGGER_MESSAGES } from '../sam/cli/samCliLocalInvoke'
+import { SettingsConfiguration } from '../settingsConfiguration'
 import { Datum, TelemetryNamespace } from '../telemetry/telemetryTypes'
 import { registerCommand } from '../telemetry/telemetryUtils'
 import { getChannelLogger, getDebugPort } from '../utilities/vsCodeUtils'
@@ -51,12 +52,43 @@ const getSamProjectDirPathForFile = async (filepath: string): Promise<string> =>
     return path.dirname(filepath)
 }
 
-const getLambdaHandlerCandidates = async ({ uri }: { uri: vscode.Uri }): Promise<LambdaHandlerCandidate[]> => {
+async function getLambdaHandlerCandidates({
+    uri,
+    pythonSettings
+}: {
+    uri: vscode.Uri
+    pythonSettings: SettingsConfiguration
+}): Promise<LambdaHandlerCandidate[]> {
+    const PYTHON_JEDI_ENABLED_KEY = 'jediEnabled'
+    const RETRY_INTERVAL_MS = 1000
+    const MAX_RETRIES = 10
+
     const logger = getLogger()
     const filename = uri.fsPath
-    const symbols: vscode.DocumentSymbol[] =
-        (await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', uri)) ||
-        []
+
+    let symbols: vscode.DocumentSymbol[] =
+        await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', uri) || []
+
+    // A recent regression in vscode-python stops codelenses from rendering if we first return an empty array
+    // (because symbols have not yet been loaded), then a non-empty array (when our codelens provider is re-invoked
+    // upon symbols loading). To work around this, we attempt to wait for symbols to load before returning. We cannot
+    // distinguish between "the document does not contain any symbols" and "the symbols have not yet been loaded", so
+    // we stop retrying if we are still getting an empty result after several retries.
+    //
+    // This issue only surfaces when the setting `python.jediEnabled` is not set to false.
+    // TODO: When the above issue is resolved, remove this workaround AND bump the minimum
+    //       required VS Code version and/or add a minimum supported version for the Python
+    //       extension.
+    const jediEnabled = pythonSettings.readSetting<boolean>(PYTHON_JEDI_ENABLED_KEY, true)
+    if (jediEnabled) {
+        for (let i = 0; i < MAX_RETRIES && !symbols.length; i++) {
+            await new Promise<void>(resolve => setTimeout(resolve, RETRY_INTERVAL_MS))
+            symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+                'vscode.executeDocumentSymbolProvider',
+                uri
+            ) || []
+        }
+    }
 
     return symbols
         .filter(sym => sym.kind === vscode.SymbolKind.Function)
@@ -358,7 +390,9 @@ async function deleteFile(filePath: string): Promise<void> {
     }
 }
 
-export async function makePythonCodeLensProvider(): Promise<vscode.CodeLensProvider> {
+export async function makePythonCodeLensProvider(
+    pythonSettings: SettingsConfiguration
+): Promise<vscode.CodeLensProvider> {
     const logger = getLogger()
 
     return {
@@ -367,7 +401,10 @@ export async function makePythonCodeLensProvider(): Promise<vscode.CodeLensProvi
             document: vscode.TextDocument,
             token: vscode.CancellationToken
         ): Promise<vscode.CodeLens[]> => {
-            const handlers: LambdaHandlerCandidate[] = await getLambdaHandlerCandidates({ uri: document.uri })
+            const handlers: LambdaHandlerCandidate[] = await getLambdaHandlerCandidates({
+                uri: document.uri,
+                pythonSettings
+            })
             logger.debug(
                 'pythonCodeLensProvider.makePythonCodeLensProvider handlers:',
                 JSON.stringify(handlers, undefined, 2)
