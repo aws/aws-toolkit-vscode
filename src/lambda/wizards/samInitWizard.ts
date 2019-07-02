f2f49a0d14ef98aa938967ec30f910a41733f90c
Christopher Christou <chrstou@amazon.com>
2019-07-02 08:04:55 -0700

    The "Choose a different folder" picker item is always visible to reduce confusion when typing in the filter box

--- src/lambda/wizards/samInitWizard.ts
+++ src/lambda/wizards/samInitWizard.ts
@@ -251,6 +251,8 @@ class BrowseFolderQuickPickItem implements FolderQuickPickItem {
         'Choose a different folder...'
     )
 
+    public alwaysShow: boolean = true
+
     public constructor(
         private readonly context: CreateNewSamAppWizardContext
     ) {
