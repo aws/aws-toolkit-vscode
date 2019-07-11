5cf1e25f49f03374bb0e3826ed88e3007bf06a09
Hunter Werlla <werlla@amazon.com>
2019-07-11 10:51:35 -0700

    Port over programmatic changelog script (#639)

---
+++ build-scripts/createRelease.ts
@@ -0,0 +1,64 @@
+/*!
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as child_process from 'child_process'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+
+const changesDirectory = '.changes'
+const nextReleaseDirectory = path.join(changesDirectory, 'next-release')
+// tslint:disable-next-line:no-var-requires no-unsafe-any
+const releaseVersion = require(path.join('..', 'package.json')).version
+const changesFile = path.join(changesDirectory, `${releaseVersion}.json`)
+
+fs.mkdirpSync(nextReleaseDirectory)
+
+const changeFiles = fs.readdirSync(nextReleaseDirectory)
+if (changeFiles.length === 0) {
+    console.log('Error! no changes to release!')
+    process.exit(-1)
+}
+try {
+    fs.accessSync(changesFile)
+    console.log(`Error! changelog file ${changesFile} already exists for version ${releaseVersion}!`)
+    process.exit(-1)
+} catch (err) {
+    // This is what we want to happen, the file should not exist
+}
+
+const timestamp = new Date().toISOString().split('T')[0]
+const changelog: any = {
+    date: timestamp,
+    version: releaseVersion,
+    entries: []
+}
+
+for (const changeFile of changeFiles) {
+    const file = JSON.parse(fs.readFileSync(path.join(nextReleaseDirectory, changeFile)).toString())
+    // tslint:disable-next-line: no-unsafe-any
+    changelog.entries.push(file)
+}
+
+// tslint:disable-next-line: no-unsafe-any
+changelog.entries.sort((x: {type: string}, y: {type: string}) => x.type.localeCompare(y.type))
+
+// Write changelog file
+fs.writeFileSync(changesFile, JSON.stringify(changelog, undefined, '\t'))
+const fileData = fs.readFileSync('CHANGELOG.md')
+let append = `## ${releaseVersion} ${timestamp}\n\n`
+// tslint:disable-next-line: no-unsafe-any
+for (const file of changelog.entries) {
+    // tslint:disable-next-line: no-unsafe-any
+    append += `- **${file.type}** ${file.description}\n`
+}
+
+append += '\n' + fileData.toString()
+fs.writeFileSync('CHANGELOG.md', append)
+
+child_process.execSync(`git add ${changesDirectory}`)
+child_process.execSync(`git rm -rf ${nextReleaseDirectory}`)
+child_process.execSync('git add CHANGELOG.md')
+
+console.log(changesFile)
---
+++ build-scripts/newChange.ts
@@ -0,0 +1,66 @@
+/*!
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as child_process from 'child_process'
+import * as fs from 'fs-extra'
+import { join } from 'path'
+// tslint:disable-next-line:no-implicit-dependencies
+import * as readlineSync from 'readline-sync'
+import { v4 as uuid } from 'uuid'
+
+const directory = '.changes/next-release'
+
+const changeTypes = [
+    'Breaking Change',
+    'Feature',
+    'Bug Fix',
+    'Deprecation',
+    'Removal',
+    'Test'
+]
+
+interface NewChange {
+    type: string,
+    description: string
+}
+
+function promptForType(): string {
+    while (true) {
+        const response = readlineSync.keyInSelect(changeTypes, 'Please enter the type of change')
+        if (response === -1) {
+            console.log('Cancelling change')
+            process.exit(0)
+        }
+        if (response >= 0 && response < changeTypes.length) {
+            return changeTypes[response]
+        }
+        console.log('Invalid change type, change type must be between 0 and 5')
+    }
+}
+
+function promptForChange(): string {
+    while (true) {
+        const response = readlineSync.question('Change message: ').trim()
+        if (response) {
+            return response
+        }
+    }
+}
+
+fs.mkdirpSync(directory)
+
+const type = promptForType()
+const description = promptForChange()
+const contents: NewChange = {
+    type: type,
+    description: description
+}
+const fileName = `${type}-${uuid()}.json`
+const path = join(directory, fileName)
+fs.writeFileSync(path, JSON.stringify(contents, undefined, '\t'))
+
+console.log(`Change log written to ${path}`)
+child_process.execSync(`git add ${directory}`)
+console.log('Change log added to git working directory')
