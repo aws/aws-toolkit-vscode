32643f9cb2e46ea6957b4a57232db05406af3a42
Christopher Christou <chrstou@amazon.com>
2019-07-14 20:27:47 -0700

    wip - command registration

--- src/shared/sam/activation.ts
+++ src/shared/sam/activation.ts
@@ -17,14 +17,14 @@ import { CodeLensProviderParams } from '../codelens/codeLensUtils'
 import * as csLensProvider from '../codelens/csharpCodeLensProvider'
 import * as pyLensProvider from '../codelens/pythonCodeLensProvider'
 import * as tsLensProvider from '../codelens/typescriptCodeLensProvider'
-import { getLogger, Logger } from '../logger'
+import { getLogger } from '../logger'
 import { RegionProvider } from '../regions/regionProvider'
 import { DefaultSettingsConfiguration, SettingsConfiguration } from '../settingsConfiguration'
 import { TelemetryService } from '../telemetry/telemetryService'
 import { Datum, TelemetryNamespace } from '../telemetry/telemetryTypes'
 import { defaultMetricDatum, registerCommand } from '../telemetry/telemetryUtils'
 import { PromiseSharer } from '../utilities/promiseUtilities'
-import { getChannelLogger } from '../utilities/vsCodeUtils'
+import { ChannelLogger, getChannelLogger } from '../utilities/vsCodeUtils'
 import { initialize as initializeSamCliContext } from './cli/samCliContext'
 import { detectSamCli } from './cli/samCliDetection'
 
@@ -55,67 +55,90 @@ export async function activate(activateArguments: {
             activateArguments.telemetryService)
     )
 
-    // TODO : CC : handle command disposables
-    registerCommand({
-        command: 'aws.samcli.detect',
-        callback: async () => await PromiseSharer.getExistingPromiseOrCreate(
-            'samcli.detect',
-            async () => await detectSamCli(true)
-        )
+    await registerServerlessCommands({
+        awsContext: activateArguments.awsContext,
+        extensionContext: activateArguments.extensionContext,
+        regionProvider: activateArguments.regionProvider,
+        channelLogger,
     })
 
-    registerCommand({
-        command: 'aws.lambda.createNewSamApp',
-        callback: async (): Promise<{ datum: Datum }> => {
-            const createNewSamApplicationResults: CreateNewSamApplicationResults = await createNewSamApplication(
-                channelLogger,
-                activateArguments.extensionContext,
-            )
-            const datum = defaultMetricDatum('new')
-            datum.metadata = new Map()
-            applyResultsToMetadata(createNewSamApplicationResults, datum.metadata)
-
-            return {
-                datum
-            }
-        },
-        telemetryName: {
-            namespace: TelemetryNamespace.Project,
-            name: 'new'
-        }
-    })
-
-    registerCommand({
-        command: 'aws.deploySamApplication',
-        callback: async () => await deploySamApplication(
+    activateArguments.extensionContext.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
             {
-                channelLogger,
-                regionProvider: activateArguments.regionProvider,
-                extensionContext: activateArguments.extensionContext
+                language: 'json',
+                scheme: 'file',
+                pattern: '**/.aws/parameters.json'
             },
-            {
-                awsContext: activateArguments.awsContext
-            }
-        ),
-        telemetryName: {
-            namespace: TelemetryNamespace.Lambda,
-            name: 'deploy'
-        }
-    })
-
-    vscode.languages.registerCompletionItemProvider(
-        {
-            language: 'json',
-            scheme: 'file',
-            pattern: '**/.aws/parameters.json'
-        },
-        new SamParameterCompletionItemProvider(),
-        '"'
+            new SamParameterCompletionItemProvider(),
+            '"'
+        )
     )
 
     await detectSamCli(false)
 
     await resumeCreateNewSamApp()
+}
+
+async function registerServerlessCommands(
+    params: {
+        extensionContext: vscode.ExtensionContext,
+        awsContext: AwsContext,
+        regionProvider: RegionProvider,
+        channelLogger: ChannelLogger
+    }
+): Promise<void> {
+    params.extensionContext.subscriptions.push(
+        registerCommand({
+            command: 'aws.samcli.detect',
+            callback: async () => await PromiseSharer.getExistingPromiseOrCreate(
+                'samcli.detect',
+                async () => await detectSamCli(true)
+            )
+        })
+    )
+
+    params.extensionContext.subscriptions.push(
+        registerCommand({
+            command: 'aws.lambda.createNewSamApp',
+            callback: async (): Promise<{ datum: Datum }> => {
+                const createNewSamApplicationResults: CreateNewSamApplicationResults = await createNewSamApplication(
+                    params.channelLogger,
+                    params.extensionContext,
+                )
+                const datum = defaultMetricDatum('new')
+                datum.metadata = new Map()
+                applyResultsToMetadata(createNewSamApplicationResults, datum.metadata)
+
+                return {
+                    datum
+                }
+            },
+            telemetryName: {
+                namespace: TelemetryNamespace.Project,
+                name: 'new'
+            }
+        })
+    )
+
+    params.extensionContext.subscriptions.push(
+        registerCommand({
+            command: 'aws.deploySamApplication',
+            callback: async () => await deploySamApplication(
+                {
+                    channelLogger: params.channelLogger,
+                    regionProvider: params.regionProvider,
+                    extensionContext: params.extensionContext
+                },
+                {
+                    awsContext: params.awsContext
+                }
+            ),
+            telemetryName: {
+                namespace: TelemetryNamespace.Lambda,
+                name: 'deploy'
+            }
+        })
+    )
 }
 
 async function activateCodeLensProviders(
