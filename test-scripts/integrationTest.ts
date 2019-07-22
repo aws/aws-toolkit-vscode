fabfde589debc1f7b27d191647973428680a2b5e
Hunter Werlla <werlla@amazon.com>
2019-07-22 15:50:19 -0700

    add command line running of integration tests

--- test-scripts/integrationTest.ts
+++ test-scripts/integrationTest.ts
@@ -3,13 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict'
-
-var path = require('path');
+import { join } from 'path'
 
 // Set up some environment variables so the VS Code Test script knows where the tests are located
-process.env.CODE_TESTS_PATH = path.join(process.cwd(), 'out', 'src', 'test')
+process.env.CODE_TESTS_PATH = join(process.cwd(), 'out', 'src', 'integrationTest')
 process.env.CODE_EXTENSIONS_PATH = process.cwd()
 
 // Launch the VS Code Test Script
+// tslint:disable-next-line: no-var-requires
 require('../node_modules/vscode/bin/test')
