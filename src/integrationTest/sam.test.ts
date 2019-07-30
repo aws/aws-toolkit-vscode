5c6894c0b596869a2a2af71ebdb140b8ffc9bcec
Hunter Werlla <werlla@amazon.com>
2019-07-30 08:34:37 -0700

    ??

--- src/integrationTest/sam.test.ts
+++ src/integrationTest/sam.test.ts
@@ -12,9 +12,17 @@ import { getSamCliContext } from '../../src/shared/sam/cli/samCliContext'
 import { runSamCliInit, SamCliInitArgs } from '../../src/shared/sam/cli/samCliInit'
 import { activateExtension, sleep, TIMEOUT } from './integrationTestsUtilities'
 
-let projectSDK = 'nodejs10.x'
-let projectPath = 'testProject/hello-world/app.js'
 const projectFolder = `${__dirname}`
+let projectSDK = ''
+let projectPath = ''
+
+const runtimes = [
+    {name: 'nodejs10.x', path: 'testProject/hello-world/app.js'},
+    {name: 'dotnetcore2.1', path: 'testProject/src/HelloWorld/Function.cs'},
+    {name: 'python2.7', path: 'testProject/hello_world/app.py'},
+    // {name: 'python3.6', path: 'testProject/hello_world/app.py'},
+    // {name: 'python3.7', path: 'testProject/hello_world/app.py'}
+]
 
 async function openSamProject(): Promise<vscode.Uri> {
     const documentPath = path.join(projectFolder, projectPath)
@@ -37,109 +45,105 @@ async function getCodeLenses(): Promise<vscode.CodeLens[]> {
     return codeLenses as vscode.CodeLens[]
 }
 
-describe('SAM Integration', async () => {
-    before(async function () {
-        // tslint:disable-next-line: no-invalid-this
-        this.timeout(TIMEOUT)
-        await activateExtension()
-        /* read in from the environment what to test.
-           This has to be done through environment variables or dynamically generating
-           test files. Using the test generation APIs of Mocha will not work and results in
-           promises timing out and other issues.
-        */
-        if (process.env.SAM_PROJECT_SDK) {
-            projectSDK = process.env.SAM_PROJECT_SDK
-        }
-        if (process.env.SAM_PROJECT_PATH) {
-            projectPath = process.env.SAM_PROJECT_PATH
-        }
-        console.log(`Using SDK ${projectSDK} with project in path ${projectPath}`)
-        // this is really test 1, but since it has to run before everything it's in the before section
-        try {
-            removeSync(path.join(projectFolder, 'testProject'))
-        } catch (e) { }
-        mkdirpSync(projectFolder)
-        const initArguments: SamCliInitArgs = {
-            name: 'testProject',
-            location: projectFolder,
-            runtime: projectSDK as SamLambdaRuntime
-        }
-        const samCliContext = getSamCliContext()
-        await runSamCliInit(initArguments, samCliContext.invoker)
-        // we have to restore dotnet projects before we do anything, so we need this step just for dotnet
-        if (projectSDK.includes('dotnet')) {
-            console.log('Runtime under test is dotnet, will wait for extension to activate first')
-            await openSamProject()
-            // to add to this we have to wait for the .net extension to active to restore
-            await sleep(10000)
-        }
-    })
-
-    it('Generates a template with a proper runtime', async () => {
-        const fileContents = readFileSync(`${projectFolder}/testProject/template.yaml`).toString()
-        assert.ok(fileContents.includes(`Runtime: ${projectSDK}`))
-    })
-
-    it('Fails to create template when it already exists', async () => {
-        const initArguments: SamCliInitArgs = {
-            name: 'testProject',
-            location: projectFolder,
-            runtime: projectSDK as SamLambdaRuntime
-        }
-        console.log(initArguments.location)
-        const samCliContext = getSamCliContext()
-        await runSamCliInit(initArguments, samCliContext.invoker).catch((e: Error) => {
-            assert(e.message.includes('directory already exists'))
+// Iterate through and test all runtimes
+for (const runtime of runtimes) {
+    describe(`SAM Integration tests ${runtime.name}`, async () => {
+        before(async function () {
+            // tslint:disable-next-line: no-invalid-this
+            this.timeout(TIMEOUT)
+            projectSDK = runtime.name
+            projectPath = runtime.path
+            await activateExtension()
+            console.log(`Using SDK ${projectSDK} with project in path ${projectPath}`)
+            // this is really test 1, but since it has to run before everything it's in the before section
+            try {
+                removeSync(path.join(projectFolder, 'testProject'))
+            } catch (e) { }
+            mkdirpSync(projectFolder)
+            const initArguments: SamCliInitArgs = {
+                name: 'testProject',
+                location: projectFolder,
+                runtime: projectSDK as SamLambdaRuntime
+            }
+            const samCliContext = getSamCliContext()
+            await runSamCliInit(initArguments, samCliContext.invoker)
+            // we have to restore dotnet projects before we do anything, so we need this step just for dotnet
+            if (projectSDK.includes('dotnet')) {
+                console.log('Runtime under test is dotnet, will wait for extension to activate first')
+                await openSamProject()
+                // to add to this we have to wait for the .net extension to active to restore
+                await sleep(10000)
+            }
         })
-    }).timeout(TIMEOUT)
 
-    it('Invokes the run codelens', async () => {
-        const [ runCodeLens ] = await getCodeLenses()
-        assert.ok(runCodeLens.command)
-        const command = runCodeLens.command!
-        assert.ok(command.arguments)
-        const runResult: any | undefined = await vscode.commands.executeCommand(
-            command.command,
-            ...command.arguments!
-        )
-        assert.ok(runResult)
-        // tslint:disable: no-unsafe-any
-        const datum = runResult!.datum
-        assert.strictEqual(datum.name, 'invokelocal')
-        assert.strictEqual(datum.value, 1)
-        assert.strictEqual(datum.unit, 'Count')
+        it('Generates a template with a proper runtime', async () => {
+            const fileContents = readFileSync(`${projectFolder}/testProject/template.yaml`).toString()
+            assert.ok(fileContents.includes(`Runtime: ${projectSDK}`))
+        })
 
-        assert.ok(datum.metadata)
-        const metadata = datum.metadata!
-        assert.strictEqual(metadata.get('runtime'), projectSDK)
-        assert.strictEqual(metadata.get('debug'), 'false')
-    }).timeout(TIMEOUT)
+        it('Fails to create template when it already exists', async () => {
+            const initArguments: SamCliInitArgs = {
+                name: 'testProject',
+                location: projectFolder,
+                runtime: projectSDK as SamLambdaRuntime
+            }
+            console.log(initArguments.location)
+            const samCliContext = getSamCliContext()
+            await runSamCliInit(initArguments, samCliContext.invoker).catch((e: Error) => {
+                assert(e.message.includes('directory already exists'))
+            })
+        }).timeout(TIMEOUT)
 
-    it('Invokes the debug codelens', async () => {
-        const [, debugCodeLens ] = await getCodeLenses()
-        assert.ok(debugCodeLens.command)
-        const command = debugCodeLens.command!
-        assert.ok(command.arguments)
-        const runResult: any | undefined = await vscode.commands.executeCommand(
-            command.command,
-            ...command.arguments!
-        )
-        assert.ok(runResult)
-        const datum = runResult!.datum
-        assert.strictEqual(datum.name, 'invokelocal')
-        assert.strictEqual(datum.value, 1)
-        assert.strictEqual(datum.unit, 'Count')
+        it('Invokes the run codelens', async () => {
+            const [ runCodeLens ] = await getCodeLenses()
+            assert.ok(runCodeLens.command)
+            const command = runCodeLens.command!
+            assert.ok(command.arguments)
+            const runResult: any | undefined = await vscode.commands.executeCommand(
+                command.command,
+                ...command.arguments!
+            )
+            assert.ok(runResult)
+            // tslint:disable: no-unsafe-any
+            const datum = runResult!.datum
+            assert.strictEqual(datum.name, 'invokelocal')
+            assert.strictEqual(datum.value, 1)
+            assert.strictEqual(datum.unit, 'Count')
 
-        assert.ok(datum.metadata)
-        const metadata = datum.metadata!
-        assert.strictEqual(metadata.get('runtime'), projectSDK)
-        assert.strictEqual(metadata.get('debug'), 'true')
-    // This timeout is significantly longer, mostly to accommodate the long first time .net debugger
-    }).timeout(TIMEOUT * 2)
+            assert.ok(datum.metadata)
+            const metadata = datum.metadata!
+            assert.strictEqual(metadata.get('runtime'), projectSDK)
+            assert.strictEqual(metadata.get('debug'), 'false')
+        }).timeout(TIMEOUT)
 
-    after(async () => {
-        try {
-            removeSync(path.join(projectFolder, 'testProject'))
-        } catch (e) {}
+        it('Invokes the debug codelens', async () => {
+            const [, debugCodeLens ] = await getCodeLenses()
+            assert.ok(debugCodeLens.command)
+            const command = debugCodeLens.command!
+            assert.ok(command.arguments)
+            const runResult: any | undefined = await vscode.commands.executeCommand(
+                command.command,
+                ...command.arguments!
+            )
+            assert.ok(runResult)
+            const datum = runResult!.datum
+            assert.strictEqual(datum.name, 'invokelocal')
+            assert.strictEqual(datum.value, 1)
+            assert.strictEqual(datum.unit, 'Count')
+
+            assert.ok(datum.metadata)
+            const metadata = datum.metadata!
+            assert.strictEqual(metadata.get('runtime'), projectSDK)
+            assert.strictEqual(metadata.get('debug'), 'true')
+        // This timeout is significantly longer, mostly to accommodate the long first time .net debugger
+        }).timeout(TIMEOUT * 2)
+
+        after(async () => {
+            try {
+                removeSync(path.join(projectFolder, 'testProject'))
+            } catch (e) {}
+            // Sleep between tests to reduce the chances of SAM not working
+            await sleep(5000)
+        })
     })
-})
+}
