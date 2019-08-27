ad4fa1de8508e0c117ed332a5fa60e74e35f7b8f
Kyle Thomson <kiiadi@gmail.com>
2019-08-27 12:33:54 -0700

    Removing ECS nodes from the AWS Explorer - they do not interact with any toolkit functionality at present (#1185)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
@@ -5,11 +5,9 @@ package software.aws.toolkits.jetbrains.core.explorer
 
 import com.intellij.openapi.project.Project
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient
-import software.amazon.awssdk.services.ecs.EcsClient
 import software.amazon.awssdk.services.lambda.LambdaClient
 import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerServiceRootNode
 import software.aws.toolkits.jetbrains.services.cloudformation.CloudFormationServiceNode
-import software.aws.toolkits.jetbrains.services.ecs.EcsParentNode
 import software.aws.toolkits.jetbrains.services.lambda.LambdaServiceNode
 import software.aws.toolkits.resources.message
 
@@ -19,9 +17,6 @@ enum class AwsExplorerService(val serviceId: String, val displayName: String) {
     },
     LAMBDA(LambdaClient.SERVICE_NAME, message("explorer.node.lambda")) {
         override fun buildServiceRootNode(project: Project) = LambdaServiceNode(project)
-    },
-    ECS(EcsClient.SERVICE_NAME, message("explorer.node.ecs")) {
-        override fun buildServiceRootNode(project: Project) = EcsParentNode(project)
     },
     ;
 
