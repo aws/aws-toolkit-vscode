ed4b9c4b3a30b6568327153389085bba8fd07789
Christopher Christou <chrstou@amazon.com>
2019-08-27 17:49:47 -0700

    wip - this runs the main tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,50 @@
 /*!
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as testRunner from '../../third-party/test/index'
+// tslint:disable:no-implicit-dependencies
+import * as glob from 'glob'
+import * as Mocha from 'mocha'
+// tslint:enable:no-implicit-dependencies
 
-module.exports = testRunner
+import * as path from 'path'
+
+export async function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'bdd'
+    })
+    mocha.useColors(true)
+
+    const testsRoot = path.resolve(__dirname, '..')
+
+    return new Promise<void>((resolve, reject) => {
+        console.log(`Searching for tests in: ${testsRoot}`)
+
+        glob('test/**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                reject(err)
+
+                return
+            }
+
+            // Add files to the test suite
+            console.log(`Found ${files.length} file(s). Running...`)
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)))
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        reject(new Error(`${failures} tests failed.`))
+                    } else {
+                        resolve()
+                    }
+                })
+            } catch (err) {
+                reject(err)
+            }
+        })
+    })
+}
