a13fcc807c05e9dcb83e8ab4e99cfa1f363c3814
Christopher Christou <chrstou@amazon.com>
2019-09-06 08:07:59 -0700

    Removed sleep from promiseutilities test, reduced time to run from ~500ms to ~100ms

--- src/test/shared/utilities/promiseutilities.test.ts
+++ src/test/shared/utilities/promiseutilities.test.ts
@@ -4,81 +4,85 @@
  */
 
 import * as assert from 'assert'
+import * as vscode from 'vscode'
 import { PromiseSharer } from '../../../shared/utilities/promiseUtilities'
 
-// TODO : Sleeps are unstable and slow down test execution. We should manually resolve the promises, and phase sleep out
-async function sleep(ms: number): Promise<void> {
-    return new Promise<void>(resolve => {
-        setTimeout(resolve, ms)
-    })
-}
-
 describe('PromiseSharer', async () => {
+    let eventEmitter: vscode.EventEmitter<void>
+    let event: vscode.Event<void>
+    let waitForEventCount = 0
+
+    beforeEach(() => {
+        eventEmitter = new vscode.EventEmitter<void>()
+        event = eventEmitter.event
+        waitForEventCount = 0
+    })
+
+    afterEach(() => {
+        eventEmitter.dispose()
+    })
+
+    async function waitForEvent(): Promise<void> {
+        waitForEventCount++
+
+        return new Promise<void>(resolve => {
+            event(() => {
+                resolve()
+            })
+        })
+    }
+
     it('joins promises', async () => {
-        let timesCalled: number = 0
         let promisesCompleted: number = 0
 
-        const doThing = async () => {
-            await sleep(150)
-            timesCalled++
-        }
-
-        const p1 = PromiseSharer.getExistingPromiseOrCreate('abc', doThing).then(() => {
+        const p1 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent).then(() => {
             promisesCompleted++
         })
-        const p2 = PromiseSharer.getExistingPromiseOrCreate('abc', doThing).then(() => {
+        const p2 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent).then(() => {
             promisesCompleted++
         })
-        const p3 = PromiseSharer.getExistingPromiseOrCreate('abc', doThing).then(() => {
+        const p3 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent).then(() => {
             promisesCompleted++
         })
 
-        await p1
-        await p2
-        await p3
+        eventEmitter.fire()
+        await Promise.all([p1, p2, p3])
 
-        assert.strictEqual(timesCalled, 1)
+        assert.strictEqual(waitForEventCount, 1)
         assert.strictEqual(promisesCompleted, 3)
     })
 
     it('does not join different promises', async () => {
-        let timesCalled: number = 0
         let promisesCompleted: number = 0
 
-        const doThing = async () => {
-            await sleep(150)
-            timesCalled++
+        const p1 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent).then(() => {
+            promisesCompleted++
+        })
+        const p2 = PromiseSharer.getExistingPromiseOrCreate('def', waitForEvent).then(() => {
+            promisesCompleted++
+        })
+        const p3 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent).then(() => {
+            promisesCompleted++
+        })
+
+        for (const p of [p1, p2, p3]) {
+            eventEmitter.fire()
+            await p
         }
 
-        const p1 = PromiseSharer.getExistingPromiseOrCreate('abc', doThing).then(() => {
-            promisesCompleted++
-        })
-        const p2 = PromiseSharer.getExistingPromiseOrCreate('def', doThing).then(() => {
-            promisesCompleted++
-        })
-        const p3 = PromiseSharer.getExistingPromiseOrCreate('abc', doThing).then(() => {
-            promisesCompleted++
-        })
-
-        await p1
-        await p2
-        await p3
-
-        assert.strictEqual(timesCalled, 2)
+        assert.strictEqual(waitForEventCount, 2)
         assert.strictEqual(promisesCompleted, 3)
     })
 
     it('starts a new promise if previous one completed', async () => {
-        let timesCalled: number = 0
+        const p1 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent)
+        eventEmitter.fire()
+        await p1
 
-        const doThing = async () => {
-            await sleep(75)
-            timesCalled++
-        }
+        const p2 = PromiseSharer.getExistingPromiseOrCreate('abc', waitForEvent)
+        eventEmitter.fire()
+        await p2
 
-        await PromiseSharer.getExistingPromiseOrCreate('abc', doThing)
-        await PromiseSharer.getExistingPromiseOrCreate('abc', doThing)
-
-        assert.strictEqual(timesCalled, 2)
+        assert.strictEqual(waitForEventCount, 2)
     })
 })
