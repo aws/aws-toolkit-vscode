60d2f486c603d519c80f8e6c97034a35af7e0253
Kyle Thomson <kylthoms@amazon.com>
2019-09-10 13:02:21 -0700

    Adding load on create logic to the resource selector (#1205)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/ResourceSelector.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/ResourceSelector.kt
@@ -8,7 +8,9 @@ import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.ui.ColoredListCellRenderer
 import com.intellij.ui.MutableCollectionComboBoxModel
+import com.intellij.ui.SimpleColoredComponent
 import com.intellij.util.ExceptionUtil
 import org.jetbrains.annotations.TestOnly
 import software.aws.toolkits.core.utils.Either
@@ -20,6 +22,9 @@ import software.aws.toolkits.jetbrains.utils.CompatibilityUtils
 import software.aws.toolkits.jetbrains.utils.notifyError
 import software.aws.toolkits.jetbrains.utils.ui.find
 import software.aws.toolkits.resources.message
+import java.util.concurrent.CancellationException
+import java.util.concurrent.CompletableFuture
+import javax.swing.JList
 
 private typealias Selector<T> = Either<Any?, (T) -> Boolean>
 
@@ -27,18 +32,21 @@ private typealias Selector<T> = Either<Any?, (T) -> Boolean>
 class ResourceSelector<T> @JvmOverloads constructor(
     private val project: Project,
     private val resourceType: () -> Resource<out Collection<T>>?,
-    private val autoSelectSingleValue: Boolean = true
+    customRenderer: ((T, SimpleColoredComponent) -> SimpleColoredComponent)? = null,
+    loadOnCreate: Boolean = true
 ) : ComboBox<T>(MutableCollectionComboBoxModel<T>()) {
 
     @JvmOverloads
     constructor(
         project: Project,
         resourceType: Resource<out Collection<T>>,
-        autoSelectSingleValue: Boolean = true
+        customRenderer: ((T, SimpleColoredComponent) -> SimpleColoredComponent)? = null,
+        loadOnCreate: Boolean = true
     ) : this(
         project,
         { resourceType },
-        autoSelectSingleValue
+        customRenderer,
+        loadOnCreate
     )
 
     private val resourceCache = AwsResourceCache.getInstance(project)
@@ -46,9 +54,29 @@ class ResourceSelector<T> @JvmOverloads constructor(
     private var loadingStatus: Status = Status.NOT_LOADED
     private var shouldBeEnabled: Boolean = isEnabled
     private var selector: Selector<T>? = null
+    @Volatile
+    private var loadingFuture: CompletableFuture<*>? = null
 
     init {
-        if (isEnabled) {
+        customRenderer?.let {
+            setRenderer(
+                object : ColoredListCellRenderer<T>() {
+                    override fun customizeCellRenderer(
+                        list: JList<out T>,
+                        value: T?,
+                        index: Int,
+                        selected: Boolean,
+                        hasFocus: Boolean
+                    ) {
+                        value?.let {
+                            customRenderer.invoke(value, this)
+                        }
+                    }
+                }
+            )
+        }
+
+        if (loadOnCreate) {
             reload()
         }
     }
@@ -58,19 +86,28 @@ class ResourceSelector<T> @JvmOverloads constructor(
     fun reload(forceFetch: Boolean = false) {
         val previouslySelected = model.selectedItem
         loadingStatus = Status.LOADING
+
+        // If this reload will supersede a previous once, cancel it
+        loadingFuture?.cancel(true)
+
         runInEdt(ModalityState.any()) {
+            loadingStatus = Status.LOADING
             super.setEnabled(false)
             setEditable(true)
+
             super.setSelectedItem(message("loading_resource.loading"))
 
             val resource = resourceType.invoke()
             if (resource == null) {
                 processSuccess(emptyList(), null)
             } else {
-                resourceCache.getResource(resource, forceFetch = forceFetch).whenComplete { value, error ->
+                val resultFuture = resourceCache.getResource(resource, forceFetch = forceFetch).toCompletableFuture()
+                loadingFuture = resultFuture
+                resultFuture.whenComplete { value, error ->
                     when {
-                        value != null -> processSuccess(value, previouslySelected)
+                        error is CancellationException -> {} // Do nothing, results don't matter
                         error != null -> processFailure(error)
+                        value != null -> processSuccess(value, previouslySelected)
                     }
                 }
             }
@@ -78,8 +115,8 @@ class ResourceSelector<T> @JvmOverloads constructor(
     }
 
     override fun getModel(): MutableCollectionComboBoxModel<T> =
-        // javax.swing.DefaultComboBoxModel.addAll(java.util.Collection<? extends E>) isn't in Java 8
-        // The addElement method can lead to multiple selection events firing as elements are added
+    // javax.swing.DefaultComboBoxModel.addAll(java.util.Collection<? extends E>) isn't in Java 8
+    // The addElement method can lead to multiple selection events firing as elements are added
         // Use IntelliJ's to work around this short coming
         super.getModel() as MutableCollectionComboBoxModel<T>
 
@@ -120,6 +157,9 @@ class ResourceSelector<T> @JvmOverloads constructor(
         }
     }
 
+    val isLoading: Boolean
+        get() = loadingStatus == Status.LOADING
+
     @TestOnly
     fun forceLoaded() {
         loadingStatus = Status.LOADED
@@ -127,18 +167,17 @@ class ResourceSelector<T> @JvmOverloads constructor(
 
     private fun processSuccess(value: Collection<T>, previouslySelected: Any?) {
         runInEdt(ModalityState.any()) {
-            loadingStatus = Status.LOADED
             setEditable(false)
 
-            model.removeAll()
-            model.add(value.sortedBy { it.toString().toLowerCase() })
+            model.replaceAll(value.sortedBy { it.toString().toLowerCase() })
+            loadingStatus = Status.LOADED
 
             super.setEnabled(shouldBeEnabled)
 
-            if (autoSelectSingleValue && value.size == 1) {
-                super.setSelectedItem(value.first())
-            } else {
-                super.setSelectedItem(determineSelection(selector, previouslySelected))
+            when {
+                value.isEmpty() -> super.setSelectedItem(null)
+                value.size == 1 -> super.setSelectedItem(value.first())
+                else -> super.setSelectedItem(determineSelection(selector, previouslySelected))
             }
         }
     }
--- jetbrains-core/tst/software/aws/toolkits/jetbrains/ui/ResourceSelectorTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/ui/ResourceSelectorTest.kt
@@ -5,10 +5,7 @@ package software.aws.toolkits.jetbrains.ui
 
 import com.intellij.testFramework.ProjectRule
 import com.intellij.testFramework.runInEdtAndWait
-import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +13,6 @@ import software.aws.toolkits.jetbrains.core.MockResourceCache
 import software.aws.toolkits.jetbrains.core.Resource
 import software.aws.toolkits.jetbrains.utils.test.retryableAssert
 import software.aws.toolkits.resources.message
-import java.awt.event.ActionListener
 import java.util.concurrent.CompletableFuture
 
 class ResourceSelectorTest {
@@ -117,13 +113,15 @@ class ResourceSelectorTest {
     }
 
     @Test
-    fun actionListenerIsInvokedOnSelectionChangeOnce() {
+    fun actionListenerIsInvokedOnLoadingCorrectly() {
         val future = CompletableFuture<List<String>>() // Use the future to force slow load
         mockResourceCache.addEntry(mockResource, future)
-        val comboBox = ResourceSelector(projectRule.project, mockResource)
+        val comboBox = ResourceSelector(projectRule.project, mockResource, loadOnCreate = false)
 
-        val actionListener = mock<ActionListener>()
-        comboBox.addActionListener(actionListener)
+        val loadingStatus = mutableListOf<Boolean>()
+        comboBox.addActionListener { loadingStatus.add(comboBox.isLoading) }
+
+        comboBox.reload()
 
         runInEdtAndWait {
             comboBox.selectedItem = "bar"
@@ -134,7 +132,75 @@ class ResourceSelectorTest {
         retryableAssert {
             runInEdtAndWait {
                 assertThat(comboBox.selected()).isEqualTo("bar")
-                verify(actionListener, times(1)).actionPerformed(any())
+                assertThat(loadingStatus).containsExactly(true, false)
+            }
+        }
+    }
+
+    @Test
+    fun aSingleResultWillAutoSelect() {
+        mockResourceCache.addEntry(mockResource, listOf("bar"))
+        val comboBox = ResourceSelector(projectRule.project, mockResource)
+
+        runInEdtAndWait {
+            comboBox.selectedItem = "bar"
+        }
+
+        comboBox.reload()
+
+        retryableAssert {
+            runInEdtAndWait {
+                assertThat(comboBox.selected()).isEqualTo("bar")
+            }
+        }
+    }
+
+    @Test
+    fun onlyTheLastReloadIsApplied() {
+        fun createMockResource(name: String): Triple<Resource.Cached<List<String>>, CompletableFuture<List<String>>, List<String>> {
+            val future = CompletableFuture<List<String>>()
+            val resource = mock<Resource.Cached<List<String>>> {
+                on { id }.thenReturn(name)
+            }
+
+            mockResourceCache.addEntry(resource, future)
+
+            return Triple(resource, future, listOf(name))
+        }
+
+        // Create a bunch of different cache entries
+        val resultList = listOf(
+            createMockResource("1"),
+            createMockResource("2"),
+            createMockResource("3"),
+            createMockResource("4"),
+            createMockResource("5")
+        )
+
+        var counter = 0
+        val comboBox = ResourceSelector(projectRule.project, { resultList[counter].first }, loadOnCreate = false)
+
+        val loadingStatus = mutableListOf<Boolean>()
+        comboBox.addActionListener { loadingStatus.add(comboBox.isLoading) }
+
+        // Trigger a reload for each resource to simulate a bunch of reloads against different cache entries
+        runInEdtAndWait {
+            resultList.forEach { _ ->
+                comboBox.reload()
+                counter++
+            }
+        }
+
+        // Complete the cache load
+        resultList.forEach {
+            it.second.complete(it.third)
+        }
+
+        retryableAssert {
+            runInEdtAndWait {
+                assertThat(comboBox.selected()).isEqualTo("5")
+                // 5 loads = 5 loadings + 1 loaded
+                assertThat(loadingStatus).containsExactly(true, true, true, true, true, false)
             }
         }
     }
