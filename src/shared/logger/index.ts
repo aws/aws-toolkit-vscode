393cf4e0a6ef26d1e5168922f010952ea142963b
Christopher Christou <chrstou@amazon.com>
2019-10-04 08:18:13 -0700

    Index comment, moved logger test

--- src/shared/logger/index.ts
+++ src/shared/logger/index.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// This file is the common import file, intended for use by most extension code.
+// It surfaces Logger and related interfaces, types, and retrieval.
 import * as loggableType from './loggableType'
 import * as logger from './logger'
 
