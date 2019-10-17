a685278891ce04165e9c790b3e292a472adda9ec
Austin Brooks <abrooksv@users.noreply.github.com>
2019-10-17 12:51:38 -0700

    Keep using our workaround for Tabs in gui tests (#1277)

--- jetbrains-core-gui/tst/software/aws/toolkits/jetbrains/fixtures/JBEditorTabsUtils.kt
+++ jetbrains-core-gui/tst/software/aws/toolkits/jetbrains/fixtures/JBEditorTabsUtils.kt
@@ -6,13 +6,13 @@ package software.aws.toolkits.jetbrains.fixtures
 import com.intellij.testGuiFramework.framework.Timeouts
 import com.intellij.testGuiFramework.impl.findComponentWithTimeout
 import com.intellij.testGuiFramework.util.step
-import com.intellij.ui.tabs.impl.TabLabel
 import org.fest.swing.core.Robot
 import org.fest.swing.fixture.ContainerFixture
 import org.fest.swing.timing.Timeout
 import java.awt.Container
+import javax.swing.JPanel
 
-class JBTabsFixture(private val robot: Robot, private val tabLabel: TabLabel) {
+class JBTabsFixture(private val robot: Robot, private val tabLabel: JPanel) {
     fun selectTab() {
         robot.click(tabLabel)
     }
@@ -20,6 +20,6 @@ class JBTabsFixture(private val robot: Robot, private val tabLabel: TabLabel) {
 
 fun <C : Container> ContainerFixture<C>.jbTab(tabTitle: String, timeout: Timeout = Timeouts.defaultTimeout) =
     step("search JBTabs with label '$tabTitle'") {
-        val tabLabel: TabLabel = findComponentWithTimeout(timeout) { it.info.text == tabTitle }
+        val tabLabel: JPanel = findComponentWithTimeout(timeout) { it.toString() == tabTitle }
         JBTabsFixture(robot(), tabLabel)
     }
