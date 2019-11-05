7750e94812c2a2774ce7acabc7b824a394c93dac
Austin Brooks <abrooksv@users.noreply.github.com>
2019-11-05 07:14:00 -0800

    Fix GUI tests on 2019.3 (#1327)
    * Fix how the SDK combo box is found due to we do not have access to the Java plugin's code anymore in 193 due to changes in framework
    * Fix the set sam CLI test sometimes failing if the tree entry was off the page
    * Change how we "bundle" the tests so that it has everything on the classpath correctly at runtime

--- jetbrains-core-gui/tst/software/aws/toolkits/jetbrains/settings/SetSamCli.kt
+++ jetbrains-core-gui/tst/software/aws/toolkits/jetbrains/settings/SetSamCli.kt
@@ -3,29 +3,50 @@
 
 package software.aws.toolkits.jetbrains.settings
 
+import com.intellij.testGuiFramework.fixtures.JDialogFixture
+import com.intellij.testGuiFramework.fixtures.SearchTextFieldFixture
 import com.intellij.testGuiFramework.impl.GuiTestCase
 import com.intellij.testGuiFramework.impl.button
+import com.intellij.testGuiFramework.impl.findComponentWithTimeout
 import com.intellij.testGuiFramework.impl.jTree
 import com.intellij.testGuiFramework.impl.textfield
+import com.intellij.testGuiFramework.util.Key
+import com.intellij.testGuiFramework.util.Modifier
+import com.intellij.testGuiFramework.util.plus
 import com.intellij.testGuiFramework.util.step
+import com.intellij.ui.SearchTextField
 import org.junit.Test
-import software.aws.toolkits.jetbrains.fixtures.openSettingsDialog
 
 class SetSamCli : GuiTestCase() {
     @Test
     fun setSamCli() {
         val samPath = System.getenv("SAM_CLI_EXEC") ?: SamExecutableDetector().detect() ?: "sam"
         welcomeFrame {
-            // this may become obsolete once jetbrains provides a util to open the settings dialog
-            openSettingsDialog()
-            dialog(defaultSettingsTitle) {
-                jTree("Tools", "AWS").clickPath()
-                step("Set SAM CLI executable path to $samPath") {
-                    val execPath = textfield("SAM CLI executable:")
-                    execPath.setText(samPath)
+            step("Open preferences page") {
+                shortcut(Modifier.CONTROL + Modifier.ALT + Key.S, Modifier.META + Key.COMMA)
+
+                dialog(defaultSettingsTitle) {
+                    // Search for AWS because sometimes it is off the screen
+                    step("Search for AWS") {
+                        findSearchTextField().click()
+
+                        robot().enterText("AWS")
+                    }
+
+                    jTree("Tools", "AWS").clickPath()
+
+                    step("Set SAM CLI executable path to $samPath") {
+                        val execPath = textfield("SAM CLI executable:")
+                        execPath.setText(samPath)
+                    }
+                    button("OK").click()
                 }
-                button("OK").click()
             }
         }
+    }
+
+    private fun JDialogFixture.findSearchTextField(): SearchTextFieldFixture {
+        val searchTextField = findComponentWithTimeout(this.target(), SearchTextField::class.java)
+        return SearchTextFieldFixture(this.robot(), searchTextField)
     }
 }
