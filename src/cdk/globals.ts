562235877f14ece3fea51ca627229b7021e57f2e
Shiv Lakshminarayan <shivlaks@amazon.com>
2019-11-12 09:48:37 -0800

    add globals to include a cdk namespace used for initialization ahead of extension.ts

---
+++ src/cdk/globals.ts
@@ -0,0 +1,29 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Namespace for common variables used globally in the extension.
+ * All variables here must be initialized in the activate() method of extension.ts
+ */
+export namespace cdk {
+    export namespace iconPaths {
+        export const dark: IconPaths = makeIconPathsObject()
+        export const light: IconPaths = makeIconPathsObject()
+    }
+}
+
+export interface IconPaths {
+    cdk: string
+    cloudFormation: string
+    settings: string
+}
+
+function makeIconPathsObject(): IconPaths {
+    return {
+        cdk: '',
+        cloudFormation: '',
+        settings: ''
+    }
+}
