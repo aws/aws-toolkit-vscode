c01f2d01a66f67f9db3715b00be9fccc712d1c9a
Christopher Christou <chrstou@amazon.com>
2019-12-17 16:06:13 -0800

    Cleanup

--- src/shared/codelens/pythonDebugAdapterHeartbeat.ts
+++ src/shared/codelens/pythonDebugAdapterHeartbeat.ts
@@ -6,6 +6,8 @@
 import * as net from 'net'
 import { getLogger, Logger } from '../logger'
 
+const DEBUG_ADAPTER_RESPONSE_TIMEOUT_MILLIS = 1500
+
 export class PythonDebugAdapterHeartbeat {
     private readonly socket: net.Socket
     private readonly logger: Logger = getLogger()
@@ -18,11 +20,10 @@ export class PythonDebugAdapterHeartbeat {
 
         return new Promise<boolean>((resolve, reject) => {
             const timeout = setTimeout(() => {
-                // TODO : CC : Cancel this if other exit routes (also same for other timeouts in file)
                 this.logger.verbose(`Timeout before connecting to port ${this.port}`)
                 // We never got a response yet/at all
                 resolve(false)
-            }, 1500)
+            }, DEBUG_ADAPTER_RESPONSE_TIMEOUT_MILLIS)
 
             this.socket.once('connect', () => {
                 clearTimeout(timeout)
@@ -32,9 +33,9 @@ export class PythonDebugAdapterHeartbeat {
             this.socket.once('error', (err: Error) => {
                 clearTimeout(timeout)
                 this.logger.verbose('Error while connecting', err)
-                // todo : only interested in ECONNREFUSED
                 resolve(false)
             })
+
             this.socket.connect(this.port)
         })
     }
@@ -47,44 +48,30 @@ export class PythonDebugAdapterHeartbeat {
                 // We never got a response yet/at all
                 this.logger.verbose('Timeout waiting for response from Debug Adapter')
                 resolve(false)
-            }, 1500)
+            }, DEBUG_ADAPTER_RESPONSE_TIMEOUT_MILLIS)
 
-            // TODO : CC : on / once ?
-            this.socket.once('data', data => {
+            this.socket.on('data', data => {
                 clearTimeout(timeout)
                 this.logger.verbose('Data received from Debug Adapter', data.toString())
-                // TODO : check data returned
                 resolve(true)
             })
 
-            // TODO : CC : LEFT OFF : We keep hitting error DISCONNECTED here.
-            // So, let's connect + disconnect each cycle
-            // Consider using this.socket.once('') calls
             this.socket.once('error', (err: Error) => {
                 clearTimeout(timeout)
                 this.logger.verbose('Error writing to Debug Adapter', err)
                 resolve(false)
             })
-            const r = this.socket.write('Content-Length: 2\r\n\r\n{}')
-            this.logger.verbose(`Data written to Debug Adapter, write result: ${r}`)
 
-            // TODO : custom message
-            // socket.write('Content-Length: 41\r\n\r\n', 'ASCII', () => {
-            //     socket.write('{"type": "request","command": "christou"}', 'utf-8')
-            // })
+            // Send an empty message, serving as a no-op.
+            // If we get a response, we know the Adapter is up and running.
+            // See Base protocol: https://microsoft.github.io/debug-adapter-protocol/overview
+            const writeResult = this.socket.write('Content-Length: 2\r\n\r\n{}')
+            this.logger.verbose(`Data written to Debug Adapter, write result: ${writeResult}`)
         })
     }
 
     public async disconnect(): Promise<void> {
         this.logger.verbose('Destroying socket')
         this.socket.destroy()
-
-        // return new Promise<void>((resolve, reject) => {
-        //     this.socket.once('close', data => {
-        //         resolve()
-        //     })
-        //     this.socket.destroy()
-        //     // this.socket.end()
-        // })
     }
 }
