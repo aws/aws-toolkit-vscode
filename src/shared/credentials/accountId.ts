9f2f4d516a65b118da635637e52ddf3a188fe772
Christopher Christou <chrstou@amazon.com>
2019-12-24 11:58:12 -0800

    Utility method: getAccountId

---
+++ src/shared/credentials/accountId.ts
@@ -0,0 +1,26 @@
+/*!
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ext } from '../extensionGlobals'
+import { getLogger } from '../logger'
+
+/**
+ * Looks up the Credentials associated Account ID with its own STS Client.
+ */
+export async function getAccountId(credentials: AWS.Credentials, region: string): Promise<string | undefined> {
+    try {
+        const sts = ext.toolkitClientBuilder.createStsClient(region, {
+            credentials: credentials
+        })
+
+        const response = await sts.getCallerIdentity()
+
+        return response.Account
+    } catch (err) {
+        getLogger().error('Error getting AccountId', err as Error)
+
+        return undefined
+    }
+}
