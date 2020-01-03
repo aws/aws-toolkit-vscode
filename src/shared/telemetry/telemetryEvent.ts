57994c0d1a0ba2f55e5112a8f6799f7f0ae9dc22
Hunter Werlla <werlla@amazon.com>
2020-01-03 08:58:33 -0800

    hard delete some types

--- src/shared/telemetry/telemetryEvent.ts
+++ src/shared/telemetry/telemetryEvent.ts
@@ -7,21 +7,20 @@ import { MetadataEntry, MetricDatum } from './clienttelemetry'
 import { Datum } from './telemetryTypes'
 
 const NAME_ILLEGAL_CHARS_REGEX = new RegExp('[^\\w+-.:]', 'g')
-const REMOVE_UNDERSCORES_REGEX = new RegExp('_', 'g')
 
 export interface TelemetryEvent {
-    namespace: string
     createTime: Date
-    data?: Datum[]
+    data: Datum[]
 }
 
 export function toMetricData(array: TelemetryEvent[]): MetricDatum[] {
     return ([] as MetricDatum[]).concat(
-        ...array.map(metricEvent => {
-            const namespace = metricEvent.namespace.replace(REMOVE_UNDERSCORES_REGEX, '')
-
-            if (metricEvent.data !== undefined) {
-                const mappedEventData = metricEvent.data.map(datum => {
+        ...array
+            .filter(item => {
+                return item.data !== undefined
+            })
+            .map(metricEvent =>
+                metricEvent.data.map(datum => {
                     let metadata: MetadataEntry[] | undefined
                     let unit = datum.unit
 
@@ -35,27 +34,14 @@ export function toMetricData(array: TelemetryEvent[]): MetricDatum[] {
                         unit = 'None'
                     }
 
-                    const name = datum.name.replace(REMOVE_UNDERSCORES_REGEX, '')
-
                     return {
-                        MetricName: `${namespace}_${name}`.replace(NAME_ILLEGAL_CHARS_REGEX, ''),
+                        MetricName: datum.name.replace(NAME_ILLEGAL_CHARS_REGEX, ''),
                         EpochTimestamp: metricEvent.createTime.getTime(),
                         Unit: unit,
                         Value: datum.value,
                         Metadata: metadata
                     }
                 })
-
-                return mappedEventData
-            }
-
-            // case where there are no datum attached to the event, but we should still publish this
-            return {
-                MetricName: namespace.replace(NAME_ILLEGAL_CHARS_REGEX, ''),
-                EpochTimestamp: metricEvent.createTime.getTime(),
-                Unit: 'None',
-                Value: 0
-            }
-        })
+            )
     )
 }
--- src/shared/telemetry/telemetryTypes.ts
+++ src/shared/telemetry/telemetryTypes.ts
@@ -5,11 +5,6 @@
 
 import { Unit } from './clienttelemetry'
 
-export interface TelemetryName {
-    namespace: TelemetryNamespace | OldTelemetryNamespace
-    name: string
-}
-
 export interface Datum {
     name: string
     value: number
@@ -17,10 +12,10 @@ export interface Datum {
     metadata?: Map<string, string>
 }
 
-type OldTelemetryNamespace = 'Command'
 export const ACCOUNT_METADATA_KEY = 'awsAccount'
 
 export enum TelemetryNamespace {
+    Command = 'Command',
     Aws = 'aws',
     Cdk = 'cdk',
     Cloudformation = 'cloudformation',
