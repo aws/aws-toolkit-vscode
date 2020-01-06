e4b0a46fd93102bebf7cc50760108130f6f5140e
Austin Brooks <abrooksv@users.noreply.github.com>
2020-01-06 14:08:16 -0800

    Remove AnActionWrapper from CreateBucketAction, make it dumb aware (#1488)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketActions/CreateBucketAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketActions/CreateBucketAction.kt
@@ -4,15 +4,15 @@ package software.aws.toolkits.jetbrains.services.s3.bucketActions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.project.DumbAwareAction
 import icons.AwsIcons
 import software.amazon.awssdk.services.s3.S3Client
-import software.aws.toolkits.jetbrains.components.telemetry.AnActionWrapper
 import software.aws.toolkits.jetbrains.core.AwsClientManager
 import software.aws.toolkits.jetbrains.services.s3.CreateS3BucketDialog
 import software.aws.toolkits.resources.message
 
-class CreateBucketAction : AnActionWrapper(message("s3.create.bucket.title"), null, AwsIcons.Resources.S3_BUCKET) {
-    override fun doActionPerformed(e: AnActionEvent) {
+class CreateBucketAction : DumbAwareAction(message("s3.create.bucket.title"), null, AwsIcons.Resources.S3_BUCKET) {
+    override fun actionPerformed(e: AnActionEvent) {
         val project = e.getRequiredData(LangDataKeys.PROJECT)
         val client: S3Client = AwsClientManager.getInstance(project).getClient()
         val dialog = CreateS3BucketDialog(project, client)
