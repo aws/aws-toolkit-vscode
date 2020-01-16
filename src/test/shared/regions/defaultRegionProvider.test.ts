69b58e1ed4ddbfc4c2c6ac5065e4b6375383e40e
Christopher Christou <chrstou@amazon.com>
2020-01-15 17:11:08 -0800

    DefaultRegionProvider test

--- src/test/shared/regions/defaultRegionProvider.test.ts
+++ src/test/shared/regions/defaultRegionProvider.test.ts
@@ -3,59 +3,64 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// 'use strict'
+'use strict'
 
-// import * as assert from 'assert'
-// import { DefaultRegionProvider } from '../../../shared/regions/defaultRegionProvider'
-// import { ResourceFetcher } from '../../../shared/resourcefetcher/resourcefetcher'
+import * as assert from 'assert'
+import { DefaultRegionProvider } from '../../../shared/regions/defaultRegionProvider'
+import { EndpointsProvider } from '../../../shared/regions/endpointsProvider'
+import { ResourceFetcher } from '../../../shared/resourcefetcher/resourcefetcher'
 
-// const sampleEndpoints = {
-//     partitions: [
-//         {
-//             partition: 'aws',
-//             partitionName: 'Standard',
-//             regions: {
-//                 region1: {
-//                     description: 'aws region one'
-//                 },
-//                 region2: {
-//                     description: 'aws region two'
-//                 },
-//                 region3: {
-//                     description: 'aws region three'
-//                 }
-//             }
-//         },
-//         {
-//             partition: 'aws-cn',
-//             partitionName: 'China',
-//             regions: {
-//                 awscnregion1: {
-//                     description: 'aws-cn region one'
-//                 }
-//             }
-//         }
-//     ]
-// }
+const sampleEndpoints = {
+    partitions: [
+        {
+            partition: 'aws',
+            partitionName: 'Standard',
+            regions: {
+                region1: {
+                    description: 'aws region one'
+                },
+                region2: {
+                    description: 'aws region two'
+                },
+                region3: {
+                    description: 'aws region three'
+                }
+            }
+        },
+        {
+            partition: 'aws-cn',
+            partitionName: 'China',
+            regions: {
+                awscnregion1: {
+                    description: 'aws-cn region one'
+                }
+            }
+        }
+    ]
+}
 
-// describe('DefaultRegionProvider', async () => {
-//     class ResourceFetcherCounter implements ResourceFetcher {
-//         public timesCalled = 0
+describe('DefaultRegionProvider', async () => {
+    const resourceFetcher: ResourceFetcher = {
+        get: async () => {
+            return JSON.stringify(sampleEndpoints)
+        }
+    }
 
-//         public async get(): Promise<string> {
-//             this.timesCalled++
+    it('returns region data', async () => {
+        const endpointsProvider = new EndpointsProvider(resourceFetcher, resourceFetcher)
+        await endpointsProvider.load()
 
-//             return JSON.stringify(sampleEndpoints)
-//         }
-//     }
+        const regionProvider = new DefaultRegionProvider(endpointsProvider)
 
-//     it('returns region data', async () => {
-//         const resourceFetcher = new ResourceFetcherCounter()
-//         const regionProvider = new DefaultRegionProvider(resourceFetcher)
+        const regions = await regionProvider.getRegionData()
 
-//         const regions = await regionProvider.getRegionData()
+        assert.strictEqual(regions.length, 3, 'Expected to retrieve three regions')
 
-//         assert.strictEqual(regions.length, 3, 'Expected to retrieve three regions')
-//         assert.strictEqual(resourceFetcher.timesCalled, 1)
-//     })
-// })
+        for (const expectedRegionId of ['region1', 'region2', 'region3']) {
+            assert.ok(
+                regions.some(r => r.regionCode === expectedRegionId),
+                `${expectedRegionId} was missing from retrieved regions`
+            )
+        }
+    })
+})
