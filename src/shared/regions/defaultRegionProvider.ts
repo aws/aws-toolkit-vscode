c28a9601a6dd50ff6afc296f3c6a6ec5cb578f4a
Christopher Christou <chrstou@amazon.com>
2020-01-15 16:14:25 -0800

    Pull back next round of region provider changes

--- src/shared/regions/defaultRegionProvider.ts
+++ src/shared/regions/defaultRegionProvider.ts
@@ -3,21 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Endpoints, Partition, Region } from './endpoints'
+import { Endpoints, Region } from './endpoints'
 import { EndpointsProvider } from './endpointsProvider'
 import { RegionInfo } from './regionInfo'
 import { RegionProvider } from './regionProvider'
 
-interface RegionData {
-    partitionId: string
-    serviceIds: string[]
-}
-
 export class DefaultRegionProvider implements RegionProvider {
-    // TODO : Deprecate _loadedRegions
     private _loadedRegions: RegionInfo[] = []
-    private partitions: Partition[] = []
-    private readonly regionIdToRegionData: Map<string, RegionData> = new Map()
 
     public constructor(endpointsProvider: EndpointsProvider) {
         endpointsProvider.onEndpointsUpdated(e => this.loadFromEndpointsProvider(e))
@@ -26,26 +18,6 @@ export class DefaultRegionProvider implements RegionProvider {
 
     public async getRegionData(): Promise<RegionInfo[]> {
         return this._loadedRegions
-    }
-
-    public getRegions(partitionId: string): Region[] {
-        let regions: Region[] = []
-
-        this.partitions
-            .filter(p => p.id === partitionId)
-            .forEach(p => {
-                regions = regions.concat(p.regions)
-            })
-
-        return regions
-    }
-
-    public getParentPartitionId(regionId: string): string | undefined {
-        return this.regionIdToRegionData.get(regionId)?.partitionId
-    }
-
-    public isServiceInRegion(serviceId: string, regionId: string): boolean {
-        return !!this.regionIdToRegionData.get(regionId)?.serviceIds.find(x => x === serviceId) ?? false
     }
 
     private loadFromEndpointsProvider(provider: EndpointsProvider) {
@@ -59,28 +31,6 @@ export class DefaultRegionProvider implements RegionProvider {
     private loadFromEndpoints(endpoints: Endpoints) {
         // TODO : Support other Partition regions : https://github.com/aws/aws-toolkit-vscode/issues/188
         this._loadedRegions = getRegionInfo(endpoints, 'aws')
-        this.partitions = endpoints.partitions
-
-        this.regionIdToRegionData.clear()
-
-        endpoints.partitions.forEach(p => {
-            p.regions.forEach(r =>
-                this.regionIdToRegionData.set(r.id, {
-                    partitionId: p.id,
-                    serviceIds: []
-                })
-            )
-
-            p.services.forEach(s => {
-                s.endpoints.forEach(e => {
-                    const regionData = this.regionIdToRegionData.get(e.regionId)
-
-                    if (regionData) {
-                        regionData.serviceIds.push(s.id)
-                    }
-                })
-            })
-        })
     }
 }
 
