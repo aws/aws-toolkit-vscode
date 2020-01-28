35f16a3ee02f59ba58809a926439e0f9d666c3a6
Alan Bogusiewicz <alabogus@amazon.com>
2020-01-27 18:27:55 -0800

    Add testing to caching utilities

--- src/stepFunctions/utils.ts
+++ src/stepFunctions/utils.ts
@@ -11,106 +11,149 @@ import { Memento } from 'vscode'
 import { ext } from '../shared/extensionGlobals'
 import { mkdir } from '../shared/filesystem'
 import { fileExists } from '../shared/filesystemUtilities'
-import { getLogger, Logger } from '../shared/logger'
+import { getLogger } from '../shared/logger'
 
-const VISUALIZATION_SCRIPT_URL = 'https://d19z89qxwgm7w9.cloudfront.net/sfn-0.0.3.js'
-const VISUALIZATION_CSS_URL = 'https://d19z89qxwgm7w9.cloudfront.net/graph-0.0.1.css'
+export const VISUALIZATION_SCRIPT_URL = 'https://d19z89qxwgm7w9.cloudfront.net/sfn-0.0.3.js'
+export const VISUALIZATION_CSS_URL = 'https://d19z89qxwgm7w9.cloudfront.net/graph-0.0.1.css'
 
 export const SCRIPTS_LAST_DOWNLOADED_URL = 'SCRIPT_LAST_DOWNLOADED_URL'
 export const CSS_LAST_DOWNLOADED_URL = 'CSS_LAST_DOWNLOADED_URL'
 
-interface UpdateCachedScriptOptions {
+export interface UpdateCachedScriptOptions {
     globalStorage: Memento
     lastDownloadedURLKey: string
     currentURL: string
     filePath: string
 }
 
-export async function updateCache(globalStorage: Memento): Promise<void> {
-    const logger: Logger = getLogger()
-
-    const scriptUpdate = updateCachedFile({
-        globalStorage,
-        lastDownloadedURLKey: SCRIPTS_LAST_DOWNLOADED_URL,
-        currentURL: VISUALIZATION_SCRIPT_URL,
-        filePath: ext.visualizationResourcePaths.visualizationLibraryScript.fsPath
-    }).catch(error => {
-        logger.error('Failed to update State Machine Graph script assets')
-
-        throw error
-    })
-
-    const cssUpdate = updateCachedFile({
-        globalStorage,
-        lastDownloadedURLKey: CSS_LAST_DOWNLOADED_URL,
-        currentURL: VISUALIZATION_CSS_URL,
-        filePath: ext.visualizationResourcePaths.visualizationLibraryCSS.fsPath
-    }).catch(error => {
-        logger.error('Failed to update State Machine Graph css assets')
-
-        throw error
-    })
-
-    await Promise.all([scriptUpdate, cssUpdate])
+interface CustomLogger {
+    error(message: string | Error): void
+    debug(message: string): void
 }
 
-async function writeToLocalStorage(destinationPath: string, data: string): Promise<void> {
-    const logger: Logger = getLogger()
+export interface StateMachineGraphCacheOptions {
+    cssFilePath?: string
+    jsFilePath?: string
+    dirPath?: string
+    scriptUrl?: string
+    cssUrl?: string
+    logger?: CustomLogger
+    writeFile?(path: string, data: string, encoding: string): Promise<void>
+    makeDir?(path: string): Promise<void>
+    getFileData?(url: string): Promise<string>
+    fileExists?(path: string): Promise<boolean>
+}
 
-    const storageFolder = ext.visualizationResourcePaths.visualizationLibraryCachePath.fsPath
+export default class StateMachineGraphCache {
+    protected makeDir: (path: string) => Promise<void>
+    protected writeFile: (path: string, data: string, encoding: string) => Promise<void>
+    protected getFileData: (url: string) => Promise<string>
+    protected fileExists: (path: string) => Promise<boolean>
+    protected logger: CustomLogger
+    protected cssFilePath: string
+    protected jsFilePath: string
+    protected dirPath: string
 
-    try {
-        if (!(await fileExists(storageFolder))) {
-            logger.debug('Folder for graphing script and styling doesnt exist. Creating it.')
+    public constructor(options: StateMachineGraphCacheOptions = {}) {
+        const {
+            makeDir,
+            writeFile: writeFileCustom,
+            logger: loggerCustom,
+            getFileData,
+            fileExists: fileExistsCustom
+        } = options
 
-            await mkdir(storageFolder)
+        this.makeDir = makeDir ?? mkdir
+        this.writeFile = writeFileCustom ?? writeFile
+        this.logger = loggerCustom ?? getLogger()
+        this.getFileData = getFileData ?? httpsGetRequestWrapper
+        this.cssFilePath = options.cssFilePath ?? ext.visualizationResourcePaths.visualizationLibraryCSS.fsPath
+        this.jsFilePath = options.jsFilePath ?? ext.visualizationResourcePaths.visualizationLibraryScript.fsPath
+        this.dirPath = options.dirPath ?? ext.visualizationResourcePaths.visualizationLibraryCachePath.fsPath
+        this.fileExists = fileExistsCustom ?? fileExists
+    }
+
+    public async updateCache(globalStorage: Memento): Promise<void> {
+        const scriptUpdate = this.updateCachedFile({
+            globalStorage,
+            lastDownloadedURLKey: SCRIPTS_LAST_DOWNLOADED_URL,
+            currentURL: VISUALIZATION_SCRIPT_URL,
+            filePath: this.jsFilePath
+        }).catch(error => {
+            this.logger.error('Failed to update State Machine Graph script assets')
+
+            throw error
+        })
+
+        const cssUpdate = this.updateCachedFile({
+            globalStorage,
+            lastDownloadedURLKey: CSS_LAST_DOWNLOADED_URL,
+            currentURL: VISUALIZATION_CSS_URL,
+            filePath: this.cssFilePath
+        }).catch(error => {
+            this.logger.error('Failed to update State Machine Graph css assets')
+
+            throw error
+        })
+
+        await Promise.all([scriptUpdate, cssUpdate])
+    }
+
+    public async updateCachedFile(options: UpdateCachedScriptOptions) {
+        const downloadedUrl = options.globalStorage.get<string>(options.lastDownloadedURLKey)
+        const cachedFileExists = await this.fileExists(options.filePath)
+
+        // if current url is different than url that was previously used to download the assets
+        // or if the file assets do not exist
+        // download and cache the assets
+        if (downloadedUrl !== options.currentURL || !cachedFileExists) {
+            const response = await this.getFileData(options.currentURL).catch(err => {
+                this.logger.error(err as Error)
+
+                throw err
+            })
+
+            await this.writeToLocalStorage(options.filePath, response).catch(err => {
+                this.logger.error(err as Error)
+
+                throw err
+            })
+
+            // save the url of the downloaded and cached assets
+            options.globalStorage.update(options.lastDownloadedURLKey, options.currentURL)
         }
+    }
 
-        await writeFile(destinationPath, data, 'utf8')
-    } catch (err) {
-        /*
-         * Was able to download the required files,
-         * but there was an error trying to write them to this extensions globalStorage location.
-         */
-        logger.error(err as Error)
+    protected async writeToLocalStorage(destinationPath: string, data: string): Promise<void> {
+        const storageFolder = this.dirPath
 
-        throw err
+        try {
+            if (!(await this.fileExists(storageFolder))) {
+                this.logger.debug('Folder for graphing script and styling doesnt exist. Creating it.')
+
+                await this.makeDir(storageFolder)
+            }
+
+            await this.writeFile(destinationPath, data, 'utf8')
+        } catch (err) {
+            /*
+             * Was able to download the required files,
+             * but there was an error trying to write them to this extensions globalStorage location.
+             */
+            this.logger.error(err as Error)
+
+            throw err
+        }
     }
 }
 
-export async function updateCachedFile(options: UpdateCachedScriptOptions) {
-    const logger: Logger = getLogger()
-    const downloadedUrl = options.globalStorage.get<string>(options.lastDownloadedURLKey)
-    const cachedFileExists = await fileExists(options.filePath)
-
-    // if current url is different than url that was previously used to download the assets
-    // or if the file assets do not exist
-    // download and cache the assets
-    if (downloadedUrl !== options.currentURL || !cachedFileExists) {
-        const response = await httpsGetRequestWrapper(options.currentURL).catch(err => {
-            logger.error(err as Error)
-
-            throw err
-        })
-
-        await writeToLocalStorage(options.filePath, response.body as string).catch(err => {
-            logger.error(err as Error)
-
-            throw err
-        })
-
-        // save the url of the downloaded and cached assets
-        options.globalStorage.update(options.lastDownloadedURLKey, options.currentURL)
-    }
-}
-
-async function httpsGetRequestWrapper(url: string): Promise<request.Response> {
+async function httpsGetRequestWrapper(url: string): Promise<string> {
     return new Promise((resolve, reject) => {
-        request(url, function(error, response) {
+        request.get(url, function(error, response) {
             if (error) {
                 reject(error)
             } else {
-                resolve(response)
+                resolve((response.body as any) as string)
             }
         })
     })
