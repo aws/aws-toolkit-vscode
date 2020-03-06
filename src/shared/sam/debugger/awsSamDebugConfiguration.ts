70d0de6e6dcf15fa1fc5d94481d8b96d7db06e97
Ito <bryceito@amazon.com>
2020-03-06 09:37:15 -0800

    Interface assumes less at invokeTarget level

--- src/shared/sam/debugger/awsSamDebugConfiguration.ts
+++ src/shared/sam/debugger/awsSamDebugConfiguration.ts
@@ -6,7 +6,7 @@
 import * as vscode from 'vscode'
 
 export interface AwsSamDebuggerConfiguration extends vscode.DebugConfiguration {
-    readonly invokeTarget: AwsSamDebuggerInvokeTargetCodeFields | AwsSamDebuggerInvokeTargetTemplateFields
+    readonly invokeTarget: AwsSamDebuggerInvokeTargetFields
     readonly lambda?: AwsSamDebuggerLambdaFields
     readonly sam?: AwsSamDebuggerSamFields
     readonly aws?: AwsSamDebuggerAwsFields
@@ -21,13 +21,21 @@ export interface AwsSamDebuggerAwsFields {
     readonly region?: string
 }
 
-export interface AwsSamDebuggerInvokeTargetCodeFields {
+export interface AwsSamDebuggerInvokeTargetFields {
+    readonly target: string
+    readonly lambdaHandler?: string
+    readonly projectRoot?: string
+    readonly samTemplatePath?: string
+    readonly samTemplateResource?: string
+}
+
+export interface AwsSamDebuggerInvokeTargetCodeFields extends AwsSamDebuggerInvokeTargetFields {
     readonly target: 'code'
     readonly lambdaHandler: string
     readonly projectRoot: string
 }
 
-export interface AwsSamDebuggerInvokeTargetTemplateFields {
+export interface AwsSamDebuggerInvokeTargetTemplateFields extends AwsSamDebuggerInvokeTargetFields {
     readonly target: 'template'
     readonly samTemplatePath: string
     readonly samTemplateResource: string
