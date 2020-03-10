f806b96374a3a722f14e5d0e241407be52c18649
bryceito <bryceito@amazon.com>
2020-03-10 12:41:49 -0700

    Initial commit

--- src/shared/sam/debugger/awsSamDebugger.ts
+++ src/shared/sam/debugger/awsSamDebugger.ts
@@ -4,20 +4,45 @@
  */
 
 import * as vscode from 'vscode'
+import { CloudFormation } from '../../cloudformation/cloudformation'
+import { CloudFormationTemplateRegistry } from '../../cloudformation/templateRegistry'
 import { AwsSamDebuggerConfiguration } from './awsSamDebugConfiguration'
 
 export const AWS_SAM_DEBUG_TYPE = 'aws-sam'
-const AWS_SAM_DEBUG_REQUEST_TYPES = new Set<string>(['direct-invoke'])
-const AWS_SAM_DEBUG_TARGET_TYPES = new Set<string>(['template', 'code'])
+const DIRECT_INVOKE_TYPE = 'direct-invoke'
+const TEMPLATE_TARGET_TYPE = 'template'
+// const CODE_TARGET_TYPE = 'code'
+
+// const AWS_SAM_DEBUG_REQUEST_TYPES = [DIRECT_INVOKE_TYPE]
+// const AWS_SAM_DEBUG_TARGET_TYPES = [TEMPLATE_TARGET_TYPE, CODE_TARGET_TYPE]
 
 export class AwsSamDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
-    public constructor() {}
+    public constructor(private readonly cftRegistry = CloudFormationTemplateRegistry.getRegistry()) {}
 
     public async provideDebugConfigurations(
         folder: vscode.WorkspaceFolder | undefined,
         token?: vscode.CancellationToken
     ): Promise<AwsSamDebuggerConfiguration[] | undefined> {
-        return undefined
+        if (folder) {
+            const debugConfigurations: AwsSamDebuggerConfiguration[] = []
+            const folderPath = folder.uri.fsPath
+            const templates = this.cftRegistry.registeredTemplates
+
+            for (const templateDatum of templates) {
+                if (templateDatum.path.startsWith(folderPath) && templateDatum.template.Resources) {
+                    for (const resourceKey of Object.keys(templateDatum.template.Resources)) {
+                        const resource = templateDatum.template.Resources[resourceKey]
+                        if (resource) {
+                            debugConfigurations.push(
+                                createSamDebugConfigurationFromTemplate(resourceKey, templateDatum.path, resource)
+                            )
+                        }
+                    }
+                }
+            }
+
+            return debugConfigurations
+        }
     }
 
     public async resolveDebugConfiguration(
@@ -25,18 +50,35 @@ export class AwsSamDebugConfigurationProvider implements vscode.DebugConfigurati
         debugConfiguration: AwsSamDebuggerConfiguration,
         token?: vscode.CancellationToken
     ): Promise<AwsSamDebuggerConfiguration | undefined> {
-        if (!AWS_SAM_DEBUG_REQUEST_TYPES.has(debugConfiguration.request)) {
-            vscode.window.showInformationMessage('Invalid request type')
-        }
-        if (
-            debugConfiguration.invokeTarget &&
-            debugConfiguration.invokeTarget.target &&
-            !AWS_SAM_DEBUG_TARGET_TYPES.has(debugConfiguration.invokeTarget.target)
-        ) {
-            vscode.window.showInformationMessage('Invalid invokeTarget.target type')
-        }
+        // if (!AWS_SAM_DEBUG_REQUEST_TYPES.has(debugConfiguration.request)) {
+        //     vscode.window.showInformationMessage('Invalid request type')
+        // }
+        // if (
+        //     debugConfiguration.invokeTarget &&
+        //     debugConfiguration.invokeTarget.target &&
+        //     !AWS_SAM_DEBUG_TARGET_TYPES.has(debugConfiguration.invokeTarget.target)
+        // ) {
+        //     vscode.window.showInformationMessage('Invalid invokeTarget.target type')
+        // }
         vscode.window.showInformationMessage('Not implemented')
 
         return undefined
+    }
+}
+
+function createSamDebugConfigurationFromTemplate(
+    resourceName: string,
+    templatePath: string,
+    resource: CloudFormation.Resource
+): AwsSamDebuggerConfiguration {
+    return {
+        type: AWS_SAM_DEBUG_TYPE,
+        request: DIRECT_INVOKE_TYPE,
+        name: resourceName,
+        invokeTarget: {
+            target: TEMPLATE_TARGET_TYPE,
+            samTemplatePath: templatePath,
+            samTemplateResource: resourceName
+        }
     }
 }
