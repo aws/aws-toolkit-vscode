3815595c8b34577a8654efbd49cd6b2da56b73ae
Ito <bryceito@amazon.com>
2020-03-20 10:14:37 -0700

    Correcting terminology

--- src/extension.ts
+++ src/extension.ts
@@ -239,7 +239,7 @@ function makeEndpointsProvider(): EndpointsProvider {
                 isCloud9()
                     ? localize(
                           'AWS.error.impactedFunctionalityReset.cloud9',
-                          'Toolkit functionality may be impacted until Cloud9 is restarted.'
+                          'Toolkit functionality may be impacted until the Cloud9 browser tab is restarted.'
                       )
                     : localize(
                           'AWS.error.impactedFunctionalityReset.vscode',
