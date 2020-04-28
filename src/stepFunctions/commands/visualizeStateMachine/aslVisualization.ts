516a82efc3f5087c745f6c177ec8eeb6539b306a
Jordan De Mello <jormello@amazon.com>
2020-04-28 15:56:25 -0700

    Fix line spacing

--- src/stepFunctions/commands/visualizeStateMachine/aslVisualization.ts
+++ src/stepFunctions/commands/visualizeStateMachine/aslVisualization.ts
@@ -2,6 +2,7 @@
  * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import * as nls from 'vscode-nls'
 const localize = nls.loadMessageBundle()
 import { debounce } from 'lodash'
