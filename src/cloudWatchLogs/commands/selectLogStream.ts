a9c61178ef1a8b6bf3b4c28b3a665f2958728f88
Bryce Ito <bryceito@amazon.com>
2020-05-26 09:49:34 -0700

    Removed duplicate IteratingAWSCallPicker

--- src/cloudWatchLogs/commands/selectLogStream.ts
+++ src/cloudWatchLogs/commands/selectLogStream.ts
@@ -50,102 +50,13 @@ export class DefaultSelectLogStreamWizardContext implements SelectLogStreamWizar
     }
 }
 
-// TODO: Cache these results?
-export class IteratingAWSCallPicker<TRequest, TResponse> {
-    private isDone: boolean = false
-    private isPaused: boolean = false
-    private items: vscode.QuickPickItem[] = []
-
-    private readonly quickPick: vscode.QuickPick<vscode.QuickPickItem>
-    private readonly moreItemsRequest: vscode.EventEmitter<void> = new vscode.EventEmitter<void>()
-
-    /**
-     * @param awsCallLogic: Object representing the call to be used, the initial request, and a function that converts from a response object to an array of quick pick items
-     * @param pickerOptions: Object representing QuickPick options, additional buttons, and any additional functionality to be called upon selecting a button.
-     */
-    public constructor(
-        private readonly awsCallLogic: {
-            // TODO: allow for creation of a new call in case we want to reload quick pick in its entirety
-            iteratingAwsCall: IteratingAWSCall<TRequest, TResponse>
-            initialRequest: TRequest
-            awsResponseToQuickPickItem: (response: TResponse) => vscode.QuickPickItem[]
-        },
-        private readonly pickerOptions: {
-            options?: vscode.QuickPickOptions & picker.AdditionalQuickPickOptions
-            buttons?: vscode.QuickInputButton[]
-            onDidTriggerButton?: (
-                button: vscode.QuickInputButton,
-                resolve: (
-                    value: vscode.QuickPickItem[] | PromiseLike<vscode.QuickPickItem[] | undefined> | undefined
-                ) => void,
-                reject: (reason?: any) => void
-            ) => void
-        } = {}
-    ) {
-        // TODO: Create default buttons for load next page, refresh
-        // TODO: Set a global throttling flag that will optionally display said load next page button
-        this.quickPick = picker.createQuickPick<vscode.QuickPickItem>({
-            options: {
-                ...this.pickerOptions.options,
-                onDidSelectItem: item => {
-                    // pause any existing execution
-                    this.isPaused = true
-                    // pass existing onDidSelectItem through if it exists
-                    if (this.pickerOptions.options?.onDidSelectItem) {
-                        this.pickerOptions.options.onDidSelectItem(item)
-                    }
-                },
-            },
-            items: this.items,
-            buttons: this.pickerOptions.buttons,
-        })
-
-        this.moreItemsRequest.event(() => this.loadItems())
-    }
-
-    /**
-     * Prompts the user with the quick pick specified by the constructor.
-     * Always attempts to load new results from the iteratingAwsCall, even if the call has been exhausted.
-     * If the call picker was previously paused, unpauses it.
-     */
-    public async promptUser(): Promise<vscode.QuickPickItem[] | undefined> {
-        // start background loading and unpause the loader (if it was paused previously by a selection)
-        this.quickPick.busy = true
-        this.isPaused = false
-        if (!this.isDone) {
-            this.moreItemsRequest.fire()
-        }
-        return await picker.promptUser<vscode.QuickPickItem>({
-            picker: this.quickPick,
-            onDidTriggerButton: this.pickerOptions.onDidTriggerButton,
-        })
-    }
-
-    // TODO: Add nodes for no items, error (error retries call from where it left off?)
-    private async loadItems(): Promise<void> {
-        const iter = this.awsCallLogic.iteratingAwsCall.getIteratorForRequest(this.awsCallLogic.initialRequest)
-
-        for await (const item of iter) {
-            if (!this.isDone && !this.isPaused) {
-                this.items = this.items.concat(this.awsCallLogic.awsResponseToQuickPickItem(item))
-                // TODO: Is there a way to append to this ReadOnlyArray so it doesn't constantly pop focus back to the top?
-                this.quickPick.items = this.items
-            } else {
-                break
-            }
-        }
-        this.isDone = true
-        this.quickPick.busy = false
-    }
-}
-
 function createDescribeLogStreamsCallPicker(
     regionCode: string,
     logGroupName: string
-): IteratingAWSCallPicker<CloudWatchLogs.DescribeLogStreamsRequest, CloudWatchLogs.DescribeLogStreamsResponse> {
+): picker.IteratingAWSCallPicker<CloudWatchLogs.DescribeLogStreamsRequest, CloudWatchLogs.DescribeLogStreamsResponse> {
     const client: CloudWatchLogsClient = ext.toolkitClientBuilder.createCloudWatchLogsClient(regionCode)
 
-    return new IteratingAWSCallPicker(
+    return new picker.IteratingAWSCallPicker(
         {
             iteratingAwsCall: new IteratingAWSCall(client.describeLogStreams.bind(client), {
                 request: 'nextToken',
