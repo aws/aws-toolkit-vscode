a8a351949abc47a5b685636065792ea1e431c482
Austin Brooks <abrooksv@users.noreply.github.com>
2020-06-02 12:21:02 -0700

    Fix tests not asserting correctly (#1813)

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/ProfileCredentialProviderFactoryTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/ProfileCredentialProviderFactoryTest.kt
@@ -3,6 +3,7 @@
 
 package software.aws.toolkits.jetbrains.core.credentials
 
+import com.intellij.notification.Notification
 import com.intellij.notification.Notifications
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
@@ -17,7 +18,6 @@ import com.intellij.testFramework.ProjectRule
 import com.intellij.testFramework.VfsTestUtil
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.argumentCaptor
-import com.nhaarman.mockitokotlin2.check
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.reset
 import com.nhaarman.mockitokotlin2.stub
@@ -103,13 +103,14 @@ class ProfileCredentialProviderFactoryTest {
     @Test
     fun testLoadingWithEmptyProfiles() {
         createProviderFactory()
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
+
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback).invoke(capture())
+
+            assertThat(firstValue.added).isEmpty()
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+        }
     }
 
     @Test
@@ -118,18 +119,18 @@ class ProfileCredentialProviderFactoryTest {
 
         createProviderFactory()
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).hasSize(4)
-                    .has(profileName(FOO_PROFILE_NAME))
-                    .has(profileName(BAR_PROFILE_NAME))
-                    .has(profileName(BAZ_PROFILE_NAME))
-                    .has(profileName(REGIONALIZED_PROFILE_NAME, defaultRegion = "us-west-2"))
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback).invoke(capture())
 
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
+            assertThat(firstValue.added).hasSize(4)
+                .has(profileName(FOO_PROFILE_NAME))
+                .has(profileName(BAR_PROFILE_NAME))
+                .has(profileName(BAZ_PROFILE_NAME))
+                .has(profileName(REGIONALIZED_PROFILE_NAME, defaultRegion = "us-west-2"))
+
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+        }
     }
 
     @Test
@@ -146,9 +147,11 @@ class ProfileCredentialProviderFactoryTest {
 
         createProviderFactory()
 
-        verify(notificationMock).notify(check {
-            assertThat(it.content).contains("Expected an '=' sign defining a property on line 2")
-        })
+        argumentCaptor<Notification>().apply {
+            verify(notificationMock).notify(capture())
+
+            assertThat(firstValue.content).contains("Expected an '=' sign defining a property on line 2")
+        }
     }
 
     @Test
@@ -168,13 +171,14 @@ class ProfileCredentialProviderFactoryTest {
         )
 
         createProviderFactory()
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).hasSize(1).has(profileName("another_profile"))
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
+
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback).invoke(capture())
+
+            assertThat(firstValue.added).hasSize(1).has(profileName("another_profile"))
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+        }
     }
 
     @Test
@@ -235,7 +239,6 @@ class ProfileCredentialProviderFactoryTest {
         }
 
         val providerFactory = createProviderFactory()
-        println(credentialChangeEvent.allValues)
         val validProfile = findCredentialIdentifier("role")
         val credentialsProvider = providerFactory.createProvider(validProfile).resolveCredentials()
 
@@ -357,14 +360,6 @@ class ProfileCredentialProviderFactoryTest {
         val validProfile = findCredentialIdentifier("foo")
         val credentialsProvider = providerFactory.createProvider(validProfile)
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).hasSize(1).has(profileName("foo"))
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
-
         assertThat(credentialsProvider.resolveCredentials()).isInstanceOfSatisfying(AwsSessionCredentials::class.java) {
             assertThat(it.accessKeyId()).isEqualTo("FooAccessKey")
             assertThat(it.secretAccessKey()).isEqualTo("FooSecretKey")
@@ -380,13 +375,17 @@ class ProfileCredentialProviderFactoryTest {
             """.trimIndent()
         )
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).hasSize(1).has(profileName("foo"))
-                assertThat(it.removed).isEmpty()
-            }
-        )
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback, times(2)).invoke(capture())
+
+            assertThat(firstValue.added).hasSize(1).has(profileName("foo"))
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+
+            assertThat(secondValue.added).isEmpty()
+            assertThat(secondValue.modified).hasSize(1).has(profileName("foo"))
+            assertThat(secondValue.removed).isEmpty()
+        }
     }
 
     @Test
@@ -416,13 +415,17 @@ class ProfileCredentialProviderFactoryTest {
             providerFactory.createProvider(validProfile)
         }.isInstanceOf(IllegalStateException::class.java)
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).hasSize(1).has(profileName("foo"))
-            }
-        )
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback, times(2)).invoke(capture())
+
+            assertThat(firstValue.added).hasSize(1).has(profileName("foo"))
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+
+            assertThat(secondValue.added).isEmpty()
+            assertThat(secondValue.modified).isEmpty()
+            assertThat(secondValue.removed).hasSize(1).has(profileName("foo"))
+        }
     }
 
     @Test
@@ -430,14 +433,6 @@ class ProfileCredentialProviderFactoryTest {
         profileFile.writeToFile("")
 
         val providerFactory = createProviderFactory()
-
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
 
         profileFile.writeToFile(
             """
@@ -457,13 +452,17 @@ class ProfileCredentialProviderFactoryTest {
             assertThat(it.sessionToken()).isEqualTo("FooSessionToken")
         }
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).hasSize(1).has(profileName("foo"))
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback, times(2)).invoke(capture())
+
+            assertThat(firstValue.added).isEmpty()
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+
+            assertThat(secondValue.added).hasSize(1).has(profileName("foo"))
+            assertThat(secondValue.modified).isEmpty()
+            assertThat(secondValue.removed).isEmpty()
+        }
     }
 
     @Test
@@ -471,14 +470,6 @@ class ProfileCredentialProviderFactoryTest {
         assertThat(profileFile).doesNotExist()
 
         val providerFactory = createProviderFactory()
-
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
 
         profileFile.writeToFile(
             """
@@ -489,14 +480,6 @@ class ProfileCredentialProviderFactoryTest {
             """.trimIndent()
         )
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).hasSize(1).has(profileName("foo"))
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).isEmpty()
-            }
-        )
-
         val validProfile = findCredentialIdentifier("foo")
         val credentialsProvider = providerFactory.createProvider(validProfile)
 
@@ -504,6 +487,18 @@ class ProfileCredentialProviderFactoryTest {
             assertThat(it.accessKeyId()).isEqualTo("FooAccessKey")
             assertThat(it.secretAccessKey()).isEqualTo("FooSecretKey")
             assertThat(it.sessionToken()).isEqualTo("FooSessionToken")
+        }
+
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback, times(2)).invoke(capture())
+
+            assertThat(firstValue.added).isEmpty()
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+
+            assertThat(secondValue.added).hasSize(1).has(profileName("foo"))
+            assertThat(secondValue.modified).isEmpty()
+            assertThat(secondValue.removed).isEmpty()
         }
     }
 
@@ -534,13 +529,17 @@ class ProfileCredentialProviderFactoryTest {
             providerFactory.createProvider(validProfile)
         }.isInstanceOf(IllegalStateException::class.java)
 
-        verify(profileLoadCallback).invoke(
-            check {
-                assertThat(it.added).isEmpty()
-                assertThat(it.modified).isEmpty()
-                assertThat(it.removed).hasSize(1).has(profileName("foo"))
-            }
-        )
+        argumentCaptor<CredentialsChangeEvent>().apply {
+            verify(profileLoadCallback, times(2)).invoke(capture())
+
+            assertThat(firstValue.added).hasSize(1).has(profileName("foo"))
+            assertThat(firstValue.modified).isEmpty()
+            assertThat(firstValue.removed).isEmpty()
+
+            assertThat(secondValue.added).isEmpty()
+            assertThat(secondValue.modified).isEmpty()
+            assertThat(secondValue.removed).hasSize(1).has(profileName("foo"))
+        }
     }
 
     private fun File.writeToFile(content: String) {
