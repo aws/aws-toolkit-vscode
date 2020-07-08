a0d0864c190fc7ab85b15d82b47fa5030e8aabfd
Bryce Ito <bryceito@amazon.com>
2020-07-08 11:00:50 -0700

    Bumping min SAM version to match Jetbrains. TODO: add dotnetcore3.1 support

--- src/shared/sam/cli/samCliValidator.ts
+++ src/shared/sam/cli/samCliValidator.ts
@@ -9,7 +9,7 @@ import { SamCliConfiguration } from './samCliConfiguration'
 import { SamCliInfoInvocation, SamCliInfoResponse } from './samCliInfo'
 import { SamCliProcessInvoker } from './samCliInvokerUtils'
 
-export const MINIMUM_SAM_CLI_VERSION_INCLUSIVE = '0.38.0'
+export const MINIMUM_SAM_CLI_VERSION_INCLUSIVE = '0.47.0'
 export const MAXIMUM_SAM_CLI_VERSION_EXCLUSIVE = '2.0.0'
 
 // Errors
