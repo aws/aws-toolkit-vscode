592205728ca66229c4e462c056b14a7f342a2e43
Bryce Ito <bryceito@amazon.com>
2020-07-24 15:22:42 -0700

    Copy Log Stream Name in command prompt, Save Current Log Stream is explicitly removed

--- src/cloudWatchLogs/commands/copyLogStreamName.ts
+++ src/cloudWatchLogs/commands/copyLogStreamName.ts
@@ -3,11 +3,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as nls from 'vscode-nls'
+const localize = nls.loadMessageBundle()
+
 import * as vscode from 'vscode'
 import { parseCloudWatchLogsUri } from '../cloudWatchLogsUtils'
 
-export async function copyLogStreamName(uri: vscode.Uri): Promise<void> {
-    const parsedUri = parseCloudWatchLogsUri(uri)
-
-    await vscode.env.clipboard.writeText(parsedUri.streamName)
+export async function copyLogStreamName(uri?: vscode.Uri): Promise<void> {
+    if (!uri) {
+        uri = vscode.window.activeTextEditor?.document.uri
+        if (!uri) {
+            return
+        }
+    }
+    try {
+        const parsedUri = parseCloudWatchLogsUri(uri)
+        await vscode.env.clipboard.writeText(parsedUri.streamName)
+    } catch (e) {
+        vscode.window.showErrorMessage(
+            localize('aws.cloudWatchLogs.invalidEditor', 'Current file is not a valid Cloudwatch Logs editor.')
+        )
+        return
+    }
 }
