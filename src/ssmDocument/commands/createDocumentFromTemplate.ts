b568922aca083a638a21eb51a1e985ff92ad5b40
Bowen Dong <bddong@amazon.com>
2020-07-24 10:41:19 -0500

    Added createLocalDocument command to SSM Document support

---
+++ src/ssmDocument/commands/createDocumentFromTemplate.ts
@@ -0,0 +1,119 @@
+/*!
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as nls from 'vscode-nls'
+const localize = nls.loadMessageBundle()
+
+import * as vscode from 'vscode'
+import * as YAML from 'yaml'
+import { getLogger, Logger } from '../../shared/logger'
+import * as picker from '../../shared/ui/picker'
+import { openAndSaveDocument } from '../util/util'
+
+import { getDocumentTemplate } from 'aws-ssm-document-language-service'
+
+export interface SsmDocumentTemplateQuickPickItem {
+    label: string
+    description: string
+    filename: string
+    language: string
+    docType: string
+}
+
+const SSMDOCUMENT_TEMPLATES: SsmDocumentTemplateQuickPickItem[] = [
+    {
+        label: localize('AWS.ssmDocument.template.command22Json.label', 'JSON Command Document 2.2'),
+        description: localize(
+            'AWS.ssmDocument.template.command22Json.description',
+            'An Example of a command document using schemaVersion 2.2 in JSON'
+        ),
+        filename: 'example22.command.ssm.json',
+        language: 'ssm-json',
+        docType: 'command',
+    },
+    {
+        label: localize('AWS.ssmDocument.template.automationJson.label', 'JSON Automation Document 0.3'),
+        description: localize(
+            'AWS.ssmDocument.template.automationJson.description',
+            'An Example of a automation document using schemaVersion 0.3 in JSON'
+        ),
+        filename: 'example.automation.ssm.json',
+        language: 'ssm-json',
+        docType: 'automation',
+    },
+    {
+        label: localize('AWS.ssmDocument.template.command22Yaml.label', 'YAML Command Document 2.2'),
+        description: localize(
+            'AWS.ssmDocument.template.command22Yaml.description',
+            'An Example of a command document using schemaVersion 2.2 in YAML'
+        ),
+        filename: 'example22.command.ssm.yaml',
+        language: 'ssm-yaml',
+        docType: 'command',
+    },
+    {
+        label: localize('AWS.ssmDocument.template.automationYaml.label', 'YAML Automation Document 0.3'),
+        description: localize(
+            'AWS.ssmDocument.template.automationYaml.description',
+            'An Example of a automation document using schemaVersion 0.3 in YAML'
+        ),
+        filename: 'example.automation.ssm.yaml',
+        language: 'ssm-yaml',
+        docType: 'automation',
+    },
+]
+
+export async function createSsmDocumentFromTemplate(): Promise<void> {
+    const logger: Logger = getLogger()
+
+    const quickPick = picker.createQuickPick<SsmDocumentTemplateQuickPickItem>({
+        options: {
+            ignoreFocusOut: true,
+            title: localize('AWS.message.prompt.selectSsmDocumentTemplate.placeholder', 'Select a document templete'),
+        },
+        buttons: [vscode.QuickInputButtons.Back],
+        items: SSMDOCUMENT_TEMPLATES,
+    })
+
+    const choices = await picker.promptUser({
+        picker: quickPick,
+        onDidTriggerButton: (_, resolve) => {
+            resolve(undefined)
+        },
+    })
+
+    const selection = picker.verifySinglePickerOutput(choices)
+
+    // User pressed escape and didn't select a template
+    if (selection === undefined) {
+        return
+    }
+
+    try {
+        logger.debug(`User selected the ${selection.label} template.`)
+        const textDocument: vscode.TextDocument = await openTextDocumentFromSelection(selection)
+        vscode.window.showTextDocument(textDocument)
+    } catch (err) {
+        logger.error(err as Error)
+        vscode.window.showErrorMessage(
+            localize(
+                'AWS.message.error.ssmDocument.openTextDocumentFromSelection',
+                'There was an error creating the SSM Document from the template, check log for details.'
+            )
+        )
+    }
+}
+
+async function openTextDocumentFromSelection(item: SsmDocumentTemplateQuickPickItem): Promise<vscode.TextDocument> {
+    const template: object = getDocumentTemplate(item.docType)
+    let content: string
+    if (item.language === 'ssm-yaml') {
+        content = YAML.stringify(template)
+    } else {
+        content = JSON.stringify(template, undefined, '\t')
+    }
+
+    return await openAndSaveDocument(content, item.filename, item.language)
+}
---
+++ src/test/ssmDocument/commands/createDocumentFromTemplate.test.ts
@@ -0,0 +1,55 @@
+/*!
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as assert from 'assert'
+import * as sinon from 'sinon'
+import * as picker from '../../../shared/ui/picker'
+import {
+    createSsmDocumentFromTemplate,
+    SsmDocumentTemplateQuickPickItem,
+} from '../../../ssmDocument/commands/createDocumentFromTemplate'
+import * as openAndSaveDocument from '../../../ssmDocument/util/util'
+
+import * as YAML from 'yaml'
+
+describe('createDocumentFromTemplate', async () => {
+    let sandbox: sinon.SinonSandbox
+    beforeEach(() => {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(() => {
+        sandbox.restore()
+    })
+
+    const fakeContent = `{
+        "schemaVersion": "2.2",
+        "mainSteps": []
+    }`
+
+    const fakeSelectionResult: SsmDocumentTemplateQuickPickItem = {
+        label: 'test template',
+        description: 'an example to test creating from template',
+        filename: 'test.command.ssm.json',
+        language: 'ssm-json',
+        docType: 'command',
+    }
+
+    const fakeSelection: SsmDocumentTemplateQuickPickItem[] = []
+    fakeSelection.push(fakeSelectionResult)
+
+    it('open and save document based on selected template', async () => {
+        sandbox.stub(picker, 'promptUser').returns(Promise.resolve(fakeSelection))
+        sandbox.stub(picker, 'verifySinglePickerOutput').returns(fakeSelectionResult)
+        sandbox.stub(JSON, 'stringify').returns(fakeContent)
+        sandbox.stub(YAML, 'stringify').returns(fakeContent)
+
+        const openAndSaveStub = sandbox.stub(openAndSaveDocument, 'openAndSaveDocument')
+        await createSsmDocumentFromTemplate()
+        assert.strictEqual(openAndSaveStub.getCall(0).args[0], fakeContent)
+        assert.strictEqual(openAndSaveStub.getCall(0).args[1], fakeSelectionResult.filename)
+        assert.strictEqual(openAndSaveStub.getCall(0).args[2], fakeSelectionResult.language)
+    })
+})
