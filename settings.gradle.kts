ecc47a5d1f14d1ae3384412280670121d0b84f80
Austin Brooks <abrooksv@users.noreply.github.com>
2020-08-20 09:46:10 -0700

    Obfuscate our build scan data (#1999)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,17 @@ include("jetbrains-core")
 include("jetbrains-ultimate")
 include("jetbrains-rider")
 include("ui-tests")
+
+plugins {
+    id("com.gradle.enterprise").version("3.4.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        obfuscation {
+            username { "<username>"}
+            hostname { "<hostname>"}
+            ipAddresses { it.map { "0.0.0.0"} }
+        }
+    }
+}
