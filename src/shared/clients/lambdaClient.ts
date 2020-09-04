462408074e1afe4b35d7e416455eb588d4b71d11
Bryce Ito <bryceito@amazon.com>
2020-09-04 08:39:13 -0700

    Upload Lambda functions from .zip files, built directories, or unbuilt directories (#1269)
    Can upload the following things:
    * ZIP file (uploaded directly)
    * Directory containing built code or interpreted languages (zipped and uploaded directly)
    * Directory containing unbuilt code (attempts to build with `sam build` and a temporary template, zipped and uploaded).

--- src/shared/clients/lambdaClient.ts
+++ src/shared/clients/lambdaClient.ts
@@ -34,4 +34,12 @@ export interface LambdaClient {
      * @param name Function name
      */
     getFunction(name: string): Promise<Lambda.GetFunctionResponse>
+
+    /**
+     * Runs AWS.Lambda.updateFunctionCode
+     * @param name Function name
+     * @param zipFile Buffer containing zip file data
+     *
+     */
+    updateFunctionCode(name: string, zipFile: Buffer): Promise<Lambda.FunctionConfiguration>
 }
