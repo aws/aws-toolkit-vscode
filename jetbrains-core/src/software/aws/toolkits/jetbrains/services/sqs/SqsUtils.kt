e6ea555743a11d41a35d29158bddc8d823a17e54
Hunter Werlla <werlla@amazon.com>
2020-09-14 13:15:41 -0700

    Add SQS queue attribute editing panel (#2054)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/SqsUtils.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/SqsUtils.kt
@@ -13,5 +13,20 @@ const val MAX_LENGTH_OF_QUEUE_NAME = 80
 // Maximum length of queue name is 80, but the maximum will be 75 for FIFO queues due to '.fifo' suffix
 const val MAX_LENGTH_OF_FIFO_QUEUE_NAME = 75
 
+// Queue attribute limits
+const val MIN_DELIVERY_DELAY = 0
+const val MAX_DELIVERY_DELAY = 900
+const val DELIVERY_DELAY_TICK = (MAX_DELIVERY_DELAY - MIN_DELIVERY_DELAY) / 30
+const val MIN_MESSAGE_SIZE_LIMIT = 1024
+const val MAX_MESSAGE_SIZE_LIMIT = 262144
+const val MIN_RETENTION_PERIOD = 60
+const val MAX_RETENTION_PERIOD = 1209600
+const val MIN_VISIBILITY_TIMEOUT = 0
+const val MAX_VISIBILITY_TIMEOUT = 43200
+const val VISIBILITY_TIMEOUT_TICK = (MAX_VISIBILITY_TIMEOUT - MIN_VISIBILITY_TIMEOUT) / 30
+const val MIN_WAIT_TIME = 0
+const val MAX_WAIT_TIME = 20
+const val WAIT_TIME_TICK = 1
+
 // Extension function to get telemetry type from Queue
 fun Queue.telemetryType() = if (isFifo) SqsQueueType.Fifo else SqsQueueType.Standard
---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/actions/EditAttributesAction.kt
@@ -0,0 +1,66 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.sqs.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.progress.PerformInBackgroundOption
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.DumbAware
+import software.amazon.awssdk.services.sqs.SqsClient
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName
+import software.amazon.awssdk.services.sqs.model.SqsException
+import software.aws.toolkits.core.utils.error
+import software.aws.toolkits.core.utils.getLogger
+import software.aws.toolkits.jetbrains.core.awsClient
+import software.aws.toolkits.jetbrains.core.explorer.actions.SingleResourceNodeAction
+import software.aws.toolkits.jetbrains.services.sqs.EditAttributesDialog
+import software.aws.toolkits.jetbrains.services.sqs.SqsQueueNode
+import software.aws.toolkits.jetbrains.services.sqs.telemetryType
+import software.aws.toolkits.jetbrains.utils.notifyError
+import software.aws.toolkits.resources.message
+import software.aws.toolkits.telemetry.Result
+import software.aws.toolkits.telemetry.SqsTelemetry
+
+class EditAttributesAction : SingleResourceNodeAction<SqsQueueNode>(message("sqs.edit.attributes.action")), DumbAware {
+    override fun actionPerformed(selected: SqsQueueNode, e: AnActionEvent) {
+        ProgressManager.getInstance().run(
+            object : Task.Backgroundable(
+                selected.project,
+                message("sqs.edit.attributes.retrieving_from_service"),
+                false,
+                PerformInBackgroundOption.ALWAYS_BACKGROUND
+            ) {
+                override fun run(indicator: ProgressIndicator) {
+                    val client = selected.nodeProject.awsClient<SqsClient>()
+                    val queue = selected.queue
+                    val attributes = try {
+                        client.getQueueAttributes {
+                            it.queueUrl(queue.queueUrl)
+                            it.attributeNames(QueueAttributeName.ALL)
+                        }.attributes()
+                    } catch (e: SqsException) {
+                        LOG.error(e) { "Getting queue attributes failed" }
+                        notifyError(
+                            project = project,
+                            title = message("sqs.service_name"),
+                            content = message("sqs.edit.attributes.failed", queue.queueName)
+                        )
+                        SqsTelemetry.editQueueAttributes(project, Result.Failed, queue.telemetryType())
+                        return
+                    }
+                    runInEdt {
+                        EditAttributesDialog(selected.nodeProject, client, queue, attributes).show()
+                    }
+                }
+            }
+        )
+    }
+
+    private companion object {
+        val LOG = getLogger<EditAttributesAction>()
+    }
+}
