f78d08a672cbff19c7eb3b4a12b5ed1ddc062818
Austin Brooks <abrooksv@users.noreply.github.com>
2020-09-24 15:44:24 -0700

    Fix Rider project wizard todo when min is 193 (#2099)

--- jetbrains-rider/src/software/aws/toolkits/jetbrains/services/lambda/dotnet/DotNetSamProjectGenerator.kt
+++ jetbrains-rider/src/software/aws/toolkits/jetbrains/services/lambda/dotnet/DotNetSamProjectGenerator.kt
@@ -34,8 +34,6 @@ import software.aws.toolkits.jetbrains.services.lambda.wizard.SamProjectGenerato
 import software.aws.toolkits.jetbrains.utils.DotNetRuntimeUtils
 import software.aws.toolkits.resources.message
 import java.awt.Dimension
-import java.awt.event.KeyAdapter
-import java.awt.event.KeyEvent
 import java.io.File
 import javax.swing.JScrollPane
 import javax.swing.JTabbedPane
@@ -226,17 +224,6 @@ class DotNetSamProjectGenerator(
 
     private fun initSolutionTextField() {
         solutionNameField.text = getPossibleName(SAM_HELLO_WORLD_PROJECT_NAME)
-
-        // "ReSharperTemplateGeneratorBase" base class has a logic that synchronize solution and project names.
-        // Project name has a constant value for SAM template. This is a workaround to persist project name unchanged.
-        // Please use "changeProjectName" flags when switch to 193 min version FIX_WHEN_MIN_IS_193
-        solutionNameField.addKeyListener(
-            object : KeyAdapter() {
-                override fun keyReleased(e: KeyEvent?) {
-                    projectNameField.text = SAM_HELLO_WORLD_PROJECT_NAME
-                }
-            }
-        )
     }
 
     /**
@@ -246,6 +233,7 @@ class DotNetSamProjectGenerator(
     private fun initProjectTextField() {
         projectNameField.text = SAM_HELLO_WORLD_PROJECT_NAME
         projectNameField.isEnabled = false
+        projectNameSetByUser = true
 
         sameDirectoryCheckBox.isEnabled = false
     }
