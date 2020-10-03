4c24593414a6c867b7a18f53cc4fcdd23878d09e
Justin M. Keyes <jmkeyes@amazon.com>
2020-10-03 12:13:56 -0700

    build/ASL: remove "copy after compile" step
    The files `dist/src/stepFunctions/asl/aslServer.js` and
    `dist/src/stepFunctions/asl/aslServer.js.map` already are created by the
    compile step. Why was `filemanager-webpack-plugin` ever needed?
    
    - Remove `copy-webpack-plugin`
    - `copy-webpack-plugin` doesn't have an `onEnd` concept like
      `filemanager-webpack-plugin` did. Instead we can move the copy step to
      the same place used by SSM. But ASL doesn't actually *need* a copy
      step (see above)...
    - where does the *.LICENSE.txt.js file come from?
    
    Test cases:
    -----------
    Using the `.vsix` artifact, observe:
    - `ps aux` shows that `aslServer` is running:
       ```
       jmkeyes 87125   0.0  0.3  8718916  44536   ??  S     8:09AM   0:00.36
       /…/Visual Studio Code.app/ …/.vscode/extensions/amazonwebservices.aws-toolkit-vscode-1.16.0-SNAPSHOT/dist/src/stepFunctions/asl/aslServer.js --node-ipc --clientProcessId=85703
       ```
    - Code-completion works on `HelloWorld.asl.json`.
    - "Render graph" feature works on `HelloWorld.asl.json`.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ const path = require('path')
 const webpack = require('webpack')
 const fs = require('fs')
 const TerserPlugin = require('terser-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
 const { NLSBundlePlugin } = require('vscode-nls-dev/lib/webpack-bundler')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 const packageJsonFile = path.join(__dirname, 'package.json')
@@ -23,7 +22,7 @@ const config = {
     target: 'node',
     entry: {
         extension: './src/extension.ts',
-        aslServer: './src/stepFunctions/asl/aslServer.ts',
+        'src/stepFunctions/asl/aslServer': './src/stepFunctions/asl/aslServer.ts',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -74,23 +73,6 @@ const config = {
         new NLSBundlePlugin(packageId),
         new webpack.DefinePlugin({
             PLUGINVERSION: JSON.stringify(packageJson.version),
-        }),
-        // @ts-ignore
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'dist/aslServer.js'),
-                    to: path.resolve(__dirname, 'dist/src/stepFunctions/asl/aslServer.js'),
-                },
-                {
-                    from: path.resolve(__dirname, 'dist/aslServer.js.LICENSE.txt'),
-                    to: path.resolve(__dirname, 'dist/src/stepFunctions/asl/aslServer.js.LICENSE.txt'),
-                },
-                {
-                    from: path.resolve(__dirname, 'dist/aslServer.js.map'),
-                    to: path.resolve(__dirname, 'dist/src/stepFunctions/asl/aslServer.js.map'),
-                },
-            ],
         }),
         new CircularDependencyPlugin({
             exclude: /node_modules/,
