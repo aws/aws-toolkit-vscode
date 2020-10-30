fd4e9eea000b18030f602012c2dd36923a9230c5
Sweetwood <gsweet@amazon.com>
2020-10-30 15:49:54 -0700

    comment to advise against sh1 hash

--- src/shared/utilities/textUtilities.ts
+++ src/shared/utilities/textUtilities.ts
@@ -29,8 +29,14 @@ export function getStringHash(text: string): string {
     return hash.digest('hex')
 }
 
+/**
+ * Using the 'sha1' hash is no longer recommended. This hash is used to specifically comply with the
+ * Spec: 'SSO Login Token Flow' and is used only to name a file. It is advised to use a more secure
+ * hash in most other cases.
+ * @param text The text to hash
+ */
 export function getSHA1StringHash(text: string): string {
     const shasum = crypto.createHash('sha1')
-    shasum.update(text)
+    shasum.update(text) //lgtm [js/weak-cryptographic-algorithm]
     return shasum.digest('hex')
 }
