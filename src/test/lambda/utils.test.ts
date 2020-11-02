2922d4c7909f9dd59e41a12347d49db3a5551db8
Bryce Ito <bryceito@amazon.com>
2020-11-02 08:36:44 -0800

    Codelenses in source files can create template-style launch configs if related to a template (#1364)
    Users can now create template-based launch configurations from source codelenses. We calculate the matches based on the CodeURI and handler for interpreted languages, and primarily off of the handler for .NET codelenses. Does not dedupe at this time.

--- src/test/lambda/utils.test.ts
+++ src/test/lambda/utils.test.ts
@@ -4,36 +4,48 @@
  */
 
 import * as assert from 'assert'
-import { getLambdaFileNameFromHandler } from '../../lambda/utils'
+import { getLambdaDetails } from '../../lambda/utils'
 import { assertThrowsError } from '../shared/utilities/assertUtils'
 
 describe('lambda utils', async () => {
-    describe('getLambdaFileNameFromHandler', () => {
-        it('returns valid filenames', () => {
-            assert(getLambdaFileNameFromHandler({ Runtime: 'nodejs12.x', Handler: 'app.lambda_handler' }), 'app.js')
-            assert(getLambdaFileNameFromHandler({ Runtime: 'python3.8', Handler: 'app.lambda_handler' }), 'app.py')
-            assert(
-                getLambdaFileNameFromHandler({ Runtime: 'nodejs12.x', Handler: 'asdf/jkl/app.lambda_handler' }),
-                'asdf/jkl/app.js'
-            )
-            assert(
-                getLambdaFileNameFromHandler({ Runtime: 'python3.8', Handler: 'asdf/jkl/app.lambda_handler' }),
-                'asdf/jkl/app.py'
-            )
+    describe('getLambdaDetails', () => {
+        it('returns valid filenames and function names', () => {
+            const jsNonNestedParsedName = getLambdaDetails({
+                Runtime: 'nodejs12.x',
+                Handler: 'app.lambda_handler',
+            })
+            const pyNonNestedParsedName = getLambdaDetails({
+                Runtime: 'python3.8',
+                Handler: 'app.lambda_handler',
+            })
+            const jsNestedParsedName = getLambdaDetails({
+                Runtime: 'nodejs12.x',
+                Handler: 'asdf/jkl/app.lambda_handler',
+            })
+            const PyNestedParsedName = getLambdaDetails({
+                Runtime: 'python3.8',
+                Handler: 'asdf/jkl/app.lambda_handler',
+            })
+            assert(jsNonNestedParsedName.fileName, 'app.js')
+            assert(pyNonNestedParsedName.fileName, 'app.py')
+            assert(jsNestedParsedName.fileName, 'asdf/jkl/app.js')
+            assert(PyNestedParsedName.fileName, 'asdf/jkl/app.py')
+            assert(jsNonNestedParsedName.functionName, 'lambda_handler')
+            assert(pyNonNestedParsedName.functionName, 'lambda_handler')
+            assert(jsNestedParsedName.functionName, 'lambda_handler')
+            assert(PyNestedParsedName.functionName, 'lambda_handler')
         })
 
         it('throws if the handler is not a supported runtime', async () => {
             // unsupported runtime for import
             await assertThrowsError(async () =>
-                getLambdaFileNameFromHandler({
+                getLambdaDetails({
                     Runtime: 'dotnetcore3.1',
                     Handler: 'HelloWorld::HelloWorld.Function::FunctionHandler',
                 })
             )
             // runtime that isn't present, period
-            await assertThrowsError(async () =>
-                getLambdaFileNameFromHandler({ Runtime: 'COBOL-60', Handler: 'asdf.asdf' })
-            )
+            await assertThrowsError(async () => getLambdaDetails({ Runtime: 'COBOL-60', Handler: 'asdf.asdf' }))
         })
     })
 })
