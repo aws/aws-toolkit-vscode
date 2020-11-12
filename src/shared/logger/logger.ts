5ffb687565f7e00a463c348d0455bc42c7b316ad
Justin M. Keyes <jmkeyes@amazon.com>
2020-11-11 16:04:55 -0800

    logger: introduce logLevelEnabled()
    Sometimes it is useful to enable certain behaviors based on the current
    log-level.
    
    Examples:
    - When invoking `sam` CLI, if the current log-level is 'debug' then it
      could make sense to use `sam --debug …`
    - Other third-party components like debuggers have verbosity levels that
      are useful to include in the Toolkit's own logs. (Else the debugger
      logs get lost or must be configured some other way.)

--- src/shared/logger/logger.ts
+++ src/shared/logger/logger.ts
@@ -18,9 +18,38 @@ export interface Logger {
     warn(error: Error): void
     error(message: string, ...meta: any[]): void
     error(error: Error): void
+    /** Returns true if the given log level is being logged.  */
+    logLevelEnabled(logLevel: LogLevel): boolean
 }
 
-export type LogLevel = keyof Logger
+/**
+ * Log levels ordered for comparison.
+ *
+ * See https://github.com/winstonjs/winston#logging-levels :
+ * > RFC5424: severity of levels is numerically ascending from most important
+ * > to least important.
+ */
+const logLevels = new Map<LogLevel, number>([
+    ['error', 1],
+    ['warn', 2],
+    ['info', 3],
+    ['verbose', 4],
+    ['debug', 5],
+])
+
+export type LogLevel = 'error' | 'warn' | 'info' | 'verbose' | 'debug'
+
+/**
+ * Compares log levels.
+ *
+ * @returns
+ * - Zero if the log levels are equal
+ * - Negative if `l1` is less than `l2`
+ * - Positive if `l1` is greater than `l2`
+ */
+export function compareLogLevel(l1: LogLevel, l2: LogLevel): number {
+    return logLevels.get(l1)! - logLevels.get(l2)!
+}
 
 /**
  * Gets the logger if it has been initialized
