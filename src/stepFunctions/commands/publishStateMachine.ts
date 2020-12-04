41a51d1326bc7a754b28a947d395b6752c1df66e
Alan Bogusiewicz <abogusiewicz@gmail.com>
2020-12-04 14:55:01 -0800

    stepfunctions: Add YAML validation/completions/snippets/etc to ASL lang service #1415
    * Adds ASL YAML linting and visualization support.
    * Small fixes to SFN ASL YAML functionality
    * Resolves peer dependency issue with "prettier"
    * Bumps up the version of asl service
    * Sync package.json and package-lock.json with parent branch
    
    Co-authored-by: Ella-Kuypers <kuyperse@amazon.com>

--- src/stepFunctions/commands/publishStateMachine.ts
+++ src/stepFunctions/commands/publishStateMachine.ts
@@ -30,6 +30,11 @@ export async function publishStateMachine(awsContext: AwsContext, outputChannel:
         throw new Error('Could not get active text editor for state machine definition')
     }
 
+    if (textDocument.languageId === 'asl-yaml') {
+        logger.error('Cannot publish state machine from Amazon States Language YAML file')
+        throw new Error('Cannot publish state machine from Amazon States Language YAML file')
+    }
+
     let region = awsContext.getCredentialDefaultRegion()
     if (!region) {
         region = DEFAULT_REGION
