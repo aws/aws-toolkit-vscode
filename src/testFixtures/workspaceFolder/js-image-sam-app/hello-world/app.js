f5e92e840e72c6a2f746ec97cae68cb1044db9f2
Justin M. Keyes <jmkeyes@amazon.com>
2021-01-05 15:45:32 -0800

    test: avoid noise in sample package.json
    We don't need these dependencies in the sample app.
    
    ref https://github.com/aws/aws-sam-cli-app-templates/pull/80

--- src/testFixtures/workspaceFolder/js-image-sam-app/hello-world/app.js
+++ src/testFixtures/workspaceFolder/js-image-sam-app/hello-world/app.js
@@ -1,5 +1,3 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
 let response
 
 /**
@@ -16,12 +14,10 @@ let response
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
         response = {
             statusCode: 200,
             body: JSON.stringify({
                 message: 'hello world',
-                // location: ret.data.trim()
             }),
         }
     } catch (err) {
