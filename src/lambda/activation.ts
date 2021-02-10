62f1695655f9fc0c712470ee3d68ce89908b8309
Bryce Ito <bryceito@amazon.com>
2021-02-10 09:57:02 -0800

    First crack at APIs

--- src/lambda/activation.ts
+++ src/lambda/activation.ts
@@ -12,14 +12,15 @@ import { LambdaFunctionNode } from './explorer/lambdaFunctionNode'
 import { importLambdaCommand } from './commands/importLambda'
 import { tryRemoveFolder } from '../shared/filesystemUtilities'
 import { registerSamInvokeVueCommand } from './vue/samInvoke'
+import { ExtContext } from '../shared/extensions'
 
 /**
  * Activates Lambda components.
  */
-export async function activate(extensionContext: vscode.ExtensionContext): Promise<void> {
+export async function activate(context: ExtContext): Promise<void> {
     const outputChannel = vscode.window.createOutputChannel('AWS Lambda')
 
-    extensionContext.subscriptions.push(
+    context.extensionContext.subscriptions.push(
         vscode.commands.registerCommand(
             'aws.deleteLambda',
             async (node: LambdaFunctionNode) =>
@@ -55,6 +56,6 @@ export async function activate(extensionContext: vscode.ExtensionContext): Promi
         vscode.commands.registerCommand('aws.uploadLambda', async (node: LambdaFunctionNode) => {
             await uploadLambdaCommand(node)
         }),
-        registerSamInvokeVueCommand(extensionContext)
+        registerSamInvokeVueCommand(context)
     )
 }
--- src/lambda/vue/samInvoke.ts
+++ src/lambda/vue/samInvoke.ts
@@ -4,30 +4,40 @@
  */
 
 import * as vscode from 'vscode'
-import { AwsSamDebuggerConfiguration } from '../../shared/sam/debugger/awsSamDebugConfiguration'
+import { LaunchConfiguration } from '../../shared/debug/launchConfiguration'
+import { ExtContext } from '../../shared/extensions'
+import {
+    AwsSamDebuggerConfiguration,
+    isCodeTargetProperties,
+    isTemplateTargetProperties,
+} from '../../shared/sam/debugger/awsSamDebugConfiguration'
+import { SamDebugConfigProvider } from '../../shared/sam/debugger/awsSamDebugger'
 import { createVueWebview } from '../../webviews/main'
 
-export function registerSamInvokeVueCommand(context: vscode.ExtensionContext): vscode.Disposable {
+export function registerSamInvokeVueCommand(context: ExtContext): vscode.Disposable {
     return vscode.commands.registerCommand('aws.lambda.vueTest', async () => {
-        await createVueWebview({
+        await createVueWebview<SamInvokerRequest | SamInvokerLaunchRequest, SamInvokerResponse, any>({
             id: 'create',
             name: 'VueTest',
             webviewJs: 'samInvokeVue.js',
-            onDidReceiveMessageFunction: handleFrontendToBackendMessage,
-            context,
+            onDidReceiveMessageFunction: async (message, postMessageFn, destroyWebviewFn) =>
+                handleFrontendToBackendMessage(message, postMessageFn, destroyWebviewFn, context),
+            context: context.extensionContext,
         })
     })
 }
 
-export interface BackendToFrontend {
-    command: 'loadLaunchConfig' | 'loadSamplePayload' | 'loadTemplates'
+// TODO: Better names for all the interfaces
+
+export interface SamInvokerResponse {
+    command: 'TODO: Define events that the frontend can use'
 }
 
-export interface FrontendToBackendBasicRequest {
+export interface SamInvokerRequest {
     command: 'loadSamLaunchConfig' | 'getSamplePayload' | 'getTemplates'
 }
 
-export interface FrontendToBackendLaunchConfigRequest {
+export interface SamInvokerLaunchRequest {
     command: 'saveLaunchConfig' | 'invokeLaunchConfig'
     data: {
         launchConfig: AwsSamDebuggerConfiguration
@@ -35,9 +45,10 @@ export interface FrontendToBackendLaunchConfigRequest {
 }
 
 async function handleFrontendToBackendMessage(
-    message: FrontendToBackendBasicRequest | FrontendToBackendLaunchConfigRequest,
-    postMessageFn: (response: BackendToFrontend) => Thenable<boolean>,
-    destroyWebviewFn: () => any
+    message: SamInvokerRequest | SamInvokerLaunchRequest,
+    postMessageFn: (response: SamInvokerResponse) => Thenable<boolean>,
+    destroyWebviewFn: () => any,
+    context: ExtContext
 ): Promise<any> {
     switch (message.command) {
         case 'loadSamLaunchConfig':
@@ -53,7 +64,7 @@ async function handleFrontendToBackendMessage(
             saveLaunchConfig(message.data.launchConfig)
             break
         case 'invokeLaunchConfig':
-            invokeLaunchConfig(message.data.launchConfig)
+            invokeLaunchConfig(message.data.launchConfig, context)
             break
     }
 }
@@ -64,21 +75,21 @@ async function handleFrontendToBackendMessage(
  * Call back into the webview with the selected launch config.
  * @param postMessageFn
  */
-async function loadSamLaunchConfig(postMessageFn: (response: BackendToFrontend) => Thenable<boolean>) {}
+async function loadSamLaunchConfig(postMessageFn: (response: SamInvokerResponse) => Thenable<boolean>): Promise<void> {}
 
 /**
  * Open a quick pick containing upstream sample payloads.
  * Call back into the webview with the contents of the payload to add to the JSON field.
  * @param postMessageFn
  */
-async function getSamplePayload(postMessageFn: (response: BackendToFrontend) => Thenable<boolean>) {}
+async function getSamplePayload(postMessageFn: (response: SamInvokerResponse) => Thenable<boolean>): Promise<void> {}
 
 /**
  * Get all templates in the registry.
  * Call back into the webview with the registry contents.
  * @param postMessageFn
  */
-async function getTemplates(postMessageFn: (response: BackendToFrontend) => Thenable<boolean>) {}
+async function getTemplates(postMessageFn: (response: SamInvokerResponse) => Thenable<boolean>): Promise<void> {}
 
 /**
  * Open a quick pick containing the names of launch configs in the `launch.json` array, plus a "Create New Entry" entry.
@@ -86,10 +97,38 @@ async function getTemplates(postMessageFn: (response: BackendToFrontend) => Then
  * On selecting "Create New Entry", prompt the user for a name and save the contents to the end of the `launch.json` array.
  * @param config Config to save
  */
-async function saveLaunchConfig(config: AwsSamDebuggerConfiguration) {}
+async function saveLaunchConfig(config: AwsSamDebuggerConfiguration): Promise<void> {
+    const uri = getUriFromLaunchConfig(config)
+    if (!uri) {
+        // TODO Localize
+        vscode.window.showErrorMessage('Toolkit requires a target resource in order to save a debug configuration')
+        return undefined
+    }
+    const launchConfig = new LaunchConfiguration(uri)
+    const existingConfigs = launchConfig.getSamDebugConfigurations()
+    // TODO: Create quick pick
+    //
+}
 
 /**
  * Validate and execute the provided launch config.
- * @param config
+ * TODO: Post validation failures back to webview?
+ * @param config Config to invoke
  */
-async function invokeLaunchConfig(config: AwsSamDebuggerConfiguration) {}
+async function invokeLaunchConfig(config: AwsSamDebuggerConfiguration, context: ExtContext): Promise<void> {
+    const provider = new SamDebugConfigProvider(context)
+
+    const targetUri = getUriFromLaunchConfig(config)
+
+    const folder = targetUri ? vscode.workspace.getWorkspaceFolder(targetUri) : undefined
+
+    await provider.resolveDebugConfiguration(folder, config)
+}
+
+function getUriFromLaunchConfig(config: AwsSamDebuggerConfiguration): vscode.Uri | undefined {
+    if (isTemplateTargetProperties(config.invokeTarget)) {
+        return vscode.Uri.file(config.invokeTarget.templatePath)
+    } else if (isCodeTargetProperties(config.invokeTarget)) {
+        return vscode.Uri.file(config.invokeTarget.target)
+    }
+}
