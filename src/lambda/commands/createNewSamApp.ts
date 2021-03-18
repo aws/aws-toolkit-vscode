aws/aws-toolkit-vscode

commit 327325a2c40b2deee5c184367162c44b54402155

telemetry: record SAM CLI version #1554

src/lambda/commands/createNewSamApp.ts
@@ -60,8 +60,12 @@ export async function resumeCreateNewSamApp(
     let createResult: Result = 'Succeeded'
     let reason: CreateReason = 'complete'
     let samInitState: SamInitState | undefined
+    let samVersion: string | undefined
+
     try {
         samInitState = activationReloadState.getSamInitState()
+        samVersion = await getSamCliVersion(getSamCliContext())
+
         const pathToLaunch = samInitState?.path
         if (!pathToLaunch) {
             return
@@ -92,6 +96,22 @@ export async function resumeCreateNewSamApp(
             samInitState?.isImage ? samInitState?.runtime : undefined
         )
         await vscode.window.showTextDocument(uri)
+    } catch (err) {
+        createResult = 'Failed'
+        reason = 'error'
+
+        const checkLogsMessage = makeCheckLogsMessage()
+
+        ext.outputChannel.show(true)
+        getLogger('channel').error(
+            localize(
+                "AWS.samcli.initWizard.resume.error",
+                "An error occured while resuming SAM Application creation. {0}",
+                checkLogsMessage
+            )
+        )
+
+        getLogger().error('Error resuming new SAM Application: %O', err as Error)
     } finally {
         activationReloadState.clearSamInitState()
         recordSamInit({
@@ -99,6 +119,7 @@ export async function resumeCreateNewSamApp(
             result: createResult,
             reason: reason,
             runtime: samInitState?.runtime as TelemetryRuntime,
+            version: samVersion,
         })
     }
 }
@@ -123,6 +144,7 @@ export async function createNewSamApplication(
     let lambdaPackageType: 'Zip' | 'Image' | undefined
     let createRuntime: Runtime | undefined
     let config: CreateNewSamAppWizardResponse | undefined
+    let samVersion: string | undefined
 
     let initArguments: SamCliInitArgs
 
@@ -132,9 +154,9 @@ export async function createNewSamApplication(
         const currentCredentials = await awsContext.getCredentials()
         const availableRegions = getRegionsForActiveCredentials(awsContext, regionProvider)
         const schemasRegions = availableRegions.filter(region => regionProvider.isServiceInRegion('schemas', region.id))
-        const samCliVersion = await getSamCliVersion(samCliContext)
+        samVersion = await getSamCliVersion(samCliContext)
 
-        const wizardContext = new DefaultCreateNewSamAppWizardContext(currentCredentials, schemasRegions, samCliVersion)
+        const wizardContext = new DefaultCreateNewSamAppWizardContext(currentCredentials, schemasRegions, samVersion)
         config = await new CreateNewSamAppWizard(wizardContext).run()
 
         if (!config) {
@@ -302,6 +324,7 @@ export async function createNewSamApplication(
             result: createResult,
             reason: reason,
             runtime: createRuntime as TelemetryRuntime,
+            version: samVersion,
         })
     }
 }

src/lambda/commands/deploySamApplication.ts
@@ -13,7 +13,7 @@ import { ext } from '../../shared/extensionGlobals'
 import { makeTemporaryToolkitFolder, tryRemoveFolder } from '../../shared/filesystemUtilities'
 import { getLogger } from '../../shared/logger'
 import { SamCliBuildInvocation } from '../../shared/sam/cli/samCliBuild'
-import { getSamCliContext, SamCliContext } from '../../shared/sam/cli/samCliContext'
+import { getSamCliContext, SamCliContext, getSamCliVersion } from '../../shared/sam/cli/samCliContext'
 import { runSamCliDeploy } from '../../shared/sam/cli/samCliDeploy'
 import { SamCliProcessInvoker } from '../../shared/sam/cli/samCliInvokerUtils'
 import { runSamCliPackage } from '../../shared/sam/cli/samCliPackage'
@@ -59,6 +59,7 @@ export async function deploySamApplication(
     }
 ): Promise<void> {
     let deployResult: Result = 'Succeeded'
+    let samVersion: string | undefined
     let deployFolder: string | undefined
     try {
         const credentials = await awsContext.getCredentials()
@@ -75,6 +76,7 @@ export async function deploySamApplication(
         }
 
         deployFolder = await makeTemporaryToolkitFolder('samDeploy')
+        samVersion = await getSamCliVersion(samCliContext)
 
         const deployParameters: DeploySamApplicationParameters = {
             deployRootFolder: deployFolder,
@@ -111,7 +113,7 @@ export async function deploySamApplication(
         outputDeployError(err as Error)
     } finally {
         await tryRemoveFolder(deployFolder)
-        recordSamDeploy({ result: deployResult })
+        recordSamDeploy({ result: deployResult, version: samVersion })
     }
 }
 

src/shared/sam/localLambdaRunner.ts
@@ -33,6 +33,7 @@ import { DefaultSamCliProcessInvokerContext } from './cli/samCliProcessInvokerCo
 import { DefaultSamCliConfiguration } from './cli/samCliConfiguration'
 import { extensionSettingsPrefix } from '../constants'
 import { DefaultSamCliLocationProvider } from './cli/samCliLocator'
+import { getSamCliContext, getSamCliVersion } from './cli/samCliContext'
 
 const localize = nls.loadMessageBundle()
 
@@ -320,8 +321,11 @@ export async function invokeLambdaFunction(
 
         // sam local invoke ...
         const command = new SamCliLocalInvokeInvocation(localInvokeArgs)
+        let samVersion: string | undefined 
         let invokeResult: telemetry.Result = 'Failed'
+
         try {
+            samVersion = await getSamCliVersion(getSamCliContext())
             await command.execute(timer)
             invokeResult = 'Succeeded'
         } catch (err) {
@@ -339,6 +343,7 @@ export async function invokeLambdaFunction(
                 result: invokeResult,
                 runtime: config.runtime as telemetry.Runtime,
                 debug: !config.noDebug,
+                version: samVersion,
             })
         }
     }

commit c32f42c424b74d8455b9e43eac83884ba97fc609

startup: fix bug in "Create Lambda SAM App" resume logic #1582
Check version *after* checking that `sam` is available.

ref #1580
regression by #1554

src/lambda/commands/createNewSamApp.ts
@@ -64,7 +64,6 @@ export async function resumeCreateNewSamApp(
 
     try {
         samInitState = activationReloadState.getSamInitState()
-        samVersion = await getSamCliVersion(getSamCliContext())
 
         const pathToLaunch = samInitState?.path
         if (!pathToLaunch) {
@@ -88,6 +87,8 @@ export async function resumeCreateNewSamApp(
 
             return
         }
+
+        samVersion = await getSamCliVersion(getSamCliContext())
 
         await addInitialLaunchConfiguration(
             extContext,