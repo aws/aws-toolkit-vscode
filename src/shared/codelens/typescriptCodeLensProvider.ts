55e904dcd689fbfdf3d1968fdfbc5f82f7f1c92d
Garret Sweetwood <gsweet@amazon.com>
2021-04-05 16:34:07 -0700

    CodeLenses appear in TypeScript files (#1607)

--- src/shared/codelens/typescriptCodeLensProvider.ts
+++ src/shared/codelens/typescriptCodeLensProvider.ts
@@ -12,9 +12,18 @@ import { findParentProjectFile } from '../utilities/workspaceUtils'
 
 export const JAVASCRIPT_LANGUAGE = 'javascript'
 
+export const TYPESCRIPT_LANGUAGE = 'typescript'
+
 export const JAVASCRIPT_ALL_FILES: vscode.DocumentFilter[] = [
     {
         language: JAVASCRIPT_LANGUAGE,
+        scheme: 'file',
+    },
+]
+
+export const TYPESCRIPT_ALL_FILES: vscode.DocumentFilter[] = [
+    {
+        language: TYPESCRIPT_LANGUAGE,
         scheme: 'file',
     },
 ]
