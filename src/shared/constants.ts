b6a4a01746c96b33676b4b0a365f5ab5a289f268
Bryce Ito <bryceito@amazon.com>
2021-04-20 15:31:04 -0700

    Users can get help after creating a new SAM app (#1661)

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -57,6 +57,12 @@ export const ssmDocumentPublishGuideUrl: string =
     'https://docs.aws.amazon.com/systems-manager/latest/userguide/create-ssm-doc.html'
 export const ssmJson: string = 'ssm-json'
 export const ssmYaml: string = 'ssm-yaml'
+
+// URL for post-Create SAM App
+export const debugNewSamAppUrl: string = isCloud9()
+    ? 'https://docs.aws.amazon.com/cloud9/latest/user-guide/serverless-apps-toolkit.html#sam-run-debug'
+    : 'https://docs.aws.amazon.com/toolkit-for-vscode/latest/userguide/serverless-apps.html#serverless-apps-debug'
+
 /**
  * Moment format for rendering readable dates.
  *
