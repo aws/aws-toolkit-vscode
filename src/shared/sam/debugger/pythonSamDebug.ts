aws/aws-toolkit-vscode

commit 52753ce87faa374e86c2f17b6a49cd492fd23d41

SAM debug: retry only once #1616
Retrying more than once is a bad experience: VSCode will show the
debugger UI again and again, and user must hit the "Stop" button every
time we retry.

TODO: can we listen to the "Stop" button and cancel retries then?

src/shared/sam/localLambdaRunner.ts
@@ -70,7 +70,7 @@ function makeResourceName(config: SamLaunchRequestArgs): string {
 
 const SAM_LOCAL_PORT_CHECK_RETRY_INTERVAL_MILLIS: number = 125
 const SAM_LOCAL_PORT_CHECK_RETRY_TIMEOUT_MILLIS_DEFAULT: number = 30000
-const MAX_DEBUGGER_RETRIES: number = 4
+const MAX_DEBUGGER_RETRIES: number = 1
 const ATTACH_DEBUGGER_RETRY_DELAY_MILLIS: number = 1000
 
 /** "sam local start-api" wrapper from the current debug-session. */

src/test/shared/codelens/localLambdaRunner.test.ts
@@ -145,7 +145,7 @@ describe('localLambdaRunner', async function () {
                 debugConfig: ({} as any) as SamLaunchRequestArgs,
                 onStartDebugging: startDebuggingReturnsFalse,
                 onRecordAttachDebuggerMetric: (attachResult: boolean | undefined, attempts: number): void => {
-                    assert.strictEqual(actualRetries, 4, 'Metrics should only be recorded once')
+                    assert.strictEqual(actualRetries, 1, 'Metrics should only be recorded once')
                     assert.notStrictEqual(attachResult, undefined, 'attachResult should not be undefined')
                 },
                 onWillRetry,
@@ -159,7 +159,7 @@ describe('localLambdaRunner', async function () {
                     folder: vscode.WorkspaceFolder | undefined,
                     nameOrConfiguration: string | vscode.DebugConfiguration
                 ): Promise<boolean> => {
-                    const retVal = actualRetries === 3 ? true : undefined
+                    const retVal = actualRetries === 0 ? true : undefined
 
                     return retVal!
                 },

commit 169f27c997709cea9e1e4c4b92b3bd0875c98834

SAM debug/python: restore retries to ensure attach #1667
Problem:
52753ce87faa374e86c2f17b6a49cd492fd23d41 caused a regression #1666. For
Python, retries proceed *without* a modal popup (which is what we
actually want). So the retries were important for Python debugging.

Solution:
Use more retries when runtime=Python. This also allows us to remove the
hardcoded delay.

Co-authored-by: Justin M. Keyes <jmkeyes@amazon.com>

src/shared/sam/debugger/pythonSamDebug.ts
@@ -21,7 +21,7 @@ import * as pathutil from '../../utilities/pathUtils'
 import { getLocalRootVariants } from '../../utilities/pathUtils'
 import { Timeout } from '../../utilities/timeoutUtils'
 import { DefaultSamLocalInvokeCommand, WAIT_FOR_DEBUGGER_MESSAGES } from '../cli/samCliLocalInvoke'
-import { invokeLambdaFunction, makeInputTemplate } from '../localLambdaRunner'
+import { invokeLambdaFunction, makeInputTemplate, waitForPort } from '../localLambdaRunner'
 import { SamLaunchRequestArgs } from './awsSamDebugger'
 import { ext } from '../../extensionGlobals'
 import { Runtime } from 'aws-sdk/clients/lambda'
@@ -216,12 +216,12 @@ export async function invokePythonLambda(
         WAIT_FOR_DEBUGGER_MESSAGES.PYTHON,
         WAIT_FOR_DEBUGGER_MESSAGES.PYTHON_IKPDB,
     ])
-    // Must not used waitForPythonDebugAdapter() for ikpdb: the socket consumes
+    // Must not used waitForPort() for ikpdb: the socket consumes
     // ikpdb's initial message and ikpdb does not have a --wait-for-client
     // mode, then cloud9 never sees the init message and waits forever.
     //
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    config.onWillAttachDebugger = config.useIkpdb ? waitForIkpdb : waitForPythonDebugAdapter
+    config.onWillAttachDebugger = config.useIkpdb ? waitForIkpdb : waitForPort
     const c = (await invokeLambdaFunction(ctx, config, async () => {})) as PythonDebugConfiguration
     return c
 }
@@ -233,12 +233,6 @@ async function waitForIkpdb(debugPort: number, timeout: Timeout) {
     getLogger().info('waitForIkpdb: wait 2 seconds')
     await new Promise<void>(resolve => {
         setTimeout(resolve, 2000)
-    })
-}
-
-export async function waitForPythonDebugAdapter(debugPort: number, timeout: Timeout) {
-    await new Promise<void>(resolve => {
-        setTimeout(resolve, 1000)
     })
 }
 

src/shared/sam/localLambdaRunner.ts
@@ -72,7 +72,6 @@ function makeResourceName(config: SamLaunchRequestArgs): string {
 
 const SAM_LOCAL_PORT_CHECK_RETRY_INTERVAL_MILLIS: number = 125
 const SAM_LOCAL_PORT_CHECK_RETRY_TIMEOUT_MILLIS_DEFAULT: number = 30000
-const MAX_DEBUGGER_RETRIES: number = 1
 const ATTACH_DEBUGGER_RETRY_DELAY_MILLIS: number = 1000
 
 /** "sam local start-api" wrapper from the current debug-session. */
@@ -520,10 +519,18 @@ export async function attachDebugger({
 
     getLogger('channel').info(localize('AWS.output.sam.local.attaching', 'Attaching debugger to SAM application...'))
 
+    // The Python extension will silently fail, so it's ok for us to automatically retry
+    // Users still will not be able to stop debugging without clicking stop a bunch, but
+    // at least it's not modal popups.
+    // TODO: figure out why the Python debug client fails to attach on the first try
+    function maxRetries() {
+        return params.debugConfig.runtimeFamily === RuntimeFamily.Python ? 8 : 1
+    }
+
     do {
         isDebuggerAttached = await onStartDebugging(undefined, params.debugConfig)
         if (!isDebuggerAttached) {
-            if (retries < MAX_DEBUGGER_RETRIES) {
+            if (retries < maxRetries()) {
                 if (onWillRetry) {
                     await onWillRetry()
                 }