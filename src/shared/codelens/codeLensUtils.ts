136a67bf64b6568eaaf5ca345b1262cb2cb0ee2d
Bryce Ito <bryceito@amazon.com>
2021-04-30 16:57:58 -0700

    Codelenses can be toggled and invoked directly via command palette (#1681)
    * Toggle codelenses via setting or "Toggle SAM hints in source files" command
    * Create debug configs with or without codelenses appearing with "Add SAM Debug Configuration" command

--- src/shared/codelens/codeLensUtils.ts
+++ src/shared/codelens/codeLensUtils.ts
@@ -16,6 +16,7 @@ import {
 } from '../sam/debugger/commands/addSamDebugConfiguration'
 import * as javaDebug from '../sam/debugger/javaSamDebug'
 import * as pythonDebug from '../sam/debugger/pythonSamDebug'
+import { SettingsConfiguration } from '../settingsConfiguration'
 import { createQuickPick, promptUser, verifySinglePickerOutput } from '../ui/picker'
 import { localize } from '../utilities/vsCodeUtils'
 import { getWorkspaceRelativePath } from '../utilities/workspaceUtils'
@@ -26,6 +27,8 @@ import * as tsCodelens from './typescriptCodeLensProvider'
 import * as goCodelens from './goCodeLensProvider'
 
 export type Language = 'python' | 'javascript' | 'csharp' | 'go' | 'java'
+
+export const STATE_NAME_ENABLE_CODELENSES = 'sam.enableCodeLenses'
 
 export async function makeCodeLenses({
     document,
@@ -125,6 +128,83 @@ function makeAddCodeSamDebugCodeLens(
 }
 
 /**
+ * Tied to the AWS.addSamDebugConfig command: lets a user create a config tied to a handler via command instead of codelens
+ * Renders a quick pick using the first 200 characters of the first line of the function declaration + function line number
+ * @param document Curr document
+ * @param lenses Codelenses returned via CodeLensProvider, which we extract the information from
+ */
+export async function invokeCodeLensCommandPalette(
+    document: Pick<vscode.TextDocument, 'getText'>,
+    lenses: vscode.CodeLens[]
+): Promise<void> {
+    const labelRenderRange = 200
+    const handlers: (vscode.QuickPickItem & { lens?: vscode.CodeLens })[] = lenses
+        .filter(lens => {
+            // remove codelenses that go to the invoker UI
+            // maybe move this into the workflow at some point (drop down to one)
+            return (
+                lens &&
+                lens.command &&
+                lens.command.arguments &&
+                lens.command.arguments.length === 3 &&
+                lens.command.arguments![2] !== true
+            )
+        })
+        .map(lens => {
+            return {
+                // lens is currently pulling the entire function, not just the declaration
+                label: document.getText(
+                    new vscode.Range(
+                        lens.range.start,
+                        new vscode.Position(lens.range.start.line, lens.range.start.character + labelRenderRange)
+                    )
+                ),
+                detail: localize(
+                    'AWS.pickDebugHandler.range',
+                    'Function on line {0}',
+                    (lens.range.start.line + 1).toString()
+                ),
+                lens,
+            }
+        })
+    if (handlers.length === 0) {
+        handlers.push({
+            label: localize('AWS.pickDebugHandler.noItems', 'No handlers found in current file'),
+            detail: localize('AWS.pickDebugHandler.noItems.detail', 'Ensure your language extension is working'),
+            description: localize('AWS.picker.dynamic.noItemsFound.detail', 'Click here to go back'),
+        })
+    }
+
+    const picker = createQuickPick<vscode.QuickPickItem & { lens?: vscode.CodeLens }>({
+        options: {
+            canPickMany: false,
+            ignoreFocusOut: false,
+            matchOnDetail: true,
+            title: localize('AWS.pickDebugHandler.prompt', 'Create a Debug Configuration from a valid handler'),
+            step: 1,
+            totalSteps: 2,
+        },
+        items: handlers,
+    })
+
+    const choices = await promptUser({ picker })
+    const val = verifySinglePickerOutput(choices)
+
+    if (!val || !val.lens || !val.lens.command) {
+        return undefined
+    }
+
+    // note: val.lens.command.arguments[2] should always be false (aka no invoke UI) based on the filter statement
+    // HACK: `exports.` is for the sake of sinon stubbing; you can't stub a function in the same file
+    await exports.pickAddSamDebugConfiguration(
+        val.lens.command.arguments![0],
+        val.lens.command.arguments![1],
+        val.lens.command.arguments![2],
+        true
+    )
+}
+
+/**
  * Wraps the addSamDebugConfiguration logic in a picker that lets the user choose to create
  * a code-type debug config, template-type debug config, or an api-type debug config using a selected template
  * TODO: Dedupe? Call out dupes at the quick pick level?
@@ -134,7 +214,8 @@ function makeAddCodeSamDebugCodeLens(
 export async function pickAddSamDebugConfiguration(
     codeConfig: AddSamDebugConfigurationInput,
     templateConfigs: AddSamDebugConfigurationInput[],
-    openWebview: boolean
+    openWebview: boolean,
+    continuationStep?: boolean
 ): Promise<void> {
     if (templateConfigs.length === 0) {
         await addSamDebugConfiguration(codeConfig, CODE_TARGET_TYPE, openWebview)
@@ -172,8 +253,8 @@ export async function pickAddSamDebugConfiguration(
                 'AWS.pickDebugConfig.prompt',
                 'Create a Debug Configuration from a CloudFormation Template'
             ),
-            step: 1,
-            totalSteps: 1,
+            step: continuationStep ? 2 : 1,
+            totalSteps: continuationStep ? 2 : 1,
         },
         items: [
             ...templateItems,
@@ -194,7 +275,10 @@ export async function pickAddSamDebugConfiguration(
         return undefined
     }
     if (val.label === noTemplate) {
-        await addSamDebugConfiguration(codeConfig, CODE_TARGET_TYPE, openWebview, { step: 2, totalSteps: 2 })
+        await addSamDebugConfiguration(codeConfig, CODE_TARGET_TYPE, openWebview, {
+            step: continuationStep ? 3 : 2,
+            totalSteps: continuationStep ? 3 : 2,
+        })
     } else {
         const templateItem = templateItemsMap.get(val.label)
         if (!templateItem) {
@@ -208,13 +292,27 @@ export async function pickAddSamDebugConfiguration(
     }
 }
 
-export async function makePythonCodeLensProvider(): Promise<vscode.CodeLensProvider> {
+export interface OverridableCodeLensProvider extends vscode.CodeLensProvider {
+    provideCodeLenses: (
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken,
+        forceProvide?: boolean
+    ) => Promise<vscode.CodeLens[]>
+}
+
+export async function makePythonCodeLensProvider(
+    configuration: SettingsConfiguration
+): Promise<OverridableCodeLensProvider> {
     return {
         // CodeLensProvider
         provideCodeLenses: async (
             document: vscode.TextDocument,
-            token: vscode.CancellationToken
+            token: vscode.CancellationToken,
+            forceProvide?: boolean
         ): Promise<vscode.CodeLens[]> => {
+            if (!forceProvide && !configuration.readSetting<boolean>(STATE_NAME_ENABLE_CODELENSES)) {
+                return []
+            }
             // Try to activate the Python Extension before requesting symbols from a python file
             await pythonDebug.activatePythonExtensionIfInstalled()
             if (token.isCancellationRequested) {
@@ -232,12 +330,18 @@ export async function makePythonCodeLensProvider(): Promise<vscode.CodeLensProvi
     }
 }
 
-export async function makeCSharpCodeLensProvider(): Promise<vscode.CodeLensProvider> {
+export async function makeCSharpCodeLensProvider(
+    configuration: SettingsConfiguration
+): Promise<OverridableCodeLensProvider> {
     return {
         provideCodeLenses: async (
             document: vscode.TextDocument,
-            token: vscode.CancellationToken
+            token: vscode.CancellationToken,
+            forceProvide?: boolean
         ): Promise<vscode.CodeLens[]> => {
+            if (!forceProvide && !configuration.readSetting<boolean>(STATE_NAME_ENABLE_CODELENSES)) {
+                return []
+            }
             const handlers: LambdaHandlerCandidate[] = await csharpCodelens.getLambdaHandlerCandidates(document)
             return makeCodeLenses({
                 document,
@@ -249,12 +353,16 @@ export async function makeCSharpCodeLensProvider(): Promise<vscode.CodeLensProvi
     }
 }
 
-export function makeTypescriptCodeLensProvider(): vscode.CodeLensProvider {
+export function makeTypescriptCodeLensProvider(configuration: SettingsConfiguration): OverridableCodeLensProvider {
     return {
         provideCodeLenses: async (
             document: vscode.TextDocument,
-            token: vscode.CancellationToken
+            token: vscode.CancellationToken,
+            forceProvide?: boolean
         ): Promise<vscode.CodeLens[]> => {
+            if (!forceProvide && !configuration.readSetting<boolean>(STATE_NAME_ENABLE_CODELENSES)) {
+                return []
+            }
             const handlers = await tsCodelens.getLambdaHandlerCandidates(document)
             return makeCodeLenses({
                 document,
@@ -266,12 +374,18 @@ export function makeTypescriptCodeLensProvider(): vscode.CodeLensProvider {
     }
 }
 
-export async function makeGoCodeLensProvider(): Promise<vscode.CodeLensProvider> {
+export async function makeGoCodeLensProvider(
+    configuration: SettingsConfiguration
+): Promise<OverridableCodeLensProvider> {
     return {
         provideCodeLenses: async (
             document: vscode.TextDocument,
-            token: vscode.CancellationToken
+            token: vscode.CancellationToken,
+            forceProvide?: boolean
         ): Promise<vscode.CodeLens[]> => {
+            if (!forceProvide && !configuration.readSetting<boolean>(STATE_NAME_ENABLE_CODELENSES)) {
+                return []
+            }
             const handlers = await goCodelens.getLambdaHandlerCandidates(document)
             return makeCodeLenses({
                 document,
@@ -283,12 +397,18 @@ export async function makeGoCodeLensProvider(): Promise<vscode.CodeLensProvider>
     }
 }
 
-export async function makeJavaCodeLensProvider(): Promise<vscode.CodeLensProvider> {
+export async function makeJavaCodeLensProvider(
+    configuration: SettingsConfiguration
+): Promise<OverridableCodeLensProvider> {
     return {
         provideCodeLenses: async (
             document: vscode.TextDocument,
-            token: vscode.CancellationToken
+            token: vscode.CancellationToken,
+            forceProvide?: boolean
         ): Promise<vscode.CodeLens[]> => {
+            if (!forceProvide && !configuration.readSetting<boolean>(STATE_NAME_ENABLE_CODELENSES)) {
+                return []
+            }
             // Try to activate the Java Extension before requesting symbols from a java file
             await javaDebug.activateJavaExtensionIfInstalled()
             if (token.isCancellationRequested) {
--- src/test/shared/codelens/codeLensUtils.test.ts
+++ src/test/shared/codelens/codeLensUtils.test.ts
@@ -16,18 +16,167 @@ import {
 } from '../../../shared/sam/debugger/awsSamDebugConfiguration'
 import * as AddSamDebugConfiguration from '../../../shared/sam/debugger/commands/addSamDebugConfiguration'
 
-describe('codeLensUtils', function () {
+describe('codeLensUtils', async function () {
+    let sandbox: sinon.SinonSandbox
+
+    beforeEach(function () {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(function () {
+        sandbox.restore()
+    })
+
+    describe('invokeCodeLensCommandPalette', async function () {
+        const range1: vscode.Range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 1))
+        const range2: vscode.Range = new vscode.Range(new vscode.Position(1, 0), new vscode.Position(1, 1))
+        const doc: Pick<vscode.TextDocument, 'getText'> = {
+            getText: (range: vscode.Range) => {
+                if (range.start.line === 0) {
+                    return 'one'
+                } else if (range.start.line === 1) {
+                    return 'two'
+                }
+
+                return 'other'
+            },
+        }
+
+        it('filters out launch configs leading to the invoker UI', async function () {
+            sandbox.stub(Picker, 'promptUser').resolves(undefined)
+            const spy = sandbox.spy(Picker, 'createQuickPick')
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [
+                {
+                    isResolved: true,
+                    range: range1,
+                    command: {
+                        command: 'foo',
+                        title: 'hasUI',
+                        arguments: ['foo', 'bar', true],
+                    },
+                },
+                {
+                    isResolved: true,
+                    range: range2,
+                    command: {
+                        command: 'foo',
+                        title: 'noUI',
+                        arguments: ['foo', 'bar', false],
+                    },
+                },
+            ])
+            assert.ok(spy.calledOnce, 'Spy called more than once')
+            assert.strictEqual(spy.args[0][0].items?.length, 1, 'createQuickPick called with multiple items')
+            assert.deepStrictEqual(
+                spy.args[0][0].items![0],
+                {
+                    label: 'two',
+                    detail: 'Function on line 2',
+                    lens: {
+                        command: {
+                            title: 'noUI',
+                            arguments: ['foo', 'bar', false],
+                            command: 'foo',
+                        },
+                        range: range2,
+                        isResolved: true,
+                    },
+                },
+                'createQuickPick called with incorrect item'
+            )
+        })
+
+        it('provides a single quick pick item with no codelens if no codelenses are provided', async function () {
+            sandbox.stub(Picker, 'promptUser').resolves(undefined)
+            const spy = sandbox.spy(Picker, 'createQuickPick')
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [])
+            assert.ok(spy.calledOnce, 'Spy called more than once')
+            assert.strictEqual(spy.args[0][0].items?.length, 1, 'createQuickPick called with multiple items')
+            assert.deepStrictEqual(
+                spy.args[0][0].items![0],
+                {
+                    label: 'No handlers found in current file',
+                    detail: 'Ensure your language extension is working',
+                    description: 'Click here to go back',
+                },
+                'createQuickPick called with incorrect item'
+            )
+        })
+
+        it('returns undefined if no value or an invalid value is chosen', async function () {
+            sandbox
+                .stub(Picker, 'promptUser')
+                .onFirstCall()
+                .resolves(undefined)
+                .onSecondCall()
+                .resolves([
+                    {
+                        label: 'noLens',
+                        detail: 'noLens',
+                    },
+                ])
+                .onThirdCall()
+                .resolves([
+                    {
+                        label: 'noCommand',
+                        detail: 'noCommand',
+                    },
+                ])
+            const createSpy = sandbox.spy(Picker, 'createQuickPick')
+            const finalSpy = sandbox.spy(codeLensUtils, 'pickAddSamDebugConfiguration')
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [
+                /* stub handles all pick logic */
+            ])
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [
+                /* stub handles all pick logic */
+            ])
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [
+                /* stub handles all pick logic */
+            ])
+
+            assert.ok(createSpy.calledThrice, 'Not all test payloads run')
+            assert.ok(finalSpy.notCalled, 'pickAddSamDebugConfiguration called; function did not return undefined')
+        })
+
+        it('can pass valid codelens contents to pickAddSamDebugConfiguration', async function () {
+            const arg0: AddSamDebugConfiguration.AddSamDebugConfigurationInput = {
+                resourceName: 'foo',
+                rootUri: vscode.Uri.parse('file:///asdf'),
+            }
+            const arg1: AddSamDebugConfiguration.AddSamDebugConfigurationInput[] = []
+            const arg2 = false
+            const target = {
+                label: 'two',
+                detail: 'Function on line 2',
+                lens: {
+                    command: {
+                        title: 'noUI',
+                        arguments: [arg0, arg1, arg2],
+                        command: 'foo',
+                    },
+                    range: range2,
+                    isResolved: true,
+                },
+            }
+            sandbox
+                .stub(Picker, 'promptUser')
+                .onFirstCall()
+                .resolves([target as any])
+            const finalStub = sandbox
+                .stub(codeLensUtils, 'pickAddSamDebugConfiguration')
+                .onFirstCall()
+                .resolves(undefined)
+
+            await codeLensUtils.invokeCodeLensCommandPalette(doc, [
+                /* stub handles all pick logic */
+            ])
+
+            assert.ok(finalStub.calledOnce, 'pickAddSamDebugConfiguration not called once and only once')
+            assert.ok(finalStub.calledWith(arg0, arg1, arg2), 'pickAddSamDebugConfiguration called with incorrect args')
+        })
+    })
+
     describe('pickAddSamDebugConfiguration', function () {
-        let sandbox: sinon.SinonSandbox
-
-        beforeEach(function () {
-            sandbox = sinon.createSandbox()
-        })
-
-        afterEach(function () {
-            sandbox.restore()
-        })
-
         it('should use CODE_TARGET_TYPE with no templateConfigs', function () {
             const addSamStub = sandbox.stub(AddSamDebugConfiguration, 'addSamDebugConfiguration')
             const codeConfig: AddSamDebugConfiguration.AddSamDebugConfigurationInput = {
