9cebe2d647180fbbf63983f2c4eece12208eb6eb
Austin Brooks <abrooksv@users.noreply.github.com>
2021-05-10 15:31:19 -0700

    Attempt to fix Jacoco server running past test life cycle (#2592)

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/jacoco/RemoteCoverage.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/jacoco/RemoteCoverage.kt
@@ -3,6 +3,8 @@
 
 package software.aws.toolkits.gradle.jacoco
 
+import org.gradle.BuildAdapter
+import org.gradle.BuildResult
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
@@ -15,6 +17,7 @@ import org.jacoco.core.data.ISessionInfoVisitor
 import org.jacoco.core.data.SessionInfo
 import org.jacoco.core.runtime.RemoteControlReader
 import org.jacoco.core.runtime.RemoteControlWriter
+import java.io.FileOutputStream
 import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.atomic.AtomicBoolean
@@ -30,6 +33,7 @@ class RemoteCoverage private constructor(task: Test) {
         task.extensions.findByType(JacocoTaskExtension::class.java)?.let {
             val execFile = it.destinationFile ?: return@let
 
+            // Use a shared service since it does not block task execution
             val jacocoServer = task.project.gradle.sharedServices.registerIfAbsent("jacocoServer", JacocoServer::class.java) {
                 if (!execFile.exists()) {
                     task.project.mkdir(execFile.parentFile)
@@ -41,6 +45,14 @@ class RemoteCoverage private constructor(task: Test) {
 
             task.doFirst {
                 jacocoServer.get().start()
+                task.project.gradle.addBuildListener(object : BuildAdapter() {
+                    override fun buildFinished(result: BuildResult) {
+                        task.project.gradle.removeListener(this)
+                        runCatching {
+                            jacocoServer.get().close()
+                        }
+                    }
+                })
             }
         } ?: task.logger.warn("$task does not have Jacoco enabled on it")
     }
@@ -51,37 +63,41 @@ class RemoteCoverage private constructor(task: Test) {
         }
 
         private val serverSocket = ServerSocket(DEFAULT_JACOCO_PORT)
-        private val signalShutdown = AtomicBoolean(false)
+        private val isRunning = AtomicBoolean(false)
 
-        private val outputStream = parameters.execFile.asFile.get().outputStream()
-        private val fileWriter = ExecutionDataWriter(outputStream)
-        private val serverThread = Thread {
-            while (!signalShutdown.get()) {
-                val clientSocket = serverSocket.accept()
-                JacocoHandler(clientSocket, fileWriter).start()
+        private val serverRunnable = Runnable {
+            parameters.execFile.asFile.get().outputStream().use {
+                while (isRunning.get()) {
+                    val clientSocket = serverSocket.accept()
+                    JacocoHandler(clientSocket, it).run()
+                }
+            }
+        }
+        private lateinit var serverThread: Thread
+
+        fun start() {
+            if (!isRunning.getAndSet(true)) {
+                serverThread = Thread(serverRunnable)
+                serverThread.start()
+            } else {
+                throw IllegalStateException("Jacoco server is already running!")
             }
         }
 
-        fun start() {
-            serverThread.start()
-        }
-
         override fun close() {
-            signalShutdown.set(true)
-            serverThread.interrupt()
-
-            outputStream.close()
+            if (isRunning.getAndSet(false)) {
+                serverThread.interrupt()
+            }
         }
     }
 
-    private class JacocoHandler(private val socket: Socket, private val fileWriter: ExecutionDataWriter) : Thread(), ISessionInfoVisitor,
-        IExecutionDataVisitor {
+    private class JacocoHandler(private val socket: Socket, private val outputFile: FileOutputStream) : ISessionInfoVisitor, IExecutionDataVisitor {
+        private val fileWriter = ExecutionDataWriter(outputFile)
 
-        override fun run() {
+        fun run() {
             socket.use {
                 socket.getInputStream().use { input ->
                     socket.getOutputStream().use { output ->
-
                         val reader = RemoteControlReader(input)
                         reader.setSessionInfoVisitor(this)
                         reader.setExecutionDataVisitor(this)
