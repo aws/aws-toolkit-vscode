6729f332c69937fa32213e005c29696cf6fc8e4c
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2021-05-21 09:49:33 -0700

    Add comment
    Co-authored-by: Justin M. Keyes <jmkeyes@amazon.com>

--- src/shared/extensions.ts
+++ src/shared/extensions.ts
@@ -17,6 +17,7 @@ export const VSCODE_EXTENSION_ID = {
     // python depends on jupyter plugin
     jupyter: 'ms-toolsai.jupyter',
     yaml: 'redhat.vscode-yaml',
+    // For redhat yaml extension.
     redhatCommon: 'redhat.vscode-commons',
     go: 'golang.go',
     java: 'redhat.java',
