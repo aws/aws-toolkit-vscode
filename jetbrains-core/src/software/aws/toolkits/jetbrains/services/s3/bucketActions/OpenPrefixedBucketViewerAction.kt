f257789cf8a84fcfd55e8cefd956fc9e9a73c5b1
Austin Brooks <abrooksv@users.noreply.github.com>
2021-06-03 13:15:05 -0700

    Fix missing text in S3 view bucket with prefix action (#2622)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketActions/OpenPrefixedBucketViewerAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketActions/OpenPrefixedBucketViewerAction.kt
@@ -6,20 +6,20 @@ package software.aws.toolkits.jetbrains.services.s3.bucketActions
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.ui.Messages
-import icons.AwsIcons
 import software.aws.toolkits.jetbrains.core.explorer.actions.SingleResourceNodeAction
 import software.aws.toolkits.jetbrains.services.s3.S3BucketNode
 import software.aws.toolkits.jetbrains.services.s3.openEditor
+import software.aws.toolkits.resources.message
 import software.aws.toolkits.telemetry.Result
 import software.aws.toolkits.telemetry.S3Telemetry
 
-class OpenPrefixedBucketViewerAction : SingleResourceNodeAction<S3BucketNode>("View Bucket with prefix..."), DumbAware {
+class OpenPrefixedBucketViewerAction : SingleResourceNodeAction<S3BucketNode>(), DumbAware {
     override fun actionPerformed(selected: S3BucketNode, e: AnActionEvent) {
         val prefix = Messages.showInputDialog(
             selected.nodeProject,
-            "dialog message",
-            "dialog title",
-            AwsIcons.Resources.S3_BUCKET
+            message("s3.open.viewer.prefix.message"),
+            message("s3.open.viewer.prefix.title"),
+            null
         )
 
         if (prefix == null) {
