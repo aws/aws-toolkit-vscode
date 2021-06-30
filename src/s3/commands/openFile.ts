692b2071645d3faf2d9d0a0dbcd3473d83056de4
daviddguez8 <dguezdavid8@gmail.com>
2021-06-30 16:34:34 -0600

    small fix

--- src/s3/commands/openFile.ts
+++ src/s3/commands/openFile.ts
@@ -5,14 +5,13 @@
 //import * as vscode from 'vscode'
 import { Window } from '../../shared/vscode/window'
 //import { localize } from '../../shared/utilities/vsCodeUtils'
-import { showErrorWithLogs } from '../../shared/utilities/messages'
+//import { showErrorWithLogs } from '../../shared/utilities/messages'
 import { S3FileNode } from '../explorer/s3FileNode'
 import { FileViewerManager, SingletonManager } from '../util/FileViewerManager'
 
 let manager: FileViewerManager
 
 export async function openFileCommand(node: S3FileNode, window = Window.vscode()): Promise<void> {
-    showErrorWithLogs('working', window)
     manager = SingletonManager.getInstance()
     manager.openTab(node)
 }
