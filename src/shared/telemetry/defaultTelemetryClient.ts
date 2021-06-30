aws/aws-toolkit-vscode

commit 9edae9a38962186652cf23a55edad3e4998ad939

refactor: improve ergonomics of AWSClientBuilder #1760
Problem:
`createAndConfigureServiceClient()` is awkward to use and hard to
understand because of indirection.

Solution:
- introduce `createAwsService()`
- document it
- remove `createAndConfigureServiceClient()`

src/shared/telemetry/defaultTelemetryClient.ts
@@ -88,8 +88,8 @@ export class DefaultTelemetryClient implements TelemetryClient {
 
         return new DefaultTelemetryClient(
             clientId,
-            await ext.sdkClientBuilder.createAndConfigureServiceClient(
-                opts => new Service(opts),
+            (await ext.sdkClientBuilder.createAwsService(
+                Service,
                 {
                     // @ts-ignore: apiConfig is internal and not in the TS declaration file
                     apiConfig: apiConfig,
@@ -98,9 +98,8 @@ export class DefaultTelemetryClient implements TelemetryClient {
                     correctClockSkew: true,
                     endpoint: DefaultTelemetryClient.DEFAULT_TELEMETRY_ENDPOINT,
                 },
-                undefined,
                 false
-            )
+            )) as ClientTelemetry
         )
     }
 }

src/test/shared/defaultAwsClientBuilder.test.ts
@@ -20,7 +20,7 @@ describe('DefaultAwsClientBuilder', function () {
 
         it('includes custom user-agent if no options are specified', async function () {
             const builder = new DefaultAWSClientBuilder(new FakeAwsContext())
-            const service = await builder.createAndConfigureServiceClient(opts => new FakeService(opts))
+            const service = await builder.createAwsService(FakeService)
 
             assert.strictEqual(!!service.config.customUserAgent, true)
             assert.strictEqual(
@@ -31,7 +31,7 @@ describe('DefaultAwsClientBuilder', function () {
 
         it('includes custom user-agent if not specified in options', async function () {
             const builder = new DefaultAWSClientBuilder(new FakeAwsContext())
-            const service = await builder.createAndConfigureServiceClient(opts => new FakeService(opts), {})
+            const service = await builder.createAwsService(FakeService, {})
 
             assert.strictEqual(!!service.config.customUserAgent, true)
             assert.notStrictEqual(service.config.customUserAgent, undefined)
@@ -39,7 +39,7 @@ describe('DefaultAwsClientBuilder', function () {
 
         it('does not override custom user-agent if specified in options', async function () {
             const builder = new DefaultAWSClientBuilder(new FakeAwsContext())
-            const service = await builder.createAndConfigureServiceClient(opts => new FakeService(opts), {
+            const service = await builder.createAwsService(FakeService, {
                 customUserAgent: 'CUSTOM USER AGENT',
             })
 

src/shared/awsClientBuilder.ts
@@ -9,11 +9,20 @@ import { AwsContext } from './awsContext'
 import { pluginVersion } from './extensionUtilities'
 
 export interface AWSClientBuilder {
-    createAndConfigureServiceClient<T>(
-        awsServiceFactory: (options: ServiceConfigurationOptions) => T,
-        awsServiceOpts?: ServiceConfigurationOptions,
+    /**
+     * Creates AWS service object of the given type, and sets options defaults.
+     *
+     * @param type  AWS service type
+     * @param options  AWS service configuration options
+     * @param region  AWS region override
+     * @param userAgent  Set the Toolkit user agent
+     * @returns
+     */
+    createAwsService<T extends AWS.Service>(
+        type: new (o: ServiceConfigurationOptions) => T,
+        options?: ServiceConfigurationOptions,
         region?: string,
-        addUserAgent?: boolean
+        userAgent?: boolean
     ): Promise<T>
 }
 
@@ -24,31 +33,29 @@ export class DefaultAWSClientBuilder implements AWSClientBuilder {
         this._awsContext = awsContext
     }
 
-    // centralized construction of transient AWS service clients, allowing us
-    // to customize requests and/or user agent
-    public async createAndConfigureServiceClient<T>(
-        awsServiceFactory: (options: ServiceConfigurationOptions) => T,
-        awsServiceOpts?: ServiceConfigurationOptions,
+    /** @inheritdoc */
+    public async createAwsService<T extends AWS.Service>(
+        type: new (o: ServiceConfigurationOptions) => T,
+        options?: ServiceConfigurationOptions,
         region?: string,
-        addUserAgent: boolean = true
+        userAgent: boolean = true
     ): Promise<T> {
-        if (!awsServiceOpts) {
-            awsServiceOpts = {}
+        const opt = { ...options } as ServiceConfigurationOptions
+
+        if (!opt.credentials) {
+            opt.credentials = await this._awsContext.getCredentials()
         }
 
-        if (!awsServiceOpts.credentials) {
-            awsServiceOpts.credentials = await this._awsContext.getCredentials()
+        if (!opt.region && region) {
+            opt.region = region
         }
 
-        if (!awsServiceOpts.region && region) {
-            awsServiceOpts.region = region
-        }
-
-        if (!awsServiceOpts.customUserAgent && addUserAgent) {
+        if (userAgent && !opt.customUserAgent) {
             const platformName = env.appName.replace(/\s/g, '-')
-            awsServiceOpts.customUserAgent = `AWS-Toolkit-For-VSCode/${pluginVersion} ${platformName}/${version}`
+            opt.customUserAgent = `AWS-Toolkit-For-VSCode/${pluginVersion} ${platformName}/${version}`
         }
 
-        return awsServiceFactory(awsServiceOpts)
+        const service = new type(opt)
+        return service
     }
 }

src/shared/clients/defaultApiGatewayClient.ts
@@ -78,10 +78,6 @@ export class DefaultApiGatewayClient implements ApiGatewayClient {
     }
 
     private async createSdkClient(): Promise<APIGateway> {
-        return await ext.sdkClientBuilder.createAndConfigureServiceClient(
-            options => new APIGateway(options),
-            undefined,
-            this.regionCode
-        )
+        return await ext.sdkClientBuilder.createAwsService(APIGateway, undefined, this.regionCode)
     }
 }

src/shared/clients/defaultCloudFormationClient.ts
@@ -52,10 +52,6 @@ export class DefaultCloudFormationClient implements CloudFormationClient {
     }
 
     private async createSdkClient(): Promise<CloudFormation> {
-        return await ext.sdkClientBuilder.createAndConfigureServiceClient(
-            options => new CloudFormation(options),
-            undefined,
-            this.regionCode
-        )
+        return await ext.sdkClientBuilder.createAwsService(CloudFormation, undefined, this.regionCode)
     }
 }

src/shared/clients/defaultCloudWatchLogsClient.ts
@@ -46,10 +46,6 @@ export class DefaultCloudWatchLogsClient implements CloudWatchLogsClient {
     }
 
     protected async createSdkClient(): Promise<CloudWatchLogs> {
-        return await ext.sdkClientBuilder.createAndConfigureServiceClient(
-            options => new CloudWatchLogs(options),
-            undefined,
-            this.regionCode
-        )
+        return await ext.sdkClientBuilder.createAwsService(CloudWatchLogs, undefined, this.regionCode)
     }
 }

src/shared/clients/defaultEcrClient.ts
@@ -70,10 +70,6 @@ export class DefaultEcrClient implements EcrClient {
     }
 
     protected async createSdkClient(): Promise<ECR> {
-        return await ext.sdkClientBuilder.createAndConfigureServiceClient(
-            options => new ECR(options),
-            undefined,
-            this.regionCode
-        )
+        return await ext.sdkClientBuilder.createAwsService(ECR, undefined, this.regionCode)
     }
 }

src/shared/clients/defaultEcsClient.ts
@@ -71,10 +71,6 @@ export class DefaultEcsClient implements EcsClient {
     }
 
     protected async createSdkClient(): Promise<ECS> {
-        return await ext.sdkClientBuilder.createAndConfigureServiceClient(
-            options => new ECS(options),
-            undefined,
-            this.regionCode
-        )
+        return await ext.sdkClientBuilder.createAwsService(ECS, undefined, this.regionCode)
     }
 }

commit 5fd459a29e7bc8f222b171b586a5e04dbf164c11

telemetry: do not include user agent
Problem:
9edae9a38962 accidentally removed an argument in the createAwsService()
call, but it was not caught because of `@ts-ignore`.

Solution:
Use a cast instead of `@ts-ignore`.

ref da236f18fd5f #1596

src/shared/telemetry/defaultTelemetryClient.ts
@@ -14,6 +14,7 @@ import { MetricDatum } from './clienttelemetry'
 import apiConfig = require('./service-2.json')
 import { TelemetryClient } from './telemetryClient'
 import { TelemetryFeedback } from './telemetryFeedback'
+import { ServiceConfigurationOptions } from 'aws-sdk/lib/service'
 
 export class DefaultTelemetryClient implements TelemetryClient {
     public static readonly DEFAULT_IDENTITY_POOL = 'us-east-1:820fd6d1-95c0-4ca4-bffb-3f01d32da842'
@@ -91,13 +92,14 @@ export class DefaultTelemetryClient implements TelemetryClient {
             (await ext.sdkClientBuilder.createAwsService(
                 Service,
                 {
-                    // @ts-ignore: apiConfig is internal and not in the TS declaration file
+                    // apiConfig is internal and not in the TS declaration file
                     apiConfig: apiConfig,
                     region: region,
                     credentials: credentials,
                     correctClockSkew: true,
                     endpoint: DefaultTelemetryClient.DEFAULT_TELEMETRY_ENDPOINT,
-                },
+                } as ServiceConfigurationOptions,
+                undefined,
                 false
             )) as ClientTelemetry
         )

src/test/shared/defaultAwsClientBuilder.test.ts
@@ -18,7 +18,7 @@ describe('DefaultAwsClientBuilder', function () {
             }
         }
 
-        it('includes custom user-agent if no options are specified', async function () {
+        it('includes Toolkit user-agent if no options are specified', async function () {
             const builder = new DefaultAWSClientBuilder(new FakeAwsContext())
             const service = await builder.createAwsService(FakeService)
 
@@ -27,14 +27,6 @@ describe('DefaultAwsClientBuilder', function () {
                 service.config.customUserAgent!.replace('---Insiders', ''),
                 `AWS-Toolkit-For-VSCode/testPluginVersion Visual-Studio-Code/${version}`
             )
-        })
-
-        it('includes custom user-agent if not specified in options', async function () {
-            const builder = new DefaultAWSClientBuilder(new FakeAwsContext())
-            const service = await builder.createAwsService(FakeService, {})
-
-            assert.strictEqual(!!service.config.customUserAgent, true)
-            assert.notStrictEqual(service.config.customUserAgent, undefined)
         })
 
         it('does not override custom user-agent if specified in options', async function () {