1d561d62f016efee31eeb9a7e38ba4e32c70a170
daviddguez8 <dguezdavid8@gmail.com>
2021-06-30 16:12:59 -0600

    Hover, icon access point

--- src/s3/commands/openFile.ts
+++ src/s3/commands/openFile.ts
@@ -1,21 +1,18 @@
 /* eslint-disable header/header */
 //TODOD:: what is the header?
+//TODOD:: change feature icon on package.json
 
 //import * as vscode from 'vscode'
 import { Window } from '../../shared/vscode/window'
 //import { localize } from '../../shared/utilities/vsCodeUtils'
 import { showErrorWithLogs, showOutputMessage } from '../../shared/utilities/messages'
 import { S3FileNode } from '../explorer/s3FileNode'
-import { FileViewerManager } from '../util/FileViewerManager'
+import { FileViewerManager, SingletonManager } from '../util/FileViewerManager'
 
-let manager: FileViewerManager | undefined
+let manager: FileViewerManager
 
 export async function openFileCommand(node: S3FileNode, window = Window.vscode()): Promise<void> {
     showErrorWithLogs('working', window)
-
-    if (!manager) {
-        manager = new FileViewerManager(window)
-    }
-
+    manager = SingletonManager.getInstance()
     manager.openTab(node)
 }
--- src/s3/util/FileViewerManager.ts
+++ src/s3/util/FileViewerManager.ts
@@ -13,14 +13,27 @@ export class FileViewerManager {
     private activeTabs: Set<S3Tab>
     private window: Window
 
-    public constructor(window: Window = Window.vscode()) {
+    public constructor(window: Window = Window.vscode(), outputChannel = ext.outputChannel) {
         this.cache = new Set<S3FileNode>()
         this.activeTabs = new Set<S3Tab>()
         this.window = window
-        showErrorWithLogs('initializing manager', window)
+        showOutputMessage('initializing manager', outputChannel)
     }
 
     public async openTab(fileNode: S3FileNode, outputChannel = ext.outputChannel): Promise<void> {
-        showOutputMessage(`manager initialized, file: ${fileNode}`, outputChannel)
+        showOutputMessage(`     manager initialized, file: ${fileNode.file.key}`, outputChannel)
+    }
+}
+
+export class SingletonManager {
+    static fileManager: FileViewerManager | undefined
+
+    private constructor() {}
+
+    public static getInstance(): FileViewerManager {
+        if (!SingletonManager.fileManager) {
+            SingletonManager.fileManager = new FileViewerManager()
+        }
+        return SingletonManager.fileManager
     }
 }
