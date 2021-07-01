3ab2334652926f3bd10e7283236f0bb876494973
daviddguez8 <dguezdavid8@gmail.com>
2021-07-01 15:15:32 -0600

    output channel initialized

--- src/s3/util/fileViewerManager.ts
+++ src/s3/util/fileViewerManager.ts
@@ -7,21 +7,26 @@ import { ext } from '../../shared/extensionGlobals'
 import { S3FileNode } from '../explorer/s3FileNode'
 import { Window } from '../../shared/vscode/window'
 import { showOutputMessage } from '../../shared/utilities/messages'
+import { OutputChannel } from 'vscode'
 export class FileViewerManager {
     //private cache: Set<S3FileNode>
     //private activeTabs: Set<S3Tab>
     //private window: Window
+    private outputChannel: OutputChannel
 
     public constructor(window: Window = Window.vscode(), outputChannel = ext.outputChannel) {
         //this.cache = new Set<S3FileNode>()
         //this.activeTabs = new Set<S3Tab>()
         //this.window = window
+        this.outputChannel = outputChannel
         showOutputMessage('initializing manager', outputChannel)
     }
 
-    public async openTab(fileNode: S3FileNode, outputChannel = ext.outputChannel): Promise<void> {
-        showOutputMessage(`     manager initialized, file: ${fileNode.file.key}`, outputChannel)
+    public async openTab(fileNode: S3FileNode): Promise<void> {
+        showOutputMessage(`     manager initialized, file: ${fileNode.file.key}`, this.outputChannel)
     }
+
+    public async getFile() {}
 }
 
 export class SingletonManager {
