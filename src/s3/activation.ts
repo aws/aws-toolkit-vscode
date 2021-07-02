90ab2f0e026ff50c10b24e24ea224a93fbbac754
Justin M. Keyes <jmkeyes@amazon.com>
2021-07-02 13:50:40 -0700

    S3: fix invalid AwsContext usage #1844
    AwsContext is a global that is initialized in extension.ts. Creating
    a new one won't give us the state we need from it.

--- src/s3/activation.ts
+++ src/s3/activation.ts
@@ -17,13 +17,13 @@ import { S3FolderNode } from './explorer/s3FolderNode'
 import { S3Node } from './explorer/s3Nodes'
 import { S3FileNode } from './explorer/s3FileNode'
 import { ext } from '../shared/extensionGlobals'
-import { DefaultAwsContext } from '../shared/defaultAwsContext'
+import { ExtContext } from '../shared/extensions'
 
 /**
  * Activates S3 components.
  */
-export async function activate(extensionContext: vscode.ExtensionContext): Promise<void> {
-    extensionContext.subscriptions.push(
+export async function activate(ctx: ExtContext): Promise<void> {
+    ctx.extensionContext.subscriptions.push(
         vscode.commands.registerCommand('aws.s3.copyPath', async (node: S3FolderNode | S3FileNode) => {
             await copyPathCommand(node)
         }),
@@ -32,7 +32,7 @@ export async function activate(extensionContext: vscode.ExtensionContext): Promi
         }),
         vscode.commands.registerCommand('aws.s3.uploadFile', async (node: S3BucketNode | S3FolderNode) => {
             if (!node) {
-                const awsContext = new DefaultAwsContext(ext.context)
+                const awsContext = ctx.awsContext
                 const regionCode = awsContext.getCredentialDefaultRegion()
                 const s3Client = ext.toolkitClientBuilder.createS3Client(regionCode)
                 const document = vscode.window.activeTextEditor?.document.uri
