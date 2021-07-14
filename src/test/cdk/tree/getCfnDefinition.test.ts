bf6d2f141e46e4c4c793729393f2b829ce632b7a
Kim <yonakim@909c4acf38a4.ant.amazon.com>
2021-07-13 18:57:06 -0700

    added unit tests for getCfnDefintion

--- src/test/cdk/tree/getCfnDefinition.test.ts
+++ src/test/cdk/tree/getCfnDefinition.test.ts
@@ -1,13 +1,10 @@
 import * as assert from 'assert'
 import * as getCfnDefinition from '../../../cdk/explorer/nodes/getCfnDefinition'
 
-
 const unescapedJsonString: string = `{"StartAt":"Submit Job","States":{"Submit Job":{"Next":"Wait X Seconds","Type":"Task","Resource":"SubmitJobFB773A16","ResultPath":"$.guid"},"Wait X Seconds":{"Type":"Wait","SecondsPath":"$.wait_time","Next":"Get Job Status"},"Get Job Status":{"Next":"Job Complete?","InputPath":"$.guid","Type":"Task","Resource":"CheckJob5FFC1D6F","ResultPath":"$.status"},"Job Complete?":{"Type":"Choice","Choices":[{"Variable":"$.status","StringEquals":"FAILED","Next":"Job Failed"},{"Variable":"$.status","StringEquals":"SUCCEEDED","Next":"Get Final Job Status"}],"Default":"Wait X Seconds"},"Job Failed":{"Type":"Fail","Error":"DescribeJob returned FAILED","Cause":"AWS Batch Job Failed"},"Get Final Job Status":{"End":true,"InputPath":"$.guid","Type":"Task","Resource":"CheckJob5FFC1D6F"}},"TimeoutSeconds":30}`
-
 const uniqueIdendifier = 'MyStateMachine'
 const cdkOutPath = __dirname.replace('/dist', '') + '/resources'
 const stackName = 'templateJsonTester'
-
 
 describe('GetCfnDefinition', function () {
     it('get the correct cfn definition for state machine with correct inputs', async function () {
