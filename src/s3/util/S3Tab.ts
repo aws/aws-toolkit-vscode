35dae4fb9263311a81124cd6c5d2b5af8cd94fff
daviddguez8 <dguezdavid8@gmail.com>
2021-07-19 09:53:46 -0700

    Tests for edit mode added

--- src/s3/util/S3Tab.ts
+++ src/s3/util/S3Tab.ts
@@ -6,7 +6,7 @@
 import * as vscode from 'vscode'
 //import { Bucket, DownloadFileRequest, File, S3Client } from '../../shared/clients/s3Client'
 
-//const fs = require('fs')
+//need to subscribe to ondidsave and upload to s3, might need the S3FileNode
 
 export class S3Tab {
     //private file: File
@@ -26,12 +26,6 @@ export class S3Tab {
     public constructor(private fileUri: vscode.Uri, window = vscode.window) {
         this.s3Uri = vscode.Uri.parse('s3:' + this.fileUri.fsPath)
         this.window = window
-
-        //if file is text, start state will be read-only
-
-        //if file is not text, open file on edit-mode with disabled edit button
-
-        //this.outputChannel = ext.outputChannel
     }
 
     public async openFileOnReadOnly(workspace = vscode.workspace): Promise<vscode.TextEditor | undefined> {
@@ -113,60 +107,3 @@ export class S3Tab {
     }
     //onPressedButton = change state, how to do this?
 }
-
-/*
-interface State {
-    openFile(context: Context, window: typeof vscode.window): void
-}
-
-
-class ReadOnlyState implements State {
-    private fileUri: vscode.Uri
-    private provider: S3DocumentProvider
-    private s3Uri: vscode.Uri
-
-    public constructor(fileUri: vscode.Uri) {
-        this.fileUri = fileUri
-        this.s3Uri = vscode.Uri.parse('s3:' + this.fileUri.fsPath)
-        this.provider = new S3DocumentProvider()
-    }
-
-    async openFile(context: Context, window: typeof vscode.window) {
-        context.state = this
-        //this.fileUri.scheme = 's3'
-        //window.showTextDocument(this.fileUri)
-        //testutil.toFile('bogus', tempFile.fsPath)
-        const doc = await vscode.workspace.openTextDocument(this.s3Uri) // calls back into the provider
-        //vscode.languages.setTextDocumentLanguage(doc, 'txt')
-        await vscode.window.showTextDocument(doc, { preview: false })
-    }
-}
-
-//TODOD:: implement all for this
-class EditModeState implements State {
-    private fileUri: vscode.Uri
-
-    public constructor(fileUri: vscode.Uri, context: Context) {
-        this.fileUri = fileUri
-    }
-
-    openFile() {}
-}
-
-class Context {
-    private fileUri: vscode.Uri
-    state?: State | undefined
-
-    public constructor(fileUri: vscode.Uri) {
-        //this.state = new ReadOnlyState(fileUri)
-        this.fileUri = fileUri
-    }
-    
-    public set state (newState: State){
-        this.state = newState
-    }
-
-    public get state () {
-        return this.state
-    }
-}*/
--- src/test/s3/util/s3Tab.test.ts
+++ src/test/s3/util/s3Tab.test.ts
@@ -12,40 +12,55 @@ import * as testutil from '../../testUtil'
 import { S3Tab } from '../../../s3/util/s3Tab'
 import { anything, instance, mock, when, capture } from '../../utilities/mockito'
 
-describe('S3Tab', async function () {
+describe.only('S3Tab', async function () {
     const message = 'can this be read'
     const fileName = 'test.txt'
+    const realWorkspace = vscode.workspace
 
+    let openedDoc: vscode.TextDocument
+    let openedS3Doc: vscode.TextDocument
     let tempFolder: string
     let fileUri: vscode.Uri
     let s3Uri: vscode.Uri
     let s3Tab: S3Tab
-    let workspace: typeof vscode.workspace
-    let window: typeof vscode.window
+    let mockedWorkspace: typeof vscode.workspace
+    let mockedWindow: typeof vscode.window
 
     before(async function () {
-        workspace = mock()
-        window = mock()
         tempFolder = await makeTemporaryToolkitFolder()
         fileUri = vscode.Uri.file(path.join(tempFolder, fileName))
         s3Uri = vscode.Uri.parse('s3:' + fileUri.fsPath)
         testutil.toFile(message, fileUri.fsPath)
+
+        openedDoc = await vscode.workspace.openTextDocument(fileUri)
+
+        mockedWorkspace = mock()
+        mockedWindow = mock()
         listTempFolder(tempFolder)
-        s3Tab = new S3Tab(fileUri, instance(window))
+        s3Tab = new S3Tab(fileUri, instance(mockedWindow))
     })
 
     it('can be opened in read-only mode', async function () {
-        when(workspace.openTextDocument(anything())).thenReturn()
-        when(window.showTextDocument(anything())).thenReturn()
+        when(mockedWorkspace.openTextDocument(anything())).thenReturn(Promise.resolve(openedS3Doc))
+        when(mockedWindow.showTextDocument(anything())).thenReturn(Promise.resolve({ document: openedS3Doc } as any))
 
-        await s3Tab.openFileOnReadOnly(instance(workspace))
+        await s3Tab.openFileOnReadOnly(instance(mockedWorkspace))
 
-        const [uri] = capture(workspace.openTextDocument).last()
+        const [uri] = capture(mockedWorkspace.openTextDocument).last()
         assert.strictEqual((uri as vscode.Uri).fsPath, s3Uri.fsPath)
         assert.strictEqual((uri as vscode.Uri).scheme, s3Uri.scheme)
     })
 
-    it('can be opened in edit mode', function () {})
+    it('can be opened in edit mode', async function () {
+        when(mockedWorkspace.openTextDocument(anything())).thenReturn(Promise.resolve(openedDoc))
+        when(mockedWindow.showTextDocument(anything())).thenReturn({ document: openedS3Doc } as any)
+
+        await s3Tab.openFileOnEditMode(instance(mockedWorkspace))
+
+        const [uri] = capture(mockedWorkspace.openTextDocument).last()
+        assert.strictEqual((uri as vscode.Uri).fsPath, fileUri.fsPath)
+        assert.strictEqual((uri as vscode.Uri).scheme, fileUri.scheme)
+    })
 
     it('saves changes back to s3', function () {})
 })
