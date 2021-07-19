ece6ee3840eaacec3624a296e0c7b9b0fd86fd93
daviddguez8 <dguezdavid8@gmail.com>
2021-07-19 11:39:46 -0700

    merged logic of opening file into one function

--- src/s3/util/s3Tab.ts
+++ src/s3/util/s3Tab.ts
@@ -15,7 +15,6 @@ export class S3Tab {
     private editor: vscode.TextEditor | undefined
 
     //private editing: boolean
-
     public constructor(private fileUri: vscode.Uri, window = vscode.window) {
         this.s3Uri = vscode.Uri.parse('s3:' + this.fileUri.fsPath)
         this.window = window
@@ -23,74 +22,45 @@ export class S3Tab {
 
     public async openFileOnReadOnly(workspace = vscode.workspace): Promise<vscode.TextEditor | undefined> {
         //find if there is any active editor for this uri
-        const openEditor = await this.getActiveEditor()
-
-        if (!openEditor) {
-            //there isn't any tab open for this uri, simply open it in read-only with the s3Uri
-            const doc = await workspace.openTextDocument(this.s3Uri)
-            this.editor = await this.window.showTextDocument(doc, { preview: false })
-
-            return this.editor
-        } else if (openEditor.document.uri.scheme === 's3') {
-            //there is one already in read-only, just shift focus to it by reopening it with the ViewColumn option
-            await this.window.showTextDocument(openEditor.document, {
-                preview: false,
-                viewColumn: openEditor.viewColumn,
-            })
-            this.editor = openEditor
-
-            return this.editor
-        } else if (openEditor.document.uri.scheme === 'file') {
-            //there is a tab open in edit-mode, it needs to be focused, then closed
-            await this.window.showTextDocument(openEditor.document, {
-                preview: false,
-                viewColumn: openEditor.viewColumn,
-            })
-            await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
-            //good to open in read-only
-            const doc = await workspace.openTextDocument(this.s3Uri)
-            this.editor = await this.window.showTextDocument(doc, { preview: false })
-
-            return this.editor
-        }
-
-        return undefined
+        return this.openFile(this.s3Uri, workspace)
     }
 
     public async openFileOnEditMode(workspace = vscode.workspace): Promise<vscode.TextEditor | undefined> {
         //find if there is any active editor for this uri
+        return this.openFile(this.fileUri, workspace)
+    }
+
+    public async openFile(uri: vscode.Uri, workspace = vscode.workspace): Promise<vscode.TextEditor | undefined> {
+        //find if there is any active editor for this uri
         const openEditor = await this.getActiveEditor()
-
-        if (!openEditor) {
-            //there wasn't any open, just display it regularly
-            const doc = await workspace.openTextDocument(this.fileUri)
-            this.editor = await this.window.showTextDocument(doc, { preview: false })
-
-            return this.editor
-        } else if (openEditor.document.uri.scheme === 'file') {
-            //there is a tab for this uri open in edit-mode, just shift focus to it by reopening it with the ViewColumn option
-            await this.window.showTextDocument(openEditor.document, {
-                preview: false,
-                viewColumn: openEditor.viewColumn,
-            })
-            this.editor = openEditor
-
-            return this.editor
-        } else if (openEditor.document.uri.scheme === 's3') {
-            // there is a read-only tab open, it needs to be focused, then closed
-            await this.window.showTextDocument(openEditor.document, {
-                preview: false,
-                viewColumn: openEditor.viewColumn,
-            })
-            await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
-            //good to open in edit-mode
-            const doc = await workspace.openTextDocument(this.fileUri)
-            this.editor = await this.window.showTextDocument(doc, { preview: false })
-
-            return this.editor
+        try {
+            const doc = await workspace.openTextDocument(uri)
+            if (!openEditor) {
+                //there wasn't any open, just display it regularly
+                this.editor = await this.window.showTextDocument(doc, { preview: false })
+            } else if (openEditor.document.uri.scheme === uri.scheme) {
+                //there is a tab for this uri scheme open, just shift focus to it by reopening it with the ViewColumn option
+                await this.window.showTextDocument(openEditor.document, {
+                    preview: false,
+                    viewColumn: openEditor.viewColumn,
+                })
+                this.editor = openEditor
+            } else if (openEditor.document.uri.scheme !== uri.scheme) {
+                // there is a read-only tab open, it needs to be focused, then closed
+                await this.window.showTextDocument(openEditor.document, {
+                    preview: false,
+                    viewColumn: openEditor.viewColumn,
+                })
+                await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+                //good to open in given mode
+                this.editor = await this.window.showTextDocument(doc, { preview: false })
+            }
+        } catch (e) {
+            this.window.showErrorMessage('Error opening file ', e)
+            this.editor = undefined
         }
 
-        return undefined
+        return this.editor
     }
 
     //will be deleted when handling usage of this.editor, need to check when tab closes to set it undefined
