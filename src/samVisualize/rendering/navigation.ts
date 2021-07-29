4f523f726220a1f7529fe7c594fc1eec44a3e9bb
Jared Bentvelsen <bentvels@amazon.com>
2021-07-29 09:06:20 -0700

    Refactor and add error condition

--- src/samVisualize/rendering/navigation.ts
+++ src/samVisualize/rendering/navigation.ts
@@ -15,20 +15,31 @@ export type ResourceLineMap = {
  * @param textDocument A TextDocument containing a CFN template from which we want to extract a ResourceLineMap
  * @returns A ResourceLineMap containing all the resources in the given template
  */
-export function generateResourceLineMap(cfnTemplate: vscode.TextDocument): ResourceLineMap {
+export function generateResourceLineMap(cfnTemplate: vscode.TextDocument): ResourceLineMap | undefined {
     const lineMap: ResourceLineMap = {}
     const contents = yaml.parseDocument(cfnTemplate.getText()).contents
 
     // This check is necessary because yaml.parseDocument could return a Scalar or just null
+    // However, it will always return a YAMLMap for a YAML template that follows Template Anatomy
+    // See https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-anatomy.html
+    let resources
     if (contents && 'items' in contents) {
         for (const pair of contents.items) {
             if (pair.key.value === 'Resources') {
-                for (const resource of pair.value.items) {
-                    const line = cfnTemplate.positionAt(resource.key.range[0]).line
-                    lineMap[resource.key.value] = line
-                }
+                // All keys under the 'Resources' key
+                resources = pair.value.items
             }
         }
+    }
+    // If the template does not follow Template Anatomy, and we cannot find 'Resources'
+    if (!resources) {
+        return undefined
+    }
+    for (const resource of resources) {
+        // resource.key.range[0] because we want the START position of the key (the resource name)
+        const line = cfnTemplate.positionAt(resource.key.range[0]).line
+        // resource.key.value is the resource name
+        lineMap[resource.key.value] = line
     }
     return lineMap
 }
