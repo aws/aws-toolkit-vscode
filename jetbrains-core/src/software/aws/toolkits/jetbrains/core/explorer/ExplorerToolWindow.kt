6cdda174e5670b32c737e9e7ba289fabe7579aed
Austin Brooks <abrooksv@users.noreply.github.com>
2021-07-30 09:10:05 -0700

    Set target component on Explorer's ToolBar (#2741)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ExplorerToolWindow.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ExplorerToolWindow.kt
@@ -86,6 +86,7 @@ class ExplorerToolWindow(project: Project) : SimpleToolWindowPanel(true, true), 
 
         toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.TOOLBAR, group, true).apply {
             layoutPolicy = WRAP_LAYOUT_POLICY
+            setTargetComponent(this@ExplorerToolWindow)
         }.component
 
         background = UIUtil.getTreeBackground()
