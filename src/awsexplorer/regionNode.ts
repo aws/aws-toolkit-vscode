73a43c745d1b536b33439d25616f7c7c774911e0
Garret Sweetwood <gsweet@amazon.com>
2021-08-06 15:50:57 -0700

    RegionNode: clear children on refresh (#1921)

--- src/awsexplorer/regionNode.ts
+++ src/awsexplorer/regionNode.ts
@@ -19,6 +19,7 @@ import { AWSTreeNodeBase } from '../shared/treeview/nodes/awsTreeNodeBase'
 import { StepFunctionsNode } from '../stepFunctions/explorer/stepFunctionsNodes'
 import { DEFAULT_PARTITION } from '../shared/regions/regionUtilities'
 import { SsmDocumentNode } from '../ssmDocument/explorer/ssmDocumentNode'
+import { LoadMoreNode } from '../shared/treeview/nodes/loadMoreNode'
 
 /**
  * An AWS Explorer node representing a region.
@@ -71,7 +72,16 @@ export class RegionNode extends AWSTreeNodeBase {
         }
     }
 
+    private tryClearChildren(): void {
+        this.childNodes.forEach(cn => {
+            if ('clearChildren' in cn) {
+                ;(cn as AWSTreeNodeBase & LoadMoreNode).clearChildren()
+            }
+        })
+    }
+
     public async getChildren(): Promise<AWSTreeNodeBase[]> {
+        this.tryClearChildren()
         return this.childNodes
     }
 
