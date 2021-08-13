2f5bda2550611155783f98fbdf0a6dafbe51d4ef
Justin M. Keyes <jmkeyes@amazon.com>
2021-08-12 19:15:53 -0700

    fixup! UI: message consistency, brevity, usability

--- src/awsexplorer/defaultRegion.ts
+++ src/awsexplorer/defaultRegion.ts
@@ -12,6 +12,7 @@ import { extensionSettingsPrefix } from '../shared/constants'
 import * as localizedText from '../shared/localizedText'
 import { createQuickPick, promptUser } from '../shared/ui/picker'
 import { AwsExplorer } from './awsExplorer'
+import { getIdeProperties } from '../shared/extensionUtilities'
 
 /**
  * The actions that can be taken when we discover that a profile's default region is not
@@ -95,9 +96,10 @@ export async function checkExplorerForDefaultRegion(
             ignoreFocusOut: true,
             title: localize(
                 'AWS.message.prompt.defaultRegionHidden',
-                'Default region "{0}" of profile "{1}" is currently hidden. Show it in the Explorer?',
+                'Default region "{0}" of profile "{1}" is currently hidden. Show it in {2} Explorer?',
                 profileRegion,
-                profileName
+                profileName,
+                getIdeProperties().company
             ),
         },
         items: items,
