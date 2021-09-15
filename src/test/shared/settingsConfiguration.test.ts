af15607783690ac1a5fae7fdcd1cf1bc1d2cee67
Garret Sweetwood <gsweet@amazon.com>
2021-09-15 12:22:16 -0700

    settings: combine "Do not show" flags into one setting #1937
    new versions of VSCode allows for a multi checkbox list in settings.

--- src/test/shared/settingsConfiguration.test.ts
+++ src/test/shared/settingsConfiguration.test.ts
@@ -13,12 +13,15 @@ describe('DefaultSettingsConfiguration', function () {
     // you attempt to update one that isn't defined in package.json. We will restore the setting value
     // at the end of the tests.
     const SETTING_KEY = 'telemetry'
+    const PROMPT_SETTING_KEY = 'suppressPrompts'
     let originalSettingValue: any
+    let originalPromptSettingValue: any
 
     let sut: DefaultSettingsConfiguration
 
     before(async function () {
         originalSettingValue = vscode.workspace.getConfiguration(extensionSettingsPrefix).get(SETTING_KEY)
+        originalPromptSettingValue = vscode.workspace.getConfiguration(extensionSettingsPrefix).get(PROMPT_SETTING_KEY)
     })
 
     beforeEach(async function () {
@@ -29,6 +32,9 @@ describe('DefaultSettingsConfiguration', function () {
         await vscode.workspace
             .getConfiguration(extensionSettingsPrefix)
             .update(SETTING_KEY, originalSettingValue, vscode.ConfigurationTarget.Global)
+        await vscode.workspace
+            .getConfiguration(extensionSettingsPrefix)
+            .update(PROMPT_SETTING_KEY, originalPromptSettingValue, vscode.ConfigurationTarget.Global)
     })
 
     const scenarios = [
@@ -72,6 +78,83 @@ describe('DefaultSettingsConfiguration', function () {
                 const savedValue = vscode.workspace.getConfiguration(extensionSettingsPrefix).get(SETTING_KEY)
 
                 assert.deepStrictEqual(savedValue, scenario.testValue)
+            })
+        })
+    })
+
+    describe('disablePrompt', async function () {
+        let defaultSetting: any
+        const promptName = 'promptName'
+        before(async function () {
+            // This gets the default settings
+            await sut.writeSetting(PROMPT_SETTING_KEY, {}, vscode.ConfigurationTarget.Global)
+            defaultSetting = sut.readSetting(PROMPT_SETTING_KEY)
+        })
+
+        const scenarios = [
+            {
+                testValue: { promptName: true, other: false },
+                expected: { promptName: true, other: false },
+                desc: 'stays suppressed',
+            },
+            { testValue: { promptName: false }, expected: { promptName: true }, desc: 'suppresses prompt' },
+            { testValue: undefined, expected: undefined, desc: 'writes nothing if undefined' },
+        ]
+        scenarios.forEach(scenario => {
+            it(scenario.desc, async () => {
+                await sut.writeSetting(PROMPT_SETTING_KEY, scenario.testValue, vscode.ConfigurationTarget.Global)
+                await sut.disablePrompt(promptName)
+                assert.deepStrictEqual(sut.readSetting(PROMPT_SETTING_KEY), { ...defaultSetting, ...scenario.expected })
+            })
+        })
+    })
+
+    describe('getSuppressPromptSetting & shouldDisplayPrompt', async function () {
+        let defaultSetting: any
+        const promptName = 'promptName'
+
+        before(async function () {
+            await sut.writeSetting(PROMPT_SETTING_KEY, {}, vscode.ConfigurationTarget.Global)
+            defaultSetting = sut.readSetting(PROMPT_SETTING_KEY)
+        })
+
+        const scenarios = [
+            {
+                testValue: { promptName: false },
+                expected: true,
+                promptAfter: { promptName: false },
+                desc: 'true when not suppressed',
+            },
+            {
+                testValue: { promptName: true },
+                expected: false,
+                promptAfter: { promptName: true },
+                desc: 'false when suppressed',
+            },
+            {
+                testValue: { wrongName: false },
+                expected: true,
+                promptAfter: { wrongName: false },
+                desc: 'true when not found',
+            },
+            {
+                testValue: { promptName: 7 },
+                expected: true,
+                promptAfter: { promptName: false },
+                desc: 'true when prompt has wrong type',
+            },
+            { testValue: 'badType', expected: true, promptAfter: {}, desc: 'reset setting if wrong type' },
+        ]
+
+        scenarios.forEach(scenario => {
+            it(scenario.desc, async () => {
+                await sut.writeSetting(PROMPT_SETTING_KEY, scenario.testValue, vscode.ConfigurationTarget.Global)
+                const result = await sut.shouldDisplayPrompt(promptName)
+                assert.deepStrictEqual(result, scenario.expected)
+                assert.deepStrictEqual(sut.readSetting(PROMPT_SETTING_KEY), {
+                    ...defaultSetting,
+                    ...scenario.promptAfter,
+                })
             })
         })
     })
