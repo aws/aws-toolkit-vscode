dfa536ff94eae40a5caf8086d7f78e0a76cab177
Justin M. Keyes <jmkeyes@amazon.com>
2021-09-15 14:30:39 -0700

    settings: validate suppressPrompts calls, rename
    - Validate the promptName by inspecting package.json.
    - Rename subitems to "apprunnerNotifyPricing", "yamlExtPrompt". Don't
      need "suppress" in the names, it is redundant and looks strange in the
      UI.
    
    followup to #1937

--- src/shared/settingsConfiguration.ts
+++ src/shared/settingsConfiguration.ts
@@ -5,6 +5,7 @@
 
 import * as vscode from 'vscode'
 import { getLogger } from './logger'
+import * as packageJson from '../../package.json'
 
 /**
  * Wraps the VSCode configuration API and provides Toolkit-related
@@ -61,6 +62,8 @@ export class DefaultSettingsConfiguration implements SettingsConfiguration {
      * @param promptName Name of prompt to suppress
      */
     public async disablePrompt(promptName: string): Promise<void> {
+        this.validatePromptSetting(promptName)
+
         const setting = await this.getSuppressPromptSetting(promptName)
         if (setting === undefined || setting[promptName]) {
             return
@@ -68,12 +71,15 @@ export class DefaultSettingsConfiguration implements SettingsConfiguration {
         setting[promptName] = true
         await this.writeSetting('suppressPrompts', setting, vscode.ConfigurationTarget.Global)
     }
+
     /**
-     * Verifies if a prompt should be displayed again.
-     * @param promptName Name of the prompt
+     * Returns true if a prompt is enabled, else false.
+     * @param promptName Prompt id
      * @returns False when prompt has been suppressed
      */
-    public async shouldDisplayPrompt(promptName: string): Promise<boolean> {
+    public async isPromptEnabled(promptName: string): Promise<boolean> {
+        this.validatePromptSetting(promptName)
+
         const promptSetting = await this.getSuppressPromptSetting(promptName)
         if (promptSetting !== undefined && promptSetting[promptName]) {
             return false
@@ -82,12 +88,15 @@ export class DefaultSettingsConfiguration implements SettingsConfiguration {
     }
 
     /**
-     * Gets the 'aws.suppressPrompts' setting. This will reset the setting to default if it does not
-     * recieve an object or the prompt's value is not a boolean.
+     * Gets an 'aws.suppressPrompts' for prompt `promptName`. Resets to a
+     * default value if the user setting has an invalid type.
+     *
      * @param promptName
-     * @returns The settings object
+     * @returns settings object
      */
     public async getSuppressPromptSetting(promptName: string): Promise<{ [prompt: string]: boolean } | undefined> {
+        this.validatePromptSetting(promptName)
+
         try {
             const setting = this.readSetting<{ [prompt: string]: boolean }>('suppressPrompts')
             if (setting === undefined) {
@@ -118,6 +127,16 @@ export class DefaultSettingsConfiguration implements SettingsConfiguration {
             return setting
         } catch (e) {
             getLogger().error('Failed to get the setting: suppressPrompts', e)
+        }
+    }
+
+    /**
+     * Throws an error if `name` is not a valid 'aws.suppressPrompts' setting.
+     */
+    private validatePromptSetting(name: string): void {
+        const m = packageJson.contributes.configuration.properties['aws.suppressPrompts'].properties
+        if (!(m as any)[name]) {
+            throw Error(`config: unknown aws.suppressPrompts item: "${name}"`)
         }
     }
 }
