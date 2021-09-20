b1e044725dbd87758467cb31b3949572350d5307
JadenSimon <sijaden@amazon.com>
2021-09-20 09:32:04 -0700

    Makeover

--- src/lambda/vue/samInvokeBackend.ts
+++ src/lambda/vue/samInvokeBackend.ts
@@ -46,6 +46,7 @@ export function registerSamInvokeVueCommand(context: ExtContext): vscode.Disposa
                 id: 'createLambda',
                 name: localize('AWS.command.launchConfigForm.title', 'SAM Debug Configuration Editor'),
                 webviewJs: 'samInvokeVue.js',
+                cssFiles: ['base.css'],
                 onDidReceiveMessageFunction: async (message, postMessageFn, destroyWebviewFn) =>
                     handleFrontendToBackendMessage(message, postMessageFn, destroyWebviewFn, context),
                 context: context.extensionContext,
