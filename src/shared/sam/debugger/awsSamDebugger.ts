44ce7cad4179635932a369090004feaab673f62e
JadenSimon <sijaden@amazon.com>
2021-09-28 13:19:34 -0700

    Remove import

--- src/shared/sam/debugger/awsSamDebugger.ts
+++ src/shared/sam/debugger/awsSamDebugger.ts
@@ -55,7 +55,6 @@ import { Credentials } from '@aws-sdk/types'
 import { CloudFormation } from '../../cloudformation/cloudformation'
 import { getSamCliVersion } from '../cli/samCliContext'
 import { ext } from '../../extensionGlobals'
-import * as pathutils from '../../utilities/pathUtils'
 import {
     MINIMUM_SAM_CLI_VERSION_INCLUSIVE_FOR_IMAGE_SUPPORT,
     MINIMUM_SAM_CLI_VERSION_INCLUSIVE_FOR_GO_SUPPORT,
