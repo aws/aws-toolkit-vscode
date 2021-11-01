2d0e7df50ef1738227695606209c2ae01ad8c33d
Kyle Thomson <kylthoms@amazon.com>
2021-11-01 15:53:13 -0700

    Porting CorrectThreadCredentialProvider to use the util method (#2902)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/CorrectThreadCredentialsProvider.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/CorrectThreadCredentialsProvider.kt
@@ -3,27 +3,16 @@
 
 package software.aws.toolkits.jetbrains.core.credentials
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.util.ThrowableComputable
 import software.amazon.awssdk.auth.credentials.AwsCredentials
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.aws.toolkits.jetbrains.utils.runUnderProgressIfNeeded
 import software.aws.toolkits.resources.message
 
 /**
  * Offloads fetching credentials to a background task and a modal progress bar if the current thread is EDT
  */
 class CorrectThreadCredentialsProvider(private val delegate: AwsCredentialsProvider) : AwsCredentialsProvider {
-    override fun resolveCredentials(): AwsCredentials = if (ApplicationManager.getApplication().isDispatchThread) {
-        ProgressManager.getInstance().runProcessWithProgressSynchronously(
-            ThrowableComputable<AwsCredentials, Exception> {
-                delegate.resolveCredentials()
-            },
-            message("credentials.retrieving"),
-            false,
-            null
-        )
-    } else {
+    override fun resolveCredentials(): AwsCredentials = runUnderProgressIfNeeded(null, message("credentials.retrieving"), cancelable = false) {
         delegate.resolveCredentials()
     }
 }
