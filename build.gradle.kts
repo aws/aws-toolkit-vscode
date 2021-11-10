8400f3ae074bd3c605683d270a13fcdf37ef5ade
Austin Brooks <abrooksv@users.noreply.github.com>
2021-11-10 14:23:20 -0800

    Tell IntelliJ to auto-gen code after Gradle sync (#2957)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import org.jetbrains.gradle.ext.ProjectSettings
+import org.jetbrains.gradle.ext.TaskTriggersConfig
 import software.aws.toolkits.gradle.changelog.tasks.GenerateGithubChangeLog
 
 plugins {
     id("base")
     id("toolkit-changelog")
     id("toolkit-jacoco-report")
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.1"
 }
 
 val codeArtifactUrl: Provider<String> = providers.environmentVariable("CODEARTIFACT_URL").forUseAtConfigurationTime()
@@ -47,3 +50,20 @@ tasks.register("runIde") {
         throw GradleException("Use project specific runIde command, i.e. :jetbrains-core:runIde, :intellij:runIde")
     }
 }
+
+if (idea.project != null) { // may be null during script compilation
+    idea {
+        project {
+            settings {
+                taskTriggers {
+                    afterSync(":sdk-codegen:generateSdks")
+                    afterSync(":jetbrains-core:generateTelemetry")
+                }
+            }
+        }
+    }
+}
+
+fun org.gradle.plugins.ide.idea.model.IdeaProject.settings(configuration: ProjectSettings.() -> Unit) = (this as ExtensionAware).configure(configuration)
+fun ProjectSettings.taskTriggers(action: TaskTriggersConfig.() -> Unit, ) = (this as ExtensionAware).extensions.configure("taskTriggers", action)
+
