0f6b64d5ddfa348358fda46adcb13deeee351613
gsweetwood <garretsweetwood6@gmail.com>
2021-11-17 21:11:16 -0800

    ecs exec: remove commas and ansi from output
    Problem: Output from command execution contained unexpected commas.
    Solution: Remove the separator from the 'join' calls in the ChildProcess
    class.  Remove ansi from output text.

--- src/ecs/commands/runCommandInContainer.ts
+++ src/ecs/commands/runCommandInContainer.ts
@@ -17,6 +17,7 @@ import { extensionSettingsPrefix } from '../../shared/constants'
 import { showOutputMessage, showViewLogsMessage } from '../../shared/utilities/messages'
 import { ext } from '../../shared/extensionGlobals'
 import { getOrInstallCli } from '../../shared/utilities/cliUtils'
+import { removeAnsi } from '../../shared/utilities/textUtilities'
 
 export async function runCommandInContainer(
     node: EcsContainerNode,
@@ -131,11 +132,11 @@ export async function runCommandInContainer(
         const cp = await new ChildProcess(ssmPlugin, args).run()
         if (cp.exitCode !== 0) {
             result = 'Failed'
-            showOutputMessage(cp.stderr, outputChannel)
+            showOutputMessage(removeAnsi(cp.stderr), outputChannel)
             throw cp.error
         } else {
             result = 'Succeeded'
-            showOutputMessage(cp.stdout, outputChannel)
+            showOutputMessage(removeAnsi(cp.stdout), outputChannel)
         }
     } catch (error) {
         getLogger().error('Failed to execute command in container, %O', error)
