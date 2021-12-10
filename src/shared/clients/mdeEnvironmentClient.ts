cd6c352698e71c06d041b4a8318a87d2b64ce5e7
JadenSimon <sijaden@amazon.com>
2021-12-10 10:51:18 -0800

    Use token

--- src/shared/clients/mdeEnvironmentClient.ts
+++ src/shared/clients/mdeEnvironmentClient.ts
@@ -4,9 +4,10 @@
  */
 
 import { ClassToInterfaceType } from '../utilities/tsUtils'
-import got from 'got'
+import got, { Got } from 'got'
 
 const ENVIRONMENT_ARN_KEY = '__ENVIRONMENT_ARN'
+const ENVIRONMENT_AUTH_TOKEN = '__MDE_ENV_API_AUTHORIZATION_TOKEN'
 export const MDE_ENVIRONMENT_ENDPOINT = 'http://127.0.0.1:1339'
 
 export type MdeEnvironmentClient = ClassToInterfaceType<DefaultMdeEnvironmentClient>
@@ -17,26 +18,33 @@ export class DefaultMdeEnvironmentClient {
         return process.env[ENVIRONMENT_ARN_KEY]
     }
 
-    // Start an action
-    public async startDevfile(request: StartDevfileRequest): Promise<any> {
-        await got.post(`${this.endpoint}/start`, {
-            json: request,
+    private get authToken(): string | undefined {
+        return process.env[ENVIRONMENT_AUTH_TOKEN]
+    }
+
+    private getGot(): Got {
+        return got.extend({
+            prefixUrl: this.endpoint,
             responseType: 'json',
+            // `Authorization` _should_ have two parameters (RFC 7235), MDE should probably fix that
+            headers: { Authorization: this.authToken },
         })
+    }
+
+    // Start an action
+    public async startDevfile(request: StartDevfileRequest): Promise<void> {
+        await this.getGot().post('start', { json: request })
     }
 
     // Create a devfile for the project
     public async createDevfile(request: CreateDevfileRequest): Promise<CreateDevfileResponse> {
-        const response = await got.post<CreateDevfileResponse>(`${this.endpoint}/devfile/create`, {
-            json: request,
-            responseType: 'json',
-        })
+        const response = await this.getGot().post<CreateDevfileResponse>('devfile/create', { json: request })
         return response.body
     }
 
     // Get status and action type
     public async getStatus(): Promise<GetStatusResponse> {
-        const response = await got<GetStatusResponse>(`${this.endpoint}/status`, { responseType: 'json' })
+        const response = await this.getGot()<GetStatusResponse>('status')
         return response.body
     }
 }
