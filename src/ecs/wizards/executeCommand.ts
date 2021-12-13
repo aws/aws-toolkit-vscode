fabe6d2ae24bff20648344ed5d5ab91111949c37
gsweetwood <garretsweetwood6@gmail.com>
2021-12-13 13:59:47 -0800

    ecs: run command wizard and tests

---
+++ src/ecs/wizards/executeCommand.ts
@@ -0,0 +1,89 @@
+/*!
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { createCommonButtons } from '../../shared/ui/buttons'
+import { createInputBox } from '../../shared/ui/inputPrompter'
+import { createQuickPick, DataQuickPickItem } from '../../shared/ui/pickerPrompter'
+import { localize } from '../../shared/utilities/vsCodeUtils'
+import { WIZARD_EXIT, Wizard, WIZARD_BACK } from '../../shared/wizards/wizard'
+import { EcsContainerNode } from '../explorer/ecsContainerNode'
+
+export interface CommandWizardState {
+    task: string
+    command: string
+    confirmation?: 'yes' | 'supress'
+}
+
+function createTaskPrompter(node: EcsContainerNode) {
+    const taskItems = (async function () {
+        const taskArns = await node.listTasks()
+        if (taskArns.length === 0) {
+            return []
+        }
+        // Filter for only 'Running' tasks
+        const runningTasks = (await node.describeTasks(taskArns)).filter(t => {
+            return t.lastStatus === 'RUNNING' && t.desiredStatus === 'RUNNING'
+        })
+        return runningTasks.map(task => {
+            // The last 32 digits of the task arn is the task identifier
+            return {
+                label: task.taskArn!.substr(-32),
+                detail: `Status: ${task.lastStatus}  Desired status: ${task.desiredStatus}`,
+                data: task.taskArn!.substr(-32),
+            }
+        })
+    })()
+    return createQuickPick(taskItems, {
+        title: localize('AWS.command.ecs.runCommandInContainer.chooseTask', 'Choose a task'),
+        buttons: createCommonButtons(),
+        noItemsFoundItem: { label: 'No Running Tasks for this service', data: WIZARD_BACK },
+    })
+}
+
+function createCommandPrompter(node: EcsContainerNode) {
+    return createInputBox({
+        title: localize(
+            'AWS.command.ecs.runCommandInContainer.prompt',
+            'Enter the command to run in container: {0}',
+            node.containerName
+        ),
+        placeholder: localize('AWS.command.ecs.runCommandInContainer.placeHolder', 'Command to run'),
+        buttons: createCommonButtons(),
+    })
+}
+
+function createConfirmationPrompter(node: EcsContainerNode, task: string, command: string) {
+    const choices: DataQuickPickItem<'yes' | 'supress'>[] = [
+        {
+            label: localize('AWS.generic.response.yes', 'Yes'),
+            data: 'yes' as 'yes' | 'supress',
+            detail: `Task: ${task}  Command: ${command}`,
+        },
+        {
+            label: localize('AWS.message.prompt.yesDontAskAgain', "Yes, and don't ask again"),
+            data: 'supress' as 'yes' | 'supress',
+        },
+        { label: localize('AWS.generic.response.no', 'No'), data: WIZARD_EXIT },
+    ]
+    return createQuickPick(choices, {
+        title: localize(
+            'AWS.command.ecs.runCommandInContainer.warnBeforeExecute',
+            'Command may modify the running container {0}. Are you sure?',
+            node.containerName
+        ),
+        buttons: createCommonButtons(),
+    })
+}
+
+export class CommandWizard extends Wizard<CommandWizardState> {
+    public constructor(node: EcsContainerNode, shouldShowConfirmation: boolean) {
+        super()
+        this.form.task.bindPrompter(() => createTaskPrompter(node))
+        this.form.command.bindPrompter(() => createCommandPrompter(node))
+        if (shouldShowConfirmation) {
+            this.form.confirmation.bindPrompter(state => createConfirmationPrompter(node, state.task!, state.command!))
+        }
+    }
+}
---
+++ src/test/ecs/wizards/executeCommand.test.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as _ from 'lodash'
+import { createWizardTester, WizardTester } from '../../../test/shared/wizards/wizardTestUtils'
+import { instance, mock } from '../../utilities/mockito'
+import { CommandWizard, CommandWizardState } from '../../../ecs/wizards/executeCommand'
+
+describe('CreateServiceWizard', function () {
+    let tester: WizardTester<CommandWizardState>
+    const node = mock()
+
+    beforeEach(function () {
+        tester = createWizardTester(new CommandWizard(instance(node), false))
+    })
+
+    it('should prompt for tasks and then command', function () {
+        tester.task.assertShowFirst()
+        tester.command.assertShowSecond()
+    })
+
+    it('should ask for confirmation last', function () {
+        tester = createWizardTester(new CommandWizard(instance(node), true))
+        tester.task.assertShowFirst()
+        tester.command.assertShowSecond()
+        tester.confirmation.assertShowThird()
+    })
+
+    it('should not ask for confirmation if supressed', function () {
+        tester.confirmation.assertDoesNotShow()
+    })
+})
