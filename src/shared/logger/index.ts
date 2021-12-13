1bfad5b5b77e66d628ddd61b6fb1b005835b70e0
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2021-12-13 12:24:19 -0800

    Telemetry: add way to test what metrics would be emitted (#2366)
    * Added basic way to test telemetry in unit tests
    * Full metric data is now emitted in logs when not in production

--- src/shared/logger/index.ts
+++ src/shared/logger/index.ts
@@ -5,11 +5,10 @@
 
 // This file is the common import file, intended for use by most extension code.
 // It surfaces Logger and related interfaces, types, and retrieval.
-import * as loggableType from './loggableType'
 import * as logger from './logger'
 export { showLogOutputChannel } from './outputChannel'
 
-export type Loggable = loggableType.Loggable
+export type Loggable = Error | string
 
 export type Logger = logger.Logger
 export type LogLevel = logger.LogLevel
