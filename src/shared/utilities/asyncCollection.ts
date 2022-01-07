edd8e53b56021486dc6ced913eaf90dabc6127e0
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2022-01-07 15:11:36 -0700

    util: introduce AsyncCollection (#2402)
    To make it easier to handle pagination, I've added a new utility interface called AsyncCollection. This basically allows you to treat async generators/iterables as if they were a static collection. This is great since we can manipulate them in a declarative way; no more while loops to iterate over the elements and check for done. It also helps to abstract away async generators, which most callers don't really care about. You can still iterate over the collection as if it were an AsyncIterable, but again the common-case is we just want all the results. This is provided by a promise method which outputs all results in an array.
    
    Benefits:
    * Much better ergonomics and type safety for mapping paginated API calls
    * Flexible. The collection does not hide the fact that things come in pages, so it can be used in ChildNodeLoader
    * Able to map generators themselves, which is important when generators have special meaning behind their 'return' value.
    * Collections can be passed around and recognized as AsyncIterable, hiding implementation details

---
+++ src/shared/utilities/asyncCollection.ts
@@ -0,0 +1,190 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Coalesce } from './tsUtils'
+
+/**
+ * High-level abstraction over async generator functions of the form `async function*` {@link AsyncGenerator}
+ */
+export interface AsyncCollection<T> extends AsyncIterable<T> {
+    /** Flattens the collection 1-level deep */
+    flatten(): AsyncCollection<SafeUnboxIterable<T>>
+    /** Applies a mapping transform to the output generator */
+    map<U>(fn: (obj: T) => U): AsyncCollection<U>
+    /** Filters out results which will _not_ be passed on to further transformations. */
+    filter<U extends T>(predicate: Predicate<T, U>): AsyncCollection<U>
+    /** Uses only the first 'count' number of values returned by the generator. */
+    take(count: number): AsyncCollection<T>
+    /** Converts the collection to a Promise, resolving to an array of all values returned by the generator. */
+    promise(): Promise<T[]>
+    /** Converts the collection to a Map, using either a property of the item or a function to select keys. */
+    toMap<K extends StringProperty<T>, U extends string = never>(
+        selector: KeySelector<T, U> | K
+    ): Promise<Map<Coalesce<U, T[K]>, T>>
+    /** Returns an iterator directly from the underlying generator, preserving values returned. */
+    iterator(): AsyncIterator<T, T | void>
+}
+
+/**
+ * Converts an async generator function to an {@link AsyncCollection}
+ *
+ * Generation is "lazy", i.e. the generator is not called until a resolving operation:
+ *  * Iterating over them using `for await (...)`
+ *  * Iterating over them using `.next()`
+ *  * Calling one of the conversion functions `toMap` or `promise`
+ *
+ * Collections are *immutable* in the sense that any transformation will not consume the underlying generator
+ * function. That is, any 'final' operation uses its own contextually bound generator function separate from
+ * any predecessor collections.
+ */
+export function toCollection<T>(generator: () => AsyncGenerator<T, T | undefined | void>): AsyncCollection<T> {
+    async function* unboxIter() {
+        const last = yield* generator()
+        if (last !== undefined) {
+            yield last
+        }
+    }
+
+    const iterable: AsyncIterable<T> = {
+        [Symbol.asyncIterator]: unboxIter,
+    }
+
+    return Object.assign(iterable, {
+        flatten: () => toCollection<SafeUnboxIterable<T>>(() => delegateGenerator(generator(), flatten)),
+        filter: <U extends T>(predicate: Predicate<T, U>) =>
+            toCollection<U>(() => filterGenerator<T, U>(generator(), predicate)),
+        map: <U>(fn: (item: T) => U) => toCollection<U>(() => mapGenerator(generator(), fn)),
+        take: (count: number) => toCollection(() => delegateGenerator(generator(), takeFrom(count))),
+        promise: () => promise(iterable),
+        toMap: <U extends string = never, K extends StringProperty<T> = never>(selector: KeySelector<T, U> | K) =>
+            asyncIterableToMap(iterable, selector),
+        iterator: generator,
+    })
+}
+
+async function* mapGenerator<T, U, R = T>(
+    generator: AsyncGenerator<T, R | undefined | void>,
+    fn: (item: T | R) => U
+): AsyncGenerator<U, U | undefined> {
+    while (true) {
+        const { value, done } = await generator.next()
+        if (done) {
+            return value !== undefined ? fn(value) : undefined
+        }
+        if (value !== undefined) {
+            yield fn(value)
+        }
+    }
+}
+
+async function* filterGenerator<T, U extends T, R = T>(
+    generator: AsyncGenerator<T, R | undefined | void>,
+    predicate: Predicate<T | R, U>
+): AsyncGenerator<U, U | undefined> {
+    while (true) {
+        const { value, done } = await generator.next()
+        if (value === undefined || !predicate(value)) {
+            if (done) {
+                break
+            }
+            continue
+        }
+        if (done) {
+            return value
+        }
+        yield value
+    }
+}
+
+async function* delegateGenerator<T, U, R = T>(
+    generator: AsyncGenerator<T, R | undefined | void>,
+    fn: (item: T | R, ret: () => void) => AsyncGenerator<U, void>
+): AsyncGenerator<U, U | undefined> {
+    while (true) {
+        let last: U | undefined
+        const { value, done } = await generator.next()
+        if (value !== undefined) {
+            const delegate = fn(value, generator.return.bind(generator))
+            while (true) {
+                const sub = await delegate.next()
+                if (sub.done) {
+                    break
+                }
+                last = sub.value
+                if (!done) {
+                    yield last
+                }
+            }
+        }
+        if (done) {
+            return last
+        }
+    }
+}
+
+async function* flatten<T, U extends SafeUnboxIterable<T>>(item: T) {
+    if (isIterable<U>(item)) {
+        yield* item
+    } else {
+        yield item as U
+    }
+}
+
+function takeFrom<T>(count: number) {
+    return async function* (item: T, ret: () => void) {
+        if (--count < 0) {
+            return ret()
+        }
+        yield item
+    }
+}
+
+/** Either 'unbox' an Iterable value or leave it as-is if it's not an Iterable */
+type SafeUnboxIterable<T> = T extends Iterable<infer U> ? U : T
+
+function isIterable<T>(obj: any): obj is Iterable<T> {
+    return obj !== undefined && typeof obj[Symbol.iterator] === 'function'
+}
+
+async function promise<T>(iterable: AsyncIterable<T>): Promise<T[]> {
+    const result: T[] = []
+
+    for await (const item of iterable) {
+        result.push(item)
+    }
+
+    return result
+}
+
+type Predicate<T, U extends T> = ((item: T) => item is U) | ((item: T) => boolean)
+
+function addToMap<T, U extends string>(map: Map<string, T>, selector: KeySelector<T, U> | StringProperty<T>, item: T) {
+    const key = typeof selector === 'function' ? selector(item) : item[selector]
+    if (key) {
+        if (map.has(key as keyof typeof map['keys'])) {
+            throw new Error(`Duplicate key found when converting AsyncIterable to map: ${key}`)
+        }
+
+        map.set(key as keyof typeof map['keys'], item)
+    }
+}
+
+// Type 'U' is constrained to be either a key of 'T' or a string returned by a function parsing 'T'
+type KeySelector<T, U extends string> = (item: T) => U | undefined
+type StringProperty<T> = { [P in keyof T]: T[P] extends string ? P : never }[keyof T]
+
+// TODO: apply this to different iterables and replace the old 'map' code
+async function asyncIterableToMap<T, K extends StringProperty<T>, U extends string = never>(
+    iterable: AsyncIterable<T>,
+    selector: KeySelector<T, U> | K
+): Promise<Map<Coalesce<U, T[K]>, T>> {
+    const result = new Map<Coalesce<U, T[K] & string>, T>()
+
+    for await (const item of iterable) {
+        addToMap(result, selector, item)
+    }
+
+    return result
+}
