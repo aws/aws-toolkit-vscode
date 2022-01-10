879aeaae49038f01cf3df0a4a816fba856d5c8ae
JadenSimon <sijaden@amazon.com>
2022-01-10 09:31:01 -0800

    Faster updates

--- src/s3/progressReporter.ts
+++ src/s3/progressReporter.ts
@@ -9,7 +9,7 @@ import { throttle } from 'lodash'
 import { getLogger } from '../shared/logger/logger'
 import * as bytes from 'bytes'
 
-const DEFAULT_REPORTING_INTERVAL_MILLIS = 500
+const DEFAULT_REPORTING_INTERVAL_MILLIS = 250
 
 interface ProgressReporterOptions {
     readonly totalBytes?: number
