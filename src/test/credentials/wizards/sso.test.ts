784fde257f7b5c65444464e87bd576d595828912
JadenSimon <sijaden@amazon.com>
2022-04-04 16:03:36 -0700

    Duplicate header

--- src/test/credentials/wizards/sso.test.ts
+++ src/test/credentials/wizards/sso.test.ts
@@ -1,10 +1,5 @@
 /*!
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/*!
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
