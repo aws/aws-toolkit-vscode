aws/aws-toolkit-vscode

commit 5e40506dd627258dbd85d14fbdbd04050b5e9db1

feat(sam): auto-connect on Deploy, Debug #1715
Problem:
7400c5f55070 #1708 changed Toolkit to attempt auto-connect only when AWS
Explorer is made visible. But the "AWS: Deploy SAM Application" command
requires credentials. SAM local Run/Debug also wants credentials since
2b347ebdf0b7.

Solution:
Try auto-connect in the central Commands hook.

src/credentials/loginManager.ts
@@ -3,20 +3,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as vscode from 'vscode'
+import globals from '../shared/extensionGlobals'
 import { CancellationError } from '../shared/utilities/timeoutUtils'
 import { AwsContext } from '../shared/awsContext'
 import { getAccountId } from '../shared/credentials/accountId'
 import { getLogger } from '../shared/logger'
 import { recordAwsValidateCredentials, recordVscodeActiveRegions, Result } from '../shared/telemetry/telemetry'
 import { CredentialsStore } from './credentialsStore'
-import { notifyUserInvalidCredentials } from './credentialsUtilities'
+import { CredentialsSettings, notifyUserInvalidCredentials } from './credentialsUtilities'
 import {
     asString,
     CredentialsProvider,
     CredentialsId,
     credentialsProviderToTelemetryType,
+    fromString,
 } from './providers/credentials'
 import { CredentialsProviderManager } from './providers/credentialsProviderManager'
+import { getIdeProperties, isCloud9 } from '../shared/extensionUtilities'
+import { SharedCredentialsProvider } from './providers/sharedCredentialsProvider'
+import { localize } from 'vscode-nls'
+import { showViewLogsMessage } from '../shared/utilities/messages'
 
 export class LoginManager {
     private readonly defaultCredentialsRegion = 'us-east-1'
@@ -99,5 +106,124 @@ export class LoginManager {
      */
     public async logout(force?: boolean): Promise<void> {
         await this.awsContext.setCredentials(undefined, force)
+    }
+
+    private static didTryAutoConnect = false
+
+    public static async tryAutoConnect(awsContext: AwsContext = globals.awsContext): Promise<boolean> {
+        if (await awsContext.getCredentials()) {
+            return true // Already connected.
+        }
+        if (LoginManager.didTryAutoConnect) {
+            return false
+        }
+        LoginManager.didTryAutoConnect = true
+        try {
+            getLogger().debug('credentials: attempting autoconnect...')
+            const loginManager = new LoginManager(awsContext, new CredentialsStore())
+            await loginWithMostRecentCredentials(new CredentialsSettings(), loginManager)
+        } catch (err) {
+            getLogger().error('credentials: failed to auto-connect: %O', err)
+            showViewLogsMessage(localize('AWS.credentials.autoconnect.fatal', 'Exception occurred while connecting'))
+        }
+        return !!(await awsContext.getCredentials())
+    }
+}
+
+/**
+ * Auto-connects with the last-used credentials, else the "default" profile,
+ * else randomly tries the first three profiles (for Cloud9, ECS, or other
+ * container-like environments).
+ */
+export async function loginWithMostRecentCredentials(
+    settings: CredentialsSettings,
+    loginManager: LoginManager
+): Promise<void> {
+    const defaultName = 'profile:default'
+    const manager = CredentialsProviderManager.getInstance()
+    const previousCredentialsId = settings.get('profile', '')
+
+    async function tryConnect(creds: CredentialsId, popup: boolean): Promise<boolean> {
+        const provider = await manager.getCredentialsProvider(creds)
+        // 'provider' may be undefined if the last-used credentials no longer exists.
+        if (!provider) {
+            getLogger().warn('autoconnect: getCredentialsProvider() lookup failed for profile: %O', asString(creds))
+        } else if (provider.canAutoConnect()) {
+            if (!(await loginManager.login({ passive: true, providerId: creds }))) {
+                getLogger().warn('autoconnect: failed to connect: "%s"', asString(creds))
+                return false
+            }
+            getLogger().info('autoconnect: connected: %O', asString(creds))
+            if (popup) {
+                vscode.window.showInformationMessage(
+                    localize(
+                        'AWS.message.credentials.connected',
+                        'Connected to {0} with {1}',
+                        getIdeProperties().company,
+                        asString(creds)
+                    )
+                )
+            }
+            return true
+        }
+        return false
+    }
+
+    // Auto-connect if there is a recently-used profile.
+    if (previousCredentialsId) {
+        // Migrate from old Toolkits: default to "shared" provider type.
+        const loginCredentialsId = tryMakeCredentialsProviderId(previousCredentialsId) ?? {
+            credentialSource: SharedCredentialsProvider.getProviderType(),
+            credentialTypeId: previousCredentialsId,
+        }
+        if (await tryConnect(loginCredentialsId, false)) {
+            return
+        }
+        getLogger().warn('autoconnect: login failed: "%s"', previousCredentialsId)
+    }
+
+    const providerMap = await manager.getCredentialProviderNames()
+    const profileNames = Object.keys(providerMap)
+    // Look for "default" profile or exactly one (any name).
+    const defaultProfile = profileNames.includes(defaultName)
+        ? defaultName
+        : profileNames.length === 1
+        ? profileNames[0]
+        : undefined
+
+    if (!previousCredentialsId && profileNames.length === 0) {
+        await loginManager.logout(true)
+        getLogger().info('autoconnect: skipped (profileNames=%d)', profileNames.length)
+        return
+    }
+
+    // Try to auto-connect the default profile.
+    if (defaultProfile) {
+        getLogger().info('autoconnect: trying "%s"', defaultProfile)
+        if (await tryConnect(providerMap[defaultProfile], !isCloud9())) {
+            return
+        }
+    }
+
+    // Try to auto-connect up to 3 other profiles (useful for Cloud9, ECS, …).
+    for (let i = 0; i < 4 && i < profileNames.length; i++) {
+        const p = profileNames[i]
+        if (p === defaultName) {
+            continue
+        }
+        getLogger().info('autoconnect: trying "%s"', p)
+        if (await tryConnect(providerMap[p], !isCloud9())) {
+            return
+        }
+    }
+
+    await loginManager.logout(true)
+}
+
+function tryMakeCredentialsProviderId(credentials: string): CredentialsId | undefined {
+    try {
+        return fromString(credentials)
+    } catch (err) {
+        return undefined
     }
 }

src/awsexplorer/activation.ts
@@ -4,20 +4,21 @@
  */
 
 import * as vscode from 'vscode'
-import { loginWithMostRecentCredentials } from '../credentials/activation'
 import { LoginManager } from '../credentials/loginManager'
 import { submitFeedback } from '../feedback/commands/submitFeedback'
 import { deleteCloudFormation } from '../lambda/commands/deleteCloudFormation'
 import { CloudFormationStackNode } from '../lambda/explorer/cloudFormationNodes'
 import { AwsContext } from '../shared/awsContext'
 import { AwsContextTreeCollection } from '../shared/awsContextTreeCollection'
-
+import globals from '../shared/extensionGlobals'
+import { ExtContext } from '../shared/extensions'
 import { getLogger } from '../shared/logger'
 import { RegionProvider } from '../shared/regions/regionProvider'
 import { recordAwsRefreshExplorer, recordAwsShowRegion, recordVscodeActiveRegions } from '../shared/telemetry/telemetry'
 import { AWSResourceNode } from '../shared/treeview/nodes/awsResourceNode'
 import { AWSTreeNodeBase } from '../shared/treeview/nodes/awsTreeNodeBase'
 import { LoadMoreNode } from '../shared/treeview/nodes/loadMoreNode'
+import { Commands } from '../shared/vscode/commands2'
 import { downloadStateMachineDefinition } from '../stepFunctions/commands/downloadStateMachineDefinition'
 import { executeStateMachine } from '../stepFunctions/commands/executeStateMachine'
 import { StateMachineNode } from '../stepFunctions/explorer/stepFunctionsNodes'
@@ -26,16 +27,6 @@ import { copyArnCommand } from './commands/copyArn'
 import { copyNameCommand } from './commands/copyName'
 import { loadMoreChildrenCommand } from './commands/loadMoreChildren'
 import { checkExplorerForDefaultRegion } from './defaultRegion'
-import { CredentialsStore } from '../credentials/credentialsStore'
-import { showViewLogsMessage } from '../shared/utilities/messages'
-
-let didTryAutoConnect = false
-
-import * as nls from 'vscode-nls'
-import globals from '../shared/extensionGlobals'
-import { ExtContext } from '../shared/extensions'
-import { CredentialsSettings } from '../credentials/credentialsUtilities'
-const localize = nls.loadMessageBundle()
 
 /**
  * Activates the AWS Explorer UI and related functionality.
@@ -60,7 +51,7 @@ export async function activate(args: {
     globals.context.subscriptions.push(
         view.onDidChangeVisibility(async e => {
             if (e.visible) {
-                await tryAutoConnect(args.context.awsContext)
+                await LoginManager.tryAutoConnect(args.context.awsContext)
             }
         })
     )
@@ -70,63 +61,37 @@ export async function activate(args: {
     updateAwsExplorerWhenAwsContextCredentialsChange(awsExplorer, args.context.awsContext, globals.context)
 }
 
-async function tryAutoConnect(awsContext: AwsContext) {
-    try {
-        if (!didTryAutoConnect && !(await awsContext.getCredentials())) {
-            getLogger().debug('credentials: attempting autoconnect...')
-            didTryAutoConnect = true
-            const loginManager = new LoginManager(awsContext, new CredentialsStore())
-            await loginWithMostRecentCredentials(new CredentialsSettings(), loginManager)
-        }
-    } catch (err) {
-        getLogger().error('credentials: failed to auto-connect: %O', err)
-        showViewLogsMessage(localize('AWS.credentials.autoconnect.fatal', 'Exception occurred while connecting'))
-    }
-}
-
 async function registerAwsExplorerCommands(
     context: ExtContext,
     awsExplorer: AwsExplorer,
     toolkitOutputChannel: vscode.OutputChannel
 ): Promise<void> {
     context.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand('aws.showRegion', async () => {
+        Commands.register({ id: 'aws.showRegion', autoconnect: false }, async () => {
             try {
                 await globals.awsContextCommands.onCommandShowRegion()
             } finally {
                 recordAwsShowRegion()
                 recordVscodeActiveRegions({ value: awsExplorer.getRegionNodesSize() })
             }
-        })
-    )
-
-    context.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand('aws.submitFeedback', async () => {
+        }),
+        Commands.register({ id: 'aws.submitFeedback', autoconnect: false }, async () => {
             await submitFeedback(context)
-        })
-    )
-
-    context.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand('aws.refreshAwsExplorer', async (passive: boolean = false) => {
+        }),
+        Commands.register({ id: 'aws.refreshAwsExplorer', autoconnect: true }, async (passive: boolean = false) => {
             awsExplorer.refresh()
 
             if (!passive) {
                 recordAwsRefreshExplorer()
             }
-        })
-    )
-
-    context.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand(
-            'aws.deleteCloudFormation',
+        }),
+        Commands.register(
+            { id: 'aws.deleteCloudFormation', autoconnect: true },
             async (node: CloudFormationStackNode) =>
                 await deleteCloudFormation(() => awsExplorer.refresh(node.parent), node)
-        )
-    )
-
-    context.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand(
-            'aws.downloadStateMachineDefinition',
+        ),
+        Commands.register(
+            { id: 'aws.downloadStateMachineDefinition', autoconnect: true },
             async (node: StateMachineNode) =>
                 await downloadStateMachineDefinition({
                     stateMachineNode: node,

src/credentials/activation.ts
@@ -7,18 +7,8 @@ import * as vscode from 'vscode'
 import { AwsContext } from '../shared/awsContext'
 import { profileSettingKey } from '../shared/constants'
 import { CredentialsProfileMru } from '../shared/credentials/credentialsProfileMru'
-import { LoginManager } from './loginManager'
-import { asString, CredentialsId, fromString } from './providers/credentials'
-import { CredentialsProviderManager } from './providers/credentialsProviderManager'
-import { SharedCredentialsProvider } from './providers/sharedCredentialsProvider'
-import { getIdeProperties } from '../shared/extensionUtilities'
-
-import * as nls from 'vscode-nls'
-import { isCloud9 } from '../shared/extensionUtilities'
-import { getLogger } from '../shared/logger/logger'
-import { CredentialsSettings } from './credentialsUtilities'
 import { Settings } from '../shared/settings'
-const localize = nls.loadMessageBundle()
+import { CredentialsSettings } from './credentialsUtilities'
 
 export async function initialize(
     extensionContext: vscode.ExtensionContext,
@@ -28,96 +18,6 @@ export async function initialize(
     const credentialSettings = new CredentialsSettings(settings)
     updateMruWhenAwsContextChanges(awsContext, extensionContext)
     updateConfigurationWhenAwsContextChanges(credentialSettings, awsContext, extensionContext)
-}
-
-/**
- * Auto-connects with the last-used credentials, else the "default" profile,
- * else randomly tries the first three profiles (for Cloud9, ECS, or other
- * container-like environments).
- */
-export async function loginWithMostRecentCredentials(
-    settings: CredentialsSettings,
-    loginManager: LoginManager
-): Promise<void> {
-    const defaultName = 'profile:default'
-    const manager = CredentialsProviderManager.getInstance()
-    const previousCredentialsId = settings.get('profile', '')
-
-    async function tryConnect(creds: CredentialsId, popup: boolean): Promise<boolean> {
-        const provider = await manager.getCredentialsProvider(creds)
-        // 'provider' may be undefined if the last-used credentials no longer exists.
-        if (!provider) {
-            getLogger().warn('autoconnect: getCredentialsProvider() lookup failed for profile: %O', asString(creds))
-        } else if (provider.canAutoConnect()) {
-            if (!(await loginManager.login({ passive: true, providerId: creds }))) {
-                getLogger().warn('autoconnect: failed to connect: "%s"', asString(creds))
-                return false
-            }
-            getLogger().info('autoconnect: connected: %O', asString(creds))
-            if (popup) {
-                vscode.window.showInformationMessage(
-                    localize(
-                        'AWS.message.credentials.connected',
-                        'Connected to {0} with {1}',
-                        getIdeProperties().company,
-                        asString(creds)
-                    )
-                )
-            }
-            return true
-        }
-        return false
-    }
-
-    // Auto-connect if there is a recently-used profile.
-    if (previousCredentialsId) {
-        // Migrate from old Toolkits: default to "shared" provider type.
-        const loginCredentialsId = tryMakeCredentialsProviderId(previousCredentialsId) ?? {
-            credentialSource: SharedCredentialsProvider.getProviderType(),
-            credentialTypeId: previousCredentialsId,
-        }
-        if (await tryConnect(loginCredentialsId, false)) {
-            return
-        }
-        getLogger().warn('autoconnect: login failed: "%s"', previousCredentialsId)
-    }
-
-    const providerMap = await manager.getCredentialProviderNames()
-    const profileNames = Object.keys(providerMap)
-    // Look for "default" profile or exactly one (any name).
-    const defaultProfile = profileNames.includes(defaultName)
-        ? defaultName
-        : profileNames.length === 1
-        ? profileNames[0]
-        : undefined
-
-    if (!previousCredentialsId && profileNames.length === 0) {
-        await loginManager.logout(true)
-        getLogger().info('autoconnect: skipped (profileNames=%d)', profileNames.length)
-        return
-    }
-
-    // Try to auto-connect the default profile.
-    if (defaultProfile) {
-        getLogger().info('autoconnect: trying "%s"', defaultProfile)
-        if (await tryConnect(providerMap[defaultProfile], !isCloud9())) {
-            return
-        }
-    }
-
-    // Try to auto-connect up to 3 other profiles (useful for Cloud9, ECS, …).
-    for (let i = 0; i < 4 && i < profileNames.length; i++) {
-        const p = profileNames[i]
-        if (p === defaultName) {
-            continue
-        }
-        getLogger().info('autoconnect: trying "%s"', p)
-        if (await tryConnect(providerMap[p], !isCloud9())) {
-            return
-        }
-    }
-
-    await loginManager.logout(true)
 }
 
 function updateMruWhenAwsContextChanges(awsContext: AwsContext, extensionContext: vscode.ExtensionContext) {
@@ -150,12 +50,4 @@ function updateConfigurationWhenAwsContextChanges(
             }
         })
     )
-}
-
-function tryMakeCredentialsProviderId(credentials: string): CredentialsId | undefined {
-    try {
-        return fromString(credentials)
-    } catch (err) {
-        return undefined
-    }
 }

src/lambda/commands/deploySamApplication.ts
@@ -56,10 +56,15 @@ export async function deploySamApplication(
         awsContext,
         settings,
         window = getDefaultWindowFunctions(),
+        refreshFn = () => {
+            // no need to await, doesn't need to block further execution (true -> no telemetry)
+            vscode.commands.executeCommand('aws.refreshAwsExplorer', true)
+        },
     }: {
         awsContext: Pick<AwsContext, 'getCredentials' | 'getCredentialProfileName'>
         settings: SamCliSettings
         window?: WindowFunctions
+        refreshFn?: () => void
     }
 ): Promise<void> {
     let deployResult: Result = 'Succeeded'
@@ -112,8 +117,7 @@ export async function deploySamApplication(
         )
 
         await deployApplicationPromise
-        // no need to await, doesn't need to block further execution (true -> no telemetry)
-        vscode.commands.executeCommand('aws.refreshAwsExplorer', true)
+        refreshFn()
 
         // successful deploy: retain S3 bucket for quick future access
         const profile = awsContext.getCredentialProfileName()

src/shared/sam/activation.ts
@@ -38,6 +38,7 @@ import { lazyLoadSamTemplateStrings } from '../../lambda/models/samTemplates'
 import { PromptSettings } from '../settings'
 import { shared } from '../utilities/functionUtils'
 import { migrateLegacySettings, SamCliSettings } from './cli/samCliSettings'
+import { Commands } from '../vscode/commands2'
 
 const sharedDetectSamCli = shared(detectSamCli)
 
@@ -88,15 +89,18 @@ export async function activate(ctx: ExtContext): Promise<void> {
 async function registerServerlessCommands(ctx: ExtContext, settings: SamCliSettings): Promise<void> {
     lazyLoadSamTemplateStrings()
     ctx.extensionContext.subscriptions.push(
-        vscode.commands.registerCommand('aws.samcli.detect', () =>
+        Commands.register({ id: 'aws.samcli.detect', autoconnect: false }, () =>
             sharedDetectSamCli({ passive: false, showMessage: true })
         ),
-        vscode.commands.registerCommand('aws.lambda.createNewSamApp', async () => {
+        Commands.register({ id: 'aws.lambda.createNewSamApp', autoconnect: false }, async () => {
             await createNewSamApplication(ctx)
         }),
-        vscode.commands.registerCommand('aws.addSamDebugConfiguration', addSamDebugConfiguration),
-        vscode.commands.registerCommand('aws.pickAddSamDebugConfiguration', codelensUtils.pickAddSamDebugConfiguration),
-        vscode.commands.registerCommand('aws.deploySamApplication', async arg => {
+        Commands.register({ id: 'aws.addSamDebugConfiguration', autoconnect: false }, addSamDebugConfiguration),
+        Commands.register(
+            { id: 'aws.pickAddSamDebugConfiguration', autoconnect: false },
+            codelensUtils.pickAddSamDebugConfiguration
+        ),
+        Commands.register({ id: 'aws.deploySamApplication', autoconnect: true }, async arg => {
             // `arg` is one of :
             //  - undefined
             //  - regionNode (selected from AWS Explorer)

src/shared/sam/debugger/awsSamDebugger.ts
@@ -64,6 +64,7 @@ import {
 import { getIdeProperties, isCloud9 } from '../../extensionUtilities'
 import { resolve } from 'path'
 import globals from '../../extensionGlobals'
+import { LoginManager } from '../../../credentials/loginManager'
 
 const localize = nls.loadMessageBundle()
 
@@ -617,6 +618,7 @@ export class SamDebugConfigProvider implements vscode.DebugConfigurationProvider
      * Performs the EXECUTE phase of SAM run/debug.
      */
     public async invokeConfig(config: SamLaunchRequestArgs): Promise<SamLaunchRequestArgs> {
+        await LoginManager.tryAutoConnect()
         switch (config.runtimeFamily) {
             case RuntimeFamily.NodeJS: {
                 config.type = 'node'

src/shared/vscode/commands2.ts
@@ -9,6 +9,7 @@ import { AWSTreeNodeBase } from '../treeview/nodes/awsTreeNodeBase'
 import { isNameMangled } from './env'
 import { UnknownError } from '../toolkitError'
 import { getLogger, NullLogger } from '../logger/logger'
+import { LoginManager } from '../../credentials/loginManager'
 
 type Callback = (...args: any[]) => any
 type CommandFactory<T extends Callback, U extends any[]> = (...parameters: U) => T
@@ -69,15 +70,28 @@ export class Commands {
                 throw new Error(`Command "${id}" cannot be re-registered`)
             }
 
-            return new CommandResource<Callback>({ id, factory: throwOnRegister }, this.commands)
+            const info = { id }
+            return new CommandResource<Callback>({ info, factory: throwOnRegister }, this.commands)
         }
     }
 
     /**
      * Registers a new command with the VS Code API.
+     *
+     * @param info command id (string) or {@link CommandInfo} object
+     * @param callback command implementation
      */
-    public register<T extends Callback>(id: string, callback: T): RegisteredCommand<T> {
-        const resource = new CommandResource({ id, factory: () => callback }, this.commands)
+    public register<T extends Callback>(
+        info: string | Omit<CommandInfo<T>, 'args' | 'label'>,
+        callback: T
+    ): RegisteredCommand<T> {
+        const resource = new CommandResource(
+            {
+                info: typeof info === 'string' ? { id: info } : info,
+                factory: () => callback,
+            },
+            this.commands
+        )
 
         return this.addResource(resource).register()
     }
@@ -89,10 +103,10 @@ export class Commands {
      * not just the command signature but also its immediate dependencies.
      */
     public declare<T extends Callback, D extends any[]>(
-        id: string | { id: string; name: string },
+        id: string | Omit<CommandInfo<T>, 'args' | 'label'>,
         factory: CommandFactory<T, D>
     ): DeclaredCommand<T, D> {
-        const resource = typeof id === 'string' ? { id, factory } : { ...id, factory }
+        const resource = typeof id === 'string' ? { info: { id }, factory } : { info: { ...id }, factory }
 
         return this.addResource(new CommandResource(resource, this.commands))
     }
@@ -218,8 +232,7 @@ interface Builder {
 }
 
 interface Deferred<T extends Callback, U extends any[]> {
-    readonly id: string
-    readonly name?: string
+    readonly info: Omit<CommandInfo<T>, 'args' | 'label'>
     readonly factory: CommandFactory<T, U>
 }
 
@@ -234,7 +247,7 @@ class CommandResource<T extends Callback = Callback, U extends any[] = any[]> {
     private disposed?: boolean
     private subscription?: vscode.Disposable
     private static counter = 0 // Used to generated unique-ish tree item IDs
-    public readonly id = this.resource.id
+    public readonly id = this.resource.info.id
 
     public constructor(private readonly resource: Deferred<T, U>, private readonly commands = vscode.commands) {}
 
@@ -247,7 +260,7 @@ class CommandResource<T extends Callback = Callback, U extends any[] = any[]> {
     }
 
     public build(...args: Parameters<T>): Builder {
-        const id = this.resource.id
+        const id = this.resource.info.id
 
         return {
             asUri: this.buildUri(id, args),
@@ -258,17 +271,25 @@ class CommandResource<T extends Callback = Callback, U extends any[] = any[]> {
     }
 
     public register(...args: U): RegisteredCommand<T> {
-        const { id, name } = this.resource
+        const { id, name } = this.resource.info
         const label = name ? `"${name}" (id: ${id})` : `"${id}"`
         const target = this.resource.factory(...args)
-        const instrumented = (...args: Parameters<T>) => runCommand(target, { label, args })
-        this.subscription = this.commands.registerCommand(this.resource.id, instrumented)
+        const instrumented = (...args: Parameters<T>) => {
+            const info: CommandInfo<T> = {
+                ...this.resource.info,
+                id: id,
+                label: label,
+                args: args,
+            }
+            return runCommand(target, info)
+        }
+        this.subscription = this.commands.registerCommand(this.resource.info.id, instrumented)
 
         return this
     }
 
     public async execute(...args: Parameters<T>): Promise<ReturnType<T> | undefined> {
-        return this.commands.executeCommand<ReturnType<T>>(this.resource.id, ...args)
+        return this.commands.executeCommand<ReturnType<T>>(this.resource.info.id, ...args)
     }
 
     public dispose(): void {
@@ -306,9 +327,14 @@ class CommandResource<T extends Callback = Callback, U extends any[] = any[]> {
 }
 
 interface CommandInfo<T extends Callback> {
-    readonly label: string
+    readonly id: string
+    readonly name?: string
+    /** Display label, generated from id + name. */
+    readonly label?: string
     readonly args: Parameters<T>
     readonly logging?: boolean
+    /** Does the command require credentials? (default: false) */
+    readonly autoconnect?: boolean
     readonly errorHandler?: (error: Error) => ReturnType<T> | void
 }
 
@@ -322,6 +348,9 @@ async function runCommand<T extends Callback>(fn: T, info: CommandInfo<T>): Prom
     logger.debug(`command: running ${label}${withArgs}`)
 
     try {
+        if (info.autoconnect === true) {
+            await LoginManager.tryAutoConnect()
+        }
         const result = await fn(...args)
         logger.debug(`command: ${label} succeeded`)
 

commit ea64e4b4c97f12c689263b4eb86e1e61db4c5f30

fix(ci): tryAutoConnect() fails in CI
regression by #1715

src/credentials/loginManager.ts
@@ -24,6 +24,7 @@ import { getIdeProperties, isCloud9 } from '../shared/extensionUtilities'
 import { SharedCredentialsProvider } from './providers/sharedCredentialsProvider'
 import { localize } from 'vscode-nls'
 import { showViewLogsMessage } from '../shared/utilities/messages'
+import { isAutomation } from '../shared/vscode/env'
 
 export class LoginManager {
     private readonly defaultCredentialsRegion = 'us-east-1'
@@ -111,6 +112,9 @@ export class LoginManager {
     private static didTryAutoConnect = false
 
     public static async tryAutoConnect(awsContext: AwsContext = globals.awsContext): Promise<boolean> {
+        if (!isAutomation()) {
+            return false
+        }
         if (await awsContext.getCredentials()) {
             return true // Already connected.
         }