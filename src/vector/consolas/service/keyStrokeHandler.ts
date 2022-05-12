e7ab61e68739e3a0df0236faddf786156f9f4ac2
Prateek Chauhan <prateekchauhan091@gmail.com>
2022-05-11 17:06:36 -0700

    Fix bug of global var invocationContext.isPendingResponse

--- src/vector/consolas/service/keyStrokeHandler.ts
+++ src/vector/consolas/service/keyStrokeHandler.ts
@@ -349,8 +349,11 @@ export async function getServiceResponse(
     triggerType: telemetry.ConsolasTriggerType,
     isManualTriggerOn: boolean
 ): Promise<any> {
+    invocationContext.isPendingResponse = true
+    const consolasPromise = client.generateRecommendations(req).finally(() => {
+        invocationContext.isPendingResponse = false
+    })
     if (isManualTriggerOn && triggerType === 'OnDemand') {
-        invocationContext.isPendingResponse = true
         return vscode.window.withProgress(
             {
                 location: vscode.ProgressLocation.Notification,
@@ -358,17 +361,11 @@ export async function getServiceResponse(
                 cancellable: false,
             },
             async () => {
-                const consolasPromise = client.generateRecommendations(req).finally(() => {
-                    invocationContext.isPendingResponse = false
-                })
                 return await asyncCallWithTimeout(consolasPromise, ConsolasConstants.PROMISE_TIMEOUT_LIMIT * 1000)
             }
         )
-    } else {
-        return client.generateRecommendations(req).then(result => {
-            return result
-        })
     }
+    return await asyncCallWithTimeout(consolasPromise, ConsolasConstants.PROMISE_TIMEOUT_LIMIT * 1000)
 }
 
 /**
