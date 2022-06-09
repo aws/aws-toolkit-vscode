9f82e025ad55eca9078e8db2fd9589dbc27f0340
Justin M. Keyes <jmkeyes@amazon.com>
2022-06-09 12:22:35 -0700

    fixup! fixup! fixup! refactor(copyurl): remove redundant code

--- src/apprunner/activation.ts
+++ src/apprunner/activation.ts
@@ -14,7 +14,7 @@ import { pauseService } from './commands/pauseService'
 import { deleteService } from './commands/deleteService'
 import { createFromEcr } from './commands/createServiceFromEcr'
 import { ExtContext } from '../shared/extensions'
-import { copyToClipboard } from 'src/shared/vscode/env'
+import { copyToClipboard } from '../shared/vscode/env'
 
 const localize = nls.loadMessageBundle()
 
