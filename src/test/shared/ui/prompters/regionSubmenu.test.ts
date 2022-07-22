2d07e646895497e6d7094a199912d9790db59a3d
Hweinstock <hkobew@amazon.com>
2022-07-22 08:45:33 -0700

    Start of additional tests

--- src/test/shared/ui/prompters/regionSubmenu.test.ts
+++ src/test/shared/ui/prompters/regionSubmenu.test.ts
@@ -4,7 +4,7 @@
  */
 
 import { RegionSubmenu } from '../../../../shared/ui/common/regionSubmenu'
-import { DataQuickPickItem } from '../../../../shared/ui/pickerPrompter'
+import { DataQuickPickItem, QuickPickPrompter } from '../../../../shared/ui/pickerPrompter'
 import { createQuickPickTester, QuickPickTester } from '../testUtils'
 
 describe('regionSubmenu', function () {
@@ -42,5 +42,20 @@ describe('regionSubmenu', function () {
         menuTester.assertItems(expectedMenuItems1)
         menuTester.acceptItem('group1a')
         await menuTester.result()
+    })
+
+    it('Able to swap regions via escape hatch', async function () {
+        type Inner<T> = T extends QuickPickPrompter<infer U> ? U : never
+        type Combine<T> = QuickPickPrompter<Inner<T>>
+        const submenuPrompt = createQuickPickTester(
+            SubmenuPrompter.activePrompter as Combine<typeof SubmenuPrompter.activePrompter>
+        )
+        submenuPrompt.acceptItem('Switch region')
+        await submenuPrompt.result()
+        const regionPrompt = createQuickPickTester(
+            SubmenuPrompter.activePrompter as Combine<typeof SubmenuPrompter.activePrompter>
+        )
+        regionPrompt.acceptItem('us-west-2')
+        await regionPrompt.result()
     })
 })
