670b0c9e587145bf13a020e18f28516f27103bad
manodnyab <66754471+manodnyab@users.noreply.github.com>
2022-07-26 10:28:10 -0700

    Add error messaging to Filter Streams failures (#3222)
    * revert changes
    
    * added messaging
    
    * detekt changes

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/CloudWatchActor.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/CloudWatchActor.kt
@@ -4,7 +4,9 @@
 package software.aws.toolkits.jetbrains.services.cloudwatch.logs
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
 import com.intellij.ui.TableUtil
 import com.intellij.ui.table.TableView
 import com.intellij.util.ExceptionUtil
@@ -255,12 +257,17 @@ class LogStreamFilterActor(
         return getSearchLogEvents(request)
     }
 
-    private fun getSearchLogEvents(request: FilterLogEventsRequest): List<LogStreamEntry> {
+    private fun getSearchLogEvents(request: FilterLogEventsRequest): List<LogStreamEntry> = try {
         val response = client.filterLogEvents(request)
         val events = response.events().filterNotNull().map { it.toLogStreamEntry() }
         nextForwardToken = response.nextToken()
 
-        return events
+        events
+    } catch (e: Exception) {
+        runInEdt {
+            Messages.showErrorDialog(project, e.localizedMessage, message("cloudwatch.logs.failed_to_load_stream", logStream))
+        }
+        listOf()
     }
 }
 
