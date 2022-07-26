8ae41d1ff6f8a484e34ea07f32e09f745e319a13
Hweinstock <hkobew@amazon.com>
2022-07-26 13:59:07 -0700

    Refactored testing file

--- src/test/cloudWatchLogs/commands/searchLogGroup.test.ts
+++ src/test/cloudWatchLogs/commands/searchLogGroup.test.ts
@@ -16,94 +16,100 @@ import { createWizardTester, WizardTester } from '../../shared/wizards/wizardTes
 import { createQuickPickTester, QuickPickTester } from '../../shared/ui/testUtils'
 
 describe('searchLogGroup', async function () {
-    const fakeLogGroups: string[] = []
-    let filterPatternInputBox: ExposeEmitters<
-        vscode.InputBox,
-        'onDidAccept' | 'onDidChangeValue' | 'onDidTriggerButton'
-    >
-    let filterPatternPrompter: InputBoxPrompter
+    describe('Wizard', async function () {
+        let testWizard: WizardTester<SearchLogGroupWizard>
+        let filterPatternInputBox: ExposeEmitters<
+            vscode.InputBox,
+            'onDidAccept' | 'onDidChangeValue' | 'onDidTriggerButton'
+        >
+        let filterPatternPrompter: InputBoxPrompter
 
-    let testWizard: WizardTester<SearchLogGroupWizard>
-    let testTimeRangeMenu: TimeFilterSubmenu
+        before(function () {
+            filterPatternPrompter = createFilterpatternPrompter()
+            testWizard = createWizardTester(new SearchLogGroupWizard())
+            filterPatternInputBox = exposeEmitters(filterPatternPrompter.inputBox, [
+                'onDidAccept',
+                'onDidChangeValue',
+                'onDidTriggerButton',
+            ])
+        })
+        it('shows logGroup prompt first and filterPattern second, then timerange submenu', function () {
+            testWizard = createWizardTester(new SearchLogGroupWizard())
+            testWizard.submenuResponse.assertShowFirst()
+            testWizard.filterPattern.assertShowSecond()
+            testWizard.timeRange.assertShowThird()
+        })
 
-    let customTimeRangePrompter: InputBoxPrompter
-    let defaultTimeRangePrompter: QuickPickTester<any>
+        it('prompts for filterPattern and accepts input', async function () {
+            /** Sets the input box's value then fires an accept event */
+            // copied from 'src/test/shared/ui/inputPrompter.test.ts'
+            function accept(value: string): void {
+                filterPatternInputBox.value = value
+                filterPatternInputBox.fireOnDidAccept()
+            }
 
-    let testDateBox: ExposeEmitters<vscode.InputBox, 'onDidAccept' | 'onDidChangeValue' | 'onDidTriggerButton'>
-
-    before(function () {
-        fakeLogGroups.push('group-1', 'group-2', 'group-3')
-        filterPatternPrompter = createFilterpatternPrompter()
-        testTimeRangeMenu = new TimeFilterSubmenu()
-        defaultTimeRangePrompter = createQuickPickTester(testTimeRangeMenu.defaultPrompter)
-        customTimeRangePrompter = testTimeRangeMenu.customPrompter
-
-        filterPatternInputBox = exposeEmitters(filterPatternPrompter.inputBox, [
-            'onDidAccept',
-            'onDidChangeValue',
-            'onDidTriggerButton',
-        ])
-        testDateBox = exposeEmitters(customTimeRangePrompter.inputBox, [
-            'onDidAccept',
-            'onDidChangeValue',
-            'onDidTriggerButton',
-        ])
+            const testInput = 'this is my filterPattern'
+            const result = filterPatternPrompter.prompt()
+            accept(testInput)
+            assert.strictEqual(await result, testInput)
+        })
     })
 
-    beforeEach(function () {
-        testWizard = createWizardTester(new SearchLogGroupWizard())
-    })
+    describe('TimeFilterSubmenu', async function () {
+        let defaultTimeRangePrompter: QuickPickTester<any>
+        let testTimeRangeMenu: TimeFilterSubmenu
 
-    it('shows logGroup prompt first and filterPattern second, then timerange submenu', function () {
-        testWizard.submenuResponse.assertShowFirst()
-        testWizard.filterPattern.assertShowSecond()
-        testWizard.timeRange.assertShowThird()
-    })
+        before(function () {
+            testTimeRangeMenu = new TimeFilterSubmenu()
+            defaultTimeRangePrompter = createQuickPickTester(testTimeRangeMenu.defaultPrompter)
+        })
 
-    it('filterPattern InputBox accepts input', async function () {
-        /** Sets the input box's value then fires an accept event */
-        // copied from 'src/test/shared/ui/inputPrompter.test.ts'
-        function accept(value: string): void {
-            filterPatternInputBox.value = value
-            filterPatternInputBox.fireOnDidAccept()
-        }
+        it('gives option for custom input', async function () {
+            defaultTimeRangePrompter.assertContainsItems('Custom time range')
+            defaultTimeRangePrompter.acceptItem('Custom time range')
+            await defaultTimeRangePrompter.result()
+        })
 
-        const testInput = 'this is my filterPattern'
-        const result = filterPatternPrompter.prompt()
-        accept(testInput)
-        assert.strictEqual(await result, testInput)
-    })
+        describe('Custom Date Box', async function () {
+            let testDateBox: ExposeEmitters<vscode.InputBox, 'onDidAccept' | 'onDidChangeValue' | 'onDidTriggerButton'>
+            let customTimeRangePrompter: InputBoxPrompter
 
-    it('Timerange Submenu gives option for custom input', async function () {
-        defaultTimeRangePrompter.assertContainsItems('Custom time range')
-        defaultTimeRangePrompter.acceptItem('Custom time range')
-        await defaultTimeRangePrompter.result()
-    })
+            before(function () {
+                customTimeRangePrompter = testTimeRangeMenu.customPrompter
+                testDateBox = exposeEmitters(customTimeRangePrompter.inputBox, [
+                    'onDidAccept',
+                    'onDidChangeValue',
+                    'onDidTriggerButton',
+                ])
+            })
 
-    it('Datebox accepts valid date format of (YYYY/MM/DD-YYYY/MM/DD)', async function () {
-        function accept(value: string): void {
-            testDateBox.value = value
-            testDateBox.fireOnDidAccept()
-        }
+            it('accepts valid date format of (YYYY/MM/DD-YYYY/MM/DD)', async function () {
+                function accept(value: string): void {
+                    testDateBox.value = value
+                    testDateBox.fireOnDidAccept()
+                }
+                customTimeRangePrompter = testTimeRangeMenu.customPrompter
 
-        const validInput = '2000/10/06-2001/11/08'
-        const result = customTimeRangePrompter.prompt()
-        accept(validInput)
-        assert.strictEqual(await result, validInput)
-    })
+                const validInput = '2000/10/06-2001/11/08'
+                const result = customTimeRangePrompter.prompt()
+                accept(validInput)
+                assert.strictEqual(await result, validInput)
+            })
 
-    it("Datebox validator won't accept invalid date formats", function () {
-        assert(testTimeRangeMenu.validateDate('2000/11/07,2000/12/08')) // No seperator
-        assert(testTimeRangeMenu.validateDate('2002/13/06-2001/11/05')) // Invalid start date
-        assert(testTimeRangeMenu.validateDate('2002/09/06-2001/00/05')) // Invalid end date
-        assert(testTimeRangeMenu.validateDate('10/02/2001-08/05/2001')) // Invalid format of dates
-        assert(testTimeRangeMenu.validateDate('2000/10/01-2000/10/01')) // Same date
-        assert(testTimeRangeMenu.validateDate('2000/10/02-2000/10/01')) // second date occuring earlier
-        assert(testTimeRangeMenu.validateDate('2000/12/01-2099/10/03')) // second date can't be in the future
+            it("validator won't accept invalid date formats", function () {
+                assert(testTimeRangeMenu.validateDate('2000/11/07,2000/12/08')) // No seperator
+                assert(testTimeRangeMenu.validateDate('2002/13/06-2001/11/05')) // Invalid start date
+                assert(testTimeRangeMenu.validateDate('2002/09/06-2001/00/05')) // Invalid end date
+                assert(testTimeRangeMenu.validateDate('10/02/2001-08/05/2001')) // Invalid format of dates
+                assert(testTimeRangeMenu.validateDate('2000/10/01-2000/10/01')) // Same date
+                assert(testTimeRangeMenu.validateDate('2000/10/02-2000/10/01')) // second date occuring earlier
+                assert(testTimeRangeMenu.validateDate('2000/12/01-2099/10/03')) // second date can't be in the future
 
-        // A few valid dates
-        assert(!testTimeRangeMenu.validateDate('2000/10/01-2000/10/03'))
-        assert(!testTimeRangeMenu.validateDate('2000/12/01-2001/10/03'))
-        assert(!testTimeRangeMenu.validateDate('2022/01/01-2022/05/03'))
+                // A few valid dates
+                assert(!testTimeRangeMenu.validateDate('2000/10/01-2000/10/03'))
+                assert(!testTimeRangeMenu.validateDate('2000/12/01-2001/10/03'))
+                assert(!testTimeRangeMenu.validateDate('2022/01/01-2022/05/03'))
+            })
+        })
     })
 })
