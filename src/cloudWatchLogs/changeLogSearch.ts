0538018cab196dcafb729e6e9504cf478a9d0c0b
Hweinstock <hkobew@amazon.com>
2022-07-27 16:01:01 -0700

    add a test case

--- src/cloudWatchLogs/changeLogSearch.ts
+++ src/cloudWatchLogs/changeLogSearch.ts
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import * as telemetry from '../shared/telemetry/telemetry'
+import * as vscode from 'vscode'
 import { showInputBox } from '../shared/ui/inputPrompter'
-import { createURIFromArgs, isLogStream } from './cloudWatchLogsUtils'
+import { isLogStream } from './cloudWatchLogsUtils'
 import { prepareDocument } from './commands/searchLogGroup'
 import { getActiveUri } from './document/logStreamDocumentProvider'
 import { CloudWatchLogsData, filterLogEventsFromUriComponents, LogStreamRegistry } from './registry/logStreamRegistry'
@@ -12,10 +13,11 @@ import { isViewAllEvents, TimeFilterResponse, TimeFilterSubmenu } from './timeFi
 
 type ChangeableParam = 'filterPattern' | 'timeFilter'
 
-export async function changeLogSearchParams(registry: LogStreamRegistry, param: ChangeableParam): Promise<void> {
-    let result: telemetry.Result = 'Succeeded'
-
-    const oldUri = getActiveUri(registry)
+export async function getNewData(
+    registry: LogStreamRegistry,
+    param: ChangeableParam,
+    oldUri: vscode.Uri
+): Promise<CloudWatchLogsData | undefined> {
     const oldData = registry.getLogData(oldUri) as CloudWatchLogsData
     const newData: CloudWatchLogsData = {
         ...oldData,
@@ -31,7 +33,6 @@ export async function changeLogSearchParams(registry: LogStreamRegistry, param: 
                 placeholder: oldData.parameters.filterPattern ?? 'Enter Text Here',
             })
             if (newPattern === undefined) {
-                result = 'Cancelled'
                 return
             }
             newData.parameters.filterPattern = newPattern
@@ -41,7 +42,6 @@ export async function changeLogSearchParams(registry: LogStreamRegistry, param: 
         case 'timeFilter':
             const newTimeRange = (await new TimeFilterSubmenu().prompt()) as TimeFilterResponse
             if (newTimeRange === undefined) {
-                result = 'Cancelled'
                 return
             }
             newData.parameters.startTime = isViewAllEvents(newTimeRange) ? undefined : newTimeRange.start
@@ -49,16 +49,27 @@ export async function changeLogSearchParams(registry: LogStreamRegistry, param: 
 
             break
     }
-
     if (newData.parameters.streamName) {
         newData.retrieveLogsFunction = filterLogEventsFromUriComponents
         newData.parameters.streamNameOptions = [newData.parameters.streamName]
         newData.parameters.streamName = undefined
     }
 
-    registry.deregisterLog(oldUri)
-    const newUri = createURIFromArgs(oldData.logGroupInfo, oldData.parameters)
-    await registry.registerLog(newUri, newData)
+    return newData
+}
+
+export async function changeLogSearchParams(registry: LogStreamRegistry, param: ChangeableParam): Promise<void> {
+    let result: telemetry.Result = 'Succeeded'
+
+    const oldUri = getActiveUri(registry)
+    const newData = await getNewData(registry, param, oldUri)
+
+    if (!newData) {
+        result = 'Cancelled'
+        return
+    }
+
+    const newUri = await registry.registerLogWithNewUri(oldUri, newData)
 
     result = await prepareDocument(newUri, registry)
 
