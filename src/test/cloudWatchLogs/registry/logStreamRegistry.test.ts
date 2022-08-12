4bd9d523b87fe3f9623c88bbcc4717caf73725f9
Hweinstock <hkobew@amazon.com>
2022-08-12 10:44:34 -0700

    add category for streamIds

--- src/test/cloudWatchLogs/registry/logStreamRegistry.test.ts
+++ src/test/cloudWatchLogs/registry/logStreamRegistry.test.ts
@@ -88,69 +88,71 @@ describe('LogStreamRegistry', async function () {
             )
         })
 
-        it('registers stream ids to map and clears it on document close', async function () {
-            await registry.updateLog(searchLogGroupUri)
-            registry.getLogContent(searchLogGroupUri) // We run this to create the mappings
-            const doc = await vscode.workspace.openTextDocument(searchLogGroupUri)
-            let streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-            const expectedMap = new Map<number, string>([
-                [0, testStreamNames[0]],
-                [1, testStreamNames[1]],
-            ])
-            assert.deepStrictEqual(streamIDMap, expectedMap)
-            registry.disposeRegistryData(doc.uri)
-            // We want to re-register log here otherwise this returns undefined.
-            registry.setLogData(searchLogGroupUri, logGroupsStream)
-            streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-            assert.deepStrictEqual(streamIDMap, new Map<number, string>())
-        })
-
-        it('handles newlines within event messages', function () {
-            const oldData = registry.getLogData(searchLogGroupUri)
-            assert(oldData)
-            registry.setLogData(searchLogGroupUri, {
-                ...oldData,
-                data: [
-                    {
-                        message: 'This \n is \n a \n message \n spanning \n many \n lines',
-                        logStreamName: 'stream1',
-                    },
-                    {
-                        message: 'Here \n is \n another \n one.',
-                        logStreamName: 'stream2',
-                    },
-                    {
-                        message: 'and \n just \n one \n more',
-                        logStreamName: 'stream1',
-                    },
-                    {
-                        message: 'and thats it.',
-                        logStreamName: 'stream3',
-                    },
-                ],
+        describe('setStreamIds', function () {
+            it('registers stream ids to map and clears it on document close', async function () {
+                await registry.updateLog(searchLogGroupUri)
+                registry.getLogContent(searchLogGroupUri) // We run this to create the mappings
+                const doc = await vscode.workspace.openTextDocument(searchLogGroupUri)
+                let streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
+                const expectedMap = new Map<number, string>([
+                    [0, testStreamNames[0]],
+                    [1, testStreamNames[1]],
+                ])
+                assert.deepStrictEqual(streamIDMap, expectedMap)
+                registry.disposeRegistryData(doc.uri)
+                // We want to re-register log here otherwise this returns undefined.
+                registry.setLogData(searchLogGroupUri, logGroupsStream)
+                streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
+                assert.deepStrictEqual(streamIDMap, new Map<number, string>())
             })
-            registry.getLogContent(searchLogGroupUri)
-            const streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-            const expectedMap = new Map<number, string>([
-                [0, 'stream1'],
-                [1, 'stream1'],
-                [2, 'stream1'],
-                [3, 'stream1'],
-                [4, 'stream1'],
-                [5, 'stream1'],
-                [6, 'stream1'],
-                [7, 'stream2'],
-                [8, 'stream2'],
-                [9, 'stream2'],
-                [10, 'stream2'],
-                [11, 'stream1'],
-                [12, 'stream1'],
-                [13, 'stream1'],
-                [14, 'stream1'],
-                [15, 'stream3'],
-            ])
-            assert.deepStrictEqual(streamIDMap, expectedMap)
-            registry.setLogData(searchLogGroupUri, oldData)
+
+            it('handles newlines within event messages', function () {
+                const oldData = registry.getLogData(searchLogGroupUri)
+                assert(oldData)
+                registry.setLogData(searchLogGroupUri, {
+                    ...oldData,
+                    data: [
+                        {
+                            message: 'This \n is \n a \n message \n spanning \n many \n lines',
+                            logStreamName: 'stream1',
+                        },
+                        {
+                            message: 'Here \n is \n another \n one.',
+                            logStreamName: 'stream2',
+                        },
+                        {
+                            message: 'and \n just \n one \n more',
+                            logStreamName: 'stream1',
+                        },
+                        {
+                            message: 'and thats it.',
+                            logStreamName: 'stream3',
+                        },
+                    ],
+                })
+                registry.getLogContent(searchLogGroupUri)
+                const streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
+                const expectedMap = new Map<number, string>([
+                    [0, 'stream1'],
+                    [1, 'stream1'],
+                    [2, 'stream1'],
+                    [3, 'stream1'],
+                    [4, 'stream1'],
+                    [5, 'stream1'],
+                    [6, 'stream1'],
+                    [7, 'stream2'],
+                    [8, 'stream2'],
+                    [9, 'stream2'],
+                    [10, 'stream2'],
+                    [11, 'stream1'],
+                    [12, 'stream1'],
+                    [13, 'stream1'],
+                    [14, 'stream1'],
+                    [15, 'stream3'],
+                ])
+                assert.deepStrictEqual(streamIDMap, expectedMap)
+                registry.setLogData(searchLogGroupUri, oldData)
+            })
         })
     })
 
