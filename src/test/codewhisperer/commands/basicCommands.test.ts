859fd239a730091eec4eac6f0d729d5339d6da3b
Shao-Hua Lo <xshaohua@amazon.com>
2022-08-16 16:52:01 -0700

    minor fixes

--- src/test/codewhisperer/commands/basicCommands.test.ts
+++ src/test/codewhisperer/commands/basicCommands.test.ts
@@ -7,7 +7,7 @@ import * as assert from 'assert'
 import { beforeEach } from 'mocha'
 import * as sinon from 'sinon'
 import { resetCodeWhispererGlobalVariables } from '../testUtil'
-import { assertTelemetryCurried, testCommand } from '../../testUtil'
+import { assertTelemetryCurried } from '../../testUtil'
 import { CodeWhispererConstants } from '../../../codewhisperer/models/constants'
 import { toggleCodeSuggestions, get, set } from '../../../codewhisperer/commands/basicCommands'
 import { FakeExtensionContext, FakeMemento } from '../../fakeExtensionContext'
@@ -18,7 +18,7 @@ import { RegionProvider } from '../../../shared/regions/regionProvider'
 import { OutputChannel } from 'vscode'
 import { TelemetryService } from '../../../shared/telemetry/telemetryService'
 import { CredentialsStore } from '../../../credentials/credentialsStore'
-import { RegisteredCommand } from '../../../shared/vscode/commands2'
+import { testCommand } from '../../shared/vscode/testUtils'
 
 describe('CodeWhisperer-basicCommands', function () {
     describe('CodeWhisperer-basicCommands', function () {
@@ -68,13 +68,13 @@ describe('CodeWhisperer-basicCommands', function () {
                 credentialsStore: {} as CredentialsStore,
                 invokeOutputChannel: {} as OutputChannel,
             }
-            const toggleCodeSuggestionsTestCommand = testCommand(toggleCodeSuggestions, fakeExtContext)
+            const targetCommand = testCommand(toggleCodeSuggestions, fakeExtContext)
 
             assert.strictEqual(
                 fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey),
                 undefined
             )
-            await toggleCodeSuggestionsTestCommand.execute()
+            await targetCommand.execute()
             const res = fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey)
             assert.strictEqual(res, true)
             assertTelemetryCurried('aws_modifySetting')({
@@ -82,38 +82,38 @@ describe('CodeWhisperer-basicCommands', function () {
                 settingState: CodeWhispererConstants.AutoSuggestion.activated,
             })
 
-            ;(toggleCodeSuggestionsTestCommand as RegisteredCommand).dispose()
-        })
-    })
-
-    it('should emit aws_modifySetting event on user toggling autoSuggestion - deactivate', async function () {
-        const fakeExtContext: ExtContext = {
-            extensionContext: await FakeExtensionContext.create(),
-            awsContext: {} as AwsContext,
-            samCliContext: () => {
-                return {} as SamCliContext
-            },
-            regionProvider: {} as RegionProvider,
-            outputChannel: {} as OutputChannel,
-            telemetryService: {} as TelemetryService,
-            credentialsStore: {} as CredentialsStore,
-            invokeOutputChannel: {} as OutputChannel,
-        }
-        const toggleCodeSuggestionsTestCommand = testCommand(toggleCodeSuggestions, fakeExtContext)
-        fakeExtContext.extensionContext.globalState.update(CodeWhispererConstants.autoTriggerEnabledKey, true)
-        assert.strictEqual(
-            fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey),
-            true
-        )
-
-        await toggleCodeSuggestionsTestCommand.execute()
-        const res = fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey)
-        assert.strictEqual(res, false)
-        assertTelemetryCurried('aws_modifySetting')({
-            settingId: CodeWhispererConstants.AutoSuggestion.settingId,
-            settingState: CodeWhispererConstants.AutoSuggestion.deactivated,
+            targetCommand.dispose()
         })
 
-        ;(toggleCodeSuggestionsTestCommand as RegisteredCommand).dispose()
+        it('should emit aws_modifySetting event on user toggling autoSuggestion - deactivate', async function () {
+            const fakeExtContext: ExtContext = {
+                extensionContext: await FakeExtensionContext.create(),
+                awsContext: {} as AwsContext,
+                samCliContext: () => {
+                    return {} as SamCliContext
+                },
+                regionProvider: {} as RegionProvider,
+                outputChannel: {} as OutputChannel,
+                telemetryService: {} as TelemetryService,
+                credentialsStore: {} as CredentialsStore,
+                invokeOutputChannel: {} as OutputChannel,
+            }
+            const targetCommand = testCommand(toggleCodeSuggestions, fakeExtContext)
+            fakeExtContext.extensionContext.globalState.update(CodeWhispererConstants.autoTriggerEnabledKey, true)
+            assert.strictEqual(
+                fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey),
+                true
+            )
+
+            await targetCommand.execute()
+            const res = fakeExtContext.extensionContext.globalState.get(CodeWhispererConstants.autoTriggerEnabledKey)
+            assert.strictEqual(res, false)
+            assertTelemetryCurried('aws_modifySetting')({
+                settingId: CodeWhispererConstants.AutoSuggestion.settingId,
+                settingState: CodeWhispererConstants.AutoSuggestion.deactivated,
+            })
+
+            targetCommand.dispose()
+        })
     })
 })
