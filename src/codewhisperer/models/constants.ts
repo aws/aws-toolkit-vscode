bc898022b6c493d01b734505b1df635f77d6a6ba
Shao-Hua Lo <xshaohua@amazon.com>
2022-09-09 10:59:11 -0700

    remove name space and directly export const

--- src/codewhisperer/models/constants.ts
+++ src/codewhisperer/models/constants.ts
@@ -3,193 +3,191 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export namespace CodeWhispererConstants {
-    /**
-     * SDK Client
-     */
-    export const endpoint = 'https://codewhisperer.us-east-1.amazonaws.com/'
+/**
+ * SDK Client
+ */
+export const endpoint = 'https://codewhisperer.us-east-1.amazonaws.com/'
 
-    export const region = 'us-east-1'
+export const region = 'us-east-1'
 
-    /**
-     * Automated and manual trigger
-     */
-    export const invocationTimeIntervalThreshold = 2 // seconds
+/**
+ * Automated and manual trigger
+ */
+export const invocationTimeIntervalThreshold = 2 // seconds
 
-    export const promiseTimeoutLimit = 15 // seconds
+export const promiseTimeoutLimit = 15 // seconds
 
-    export const invocationKeyThreshold = 15
+export const invocationKeyThreshold = 15
 
-    export const specialCharactersList = ['{', '[', '(', ':', '\t', '\n']
+export const specialCharactersList = ['{', '[', '(', ':', '\t', '\n']
 
-    export const normalTextChangeRegex = /[A-Za-z0-9]/g
+export const normalTextChangeRegex = /[A-Za-z0-9]/g
 
-    export namespace AutoSuggestion {
-        export const settingId = 'codewhisperer_autoSuggestionActivation'
-        export const activated = 'Activated'
-        export const deactivated = 'Deactivated'
-    }
+export const autoSuggestionConfig = {
+    settingId: 'codewhisperer_autoSuggestionActivation',
+    activated: 'Activated',
+    deactivated: 'Deactivated',
+}
 
-    /**
-     * EditorCon context
-     */
-    export const charactersLimit = 10240
+/**
+ * EditorCon context
+ */
+export const charactersLimit = 10240
 
-    export const filenameCharsLimit = 1024
+export const filenameCharsLimit = 1024
 
-    export const naturalLanguage = 'en-US'
+export const naturalLanguage = 'en-US'
 
-    export const maxRecommendations = 1
+export const maxRecommendations = 1
 
-    export const space = ' '
+export const space = ' '
 
-    export const lineBreak = '\n'
+export const lineBreak = '\n'
 
-    export const lineBreakWin = '\r\n'
+export const lineBreakWin = '\r\n'
 
-    /**
-     * Ux of recommendations
-     */
-    export const labelLength = 20
+/**
+ * Ux of recommendations
+ */
+export const labelLength = 20
 
-    export const completionDetail = 'CodeWhisperer'
+export const completionDetail = 'CodeWhisperer'
 
-    /**
-     * CodeWhisperer in configuration
-     */
-    export const codewhisperer = 'CodeWhisperer'
+/**
+ * CodeWhisperer in configuration
+ */
+export const codewhisperer = 'CodeWhisperer'
 
-    /**
-     * Supported languages
-     */
-    export const java = 'java'
+/**
+ * Supported languages
+ */
+export const java = 'java'
 
-    export const python = 'python'
+export const python = 'python'
 
-    export const javascript = 'javascript'
+export const javascript = 'javascript'
 
-    export const typescript = 'typescript'
+export const typescript = 'typescript'
 
-    export const supportedLanguages = ['java', 'python', 'javascript', 'typescript']
+export const supportedLanguages = ['java', 'python', 'javascript', 'typescript']
 
-    /**
-     * Prompt
-     */
-    export const pendingResponse = 'Waiting for CodeWhisperer...'
+/**
+ * Prompt
+ */
+export const pendingResponse = 'Waiting for CodeWhisperer...'
 
-    export const runningSecurityScan = 'Running security scan...'
+export const runningSecurityScan = 'Running security scan...'
 
-    export const noSuggestions = 'No suggestions from CodeWhisperer'
+export const noSuggestions = 'No suggestions from CodeWhisperer'
 
-    export const licenseFilter = 'CodeWhisperer suggestions were filtered due to reference setting'
+export const licenseFilter = 'CodeWhisperer suggestions were filtered due to reference setting'
 
-    /**
-     * Beta landing page file
-     */
-    export const welcomeCodeWhispererReadmeFileSource = 'resources/markdown/WelcomeToCodeWhisperer.md'
+/**
+ * Beta landing page file
+ */
+export const welcomeCodeWhispererReadmeFileSource = 'resources/markdown/WelcomeToCodeWhisperer.md'
 
-    export const welcomeCodeWhispererCloud9ReadmeFileSource = 'resources/markdown/WelcomeToCodeWhispererCloud9.md'
+export const welcomeCodeWhispererCloud9ReadmeFileSource = 'resources/markdown/WelcomeToCodeWhispererCloud9.md'
 
-    export const welcomeMessageKey = 'CODEWHISPERER_WELCOME_MESSAGE'
+export const welcomeMessageKey = 'CODEWHISPERER_WELCOME_MESSAGE'
 
-    /**
-     * Key bindings JSON file path
-     */
-    export const keyBindingPathMac = 'Library/Application Support/Code/User/keybindings.json'
+/**
+ * Key bindings JSON file path
+ */
+export const keyBindingPathMac = 'Library/Application Support/Code/User/keybindings.json'
 
-    export const keyBindingPathLinux = '.config/Code/User/keybindings.json'
+export const keyBindingPathLinux = '.config/Code/User/keybindings.json'
 
-    export const keyBindingPathWin = 'Code/User/keybindings.json'
+export const keyBindingPathWin = 'Code/User/keybindings.json'
 
-    /**
-     * Length of left context preview in output channel
-     */
-    export const contextPreviewLen = 20
+/**
+ * Length of left context preview in output channel
+ */
+export const contextPreviewLen = 20
 
-    /**
-     * Unsupported language cache
-     */
-    export const unsupportedLanguagesCacheTTL = 10 * 60 * 60 * 1000
+/**
+ * Unsupported language cache
+ */
+export const unsupportedLanguagesCacheTTL = 10 * 60 * 60 * 1000
 
-    export const unsupportedLanguagesKey = 'CODEWHISPERER_UNSUPPORTED_LANGUAGES_KEY'
+export const unsupportedLanguagesKey = 'CODEWHISPERER_UNSUPPORTED_LANGUAGES_KEY'
 
-    export const autoTriggerEnabledKey = 'CODEWHISPERER_AUTO_TRIGGER_ENABLED'
+export const autoTriggerEnabledKey = 'CODEWHISPERER_AUTO_TRIGGER_ENABLED'
 
-    export const termsAcceptedKey = 'CODEWHISPERER_TERMS_ACCEPTED'
+export const termsAcceptedKey = 'CODEWHISPERER_TERMS_ACCEPTED'
 
-    export const serviceActiveKey = 'CODEWHISPERER_SERVICE_ACTIVE'
+export const serviceActiveKey = 'CODEWHISPERER_SERVICE_ACTIVE'
 
-    export const accessToken = 'CODEWHISPERER_ACCESS_TOKEN'
+export const accessToken = 'CODEWHISPERER_ACCESS_TOKEN'
 
-    export const learnMoreUri = 'https://aws.amazon.com/codewhisperer'
+export const learnMoreUri = 'https://aws.amazon.com/codewhisperer'
 
-    export const previewSignupPortal = 'https://pages.awscloud.com/codewhisperer-sign-up-form.html'
+export const previewSignupPortal = 'https://pages.awscloud.com/codewhisperer-sign-up-form.html'
 
-    export const identityPoolID = 'us-east-1:70717e99-906f-4add-908c-bd9074a2f5b9'
+export const identityPoolID = 'us-east-1:70717e99-906f-4add-908c-bd9074a2f5b9'
 
-    /**
-     * the interval of the background thread invocation, which is triggered by the timer
-     */
-    export const defaultCheckPeriodMillis = 1000 * 60 * 5
+/**
+ * the interval of the background thread invocation, which is triggered by the timer
+ */
+export const defaultCheckPeriodMillis = 1000 * 60 * 5
 
-    // suggestion show delay, in milliseconds
-    export const suggestionShowDelay = 250
+// suggestion show delay, in milliseconds
+export const suggestionShowDelay = 250
 
-    export const referenceLog = 'CodeWhisperer Reference Log'
+export const referenceLog = 'CodeWhisperer Reference Log'
 
-    export const suggestionDetailReferenceText = (licenses: string) =>
-        `Reference code under ${licenses}. View full details in CodeWhisperer reference log.`
+export const suggestionDetailReferenceText = (licenses: string) =>
+    `Reference code under ${licenses}. View full details in CodeWhisperer reference log.`
 
-    export const hoverInlayText = (licenseName: string | undefined, repository: string | undefined) =>
-        `Reference code under the ${licenseName} license from repository ${repository}`
+export const hoverInlayText = (licenseName: string | undefined, repository: string | undefined) =>
+    `Reference code under the ${licenseName} license from repository ${repository}`
 
-    export const referenceLogText = (
-        code: string,
-        license: string,
-        repository: string,
-        filePath: string,
-        lineInfo: string
-    ) =>
-        `with code ${code} provided with reference under ${license} from repository ${repository}. Added to ${filePath} ${lineInfo}.`
+export const referenceLogText = (
+    code: string,
+    license: string,
+    repository: string,
+    filePath: string,
+    lineInfo: string
+) =>
+    `with code ${code} provided with reference under ${license} from repository ${repository}. Added to ${filePath} ${lineInfo}.`
 
-    export const referenceLogPromptText = `Don\'t want suggestions that include code with references? Uncheck this option in 
+export const referenceLogPromptText = `Don\'t want suggestions that include code with references? Uncheck this option in 
     <a href="#" onclick="openSettings();return false;">CodeWhisperer Settings</a>`
 
-    /**
-     * Security Scan
-     */
-    export const codeScanJavaPayloadSizeLimitBytes = Math.pow(2, 20) // 1 MB
+/**
+ * Security Scan
+ */
+export const codeScanJavaPayloadSizeLimitBytes = Math.pow(2, 20) // 1 MB
 
-    export const codeScanPythonPayloadSizeLimitBytes = 200 * Math.pow(2, 10) // 200 KB
+export const codeScanPythonPayloadSizeLimitBytes = 200 * Math.pow(2, 10) // 200 KB
 
-    export const codeScanTruncDirPrefix = 'codewhisperer_scan'
+export const codeScanTruncDirPrefix = 'codewhisperer_scan'
 
-    export const codeScanZipExt = '.zip'
+export const codeScanZipExt = '.zip'
 
-    export const contextTruncationTimeoutSeconds = 10
+export const contextTruncationTimeoutSeconds = 10
 
-    export const codeScanJobTimeoutSeconds = 50
+export const codeScanJobTimeoutSeconds = 50
 
-    export const projectSizeCalculateTimeoutSeconds = 10
+export const projectSizeCalculateTimeoutSeconds = 10
 
-    export const codeScanJobPollingIntervalSeconds = 5
+export const codeScanJobPollingIntervalSeconds = 5
 
-    export const artifactTypeSource = 'SourceCode'
+export const artifactTypeSource = 'SourceCode'
 
-    export const artifactTypeBuild = 'BuiltJars'
+export const artifactTypeBuild = 'BuiltJars'
 
-    export const codeScanFindingsSchema = 'codescan/findings/1.0'
+export const codeScanFindingsSchema = 'codescan/findings/1.0'
 
-    // telemetry experiment id
-    export const experimentId = 'codeWhisperer'
+// telemetry experiment id
+export const experimentId = 'codeWhisperer'
 
-    // wait time for editor to update editor.selection.active (in milliseconds)
-    export const vsCodeCursorUpdateDelay = 3
+// wait time for editor to update editor.selection.active (in milliseconds)
+export const vsCodeCursorUpdateDelay = 3
 
-    // cloud9 access state
-    export const cloud9AccessStateKey = 'cloud9AccessStateKey'
+// cloud9 access state
+export const cloud9AccessStateKey = 'cloud9AccessStateKey'
 
-    export const cloud9AccessSent = 'Access requested!'
+export const cloud9AccessSent = 'Access requested!'
 
-    export const cloud9AccessAlreadySent = 'Access has already been requested, we are still processing it.'
-}
+export const cloud9AccessAlreadySent = 'Access has already been requested, we are still processing it.'
