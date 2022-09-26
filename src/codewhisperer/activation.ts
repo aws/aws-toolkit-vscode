aws/aws-toolkit-vscode

commit f82944e81c155ef938ab2e264b6747b47d54fa34

refactor(codewhisperer): Simplify pagination  (#2904)

src/codewhisperer/service/inlineCompletionService.ts
@@ -174,11 +174,15 @@ export class InlineCompletionService {
     private hide: vscode.Disposable
     private next: vscode.Disposable
     private prev: vscode.Disposable
+    private _isPaginationRunning = false
 
     constructor() {
         this.prev = prevCommand.register(this)
         this.next = nextCommand.register(this)
         this.hide = hideCommand.register(this)
+        RecommendationHandler.instance.onDidReceiveRecommendation(e => {
+            this.tryShowRecommendation()
+        })
     }
 
     static #instance: InlineCompletionService
@@ -196,7 +200,7 @@ export class InlineCompletionService {
         this.hide = hideCommand.register(this)
     }
 
-    subscribeCommands() {
+    subscribeSuggestionCommands() {
         this.disposeCommandOverrides()
         this.registerCommandOverrides()
         globals.context.subscriptions.push(this.prev)
@@ -244,6 +248,28 @@ export class InlineCompletionService {
         this.clearRejectionTimer()
     }
 
+    async tryShowRecommendation() {
+        const editor = vscode.window.activeTextEditor
+        if (this.isSuggestionVisible() || editor === undefined) return
+        if (
+            editor.selection.active.isBefore(RecommendationHandler.instance.startPos) ||
+            editor.document.uri.fsPath !== this.documentUri?.fsPath
+        ) {
+            RecommendationHandler.instance.cancelPaginatedRequest()
+            RecommendationHandler.instance.recommendations.forEach((r, i) => {
+                RecommendationHandler.instance.setSuggestionState(i, 'Discard')
+            })
+            RecommendationHandler.instance.reportUserDecisionOfCurrentRecommendation(editor, -1)
+            RecommendationHandler.instance.clearRecommendations()
+        } else if (RecommendationHandler.instance.recommendations.length > 0) {
+            RecommendationHandler.instance.moveStartPositionToSkipSpaces(editor)
+            await HoverConfigUtil.instance.overwriteHoverConfig()
+            this.subscribeSuggestionCommands()
+            this.startRejectionTimer(editor)
+            await this.showRecommendation(0, true)
+        }
+    }
+
     async getPaginatedRecommendation(
         client: DefaultCodeWhispererClient,
         editor: vscode.TextEditor,
@@ -251,11 +277,9 @@ export class InlineCompletionService {
         config: ConfigurationEntry,
         autoTriggerType?: CodewhispererAutomatedTriggerType
     ) {
-        if (vsCodeState.isCodeWhispererEditing || this.isPaginationRunning()) return
+        if (vsCodeState.isCodeWhispererEditing || this._isPaginationRunning) return
         await this.clearInlineCompletionStates(editor)
         this.setCodeWhispererStatusBarLoading()
-        await HoverConfigUtil.instance.overwriteHoverConfig()
-        this.subscribeCommands()
         RecommendationHandler.instance.checkAndResetCancellationTokens()
         this.documentUri = editor.document.uri
         try {
@@ -270,32 +294,12 @@ export class InlineCompletionService {
                     true,
                     page
                 )
-
                 if (RecommendationHandler.instance.checkAndResetCancellationTokens()) {
                     RecommendationHandler.instance.reportUserDecisionOfCurrentRecommendation(editor, -1)
                     RecommendationHandler.instance.clearRecommendations()
                     this.setCodeWhispererStatusBarOk()
                     return
                 }
-
-                if (!this.isSuggestionVisible()) {
-                    if (
-                        editor.selection.active.isBefore(RecommendationHandler.instance.startPos) ||
-                        editor.document.uri.fsPath !== this.documentUri?.fsPath
-                    ) {
-                        RecommendationHandler.instance.cancelPaginatedRequest()
-                        RecommendationHandler.instance.recommendations.forEach((r, i) => {
-                            RecommendationHandler.instance.setSuggestionState(i, 'Discard')
-                        })
-                        RecommendationHandler.instance.reportUserDecisionOfCurrentRecommendation(editor, -1)
-                        RecommendationHandler.instance.clearRecommendations()
-                    } else if (RecommendationHandler.instance.recommendations.length > 0) {
-                        RecommendationHandler.instance.moveStartPositionToSkipSpaces(editor)
-                        this.startRejectionTimer(editor)
-                        await this.showRecommendation(0, true)
-                    }
-                }
-
                 if (!RecommendationHandler.instance.hasNextToken()) break
                 page++
             }
@@ -309,9 +313,6 @@ export class InlineCompletionService {
             } else {
                 showTimedMessage(CodeWhispererConstants.noSuggestions, 2000)
             }
-        }
-        if (RecommendationHandler.instance.recommendations.length === 0) {
-            await HoverConfigUtil.instance.restoreHoverConfig()
         }
     }
 
@@ -349,11 +350,13 @@ export class InlineCompletionService {
     }
 
     setCodeWhispererStatusBarLoading() {
+        this._isPaginationRunning = true
         this.statusBar.text = ` $(loading~spin)CodeWhisperer`
         this.statusBar.show()
     }
 
     setCodeWhispererStatusBarOk() {
+        this._isPaginationRunning = false
         this.statusBar.text = ` $(check)CodeWhisperer`
         this.statusBar.show()
     }
@@ -364,10 +367,6 @@ export class InlineCompletionService {
 
     isSuggestionVisible(): boolean {
         return this.inlineCompletionProvider?.getActiveItemIndex !== undefined
-    }
-
-    isPaginationRunning() {
-        return this.statusBar.text === ` $(loading~spin)CodeWhisperer`
     }
 
     private clearRejectionTimer() {

src/codewhisperer/service/recommendationHandler.ts
@@ -45,6 +45,8 @@ export class RecommendationHandler {
     private cancellationToken: vscode.CancellationTokenSource
     public errorMessagePrompt: string
     public isGenerateRecommendationInProgress: boolean
+    private _onDidReceiveRecommendation: vscode.EventEmitter<void> = new vscode.EventEmitter<void>()
+    public readonly onDidReceiveRecommendation: vscode.Event<void> = this._onDidReceiveRecommendation.event
 
     constructor() {
         this.requestId = ''
@@ -279,6 +281,7 @@ export class RecommendationHandler {
                 }
             })
             this.recommendations = isCloud9() ? recommendation : this.recommendations.concat(recommendation)
+            this._onDidReceiveRecommendation.fire()
         } else {
             TelemetryHelper.instance.recordUserDecisionTelemetryForEmptyList(
                 requestId,

commit 6e625ece4b922ca2dcc5f0ceae100f3a37b2c8fa

fix(codewhisperer): Sometimes not show recommendation bug in new inline code path (#2909)
## Problem
Fix a critical bug that were introduced in f82944e

When changing the hover config in inlineCompletionService, a document text close event will be sent and received by the listener in activation.ts, which removed the recommendations.

This bug will make new inline completion not showing the recommendation if there is only one recommendation in paginated response.

No changelog item is needed for this fix.

## Solution
Let the doc close listener only remove recommendation when the closed file is the invocation file.

src/codewhisperer/activation.ts
@@ -174,6 +174,7 @@ export async function activate(context: ExtContext): Promise<void> {
         acceptSuggestion.register(context),
         // on text document close.
         vscode.workspace.onDidCloseTextDocument(e => {
+            if (isInlineCompletionEnabled() && e.uri.fsPath !== InlineCompletionService.instance.filePath()) return
             RecommendationHandler.instance.reportUserDecisionOfCurrentRecommendation(vscode.window.activeTextEditor, -1)
             RecommendationHandler.instance.clearRecommendations()
         }),

src/codewhisperer/service/inlineCompletionService.ts
@@ -191,6 +191,10 @@ export class InlineCompletionService {
         return (this.#instance ??= new this())
     }
 
+    filePath(): string | undefined {
+        return this.documentUri?.fsPath
+    }
+
     // These commands override the vs code inline completion commands
     // They are subscribed when suggestion starts and disposed when suggestion is accepted/rejected
     // to avoid impacting other plugins or user who uses this API