a5ddd393b0e60e7f3fd2dcb242554a3200c0c443
Richard Li <742829+rli@users.noreply.github.com>
2022-10-17 18:38:47 -0700

    Add 213 deprecation message (#3328)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/notification/MinimumVersionChange.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/notification/MinimumVersionChange.kt
@@ -55,8 +55,8 @@ class MinimumVersionChange @JvmOverloads constructor(isUnderTest: Boolean = fals
     }
 
     companion object {
-        const val MIN_VERSION = 213
-        const val MIN_VERSION_HUMAN = "2021.3"
+        const val MIN_VERSION = 221
+        const val MIN_VERSION_HUMAN = "2022.1"
 
         // Used by tests to make sure the prompt never shows up
         const val SKIP_PROMPT = "aws.suppress_deprecation_prompt"
