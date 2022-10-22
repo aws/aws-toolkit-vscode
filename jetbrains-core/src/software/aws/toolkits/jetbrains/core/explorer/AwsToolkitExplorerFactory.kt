b653f0105e2e90c1f957d5f313947e50e1b33f49
Richard Li <742829+rli@users.noreply.github.com>
2022-10-21 21:47:53 -0700

    Make explorer actions consistent across tabs (#3343)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsToolkitExplorerFactory.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsToolkitExplorerFactory.kt
@@ -3,13 +3,19 @@
 
 package software.aws.toolkits.jetbrains.core.explorer
 
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ex.ToolWindowEx
 import com.intellij.ui.content.ContentManagerEvent
 import com.intellij.ui.content.ContentManagerListener
+import software.aws.toolkits.jetbrains.core.experiments.ExperimentsActionGroup
 import software.aws.toolkits.jetbrains.core.help.HelpIds
+import software.aws.toolkits.jetbrains.utils.actions.OpenBrowserAction
 import software.aws.toolkits.resources.message
 
 class AwsToolkitExplorerFactory : ToolWindowFactory, DumbAware {
@@ -26,6 +32,38 @@ class AwsToolkitExplorerFactory : ToolWindowFactory, DumbAware {
                 }
             }
         })
+
+        if (toolWindow is ToolWindowEx) {
+            val actionManager = ActionManager.getInstance()
+            toolWindow.setTitleActions(listOf(actionManager.getAction("aws.toolkit.explorer.titleBar")))
+            toolWindow.setAdditionalGearActions(
+                DefaultActionGroup().apply {
+                    add(
+                        OpenBrowserAction(
+                            title = message("explorer.view_documentation"),
+                            url = "https://docs.aws.amazon.com/console/toolkit-for-jetbrains"
+                        )
+                    )
+                    add(
+                        OpenBrowserAction(
+                            title = message("explorer.view_source"),
+                            icon = AllIcons.Vcs.Vendors.Github,
+                            url = "https://github.com/aws/aws-toolkit-jetbrains"
+                        )
+                    )
+                    add(
+                        OpenBrowserAction(
+                            title = message("explorer.create_new_issue"),
+                            icon = AllIcons.Vcs.Vendors.Github,
+                            url = "https://github.com/aws/aws-toolkit-jetbrains/issues/new/choose"
+                        )
+                    )
+                    add(actionManager.getAction("aws.toolkit.showFeedback"))
+                    add(ExperimentsActionGroup())
+                    add(actionManager.getAction("aws.settings.show"))
+                }
+            )
+        }
 
         // content will be added to tool window in initialization of AwsToolkitExplorerToolWindow
         AwsToolkitExplorerToolWindow.getInstance(project)
--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ToolkitToolWindowListener.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ToolkitToolWindowListener.kt
@@ -3,50 +3,18 @@
 
 package software.aws.toolkits.jetbrains.core.explorer
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.openapi.wm.ex.ToolWindowEx
-import software.aws.toolkits.jetbrains.core.experiments.ExperimentsActionGroup
-import software.aws.toolkits.jetbrains.utils.actions.OpenBrowserAction
 import software.aws.toolkits.resources.message
 
 class ToolkitToolWindowListener(project: Project) {
     private val toolWindowId = AwsToolkitExplorerFactory.TOOLWINDOW_ID
     private val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(toolWindowId)
         ?: throw IllegalStateException("Can't find tool window $toolWindowId")
-    private val defaultActions by lazy { DefaultActionGroup() }
     private val actionManager by lazy { ActionManager.getInstance() }
     private val explorerActions by lazy { listOf(actionManager.getAction("aws.toolkit.explorer.titleBar")) }
-    private val explorerGearActions by lazy {
-        DefaultActionGroup().apply {
-            add(
-                OpenBrowserAction(
-                    title = message("explorer.view_documentation"),
-                    url = "https://docs.aws.amazon.com/console/toolkit-for-jetbrains"
-                )
-            )
-            add(
-                OpenBrowserAction(
-                    title = message("explorer.view_source"),
-                    icon = AllIcons.Vcs.Vendors.Github,
-                    url = "https://github.com/aws/aws-toolkit-jetbrains"
-                )
-            )
-            add(
-                OpenBrowserAction(
-                    title = message("explorer.create_new_issue"),
-                    icon = AllIcons.Vcs.Vendors.Github,
-                    url = "https://github.com/aws/aws-toolkit-jetbrains/issues/new/choose"
-                )
-            )
-            add(actionManager.getAction("aws.toolkit.showFeedback"))
-            add(ExperimentsActionGroup())
-            add(actionManager.getAction("aws.settings.show"))
-        }
-    }
 
     private val developerToolsActions by lazy {
         listOf(
@@ -58,10 +26,8 @@ class ToolkitToolWindowListener(project: Project) {
         if (toolWindow is ToolWindowEx) {
             if (tabName == message("explorer.toolwindow.title")) {
                 toolWindow.setTitleActions(explorerActions)
-                toolWindow.setAdditionalGearActions(explorerGearActions)
             } else if (tabName == message("aws.developer.tools.tab.title")) {
                 toolWindow.setTitleActions(developerToolsActions)
-                toolWindow.setAdditionalGearActions(defaultActions)
             }
         }
     }
