b5a88989b373b480b55be5193fcb4583fbf4ab33
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2022-12-02 14:06:17 -0800

    feat(codecatalyst): guide users to connect if not already connected (#3026)

--- src/codecatalyst/commands.ts
+++ src/codecatalyst/commands.ts
@@ -133,11 +133,12 @@ function createClientInjector(
     clientFactory: () => Promise<CodeCatalystClient>
 ): ClientInjector {
     return async (command, ...args) => {
-        const client = await clientFactory()
+        let client = await clientFactory()
 
         try {
             if (!client.connected) {
-                throw new ToolkitError('Not connected to CodeCatalyst', { code: 'NoConnection' })
+                const conn = await authProvider.promptNotConnected()
+                client = await client.setCredentials(async () => (await conn.getToken()).accessToken)
             }
 
             return await command(client, ...args)
