1e1d25182c6bf5f61ad6722565c8256399113b68
JadenSimon <sijaden@amazon.com>
2022-12-05 10:16:13 -0800

    Fix tests again

--- src/test/shared/credentials/credentialsStore.test.ts
+++ src/test/shared/credentials/credentialsStore.test.ts
@@ -38,7 +38,7 @@ describe('CredentialsStore', async function () {
     ): CredentialsProvider {
         return {
             getCredentials: async () => testCredentials,
-            getCredentialsId: () => ({ credentialSource: 'profile', credentialTypeId: 'default' }),
+            getCredentialsId: () => sampleCredentialsId,
             getHashCode: () => credentialsHashCode,
         } as unknown as CredentialsProvider
     }
