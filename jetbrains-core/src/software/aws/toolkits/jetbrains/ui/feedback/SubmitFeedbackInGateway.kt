c35ffdbd545c104c2cc5e23bb4dbf02bdc08953a
manodnyab <66754471+manodnyab@users.noreply.github.com>
2023-01-23 10:41:25 -0800

    Feedback option in gateway (#3441)
    * added feedback option in gateway
    
    * feedback changes

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/feedback/SubmitFeedbackInGateway.kt
@@ -0,0 +1,18 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.ui.feedback
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.project.DefaultProjectFactory
+import com.intellij.openapi.project.DumbAwareAction
+import software.aws.toolkits.resources.message
+
+class SubmitFeedbackInGateway : DumbAwareAction(message("feedback.title")) {
+    override fun actionPerformed(e: AnActionEvent) {
+        runInEdt {
+            FeedbackDialog(DefaultProjectFactory.getInstance().defaultProject).show()
+        }
+    }
+}
