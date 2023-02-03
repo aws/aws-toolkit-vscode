7ed2e5ec6f9cb2de1d2f13d4939a5fd514d5a139
Nikolas Komonen <nkomonen@amazon.com>
2023-02-03 14:29:09 -0500

    refactor: generateTextFromLogEvents test
    - Updates the generateTextFromLogEvents test
      to work with the new refactor
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/cloudWatchLogs/document/textContent.ts
+++ src/cloudWatchLogs/document/textContent.ts
@@ -9,6 +9,7 @@ import { INSIGHTS_TIMESTAMP_FORMAT } from '../../shared/constants'
 import { CloudWatchLogsEvent } from '../registry/logDataRegistry'
 
 export type StreamIdMap = Map<number, string>
+export const timestampSpaceEquivalent = '                             '
 
 export function generateTextFromLogEvents(
     events: CloudWatchLogsEvent[],
@@ -16,7 +17,6 @@ export function generateTextFromLogEvents(
 ): { text: string; streamIdMap: StreamIdMap } {
     const inlineNewLineRegex = /((\r\n)|\n|\r)(?!$)/g
     // if no timestamp for some reason, entering a blank of equal length (29 characters long)
-    const timestampSpaceEquivalent = '                             '
 
     const streamIdMap: StreamIdMap = new Map()
     let text: string = ''
@@ -26,7 +26,7 @@ export function generateTextFromLogEvents(
         if (formatting?.timestamps) {
             // TODO: Handle different timezones and unix timestamps?
             const timestamp = event.timestamp
-                ? moment(event.timestamp).format(INSIGHTS_TIMESTAMP_FORMAT)
+                ? moment(event.timestamp).utc().format(INSIGHTS_TIMESTAMP_FORMAT)
                 : timestampSpaceEquivalent
             line = timestamp.concat('\t', line)
             // log entries containing newlines are indented to the same length as the timestamp.
--- src/test/cloudWatchLogs/document/textContent.test.ts
+++ src/test/cloudWatchLogs/document/textContent.test.ts
@@ -1,105 +1,76 @@
-// describe('getLogContent', function () {
-//     it('gets unformatted log content', function () {
-//         const text = registry.getLogContent(registeredUri)
+/*!
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-//         assert.strictEqual(
-//             text,
-//             `${testLogData.events[0].message}${testLogData.events[1].message}${testLogData.events[2].message}${testLogData.events[3].message}`
-//         )
-//     })
+import * as assert from 'assert'
+import { generateTextFromLogEvents, timestampSpaceEquivalent } from '../../../cloudWatchLogs/document/textContent'
+import { CloudWatchLogsEvent } from '../../../cloudWatchLogs/registry/logDataRegistry'
 
-//     it('gets log content formatted to show timestamps', function () {
-//         const text = registry.getLogContent(registeredUri, { timestamps: true })
+describe('generateTextFromLogEvents', function () {
+    it('no formatting', () => {
+        const events: CloudWatchLogsEvent[] = [
+            { message: 'The first log event', logStreamName: 'stream1' },
+            { message: 'The second log event', logStreamName: 'stream2' },
+        ]
 
-//         assert.strictEqual(
-//             text,
-//             `${moment(1).format(INSIGHTS_TIMESTAMP_FORMAT)}${'\t'}${testLogData.events[0].message}${moment(
-//                 2
-//             ).format(INSIGHTS_TIMESTAMP_FORMAT)}${'\t'}${testLogData.events[1].message}${moment(3).format(
-//                 INSIGHTS_TIMESTAMP_FORMAT
-//             )}${'\t'}${testLogData.events[2].message}                             ${'\t'}${
-//                 testLogData.events[3].message
-//             }`
-//         )
-//     })
+        const { text, streamIdMap } = generateTextFromLogEvents(events)
 
-//     it('indents log entries with newlines of all flavors if timestamps are shown but otherwise does not act on them', function () {
-//         const timestampText = registry.getLogContent(newLineUri, { timestamps: true })
-//         const noTimestampText = registry.getLogContent(newLineUri)
+        const expectedText = `The first log event
+The second log event
+`
+        const expectedStreamIdMap = new Map([
+            [0, 'stream1'],
+            [1, 'stream2'],
+        ])
+        assert.strictEqual(text, expectedText)
+        assert.deepStrictEqual(streamIdMap, expectedStreamIdMap)
+    })
 
-//         assert.strictEqual(noTimestampText, newLineData.events[0].message)
-//         assert.strictEqual(
-//             timestampText,
-//             `${moment(newLineData.events[0].timestamp).format(
-//                 INSIGHTS_TIMESTAMP_FORMAT
-//             )}${'\t'}the${'\n'}                             ${'\t'}line${'\n'}                             ${'\t'}must${'\n'}                             ${'\t'}be${'\n'}                             ${'\t'}drawn${'\n'}                             ${'\t'}HERE${'\n'}                             ${'\t'}right${'\n'}                             ${'\t'}here${'\n'}                             ${'\t'}no${'\n'}                             ${'\t'}further\n`
-//         )
-//     })
+    it('has timestamps', () => {
+        const events: CloudWatchLogsEvent[] = [
+            { message: 'The first log event', logStreamName: 'stream1', timestamp: 1675451113 },
+            { message: 'The second log event', logStreamName: 'stream2', timestamp: 1675451114 },
+        ]
 
-//     describe('setStreamIds', function () {
-//         it('registers stream ids to map and clears it on document close', async function () {
-//             await registry.updateLog(searchLogGroupUri)
-//             registry.getLogContent(searchLogGroupUri) // We run this to create the mappings
-//             const doc = await vscode.workspace.openTextDocument(searchLogGroupUri)
-//             let streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-//             const expectedMap = new Map<number, string>([
-//                 [0, testStreamNames[0]],
-//                 [1, testStreamNames[1]],
-//             ])
-//             assert.deepStrictEqual(streamIDMap, expectedMap)
-//             registry.disposeRegistryData(doc.uri)
-//             // We want to re-register log here otherwise this returns undefined.
-//             registry.setLogData(searchLogGroupUri, logGroupsData)
-//             streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-//             assert.deepStrictEqual(streamIDMap, new Map<number, string>())
-//         })
+        const { text, streamIdMap } = generateTextFromLogEvents(events, { timestamps: true })
 
-//         it('handles newlines within event messages', function () {
-//             const oldData = registry.getLogData(searchLogGroupUri)
-//             assert(oldData)
-//             registry.setLogData(searchLogGroupUri, {
-//                 ...oldData,
-//                 events: [
-//                     {
-//                         message: 'This \n is \n a \n message \n spanning \n many \n lines',
-//                         logStreamName: 'stream1',
-//                     },
-//                     {
-//                         message: 'Here \n is \n another \n one.',
-//                         logStreamName: 'stream2',
-//                     },
-//                     {
-//                         message: 'and \n just \n one \n more',
-//                         logStreamName: 'stream1',
-//                     },
-//                     {
-//                         message: 'and thats it.',
-//                         logStreamName: 'stream3',
-//                     },
-//                 ],
-//             })
-//             registry.getLogContent(searchLogGroupUri)
-//             const streamIDMap = registry.getStreamIdMap(searchLogGroupUri)
-//             const expectedMap = new Map<number, string>([
-//                 [0, 'stream1'],
-//                 [1, 'stream1'],
-//                 [2, 'stream1'],
-//                 [3, 'stream1'],
-//                 [4, 'stream1'],
-//                 [5, 'stream1'],
-//                 [6, 'stream1'],
-//                 [7, 'stream2'],
-//                 [8, 'stream2'],
-//                 [9, 'stream2'],
-//                 [10, 'stream2'],
-//                 [11, 'stream1'],
-//                 [12, 'stream1'],
-//                 [13, 'stream1'],
-//                 [14, 'stream1'],
-//                 [15, 'stream3'],
-//             ])
-//             assert.deepStrictEqual(streamIDMap, expectedMap)
-//             registry.setLogData(searchLogGroupUri, oldData)
-//         })
-//     })
-// })
+        const expectedText = `1970-01-20T09:24:11.113+00:00\tThe first log event
+1970-01-20T09:24:11.114+00:00\tThe second log event
+`
+        const expectedStreamIdMap = new Map([
+            [0, 'stream1'],
+            [1, 'stream2'],
+        ])
+        assert.strictEqual(text, expectedText)
+        assert.deepStrictEqual(streamIdMap, expectedStreamIdMap)
+    })
+
+    it('has a multiline message + timestamps', () => {
+        const events: CloudWatchLogsEvent[] = [
+            { message: 'The first log event', logStreamName: 'stream1', timestamp: 1675451113 },
+            {
+                message: 'The second log event\nwith another line\nand another',
+                logStreamName: 'stream2',
+                timestamp: 1675451114,
+            },
+        ]
+
+        const { text, streamIdMap } = generateTextFromLogEvents(events, { timestamps: true })
+
+        const expectedText = `1970-01-20T09:24:11.113+00:00\tThe first log event
+1970-01-20T09:24:11.114+00:00\tThe second log event
+${timestampSpaceEquivalent}\twith another line
+${timestampSpaceEquivalent}\tand another
+`
+
+        const expectedStreamIdMap = new Map([
+            [0, 'stream1'],
+            [1, 'stream2'],
+            [2, 'stream2'],
+            [3, 'stream2'],
+        ])
+        assert.strictEqual(text, expectedText)
+        assert.deepStrictEqual(streamIdMap, expectedStreamIdMap)
+    })
+})
