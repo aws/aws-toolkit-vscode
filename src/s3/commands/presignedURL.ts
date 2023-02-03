94f0def0ec2adc08f6dfef5db67ff06f85319676
gsweetwood <garretsweetwood6@gmail.com>
2023-02-03 11:45:00 -0800

    remove unused code

--- src/s3/commands/presignedURL.ts
+++ src/s3/commands/presignedURL.ts
@@ -10,7 +10,6 @@ import { Env } from '../../shared/vscode/env'
 import { copyToClipboard } from '../../shared/utilities/messages'
 import { S3FileNode } from '../explorer/s3FileNode'
 import { Window } from '../../shared/vscode/window'
-import { localize } from '../../shared/utilities/vsCodeUtils'
 import { invalidNumberWarning } from '../../shared/localizedText'
 import { telemetry } from '../../shared/telemetry/telemetry'
 import { ToolkitError } from '../../shared/errors'
@@ -48,27 +47,6 @@ export async function presignedURLCommand(
         await copyToClipboard(url, 'URL', window, env)
         vscode.window.showInformationMessage(`A presigned URL has been copied to your clipboard`)
     })
-}
-
-export async function promptTime(fileName: string, window = Window.vscode()): Promise<number> {
-    const timeStr = await window.showInputBox({
-        value: '15',
-        prompt: localize(
-            'AWS.s3.promptTime.prompt',
-            'Specify the time (minutes) until URL will expire for path: {0}',
-            fileName
-        ),
-        placeHolder: localize('AWS.s3.promptTime.placeHolder', 'Defaults to 15 minutes'),
-        validateInput: validateTime,
-    })
-
-    const time = Number(timeStr)
-
-    if (isNaN(time) || time < 0) {
-        throw new ToolkitError('The provided expiration time is not a positive number', { code: 'InvalidInput' })
-    }
-
-    return time
 }
 
 function validateTime(time: string): string | undefined {
