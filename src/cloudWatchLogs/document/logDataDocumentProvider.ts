673790824cf34630fb0ece0f869856939f251634
Nikolas Komonen <nkomonen@amazon.com>
2023-02-16 17:18:33 -0500

    chore: remove cwl definition provider + extra
    - Removes the cwl definition provider since it
      will be replaced by the cwl code lens provider.
      This definition provider allows you to ctrl
      + click on a line to go to the specific log stream.
    
      Wit this change a code lens will take its place.
    
    - A function was added to this class that will
      enable users to get the log stream name at a
      given line.
    
      The reason for this is that this document provider
      class will manage the newly named, LineToLogStreamMap,
      which knows what log stream applies to which line
      of the output text.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/cloudWatchLogs/document/logDataDocumentProvider.ts
+++ src/cloudWatchLogs/document/logDataDocumentProvider.ts
@@ -2,15 +2,15 @@
  * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import { telemetry } from '../../shared/telemetry/telemetry'
 import * as vscode from 'vscode'
-import { CloudWatchLogsParameters, LogDataRegistry } from '../registry/logDataRegistry'
+import { CloudWatchLogsGroupInfo, LogDataRegistry, UriString } from '../registry/logDataRegistry'
 import { getLogger } from '../../shared/logger'
-import { parseCloudWatchLogsUri, createURIFromArgs, isCwlUri } from '../cloudWatchLogsUtils'
-import { generateTextFromLogEvents, StreamIdMap } from './textContent'
+import { isCwlUri } from '../cloudWatchLogsUtils'
+import { generateTextFromLogEvents, LineToLogStreamMap } from './textContent'
 
-export class LogDataDocumentProvider implements vscode.TextDocumentContentProvider, vscode.DefinitionProvider {
-    readonly uriToStreamIdMap: Map<string, StreamIdMap> = new Map()
+export class LogDataDocumentProvider implements vscode.TextDocumentContentProvider {
+    /** Resolves the correct {@link LineToLogStreamMap} instance for a given URI */
+    readonly lineToLogStreamMapResolver: Map<UriString, LineToLogStreamMap> = new Map()
     // Expose an event to signal changes of _virtual_ documents
     // to the editor
     private _onDidChange = new vscode.EventEmitter<vscode.Uri>()
@@ -31,72 +31,37 @@ export class LogDataDocumentProvider implements vscode.TextDocumentContentProvid
         }
         const events = this.registry.fetchCachedLogEvents(uri)
         const { text, streamIdMap } = generateTextFromLogEvents(events, { timestamps: true })
-        this.uriToStreamIdMap.set(uri.toString(), streamIdMap)
+        this.lineToLogStreamMapResolver.set(uri.toString(), streamIdMap)
         return text
     }
 
-    async provideDefinition(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken
-    ): Promise<vscode.Definition | vscode.LocationLink[] | undefined> {
-        const activeUri = document.uri
-        const logGroupInfo = parseCloudWatchLogsUri(activeUri).logGroupInfo
-
-        if (logGroupInfo.streamName) {
-            // This means we have a stream file not a log search.
-            return
+    /**
+     * Gives the Log Stream ID at a given line of a given document.
+     *
+     * The given document uri must have already been processed by this
+     * class, otherwise an error will be thrown.
+     *
+     * * @param lineNumber The line number of the document. Starts at 0, NOT 1!
+     */
+    public getLogStreamNameAtLine(
+        uri: vscode.Uri,
+        lineNumber: number
+    ): NonNullable<CloudWatchLogsGroupInfo['streamName']> {
+        let lineToLogStreamMap = this.lineToLogStreamMapResolver.get(uri.toString())
+        if (lineToLogStreamMap === undefined) {
+            // Document was not initialized yet, we will initialize and try again.
+            this.provideTextDocumentContent(uri)
+            lineToLogStreamMap = this.lineToLogStreamMapResolver.get(uri.toString())
+            if (lineToLogStreamMap === undefined) {
+                throw new Error(`${this.provideTextDocumentContent.name}() should have prevented this error.`)
+            }
         }
-        const curLine = document.lineAt(position.line)
-        try {
-            const streamIDMap = this.uriToStreamIdMap.get(activeUri.toString())
-            if (!streamIDMap || streamIDMap.size === 0) {
-                throw new Error(`cwl: No streamIDMap found for stream with uri ${activeUri.path}`)
-            }
-            const streamID = streamIDMap.get(curLine.lineNumber)
 
-            if (!streamID) {
-                throw new Error(
-                    `cwl: current line number ${curLine.lineNumber} is unregistered in streamIDMap for ${activeUri}`
-                )
-            }
-            const parameters: CloudWatchLogsParameters = {
-                limit: this.registry.configuration.get('limit', 10000),
-            }
-            logGroupInfo.streamName = streamID
-
-            const streamUri = createURIFromArgs(logGroupInfo, parameters)
-            this.registry.fetchNextLogEvents(streamUri)
-
-            const doc = await vscode.workspace.openTextDocument(streamUri)
-            vscode.languages.setTextDocumentLanguage(doc, 'log')
-
-            telemetry.cloudwatchlogs_open.emit({
-                result: 'Succeeded',
-                cloudWatchResourceType: 'logStream',
-                source: 'GoTo',
-            })
-
-            const startPosition = new vscode.Position(0, 0)
-            // Highlights the whole line on hover
-            const locationLink: vscode.LocationLink = {
-                originSelectionRange: new vscode.Range(
-                    position.with(undefined, curLine.firstNonWhitespaceCharacterIndex),
-                    position.with(undefined, curLine.range.end.character)
-                ),
-                targetUri: streamUri,
-                targetRange: new vscode.Range(startPosition, startPosition),
-            }
-            return [locationLink]
-        } catch (err) {
-            telemetry.cloudwatchlogs_open.emit({
-                result: 'Failed',
-                cloudWatchResourceType: 'logStream',
-                source: 'GoTo',
-            })
-
-            throw new Error(`cwl: Error determining definition for content in ${document.fileName}`)
+        const logStreamName = lineToLogStreamMap.get(lineNumber)
+        if (logStreamName === undefined) {
+            throw new Error(`Line number was not set in LineToLogStreamMap: ${lineNumber}`)
         }
+        return logStreamName
     }
 }
 
