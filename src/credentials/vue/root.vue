fadbe78b2156b01432bae939f49f295969d9db49
Nikolas Komonen <nkomonen@amazon.com>
2023-04-13 10:13:13 -0400

    auth: vue code + logic to show vue code in webview
    This is the actual vue code as well as the code to
    show that vue code in a webview.
    
    The main method showAuthWebview() is what will be
    called by the VSCode command and then the webview
    will be shown
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

---
+++ src/credentials/vue/root.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex-container" style="display: flex">
+        <div id="left-column">I am left column</div>
+        <div id="right-column">I am right column</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'MyComponent',
+})
+</script>
+
+<style>
+/** By default  */
+.flex-container {
+    display: flex;
+    flex-direction: row;
+    width: 1200px;
+}
+
+/* Makes webview responsive and changes to single column when necessary */
+@media (max-width: 1200px) {
+    .flex-container {
+        flex-direction: column;
+
+        color: red;
+    }
+}
+
+#left-column {
+    flex-grow: 1;
+    max-width: 40%;
+
+    /* This can be deleted, for development purposes */
+    background-color: yellow;
+    color: black;
+    height: 200px;
+}
+
+#right-column {
+    flex-grow: 1;
+    max-width: 60%;
+
+    /* This can be deleted, for development purposes */
+    background-color: aqua;
+    color: black;
+    height: 200px;
+}
+</style>
