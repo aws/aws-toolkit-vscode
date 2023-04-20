72459465c885bcab8e7f4b8cda435ef13fba1704
JadenSimon <sijaden@amazon.com>
2023-04-20 13:52:29 -0700

    Add permissions for parent dir too

--- src/codecatalyst/tools.ts
+++ src/codecatalyst/tools.ts
@@ -253,7 +253,7 @@ async function verifySSHHost({
 
         const sshConfigPath = getSshConfigPath()
         try {
-            await fs.ensureDir(path.dirname(path.dirname(sshConfigPath)))
+            await fs.ensureDir(path.dirname(path.dirname(sshConfigPath)), { mode: 0o755 })
             await fs.mkdir(path.dirname(sshConfigPath), 0o700)
             await fs.appendFile(sshConfigPath, section, { mode: 0o600 })
         } catch (e) {
