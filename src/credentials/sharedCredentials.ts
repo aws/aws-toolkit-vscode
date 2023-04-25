5e122f3b507a67a4fb716d4751224dbc77725010
Nikolas Komonen <nkomonen@amazon.com>
2023-04-25 12:21:31 -0400

    refactor: create module for credentials/config file i/o
    This creates a new module that focuses on the i/o of
    the credentials/config file. Mainly the reading and
    writing of data to the file.
    
    - Also moves the get credentials/config file name functions
      in to it
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/credentials/sharedCredentials.ts
+++ src/credentials/sharedCredentials.ts
@@ -4,23 +4,10 @@
  */
 
 import * as vscode from 'vscode'
-import { join } from 'path'
-import { EnvironmentVariables } from '../shared/environmentVariables'
 import { SystemUtilities } from '../shared/systemUtilities'
 import { ToolkitError } from '../shared/errors'
 import { assertHasProps } from '../shared/utilities/tsUtils'
-
-export function getCredentialsFilename(): string {
-    const env = process.env as EnvironmentVariables
-
-    return env.AWS_SHARED_CREDENTIALS_FILE || join(SystemUtilities.getHomeDirectory(), '.aws', 'credentials')
-}
-
-export function getConfigFilename(): string {
-    const env = process.env as EnvironmentVariables
-
-    return env.AWS_CONFIG_FILE || join(SystemUtilities.getHomeDirectory(), '.aws', 'config')
-}
+import { getConfigFilename, getCredentialsFilename } from './sharedCredentialsFile'
 
 export async function updateAwsSdkLoadConfigEnvVar(): Promise<void> {
     const configFileExists = await SystemUtilities.fileExists(getConfigFilename())
