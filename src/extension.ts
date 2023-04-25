c0b5a4a3001bd39a10b07e25b83d0e01b9d3409c
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2023-04-25 14:57:15 -0700

    feat(auth): improve messaging for file system permissions issues (#3358)
    ## Problem
    Several users have reported problems related to permissions. This is probably because we use the user's home directory rather than a directory provided by VSC.
    
    ## Solution
    Show more info regarding permissions issues when they occur

--- src/extension.ts
+++ src/extension.ts
@@ -125,7 +125,7 @@ export async function activate(context: vscode.ExtensionContext) {
         const settings = Settings.instance
         const experiments = Experiments.instance
 
-        await initializeCredentials(context, awsContext, settings, loginManager)
+        await initializeCredentials(context, awsContext, loginManager)
         await activateTelemetry(context, awsContext, settings)
 
         experiments.onDidChange(({ key }) => {
--- src/test/shared/systemUtilities.test.ts
+++ src/test/shared/systemUtilities.test.ts
@@ -8,11 +8,13 @@ import * as vscode from 'vscode'
 import * as fs from 'fs-extra'
 import * as os from 'os'
 import * as path from 'path'
+import * as utils from 'util'
 
 import { EnvironmentVariables } from '../../shared/environmentVariables'
 import { makeTemporaryToolkitFolder } from '../../shared/filesystemUtilities'
 import { SystemUtilities } from '../../shared/systemUtilities'
 import * as testutil from '../testUtil'
+import { PermissionsError, isFileNotFoundError } from '../../shared/errors'
 
 describe('SystemUtilities', function () {
     let tempFolder: string
@@ -122,4 +124,111 @@ describe('SystemUtilities', function () {
         const regex = /bin[\\\/](code|code-insiders)$/
         assert.ok(regex.test(vscPath), `expected regex ${regex} to match: "${vscPath}"`)
     })
+
+    if (process.platform !== 'win32') {
+        describe('permissions', function () {
+            let runCounter = 0
+
+            beforeEach(function () {
+                runCounter++
+            })
+
+            function assertError<T>(err: unknown, ctor: new (...args: any[]) => T): asserts err is T {
+                if (!(err instanceof ctor)) {
+                    throw new assert.AssertionError({
+                        message: `Error was not an instance of ${ctor.name}: ${utils.inspect(err)}`,
+                    })
+                }
+            }
+
+            describe('unrelated exceptions', function () {
+                it('bubbles up ENOENT', async function () {
+                    const dirPath = path.join(tempFolder, `dir${runCounter}`)
+                    await fs.mkdir(dirPath)
+                    const err = await SystemUtilities.readFile(path.join(dirPath, 'foo')).catch(e => e)
+                    assertError(err, Error)
+                    assert.ok(isFileNotFoundError(err))
+                })
+            })
+
+            describe('owned by user', function () {
+                it('fails writing a new file to a directory without `u+x`', async function () {
+                    const dirPath = path.join(tempFolder, `dir${runCounter}`)
+                    await fs.mkdir(dirPath, { mode: 0o677 })
+                    const err = await SystemUtilities.writeFile(path.join(dirPath, 'foo'), 'foo').catch(e => e)
+                    assertError(err, PermissionsError)
+                    assert.strictEqual(err.uri.fsPath, vscode.Uri.file(dirPath).fsPath)
+                    assert.strictEqual(err.expected, '*wx')
+                    assert.strictEqual(err.actual, 'rw-')
+                })
+
+                it('fails writing a new file to a directory without `u+w`', async function () {
+                    const dirPath = path.join(tempFolder, `dir${runCounter}`)
+                    await fs.mkdir(dirPath, { mode: 0o577 })
+                    const err = await SystemUtilities.writeFile(path.join(dirPath, 'foo'), 'foo').catch(e => e)
+                    assertError(err, PermissionsError)
+                    assert.strictEqual(err.uri.fsPath, vscode.Uri.file(dirPath).fsPath)
+                    assert.strictEqual(err.expected, '*wx')
+                    assert.strictEqual(err.actual, 'r-x')
+                })
+
+                it('fails writing an existing file without `u+w`', async function () {
+                    const filePath = path.join(tempFolder, `file${runCounter}`)
+                    await fs.writeFile(filePath, 'foo', { mode: 0o400 })
+                    const err = await SystemUtilities.writeFile(filePath, 'foo2').catch(e => e)
+                    assertError(err, PermissionsError)
+                    assert.strictEqual(err.uri.fsPath, vscode.Uri.file(filePath).fsPath)
+                    assert.strictEqual(err.expected, '*w*')
+                    assert.strictEqual(err.actual, 'r--')
+                })
+
+                it('fails reading an existing file without `u+r`', async function () {
+                    const filePath = path.join(tempFolder, `file${runCounter}`)
+                    await fs.writeFile(filePath, 'foo', { mode: 0o200 })
+                    const err = await SystemUtilities.readFile(filePath).catch(e => e)
+                    assertError(err, PermissionsError)
+                    assert.strictEqual(err.uri.fsPath, vscode.Uri.file(filePath).fsPath)
+                    assert.strictEqual(err.expected, 'r**')
+                    assert.strictEqual(err.actual, '-w-')
+                })
+
+                describe('existing files in a directory', function () {
+                    let dirPath: string
+                    let filePath: string
+
+                    beforeEach(async function () {
+                        dirPath = path.join(tempFolder, `dir${runCounter}`)
+                        await fs.mkdir(dirPath)
+                        filePath = path.join(dirPath, 'file')
+                        await fs.writeFile(filePath, 'foo')
+                    })
+
+                    afterEach(async function () {
+                        await fs.chmod(dirPath, 0o777)
+                    })
+
+                    it('fails to delete without `u+w` on the parent', async function () {
+                        await fs.chmod(dirPath, 0o577)
+                        const err = await SystemUtilities.delete(filePath).catch(e => e)
+                        assertError(err, PermissionsError)
+                        assert.strictEqual(err.uri.fsPath, vscode.Uri.file(dirPath).fsPath)
+                        assert.strictEqual(err.expected, '*wx')
+                        assert.strictEqual(err.actual, 'r-x')
+                    })
+
+                    it('fails to delete without `u+x` on the parent', async function () {
+                        await fs.chmod(dirPath, 0o677)
+                        const err = await SystemUtilities.delete(filePath).catch(e => e)
+                        assertError(err, PermissionsError)
+                        assert.strictEqual(err.uri.fsPath, vscode.Uri.file(dirPath).fsPath)
+                        assert.strictEqual(err.expected, '*wx')
+                        assert.strictEqual(err.actual, 'rw-')
+                    })
+                })
+            })
+
+            // TODO: need to use sticky bit to easily write tests for group-owned directories
+            // Or potentially spawn new process with different uid...?
+        })
+    }
 })
