f9c26f50ca97cfbd9d578b236120dde900e07468
Justin M. Keyes <jmkeyes@amazon.com>
2023-05-08 12:19:33 -0700

    build: fix husky autoinstall
    Problem:
    husky doesn't autoinstall since
    https://github.com/aws/aws-toolkit-vscode/commit/c7637c261b90e175e9aac8dc66508dc02f4fa563
    
    Solution:
    auto install husky only if not running in CI.

---
+++ scripts/build/prepare.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//
+// Performs NPM "prepare" step, except when running in CI.
+//
+// prepare: https://docs.npmjs.com/cli/v9/using-npm/scripts#prepare-and-prepublish:
+//  - Runs BEFORE the package is packed, i.e. during "npm publish" AND "npm pack".
+//  - Runs on local "npm install".
+//  - Runs AFTER `prepublish`, but BEFORE `prepublishOnly`.
+//  - Runs in the background. To see the output, run with "--foreground-scripts".
+//
+
+import * as child_process from 'child_process'
+
+/**
+ * Returns true if the current build is running on CI (build server).
+ */
+export function isCI(): boolean {
+    return undefined !== process.env['GITHUB_ACTION'] || undefined !== process.env['CODEBUILD_BUILD_ID']
+}
+
+function main() {
+    if (process.env.AWS_TOOLKIT_TEST_USER_DIR) {
+        console.log('prepare: skipped (running in CI)')
+        return
+    }
+    child_process.execSync('husky install', { stdio: 'inherit' })
+}
+
+main()
+
