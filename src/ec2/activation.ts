116336cb3cc72ec4f1a2f61b29c172f9c6a18b26
Harry Weinstock <hkobew@amazon.com>
2023-06-05 12:08:54 -0700

    hacky prompt + func to list instanceId

--- src/ec2/activation.ts
+++ src/ec2/activation.ts
@@ -4,17 +4,41 @@
  */
 
 import * as vscode from 'vscode'
+import globals from '../shared/extensionGlobals'
 import { ExtContext } from '../shared/extensions'
 import { Commands } from '../shared/vscode/commands2'
+import { createRegionPrompter } from '../shared/ui/common/region'
 
 import { selectInstance } from './commands'
+import { EC2 } from 'aws-sdk'
+import { isValidResponse } from '../shared/wizards/wizard'
+import { getLogger } from '../shared/logger/logger'
 
 export async function activate(ctx: ExtContext): Promise<void> {
     ctx.extensionContext.subscriptions.push(
-        Commands.register('aws.ec2.connectToInstance', (param?: unknown) => {
+        Commands.register('aws.ec2.connectToInstance', async (param?: unknown) => {
             console.log("You just ran the aws.ec2.connectToInstance command!")
+            
+            const regionPrompter = createRegionPrompter()
+            const selectedRegion = await regionPrompter.prompt()
+            if(isValidResponse(selectedRegion)){
+                const client = await globals.sdkClientBuilder.createAwsService(EC2, undefined, selectedRegion.id)
+                const request = client.describeInstances()
+                request.send(function (err, data) {
+                    if (err) {
+                        console.log("We got an error.")
+                    } else {
+                        console.log("Your instances are: ")
+                        data.Reservations?.forEach( (curReservation) => 
+                        curReservation.Instances?.forEach( (curInstance) => 
+                            console.log(curInstance.InstanceId)
+                        ))
+                    }
 
-            const selection = selectInstance()
+                })
+            }
+            // const selection = await selectInstance()
+            // console.log(selection)
         })
     )
 }
