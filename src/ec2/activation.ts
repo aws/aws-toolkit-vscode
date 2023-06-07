d7007c679257710dabedd539c7ab345dd0bc9f3e
Harry Weinstock <hkobew@amazon.com>
2023-06-07 09:51:07 -0700

    throw cancellationError on user cancel

--- src/ec2/activation.ts
+++ src/ec2/activation.ts
@@ -6,27 +6,27 @@
 import globals from '../shared/extensionGlobals'
 import { ExtContext } from '../shared/extensions'
 import { Commands } from '../shared/vscode/commands2'
-import { createRegionPrompter } from '../shared/ui/common/region'
-
 import { extractInstanceIds, selectInstance } from './commands'
 import { EC2 } from 'aws-sdk'
-import { isValidResponse } from '../shared/wizards/wizard'
 import { pageableToCollection } from '../shared/utilities/collectionUtils'
+import { CancellationError } from '../shared/utilities/timeoutUtils'
 
 export async function activate(ctx: ExtContext): Promise<void> {
     ctx.extensionContext.subscriptions.push(
         Commands.register('aws.ec2.connectToInstance', async (param?: unknown) => {
             console.log("You just ran the aws.ec2.connectToInstance command!")
             
-            // const regionPrompter = createRegionPrompter()
-            // const selectedRegion = await regionPrompter.prompt()
             const client = await globals.sdkClientBuilder.createAwsService(EC2, undefined, ctx.regionProvider.guessDefaultRegion())
             const requester = async (request: EC2.DescribeInstancesRequest) => 
                 client.describeInstances(request).promise() 
             const collection = extractInstanceIds(pageableToCollection(requester, {}, 'NextToken', 'Reservations'))
 
-            const selection = await selectInstance(collection.filter(instanceId => instanceId !== undefined))
-            console.log(selection)
+            const selection = await selectInstance(collection)
+            if(!selection){
+                throw new CancellationError('user')
+            } else {
+                console.log(selection)
+            }
         })
     )
 }
