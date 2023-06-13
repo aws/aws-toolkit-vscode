98c84a6294a06d7734f74c7474f5dc485b53aa53
Harry Weinstock <hkobew@amazon.com>
2023-06-13 16:32:55 -0700

    fix headers and imports

--- src/ec2/activation.ts
+++ src/ec2/activation.ts
@@ -1,10 +1,10 @@
 /*!
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 import { ExtContext } from '../shared/extensions'
 import { Commands } from '../shared/vscode/commands2'
-import { attemptConnection} from './commands'
+import { tryConnect } from './commands'
 
 
 export async function activate(ctx: ExtContext): Promise<void> {
@@ -12,7 +12,7 @@ export async function activate(ctx: ExtContext): Promise<void> {
         Commands.register('aws.ec2.connectToInstance', async (param?: unknown) => {
             console.log("You just ran the aws.ec2.connectToInstance command!")
             const defaultRegion = ctx.regionProvider.guessDefaultRegion()
-            attemptConnection(defaultRegion)
+            tryConnect(defaultRegion)
         })
     )
 }
