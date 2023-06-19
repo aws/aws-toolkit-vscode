cccc238b313e3e83abbbac9f87f100fcc396d0bf
Nikolas Komonen <118216176+nkomonen-amazon@users.noreply.github.com>
2023-06-19 18:12:51 -0400

    authWebview: Green bar (#3590)
    * greenBar: emit more detail on auth connection change
    
    Previously when an auth connection changed we would emit
    that a change happened but nothing else.
    
    Now we emit an event that has more information including
    the current status, cause, and specific auth type
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * greenBar: show when has connection
    
    This adds a green bar that tells the user they have
    a successful connection and gives more info on
    how to switch connections if necessary.
    
    This is a quick implementation and requires more work
    if we want to list the currently selected connections.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * greenBar: remove Document Type service item
    
    We do not want to show the Document Type
    service item anymore, this removes it.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * greenBar: make bar width fit content
    
    The width of the bar will adjust to fit the size
    of the content
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    ---------
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/auth/ui/vue/serviceItemContent/shared.vue
+++ src/auth/ui/vue/serviceItemContent/shared.vue
@@ -1,7 +1,5 @@
 <script lang="ts">
 import AwsExplorerContent, { ResourceExplorerContentState } from './awsExplorerContent.vue'
-import DocumentTypesContent from './documentTypesContent.vue'
-import { UnimplementedAuthStatus } from '../authForms/baseAuth.vue'
 import { AuthStatus } from '../authForms/shared.vue'
 import CodeWhispererContent, { CodeWhispererContentState } from './codeWhispererContent.vue'
 import CodeCatalystContent, { CodeCatalystContentState } from './codeCatalystContent.vue'
@@ -10,7 +8,6 @@ import { ServiceItemId } from '../types'
 
 /** Maps a service item id to its respective component */
 const serviceItemsContent: Readonly<Record<ServiceItemId, DefinedComponent>> = {
-    documentTypeSupport: DocumentTypesContent,
     resourceExplorer: AwsExplorerContent,
     codecatalyst: CodeCatalystContent,
     codewhisperer: CodeWhispererContent,
@@ -23,7 +20,6 @@ const serviceItemsContent: Readonly<Record<ServiceItemId, DefinedComponent>> = {
  * the current service.
  */
 export const serviceItemsAuthStatus: Readonly<Record<ServiceItemId, AuthStatus>> = {
-    documentTypeSupport: new UnimplementedAuthStatus(),
     resourceExplorer: new ResourceExplorerContentState(),
     codecatalyst: new CodeCatalystContentState(),
     codewhisperer: new CodeWhispererContentState(),
