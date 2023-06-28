866901c2b55062ef81b8a83468365710ab3214e5
Harry Weinstock <hkobew@amazon.com>
2023-06-28 11:27:35 -0700

    add tests for instanceNode

--- src/test/ec2/explorer/ec2InstanceNode.test.ts
+++ src/test/ec2/explorer/ec2InstanceNode.test.ts
@@ -4,3 +4,51 @@
  */
 
 import * as assert from 'assert'
+import { Ec2InstanceNode } from '../../../ec2/explorer/ec2InstanceNode'
+import { Instance } from '@aws-sdk/client-ec2'
+import { getNameOfInstance } from '../../../shared/clients/ec2Client'
+
+describe('ec2InstanceNode', function () {
+    let testNode: Ec2InstanceNode
+    let testInstance: Instance
+
+    before(function () {
+        testInstance = {
+            InstanceId: 'testId',
+            Tags: [
+                {
+                    Key: 'Name',
+                    Value: 'testName',
+                },
+            ],
+        }
+
+        testNode = new Ec2InstanceNode('testRegion', testInstance)
+    })
+
+    it('instantiates without issue', async function () {
+        assert.ok(testNode)
+    })
+
+    it('initializes the region code', async function () {
+        assert.strictEqual(testNode.regionCode, 'testRegion')
+    })
+
+    it('initializes the label', async function () {
+        assert.strictEqual(testNode.label, getNameOfInstance(testInstance))
+    })
+
+    it('initializes the functionName', async function () {
+        assert.strictEqual(testNode.name, getNameOfInstance(testInstance))
+    })
+
+    it('initializes the tooltip', async function () {
+        assert.strictEqual(testNode.tooltip, testInstance.InstanceId)
+    })
+
+    it('has no children', async function () {
+        const childNodes = await testNode.getChildren()
+        assert.ok(childNodes)
+        assert.strictEqual(childNodes.length, 0, 'Expected node to have no children')
+    })
+})
