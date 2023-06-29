6ecee834f66b2b121b8b3bd84e37275cd7e99f5e
Harry Weinstock <hkobew@amazon.com>
2023-06-29 15:29:43 -0700

    craft real ARN

--- src/ec2/explorer/ec2InstanceNode.ts
+++ src/ec2/explorer/ec2InstanceNode.ts
@@ -6,25 +6,25 @@
 import { getNameOfInstance } from '../../shared/clients/ec2Client'
 import { AWSResourceNode } from '../../shared/treeview/nodes/awsResourceNode'
 import { AWSTreeNodeBase } from '../../shared/treeview/nodes/awsTreeNodeBase'
-import { Instance } from '@aws-sdk/client-ec2'
-
+import { Ec2Instance } from '../../shared/clients/ec2Client'
+import globals from '../../shared/extensionGlobals'
 export class Ec2InstanceNode extends AWSTreeNodeBase implements AWSResourceNode {
     public constructor(
         public override readonly regionCode: string,
-        private instance: Instance,
+        private instance: Ec2Instance,
         public override readonly contextValue: string
     ) {
         super('')
         this.update(instance)
     }
 
-    public update(newInstance: Instance) {
+    public update(newInstance: Ec2Instance) {
         this.setInstance(newInstance)
         this.label = this.name
-        this.tooltip = this.instanceId
+        this.tooltip = this.InstanceId
     }
 
-    public setInstance(newInstance: Instance) {
+    public setInstance(newInstance: Ec2Instance) {
         this.instance = newInstance
     }
 
@@ -32,11 +32,13 @@ export class Ec2InstanceNode extends AWSTreeNodeBase implements AWSResourceNode 
         return getNameOfInstance(this.instance) ?? 'Unnamed instance'
     }
 
-    public get instanceId(): string {
+    public get InstanceId(): string {
         return this.instance.InstanceId!
     }
 
     public get arn(): string {
-        return 'testArn'
+        return `arn:aws:ec2:${this.regionCode}:${globals.awsContext.getCredentialAccountId()}:instance/${
+            this.InstanceId
+        }`
     }
 }
