7e18e09a9b795a4651b8315b1dbf5dad0eba238d
Shao-Hua Lo <xshaohua@amazon.com>
2023-07-06 04:42:34 -0700

    remove 1 layer of describe

--- src/test/codewhisperer/util/crossFileContextUtil.test.ts
+++ src/test/codewhisperer/util/crossFileContextUtil.test.ts
@@ -25,76 +25,68 @@ async function openATextEditorWithText(fileText: string, fileName: string): Prom
     return await vscode.window.showTextDocument(textDocument, { preview: false })
 }
 
-describe('crossfileUtil', function () {
-    describe('getRelevantFiles', async function () {
-        before(async function () {
-            this.timeout(600000)
-        })
+describe('getRelevantFiles', async function () {
+    before(async function () {
+        this.timeout(600000)
+    })
 
-        beforeEach(async function () {
-            tempFolder = await makeTemporaryToolkitFolder()
-        })
+    beforeEach(async function () {
+        tempFolder = await makeTemporaryToolkitFolder()
+    })
 
-        afterEach(async function () {
-            await fs.remove(tempFolder)
-        })
+    afterEach(async function () {
+        await fs.remove(tempFolder)
+    })
 
-        after(async function () {
-            await closeAllEditors()
-        })
+    after(async function () {
+        await closeAllEditors()
+    })
 
-        it('should return opened files in the current window and sorted ascendingly by file distance', async function () {
-            const shouldRunTheTest = semver.valid(vscode.version) && semver.gte(vscode.version, '1.68.0')
+    it('should return opened files in the current window and sorted ascendingly by file distance', async function () {
+        const shouldRunTheTest = semver.valid(vscode.version) && semver.gte(vscode.version, '1.68.0')
 
-            if (!shouldRunTheTest) {
-                this.skip()
-            }
+        if (!shouldRunTheTest) {
+            this.skip()
+        }
 
-            const targetFile = path.join('service', 'microService', 'CodeWhispererFileContextProvider.java')
-            const fileWithDistance3 = path.join('service', 'CodewhispererRecommendationService.java')
-            const fileWithDistance5 = path.join('util', 'CodeWhispererConstants.java')
-            const fileWithDistance6 = path.join('ui', 'popup', 'CodeWhispererPopupManager.java')
-            const fileWithDistance7 = path.join('ui', 'popup', 'components', 'CodeWhispererPopup.java')
-            const fileWithDistance8 = path.join(
-                'ui',
-                'popup',
-                'components',
-                'actions',
-                'AcceptRecommendationAction.java'
-            )
+        const targetFile = path.join('service', 'microService', 'CodeWhispererFileContextProvider.java')
+        const fileWithDistance3 = path.join('service', 'CodewhispererRecommendationService.java')
+        const fileWithDistance5 = path.join('util', 'CodeWhispererConstants.java')
+        const fileWithDistance6 = path.join('ui', 'popup', 'CodeWhispererPopupManager.java')
+        const fileWithDistance7 = path.join('ui', 'popup', 'components', 'CodeWhispererPopup.java')
+        const fileWithDistance8 = path.join('ui', 'popup', 'components', 'actions', 'AcceptRecommendationAction.java')
 
-            const expectedFilePaths = [
-                fileWithDistance3,
-                fileWithDistance5,
-                fileWithDistance6,
-                fileWithDistance7,
-                fileWithDistance8,
-            ]
-            const shuffledFilePaths = shuffleList(expectedFilePaths)
+        const expectedFilePaths = [
+            fileWithDistance3,
+            fileWithDistance5,
+            fileWithDistance6,
+            fileWithDistance7,
+            fileWithDistance8,
+        ]
+        const shuffledFilePaths = shuffleList(expectedFilePaths)
 
-            let cnt = 0
-            for (const file of shuffledFilePaths) {
-                await openATextEditorWithText(file, file)
-                cnt++
-                await assertTabSize(cnt)
-            }
+        let cnt = 0
+        for (const file of shuffledFilePaths) {
+            await openATextEditorWithText(file, file)
+            cnt++
+            await assertTabSize(cnt)
+        }
 
-            const editor = await openATextEditorWithText(targetFile, targetFile)
-            await assertTabSize(6)
+        const editor = await openATextEditorWithText(targetFile, targetFile)
+        await assertTabSize(6)
 
-            const actuals = await getRelevantCrossFiles(editor)
+        const actuals = await getRelevantCrossFiles(editor)
 
-            assert.ok(actuals.length === 5)
-            actuals.forEach((actual, index) => {
-                // vscode API will return normalized file path, thus /C:/Users/.../ for windows
-                // thus need to manually add '/' and normalize
-                const expected =
-                    process.platform === 'win32'
-                        ? '/' + normalize(path.join(tempFolder, expectedFilePaths[index]))
-                        : normalize(path.join(tempFolder, expectedFilePaths[index]))
+        assert.ok(actuals.length === 5)
+        actuals.forEach((actual, index) => {
+            // vscode API will return normalized file path, thus /C:/Users/.../ for windows
+            // thus need to manually add '/' and normalize
+            const expected =
+                process.platform === 'win32'
+                    ? '/' + normalize(path.join(tempFolder, expectedFilePaths[index]))
+                    : normalize(path.join(tempFolder, expectedFilePaths[index]))
 
-                assert.strictEqual(actual, expected)
-            })
+            assert.strictEqual(actual, expected)
         })
     })
 })
