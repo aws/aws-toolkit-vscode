5156ce8f3b8529d6a93910ae99a76389d18c5b27
Nikolas Komonen <118216176+nkomonen-amazon@users.noreply.github.com>
2023-07-06 11:01:05 -0400

    feat: User Activity event (#3645)
    * rename: throttle() -> debounce()
    
    The existing function is a debounce and not
    a throttle. Update name.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * feat: User Activity event
    
    This adds a new class which provides an event that triggers
    upon user activity.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    ---------
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/auth/ui/vue/show.ts
+++ src/auth/ui/vue/show.ts
@@ -36,7 +36,7 @@ import { tryAddCredentials, signout, showRegionPrompter, promptAndUseConnection,
 import { Region } from '../../../shared/regions/endpoints'
 import { CancellationError } from '../../../shared/utilities/timeoutUtils'
 import { validateSsoUrl, validateSsoUrlFormat } from '../../sso/validation'
-import { throttle } from '../../../shared/utilities/functionUtils'
+import { debounce } from '../../../shared/utilities/functionUtils'
 import { AuthError, ServiceItemId, userCancelled } from './types'
 import { awsIdSignIn } from '../../../codewhisperer/util/showSsoPrompt'
 import { connectToEnterpriseSso } from '../../../codewhisperer/util/getStartUrl'
@@ -314,12 +314,12 @@ export class AuthWebview extends VueWebview {
         ]
 
         // The event handler in the frontend refreshes all connection statuses
-        // when triggered, and multiple events can fire at the same time so we throttle.
-        const throttledFire = throttle(() => this.onDidConnectionUpdate.fire(undefined), 500)
+        // when triggered, and multiple events can fire at the same time so we debounce.
+        const debouncedFire = debounce(() => this.onDidConnectionUpdate.fire(undefined), 500)
 
         events.forEach(event =>
             event(() => {
-                throttledFire()
+                debouncedFire()
             })
         )
     }
