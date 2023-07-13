e027e4053b23f916f9bbdc47d339466b409bdbce
Harry Weinstock <hkobew@amazon.com>
2023-07-13 08:32:36 -0700

    handle case where parent node is passed to command

--- src/ec2/activation.ts
+++ src/ec2/activation.ts
@@ -8,21 +8,23 @@ import { telemetry } from '../shared/telemetry/telemetry'
 import { Ec2InstanceNode } from './explorer/ec2InstanceNode'
 import { promptUserForEc2Selection } from './prompter'
 import { Ec2ConnectionManager } from './model'
+import { Ec2ParentNode } from './explorer/ec2ParentNode'
 
 export async function activate(ctx: ExtContext): Promise<void> {
     ctx.extensionContext.subscriptions.push(
-        Commands.register('aws.ec2.openTerminal', async (node?: Ec2InstanceNode) => {
+        Commands.register('aws.ec2.openTerminal', async (node?: Ec2InstanceNode | Ec2ParentNode) => {
             await telemetry.ec2_connectToInstance.run(async span => {
                 span.record({ ec2ConnectionType: 'ssm' })
-                const selection = node ? node.toSelection() : await promptUserForEc2Selection()
+                const selection =
+                    node instanceof Ec2InstanceNode ? node.toSelection() : await promptUserForEc2Selection()
 
                 const connectionManager = new Ec2ConnectionManager(selection.region)
                 await connectionManager.attemptToOpenEc2Terminal(selection)
             })
         }),
 
-        Commands.register('aws.ec2.openRemoteConnection', async (node?: Ec2InstanceNode) => {
-            const selection = node ? node.toSelection() : await promptUserForEc2Selection()
+        Commands.register('aws.ec2.openRemoteConnection', async (node?: Ec2InstanceNode | Ec2ParentNode) => {
+            const selection = node instanceof Ec2InstanceNode ? node.toSelection() : await promptUserForEc2Selection()
             //const connectionManager = new Ec2ConnectionManager(selection.region)
             console.log(selection)
         })
