98f03fbb514a63aef6452cc9ef59c86582d41ab3
Harry Weinstock <hkobew@amazon.com>
2023-07-14 12:03:12 -0700

    change prompter to filter based on instance status

--- src/ec2/commands.ts
+++ src/ec2/commands.ts
@@ -7,8 +7,9 @@ import { InstanceStateManager, getStateManagerForSelection } from './instanceSta
 import { Ec2InstanceNode } from './explorer/ec2InstanceNode'
 import { Ec2Node } from './explorer/ec2ParentNode'
 import { Ec2ConnectionManager } from './model'
-import { Ec2Prompter } from './prompter'
+import { Ec2Prompter, instanceFilter } from './prompter'
 import { Ec2Selection } from './utils'
+import { Ec2Instance } from '../shared/clients/ec2Client'
 
 export async function openTerminal(node?: Ec2Node) {
     const selection = await getSelection(node)
@@ -24,12 +25,14 @@ export async function openRemoteConnection(node?: Ec2Node) {
 }
 
 export async function startInstance(node?: Ec2Node) {
-    const stateManager = await getStateManager(node)
+    const prompterFilter = (instance: Ec2Instance) => instance.status !== 'running'
+    const stateManager = await getStateManager(node, prompterFilter)
     await stateManager.startInstanceWithCancel()
 }
 
 export async function stopInstance(node?: Ec2Node) {
-    const stateManager = await getStateManager(node)
+    const prompterFilter = (instance: Ec2Instance) => instance.status !== 'stopped'
+    const stateManager = await getStateManager(node, prompterFilter)
     await stateManager.stopInstanceWithCancel()
 }
 
@@ -38,14 +41,14 @@ export async function rebootInstance(node?: Ec2Node) {
     await stateManager.rebootInstanceWithCancel()
 }
 
-async function getStateManager(node?: Ec2Node): Promise<InstanceStateManager> {
-    const selection = await getSelection(node)
+async function getStateManager(node?: Ec2Node, prompterFilter?: instanceFilter): Promise<InstanceStateManager> {
+    const selection = await getSelection(node, prompterFilter)
     const stateManager = getStateManagerForSelection(selection)
     return stateManager
 }
 
-async function getSelection(node?: Ec2Node): Promise<Ec2Selection> {
-    const prompter = new Ec2Prompter()
+async function getSelection(node?: Ec2Node, filter?: instanceFilter): Promise<Ec2Selection> {
+    const prompter = new Ec2Prompter(filter)
     const selection = node && node instanceof Ec2InstanceNode ? node.toSelection() : await prompter.promptUser()
     return selection
 }
