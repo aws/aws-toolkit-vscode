d4db730fbcfddf0d2a3498f34e151f05546c20f7
Nikita Mikhailov <noobgam@amazon.de>
2023-08-09 01:01:26 +0200

    feat(plugin): proxy plugin through lambda
    (cherry picked from commit 1690e90df9140ecb96c01e05f8df040056589bc8)

--- src/weaverbird/vue/chat/session.ts
+++ src/weaverbird/vue/chat/session.ts
@@ -4,6 +4,7 @@
  */
 
 import * as vscode from 'vscode'
+import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda'
 
 /**
  * Session keeps track of all the information related to a session, and persists session information to disk
@@ -33,7 +34,30 @@ export class Session {
     }
 
     async send(msg: string) {
-        this.history.push('')
-        return ''
+        // TODO: figure out how to pass environment variables
+        // We might need to pipe in the previous history here so we need to store that somewhere in the class
+
+        const client = new LambdaClient({
+            region: 'eu-west-1',
+        })
+
+        const command = new InvokeCommand({
+            FunctionName: 'arn:aws:lambda:eu-west-1:761763482860:function:tempFunc',
+            Payload: JSON.stringify({
+                original_file_contents: {},
+                task: msg,
+            }),
+        })
+
+        const { Payload } = await client.send(command)
+        const result = Buffer.from(Payload!).toString()
+
+        // Clean up the summary by stripping the description from the actual generated text contents
+        const fileBeginnings = result.split('--BEGIN-FILE')
+        const outputSummary = fileBeginnings.length > 0 ? fileBeginnings[0] : result
+
+        this.history.push(outputSummary)
+
+        return outputSummary
     }
 }
