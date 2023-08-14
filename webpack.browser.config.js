96c1d4ca1e1e18c107e1b2ba5b1dfb13adadb47e
Nikolas Komonen <118216176+nkomonen-amazon@users.noreply.github.com>
2023-08-14 12:38:11 -0400

    browser: start webpack setup to get logging working (#3699)
    * webpack: fix minor error messages
    
    Previously, it was possible that the values were empty,
    so this returns an empty array in the case it was empty.
    
    This fixes the linting errors
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * webpack: add fallback/polyfills for node related modules
    
    - Adds fallback/polyfills for node related modules
    - The section for 'process' and 'buffer' is different
      since they are globally available modules and don't
      require an import. So the polyfill needs to be done
      differently.
    - Some of the polyfilled modules are 'false' since there
      is not a clear alternative for it yet. This is being
      worked on.
    
    - Create a separate config for the browser to have faster
    webpack times for development since webpack for the
    core project takes a while.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * browser: setImmediate fallback for browser
    
    setImmedate is provided by node, but does not exist
    in the browser.
    
    This commit falls back to using setTimeout with a 0
    millisecond delay as setImmediate if we are in the
    browser context
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * browser: Add separate browser package.json
    
    This creates a separate package.json in 'src/browser'.
    All browser related dependencies should go in here,
    and when 'npm install' is run in the root, it will
    additionally pull in these dependencies to the main
    project but allow us to keep browser related
    dependencies separated.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * webpack: Get extensionWeb activation working with webpack
    
    Webpack was failing since copyFiles script required the
    nls message bundle to be loaded. This does it explicitly
    for now, but will eventually be loaded by a separate import
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * webpack: refactor in to separate configs
    
    - Moves certain webpack configs in to separate files to
      logically separate them
    - Now we have a separate config for Base, Browser, and Vue
    - webpack.config.js is the default name for the webpack
      cli to search, so it will be the main webpack file that
      collects all configs.
    
    - Additionally, adds a type to stop errors for the CircularDependencyPlugin
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    ---------
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

---
+++ webpack.browser.config.js
@@ -0,0 +1,63 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+const webpack = require('webpack')
+var { FilerWebpackPlugin } = require('filer/webpack')
+
+const baseConfig = require('./webpack.base.config')
+
+/** @type WebpackConfig */
+const webConfig = {
+    ...baseConfig,
+    name: 'web',
+    target: 'webworker',
+    entry: {
+        'src/extensionWeb': './src/extensionWeb.ts',
+    },
+    plugins: (baseConfig.plugins ?? []).concat(
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1, // disable chunks by default since web extensions must be a single bundle
+        }),
+        // some modules are provided globally and dont require an explicit import,
+        // so we need to polyfill them using this method
+        new webpack.ProvidePlugin({
+            process: require.resolve('process/browser'),
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_DEBUG: 'development',
+            READABLE_STREAM: 'disable',
+        }),
+        // polyfills global 'fs' and 'path'
+        new FilerWebpackPlugin()
+    ),
+    resolve: {
+        extensions: ['.ts', '.js'],
+        fallback: {
+            stream: require.resolve('stream-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            path: require.resolve('path-browserify'),
+
+            // *** If one of these modules actually gets used an error will be raised ***
+            // You may see something like: "TypeError: path_ignored_0.join is not a function"
+
+            // We don't need these yet, but as we start enabling functionality in the web
+            // we may need to polyfill.
+            http: false, // http: require.resolve('stream-http'),
+            https: false, // https: require.resolve('https-browserify'),
+            zlib: false, // zlib: require.resolve('browserify-zlib'),
+            assert: false, // assert: require.resolve('assert/'),
+            constants: false, //constants: require.resolve('constants-browserify'),
+            crypto: false, // crypto: require.resolve('crypto-browserify'),
+            // These do not have a straight forward replacement
+            child_process: false, // Reason for error: 'TypeError: The "original" argument must be of type Function'
+            async_hooks: false,
+        },
+    },
+    optimization: {
+        minimize: false,
+    },
+}
+
+module.exports = [webConfig]
