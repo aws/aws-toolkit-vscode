e77e66c5d3be2a01568e4d18c4c5975424e40d10
Nikita Mikhailov <noobgam@amazon.de>
2023-08-16 19:42:44 +0200

    tie with weaverbird backend poc
    (cherry picked from commit 81a63ec279552bf3d262f34286a0dab267c6edc2)

--- src/weaverbird/vue/chat/session.ts
+++ src/weaverbird/vue/chat/session.ts
@@ -5,6 +5,9 @@
 
 import * as vscode from 'vscode'
 import { createWeaverbirdSdkClient } from '../../client/weaverbird'
+import * as fs from 'fs'
+import * as path from 'path'
+import { FileMetadata, FileMetadataList } from '../../client/weaverbirdclient'
 
 export class Session {
     public readonly history: string[]
@@ -12,7 +15,7 @@ export class Session {
     public readonly sourceRoot: string
     private state: 'refinement' | 'codegen'
     private task: string = ''
-    private instruction: string = ''
+    private approach: string = ''
 
     // TODO remake private
     public onProgressEventEmitter: vscode.EventEmitter<string>
@@ -28,6 +31,7 @@ export class Session {
         this.sourceRoot = this.workspaceRoot + '/src'
         this.state = 'refinement'
         this.onProgressEventEmitter = new vscode.EventEmitter<string>()
+        this.state = 'refinement'
         this.onProgressEvent = this.onProgressEventEmitter.event
 
         this.onProgressFinishedEventEmitter = new vscode.EventEmitter<void>()
@@ -45,6 +49,53 @@ export class Session {
 
     async sendUnsafe(msg: string): Promise<string> {
         const client = await createWeaverbirdSdkClient()
-        return (await client.echo().promise()).string ?? ''
+
+        const fileList = fs.readdirSync(path.join(this.workspaceRoot, 'src'))
+
+        const files: FileMetadataList = fileList.map(fileName => {
+            const filePath = path.join(this.workspaceRoot, 'src', fileName)
+            return {
+                filePath,
+                fileContent: fs.readFileSync(filePath).toString(),
+            } as FileMetadata
+        })
+
+        if (msg.indexOf('WRITE CODE') !== -1) {
+            this.state = 'codegen'
+        } else {
+            this.task = msg
+        }
+        if (this.state === 'refinement') {
+            console.log(
+                JSON.stringify({
+                    task: this.task,
+                    originalFileContents: files,
+                })
+            )
+            const result = await client
+                .generateApproach({
+                    task: this.task,
+                    originalFileContents: files,
+                })
+                .promise()
+            this.approach = result.approach!
+            return `${result.approach}\n`
+        } else {
+            const result = await client
+                .generateCode({
+                    task: this.task,
+                    approach: this.approach,
+                    originalFileContents: files,
+                })
+                .promise()
+
+            for (const { filePath, fileContent } of result.newFileContents!) {
+                const pathUsed = path.isAbsolute(filePath) ? filePath : path.join(this.workspaceRoot, filePath)
+                fs.mkdirSync(path.dirname(pathUsed), { recursive: true })
+                fs.writeFileSync(pathUsed, fileContent as string)
+            }
+
+            return 'Changes to files done'
+        }
     }
 }
