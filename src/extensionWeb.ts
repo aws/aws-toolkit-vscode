d20d2db918b969106265d9ddd8347f52c917d8ab
Nikolas Komonen <nkomonen@amazon.com>
2023-08-18 17:55:24 -0400

    vscode-nls: Fix localize() not working in browser
    See the long comment in the code for info.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

--- src/extensionWeb.ts
+++ src/extensionWeb.ts
@@ -5,11 +5,6 @@
 
 import * as vscode from 'vscode'
 
-// The following is required so that the copyFiles script does not fail.
-// I'm assuming this generates something when run that the script can use.
-import * as nls from 'vscode-nls'
-nls.loadMessageBundle()
-
 export async function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage(
         'AWS Toolkit: Browser Mode Under Development. No features are currently provided',
