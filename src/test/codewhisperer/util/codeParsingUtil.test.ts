3a08fcb73af82de52fea5e0fb797c29059288f00
Will Lo <96078566+Will-ShaoHua@users.noreply.github.com>
2023-08-23 14:19:00 -0700

    codewhisperer: fix java test file regex pattern MyClassTests.java not captured by test supplemental context(#3763)

--- src/test/codewhisperer/util/codeParsingUtil.test.ts
+++ src/test/codewhisperer/util/codeParsingUtil.test.ts
@@ -63,7 +63,7 @@ describe('RegexValidationForJava', () => {
 
 describe('isTestFile', () => {
     it('should return true if the file name matches the test filename pattern - Java', async () => {
-        const filePaths = ['/path/to/MyClassTest.java', '/path/to/TestMyClass.java']
+        const filePaths = ['/path/to/MyClassTest.java', '/path/to/TestMyClass.java', '/path/to/MyClassTests.java']
         const language = 'java'
 
         for (const filePath of filePaths) {
