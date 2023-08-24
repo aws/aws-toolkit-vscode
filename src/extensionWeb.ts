916b0f038198ecbda8cc042d81d66beb0a018c4d
nkomonen <nkomonen@amazon.com>
2023-08-24 10:18:17 -0400

    telemetry: enable telemetry
    - enables telemetry
    
    Sub Problem:
    - We previously created the globals.regionProvider instance
      so that eventually when telemetry ran it could use that value
      to get the default aws region.
    - The problem was that to create the region provider we needed
      to make a request to get the endpoints from hosted files. But
      due to running in a browser we get a CORs error since CORs is
      not configured as expected on the server.
    
    Sub Solution:
    
    - Create a stub region provider that only has the single
      method that is required by telemetry
    - If we need this actually configured for something else later
      then we can remove this stub and take actions to get the
      full RegionProvider working
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>

--- src/extensionWeb.ts
+++ src/extensionWeb.ts
@@ -7,9 +7,18 @@ import * as vscode from 'vscode'
 import { setInBrowser } from './common/browserUtils'
 import { activate as activateLogger } from './shared/logger/activation'
 import { initializeComputeRegion } from './shared/extensionUtilities'
-import globals from './shared/extensionGlobals'
+
+import { activate as activateTelemetry } from './shared/telemetry/activation'
+import { getLogger } from './shared/logger'
+import { DefaultAwsContext } from './shared/awsContext'
+import { Settings } from './shared/settings'
+import globals, { initialize } from './shared/extensionGlobals'
+
 import { TelemetryService } from './shared/telemetry/telemetryService'
 import { TelemetryLogger } from './shared/telemetry/telemetryLogger'
+import { initializeManifestPaths } from './extensionShared'
+import { RegionProvider, defaultRegion } from './shared/regions/regionProvider'
+import { DefaultAWSClientBuilder } from './shared/awsClientBuilder'
 
 export async function activate(context: vscode.ExtensionContext) {
     setInBrowser(true) // THIS MUST ALWAYS BE FIRST
@@ -25,11 +34,35 @@ export async function activate(context: vscode.ExtensionContext) {
     await activateLogger(context, toolkitOutputChannel)
 
     await initializeComputeRegion()
+    initialize(context)
+    initializeManifestPaths(context)
 
     vscode.window.showInformationMessage(
         'AWS Toolkit: Browser Mode Under Development. No features are currently provided',
         { modal: false }
     )
+
+    try {
+        const awsContext = new DefaultAwsContext()
+        globals.awsContext = awsContext
+
+        globals.regionProvider = {
+            guessDefaultRegion: () => defaultRegion,
+        } as RegionProvider
+        globals.sdkClientBuilder = new DefaultAWSClientBuilder(awsContext)
+
+        const settings = Settings.instance
+
+        await activateTelemetry(context, awsContext, settings)
+    } catch (error) {
+        const stacktrace = (error as Error).stack?.split('\n')
+        // truncate if the stacktrace is unusually long
+        if (stacktrace !== undefined && stacktrace.length > 40) {
+            stacktrace.length = 40
+        }
+        getLogger('channel').error('error')
+        throw error
+    }
 }
 
 export async function deactivate() {}
