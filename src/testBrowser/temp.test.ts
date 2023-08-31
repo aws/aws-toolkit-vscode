b4b431d4460ab78aeac5c4b4576ee89d6bd8c0df
Nikolas Komonen <118216176+nkomonen-amazon@users.noreply.github.com>
2023-08-31 10:35:26 -0400

    refactor: various Browser related changes (#3785)
    * fix: package-lock.json not updated
    
    Problem:
    
    We recently updated the vscode engine in
    package.json.
    
    'npm install' was not run so the package-lock.json
    was not updated.
    
    Solution:
    
    Ran npm install and got the updated package-lock.json
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * npm: install webpack types package
    
    Add npm types to stop errors in webpack.browser.config.js
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * fix imports
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * refactor: only create instance when it will be used
    
    This was done due to the instance being created when
    running in the browser context and failing since the
    MetadatService of the DefaultEc2MetadataClient was being
    indicated as not existing.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: update logger utils to use new filesystem class
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * vscode-nls: Fix localize() not working in browser
    
    See the long comment in the code for info.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: move promisify call inside class
    
    Problem:
    When this module is loaded the promisify call gets called
    and it fails in the browser.
    
    We are not explicitly using the functionality at the moment.
    
    Solution:
    Move the promisify call to inside the class where it is used.
    If we do end up using this in the browser we will need to revisit
    this.
    But for now it allows things to run in the browser
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * dependency: remove 'filer' module
    
    It is not needed anymore. It just existed for
    testing purposes initially.
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * browser: polyfill 'fs' with 'memfs' temporarily
    
    Execution of the extension fails in browser mode
    partly due to 'fs' not working in the browser.
    
    Solution:
    
    polyfill 'fs' with 'memfs' TEMPORARILY during the
    development phase.
    
    Eventually we will need to see what still depends
    on 'fs' and if it is out of our control we may want
    to remove our dependency from it.
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * tests: setup initial browser tests
    
    - launch config to run the browser tests
    - temporary test the ensure things run
    - test the isInBrowser function is properly set
    
    - The browser extension is activated
    - We can test that the isInBrowser function has
      been properly set
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * feat: isInBrowser function
    
    Function that tells us if we are in the browser.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * build: remove vscode-nls copyFiles script logic
    
    During the copyFiles script we can indicate for
    nls.metadata files (that are generated by the
    now disabled loader in the base webpack config)
    to be copied to 'dist'.
    
    Since the files are not generated anymore we disable
    the copying of these files from being attempted.
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    * logger: don't fs.watch log file when in browser
    
    This will skip watching the log file for changes when
    we are in the browser.
    
    We will eventually want to remove this function as a whole
    and instead throw errors on attempts to interact
    with the file and it is not where we expect it to be.
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    
    ---------
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

---
+++ src/testBrowser/temp.test.ts
@@ -0,0 +1,12 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+
+describe('Temp Placeholder Test, this can eventually be removed', function () {
+    it('passes', function () {
+        assert.ok(true)
+    })
+})
