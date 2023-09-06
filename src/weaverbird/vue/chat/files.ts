9b586f6c9f89de2028c76fda5d7c77cb18b5f205
Joshua Pinkney <jpink@amazon.com>
2023-09-06 09:25:42 -0400

    weaverbird: fix files to use proper relative workspace paths
    The current string replacement for replacing relative workspace paths might not work in all edge cases
    
    Replace the string replacement for paths to use getWorkspaceRelativePath

--- src/weaverbird/vue/chat/files.ts
+++ src/weaverbird/vue/chat/files.ts
@@ -7,6 +7,7 @@ import * as vscode from 'vscode'
 import { FileMetadata } from '../../client/weaverbirdclient'
 import { SystemUtilities } from '../../../shared/systemUtilities'
 import { getExcludePattern } from '../../../shared/fs/watchedFiles'
+import { getWorkspaceRelativePath } from '../../../shared/utilities/workspaceUtils'
 
 export async function collectFiles(rootPath: string): Promise<FileMetadata[]> {
     const files = await vscode.workspace.findFiles(new vscode.RelativePattern(rootPath, '**'), getExcludePattern())
@@ -14,11 +15,14 @@ export async function collectFiles(rootPath: string): Promise<FileMetadata[]> {
     const storage = []
     for (const file of files) {
         const fileContent = await SystemUtilities.readFile(file)
-        storage.push({
-            // The LLM doesn't need absolute paths, only relative from the project
-            filePath: file.fsPath.replace(rootPath, 'src'),
-            fileContent: fileContent,
-        })
+        const relativePath = getWorkspaceRelativePath(file.fsPath)
+        if (relativePath) {
+            storage.push({
+                // The LLM doesn't need absolute paths, only relative from the project
+                filePath: relativePath,
+                fileContent: fileContent,
+            })
+        }
     }
     return storage
 }
