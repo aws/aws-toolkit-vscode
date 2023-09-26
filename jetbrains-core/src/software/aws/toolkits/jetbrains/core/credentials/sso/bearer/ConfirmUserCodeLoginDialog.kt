fd22cd3f5babd45706191944c2ab9f534e162e9d
Richard Li <742829+rli@users.noreply.github.com>
2023-09-25 17:29:39 -0700

    Move to new 'Confirm' device code flow (#3870)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/sso/bearer/ConfirmUserCodeLoginDialog.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/sso/bearer/ConfirmUserCodeLoginDialog.kt
@@ -9,12 +9,15 @@ import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.impl.ActionButton
+import com.intellij.openapi.editor.colors.EditorColorsUtil
 import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.components.BorderLayoutPanel
+import software.aws.toolkits.core.utils.tryOrNull
 import software.aws.toolkits.resources.message
 import software.aws.toolkits.telemetry.AwsTelemetry
 import software.aws.toolkits.telemetry.CredentialType
@@ -22,7 +25,7 @@ import software.aws.toolkits.telemetry.Result
 import java.awt.datatransfer.StringSelection
 import javax.swing.JComponent
 
-class CopyUserCodeForLoginDialog(
+class ConfirmUserCodeLoginDialog(
     private val authCode: String,
     private val dialogTitle: String,
     private val credentialType: CredentialType
@@ -30,30 +33,35 @@ class CopyUserCodeForLoginDialog(
 
     private val pane = panel {
         row {
-            text(message("aws.sso.signing.device.code.copy.dialog.text"), maxLineLength = -1)
+            label(message("aws.sso.signing.device.code.copy.dialog.text"))
         }
 
         row {
             cell(
                 BorderLayoutPanel(5, 0).apply {
                     val action = CopyUserCodeForLogin(authCode)
-                    addToCenter(JBLabel(authCode).setCopyable(true))
+                    addToCenter(
+                        JBLabel(authCode).apply {
+                            tryOrNull {
+                                JBFont.create(JBFont.decode(EditorColorsUtil.getGlobalOrDefaultColorScheme().consoleFontName)).biggerOn(9f).asBold()
+                            }?.let {
+                                font = it
+                            }
+                            setCopyable(true)
+                        }
+                    )
                     addToRight(ActionButton(action, action.templatePresentation.clone(), ActionPlaces.UNKNOWN, ActionToolbar.NAVBAR_MINIMUM_BUTTON_SIZE))
                 }
             ).horizontalAlign(HorizontalAlign.CENTER)
         }
     }
+
     override fun createCenterPanel(): JComponent? = pane
 
     init {
         title = dialogTitle
         setOKButtonText(message("aws.sso.signing.device.code"))
         super.init()
-    }
-
-    override fun doOKAction() {
-        CopyPasteManager.getInstance().setContents(StringSelection(authCode))
-        super.doOKAction()
     }
 
     override fun doCancelAction() {
