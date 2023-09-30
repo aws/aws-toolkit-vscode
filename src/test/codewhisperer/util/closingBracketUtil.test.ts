593ff38fdef42803382d96e8b81eea1189171371
Shao-Hua Lo <xshaohua@amazon.com>
2023-09-29 20:37:53 -0700

    test

--- src/test/codewhisperer/util/closingBracketUtil.test.ts
+++ src/test/codewhisperer/util/closingBracketUtil.test.ts
@@ -3,78 +3,85 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as vscode from 'vscode'
 import assert from 'assert'
-import * as sinon from 'sinon'
-import {
-    calculateBracketsLevel,
-    getBracketsToRemove,
-    removeBracketsFromRightContext,
-} from '../../../codewhisperer/util/closingBracketUtil'
-import { createMockTextEditor } from '../testUtil'
-import { getLogger } from '../../../shared/logger/logger'
-import { Position } from 'vscode'
+import { handleExtraBrackets } from '../../../codewhisperer/util/closingBracketUtil'
+import { openATextEditorWithText } from '../../testUtil'
 
+// TODO: refactor test cases
 describe('closingBracketUtil', function () {
-    describe('calculateBracketsLevel', function () {
-        it('Should return expected bracket level with index', function () {
-            const actual = calculateBracketsLevel('{{()')
-            const expected = [
-                {
-                    char: '{',
-                    count: 1,
-                    idx: 0,
-                },
-                {
-                    char: '{',
-                    count: 2,
-                    idx: 1,
-                },
-                {
-                    char: '(',
-                    count: 1,
-                    idx: 2,
-                },
-                {
-                    char: '(',
-                    count: 0,
-                    idx: 3,
-                },
-            ]
-            assert.deepStrictEqual(expected, actual)
-        })
-    })
+    describe('handleExtraBrackets', function () {
+        it('case 1 should remove extra closing', async function () {
+            const leftContext = 'function add2Numbers('
+            const rightContext = ')'
+            const recommendation = 'a: number, b: number) {\n    return a + b\n}'
 
-    describe('removeBracketsFromRightContext', function () {
-        afterEach(function () {
-            sinon.restore()
-        })
-        it('Should remove the brackets from corresponding index', async function () {
-            const mockEditor = createMockTextEditor()
-            const mockPosition = new Position(0, 0)
-            const mockIdx = [0, 1]
+            const editor = await openATextEditorWithText(leftContext + recommendation + rightContext, 'test.txt')
 
-            const loggerSpy = sinon.spy(getLogger(), 'info')
-            await removeBracketsFromRightContext(mockEditor, mockIdx, mockPosition)
-            assert.ok(loggerSpy.called)
-            const actual = loggerSpy.getCall(0).args[0]
-            assert.strictEqual(actual, `delete [{"line":0,"character":0},{"line":0,"character":1}]`)
-        })
-    })
+            await handleExtraBrackets(editor, recommendation, new vscode.Position(2, 1), new vscode.Position(0, 21))
 
-    describe('getBracketsToRemove', function () {
-        it('Should return an empty array if there is no extra bracket matched', function () {
-            const actual = getBracketsToRemove('return a+b}', '')
-            assert.ok(actual.length === 0)
+            assert.strictEqual(
+                editor.document.getText(),
+                'function add2Numbers(a: number, b: number) {\n    return a + b\n}'
+            )
         })
-        it('Should return expected bracket to remove', function () {
-            const actual = getBracketsToRemove('{return a+b}', '}')
-            const expected = [0]
-            assert.deepStrictEqual(actual, expected)
+
+        it('case 2 should remove extra closing', async function () {
+            const leftContext = 'function sum(a: number, b: number, '
+            const rightContext = ')'
+            const recommendation = 'c: number) {\n    return a + b + c\n}'
+
+            const editor = await openATextEditorWithText(leftContext + recommendation + rightContext, 'test.txt')
+
+            await handleExtraBrackets(editor, recommendation, new vscode.Position(2, 1), new vscode.Position(0, 35))
+
+            assert.strictEqual(editor.document.getText(), leftContext + recommendation)
         })
-        it('Should return expected bracket to remove if there are multiple matches', function () {
-            const actual = getBracketsToRemove(') {return a+b}', '){}')
-            const expected = [0, 1, 2]
-            assert.deepStrictEqual(actual, expected)
+
+        it('case 2 should not remove', async function () {
+            const leftContext = 'function add2Numbers('
+            const rightContext = ''
+            const recommendation = 'a: number, b: number) {\n  return a + b;\n}'
+
+            const editor = await openATextEditorWithText(leftContext + recommendation + rightContext, 'test.txt')
+
+            // const pos = editor.document.positionAt(editor.document.getText().length)
+            // editor.selection = new vscode.Selection(pos, pos)
+            // await editor.edit(
+            //     editBuilder => {
+            //         editBuilder.insert(pos, rightContext)
+            //     }
+            // )
+
+            await handleExtraBrackets(editor, recommendation, new vscode.Position(2, 1), new vscode.Position(0, 21))
+
+            assert.strictEqual(editor.document.getText(), leftContext + recommendation + rightContext)
+        })
+
+        it('case 3 should not remove', async function () {
+            const leftContext =
+                'export const launchTemplates: { [key: string]: AmazonEC2.LaunchTemplate } = {\n    lt1: { launchTemplateId: "lt-1", launchTemplateName: "foo" },\n    lt2: { launchTemplateId: "lt-2345", launchTemplateName: "bar" },\n    lt3: '
+            const rightContext = '\n};'
+            const recommendation = '{ launchTemplateId: "lt-3456", launchTemplateName: "baz" },'
+
+            const editor = await openATextEditorWithText(leftContext + recommendation + rightContext, 'test.txt')
+
+            await handleExtraBrackets(editor, recommendation, new vscode.Position(3, 68), new vscode.Position(3, 9))
+
+            assert.strictEqual(editor.document.getText(), leftContext + recommendation + rightContext)
+        })
+
+        it('case 4 should not remove', async function () {
+            const leftContext =
+                'export const launchTemplates: { [key: string]: AmazonEC2.LaunchTemplate } = {\n    lt1: { launchTemplateId: "lt-1", launchTemplateName: "foo" },\n    lt2: { launchTemplateId: "lt-2345", launchTemplateName: "bar" },\n    '
+            const rightContext = '\n};'
+            const recommendation = 'lt3: { launchTemplateId: "lt-3456", launchTemplateName: "baz" },'
+
+            const editor = await openATextEditorWithText(leftContext + recommendation + rightContext, 'test.txt')
+
+            await handleExtraBrackets(editor, recommendation, new vscode.Position(3, 68), new vscode.Position(3, 4))
+
+            assert.strictEqual(editor.document.getText(), leftContext + recommendation + rightContext)
         })
     })
 })
