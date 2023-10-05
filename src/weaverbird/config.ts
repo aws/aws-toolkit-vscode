4627aef08a8f6d1cc101b12fa9c988493aa00ad3
Matyas Boros <141938325+matboros@users.noreply.github.com>
2023-10-05 14:30:55 +0200

    feat(weaverbird): simplify local config override for testing (#1020)

--- src/weaverbird/config.ts
+++ src/weaverbird/config.ts
@@ -5,93 +5,101 @@
 
 import { getLogger } from '../shared/logger/logger'
 import type { LocalResolvedConfig } from './types'
-
-let cachedConfig: LocalResolvedConfig | undefined = undefined
+import globals from '../shared/extensionGlobals'
 
 export const getConfig: () => Promise<LocalResolvedConfig> = async () => {
-    return (
-        cachedConfig ??
-        (cachedConfig = await (async () => {
-            const appConfigFormatVersion = 2
-            const config = process.env.WEAVERBIRD_CONFIG
+    const appConfigFormatVersion = 2
+    const config = process.env.WEAVERBIRD_CONFIG
 
-            // const _betaConfig = {
-            //     endpoint: 'https://jq1ufi88qi.execute-api.us-east-1.amazonaws.com/beta',
-            //     region: 'us-east-1',
-            //     lambdaArns: {
-            //         approach: {
-            //             generate:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:GenerateApproachLambda:live',
-            //             iterate:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:IterateApproachLambda:live',
-            //         },
-            //         codegen: {
-            //             generate:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:GenerateCodeLambda:live',
-            //             iterate:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:IterateCodeLambda:live',
-            //             getResults:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:GetCodeGenerationResultLambda:live',
-            //             getIterationResults:
-            //                 'arn:aws:lambda:us-east-1:740920811238:function:GetCodeIterationResultLambda:live'
-            //         },
-            //     },
-            // }
-            const gammaConfig = {
-                endpoint: 'https://80u08f1ec9.execute-api.us-east-1.amazonaws.com/gamma',
-                region: 'us-east-1',
-                lambdaArns: {
-                    approach: {
-                        generate: 'arn:aws:lambda:us-east-1:789621683470:function:GenerateApproachLambda:live',
-                        iterate: 'arn:aws:lambda:us-east-1:789621683470:function:IterateApproachLambda:live',
-                    },
-                    codegen: {
-                        generate: 'arn:aws:lambda:us-east-1:789621683470:function:GenerateCodeLambda:live',
-                        iterate: 'arn:aws:lambda:us-east-1:789621683470:function:IterateCodeLambda:live',
-                        getResults: 'arn:aws:lambda:us-east-1:789621683470:function:GetCodeGenerationResultLambda:live',
-                        getIterationResults:
-                            'arn:aws:lambda:us-east-1:789621683470:function:GetCodeIterationResultLambda:live',
-                    },
+    const lambdaFunctionNames = {
+        approach: {
+            generate: 'GenerateApproachLambda:live',
+            iterate: 'IterateApproachLambda:live',
+        },
+        codegen: {
+            generate: 'GenerateCodeLambda:live',
+            iterate: 'IterateCodeLambda:live',
+            getResults: 'GetCodeGenerationResultLambda:live',
+            getIterationResults: 'GetCodeIterationResultLambda:live',
+        },
+    }
+    const getLambdaArns = (env: { account: string; region: string }) => ({
+        approach: {
+            generate: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.approach.generate}`,
+            iterate: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.approach.iterate}`,
+        },
+        codegen: {
+            generate: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.codegen.generate}`,
+            iterate: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.codegen.iterate}`,
+            getResults: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.codegen.getResults}`,
+            getIterationResults: `arn:aws:lambda:${env.region}:${env.account}:function:${lambdaFunctionNames.codegen.getResults}`,
+        },
+    })
+
+    const GAMMA = '789621683470'
+    const gammaEnv = { account: GAMMA, region: 'us-east-1' }
+    const gammaConfig = {
+        endpoint: 'https://80u08f1ec9.execute-api.us-east-1.amazonaws.com/gamma',
+        region: gammaEnv.region,
+        lambdaArns: getLambdaArns(gammaEnv),
+    }
+
+    const accountOverride = process.env.WEAVERBIRD_ACCOUNT_OVERRIDE
+
+    const defaultConfig =
+        (accountOverride ?? '') === ''
+            ? gammaConfig
+            : accountOverride === 'fromCredentials'
+            ? {
+                  // currently we invoke the lambdas directly, so we dont use this
+                  endpoint: 'https://invalid.url.com/gamma',
+                  region: globals.awsContext.getCredentialDefaultRegion(),
+                  lambdaArns: lambdaFunctionNames,
+              }
+            : (() => {
+                  const config = JSON.parse(accountOverride ?? '') as typeof gammaEnv
+                  return {
+                      // currently we invoke the lambdas directly, so we dont use this
+                      endpoint: 'https://invalid.url.com/gamma',
+                      region: config.region,
+                      lambdaArns: getLambdaArns(config),
+                  }
+              })()
+    try {
+        const parsedConfig = JSON.parse(config ?? `{ "version": ${appConfigFormatVersion}}`)
+        const localConfigVersion = Number.parseInt(parsedConfig.version)
+        if (isNaN(localConfigVersion) || localConfigVersion !== appConfigFormatVersion) {
+            const errorMessage = `Invalid config version, required ${appConfigFormatVersion}, found ${localConfigVersion}`
+            getLogger().error(errorMessage)
+            throw new Error(errorMessage)
+        }
+        return {
+            endpoint: (parsedConfig.endpoint as string) ?? defaultConfig.endpoint,
+            region: (parsedConfig.region as string) ?? defaultConfig.region,
+            lambdaArns: {
+                approach: {
+                    generate:
+                        (parsedConfig.lambdaArns?.approach?.generate as string) ??
+                        defaultConfig.lambdaArns.approach.generate,
+                    iterate:
+                        (parsedConfig.lambdaArns?.approach?.iterate as string) ??
+                        defaultConfig.lambdaArns.approach.iterate,
                 },
-            }
-            const defaultConfig = gammaConfig
-            try {
-                const parsedConfig = JSON.parse(config ?? `{ "version": ${appConfigFormatVersion}}`)
-                const localConfigVersion = Number.parseInt(parsedConfig.version)
-                if (isNaN(localConfigVersion) || localConfigVersion !== appConfigFormatVersion) {
-                    const errorMessage = `Invalid config version, required ${appConfigFormatVersion}, found ${localConfigVersion}`
-                    getLogger().error(errorMessage)
-                    throw new Error(errorMessage)
-                }
-                return {
-                    endpoint: (parsedConfig.endpoint as string) ?? defaultConfig.endpoint,
-                    region: (parsedConfig.region as string) ?? defaultConfig.region,
-                    lambdaArns: {
-                        approach: {
-                            generate:
-                                (parsedConfig.lambdaArns?.approach?.generate as string) ??
-                                defaultConfig.lambdaArns.approach.generate,
-                            iterate:
-                                (parsedConfig.lambdaArns?.approach?.iterate as string) ??
-                                defaultConfig.lambdaArns.approach.iterate,
-                        },
-                        codegen: {
-                            generate:
-                                (parsedConfig.lambdaArns?.codegen?.generate as string) ??
-                                defaultConfig.lambdaArns.codegen.generate,
-                            iterate:
-                                (parsedConfig.lambdaArns?.codegen?.iterate as string) ??
-                                defaultConfig.lambdaArns.codegen.iterate,
-                            getResults:
-                                (parsedConfig.lambdaArns?.codegen?.getResults as string) ??
-                                defaultConfig.lambdaArns.codegen.getResults,
-                            getIterationResults: parsedConfig.lambdaArns?.codegen?.getIterationResults as string,
-                        },
-                    },
-                }
-            } catch (e) {
-                return defaultConfig
-            }
-        })())
-    )
+                codegen: {
+                    generate:
+                        (parsedConfig.lambdaArns?.codegen?.generate as string) ??
+                        defaultConfig.lambdaArns.codegen.generate,
+                    iterate:
+                        (parsedConfig.lambdaArns?.codegen?.iterate as string) ??
+                        defaultConfig.lambdaArns.codegen.iterate,
+                    getResults:
+                        (parsedConfig.lambdaArns?.codegen?.getResults as string) ??
+                        defaultConfig.lambdaArns.codegen.getResults,
+                    getIterationResults: parsedConfig.lambdaArns?.codegen?.getIterationResults as string,
+                },
+            },
+        }
+    } catch (e) {
+        return defaultConfig
+    }
 }
