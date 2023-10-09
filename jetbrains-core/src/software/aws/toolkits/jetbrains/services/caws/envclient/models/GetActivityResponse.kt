56797f08eabf18e4e40925e9a3cbfa2ec933a5c2
manodnyab <66754471+manodnyab@users.noreply.github.com>
2023-10-09 11:03:57 -0700

    Prompt users  to continue their Dev Environment session (#3832)
    * added heartbeat
    
    * feedback changes 1
    
    * removed changes
    
    * added more tests
    
    * feedback changes 2
    
    * addressed feedback
    
    * new changes
    
    * corrected millisecond diff
    
    * detekt
    
    * feedback changes
    
    * feedbackc hanges
    
    * added tests
    
    * addressed feedback
    
    * changed test
    
    * detekt
    
    * removed unused comma
    
    * addressed feedback
    
    * fixed detekt
    
    * Added test
    
    * detekt
    
    * detekt
    
    * detekt
    
    * prop change

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/envclient/models/GetActivityResponse.kt
@@ -0,0 +1,8 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.caws.envclient.models
+
+data class GetActivityResponse(
+    val timestamp: String
+)
