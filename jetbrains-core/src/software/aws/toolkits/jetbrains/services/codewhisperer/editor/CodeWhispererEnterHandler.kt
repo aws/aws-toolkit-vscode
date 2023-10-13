4874197744a4df92da315568f51aa3ee2106a11c
Zoe Lin <60411978+zixlin7@users.noreply.github.com>
2023-10-13 12:42:31 -0700

    codewhisperer: suppress token infilling for auto-trigger (#3914)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/editor/CodeWhispererEnterHandler.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/editor/CodeWhispererEnterHandler.kt
@@ -9,12 +9,17 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import software.aws.toolkits.jetbrains.services.codewhisperer.editor.CodeWhispererEditorUtil.shouldSkipInvokingBasedOnRightContext
 import software.aws.toolkits.jetbrains.services.codewhisperer.service.CodeWhispererAutoTriggerService
 import software.aws.toolkits.jetbrains.services.codewhisperer.service.CodeWhispererAutomatedTriggerType
 
 class CodeWhispererEnterHandler(private val originalHandler: EditorActionHandler) : EnterHandler(originalHandler) {
     override fun executeWriteAction(editor: Editor, caret: Caret?, dataContext: DataContext?) {
         originalHandler.execute(editor, caret, dataContext)
+
+        if (shouldSkipInvokingBasedOnRightContext(editor)) {
+            return
+        }
 
         ApplicationManager.getApplication().executeOnPooledThread {
             CodeWhispererAutoTriggerService.getInstance().tryInvokeAutoTrigger(editor, CodeWhispererAutomatedTriggerType.Enter())
