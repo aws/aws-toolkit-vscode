39dbccc36ef39ee7600c18d3dfc8cbcb306cfc69
Tai Lai <tlaiamz@amazon.com>
2023-10-19 11:14:58 -0700

    update range on document change

--- src/codewhisperer/activation.ts
+++ src/codewhisperer/activation.ts
@@ -303,6 +303,8 @@ export async function activate(context: ExtContext): Promise<void> {
                  */
                 disposeSecurityDiagnostic(e)
 
+                SecurityIssueHoverProvider.instance.updateRanges(e)
+
                 CodeWhispererCodeCoverageTracker.getTracker(e.document.languageId)?.countTotalTokens(e)
 
                 /**
