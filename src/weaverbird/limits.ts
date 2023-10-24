9a50078b644ba046c24008d985b82868bd03aa62
Joshua Pinkney <jpink@amazon.com>
2023-10-24 15:43:46 -0400

    weaverbird: Add Project Size Too Large error
    Problem:
    - Currently users can upload projects as large as they want and the client side won't reject anything
    
    Solution:
    - Give a user upfront information about whether or not their request to upload code will fail

---
+++ src/weaverbird/limits.ts
@@ -0,0 +1,7 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Maximum size limit (in bytes) that a project should be
+export const projectSizeLimit = 25 * 1024 * 1024
