06e4fe87736fce90f8513f9488c0b7f234878a92
Richard Li <742829+rli@users.noreply.github.com>
2023-10-24 14:30:36 -0700

    Make font size for titled header bigger (#3936)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/gettingstarted/editor/GettingStartedPanel.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/gettingstarted/editor/GettingStartedPanel.kt
@@ -19,7 +19,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.ui.GotItTooltip
 import com.intellij.ui.IdeBorderFactory
-import com.intellij.ui.components.JBLabel
+import com.intellij.ui.TitledSeparator
 import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.BottomGap
@@ -122,15 +122,20 @@ class GettingStartedPanel(private val project: Project) : BorderLayoutPanel(), D
                         bottomGap(BottomGap.MEDIUM)
                     }
 
-                    group(
-                        JBLabel(message("aws.onboarding.getstarted.panel.group_title"))
-                            .apply {
-                                font = PANEL_TITLE_FONT
+                    // can't use group() because the font cant be overridden
+                    row {
+                        panel {
+                            row {
+                                cell(TitledSeparator(message("aws.onboarding.getstarted.panel.group_title"))).applyToComponent {
+                                    border = null
+                                    setTitleFont(JBFont.h1().asBold())
+                                }.align(AlignX.FILL)
                             }
-                    ) {
-                        featureSetPanel.setFeatureContent()
-                        row {
-                            cell(featureSetPanel)
+
+                            featureSetPanel.setFeatureContent()
+                            row {
+                                cell(featureSetPanel)
+                            }
                         }
                     }
 
