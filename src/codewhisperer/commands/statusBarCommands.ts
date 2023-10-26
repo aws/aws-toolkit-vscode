016ec3986024df1974560aa19ed5c6f024aed943
nkomonen <nkomonen@amazon.com>
2023-10-26 11:46:20 -0400

    fix: circular dependency
    - the refreshStatusBar in basicCommands.ts command imported inlineCompletionService.ts
    which imported basicCommands.ts
    
    - also the show quick pick command had circular dependency issues since it
      uses the codewhispererNode object. To fix this we moved the command to its
      own module
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>

---
+++ src/codewhisperer/commands/statusBarCommands.ts
@@ -0,0 +1,18 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { createExitButton } from '../../shared/ui/buttons'
+import { createQuickPick } from '../../shared/ui/pickerPrompter'
+import { Commands } from '../../shared/vscode/commands2'
+import { codewhispererNode } from '../explorer/codewhispererNode'
+
+export const showCodeWhispererQuickPickCommand = 'aws.codewhisperer.quickpick'
+export const showCodeWhispererQuickPick = Commands.declare({ id: showCodeWhispererQuickPickCommand }, () => () => {
+    createQuickPick(codewhispererNode.getChildren('item'), {
+        title: 'CodeWhisperer',
+        buttons: [createExitButton()],
+        ignoreFocusOut: false,
+    }).prompt()
+})
