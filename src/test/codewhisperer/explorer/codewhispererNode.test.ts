b89f14046d51e8521158ef690e102d50b887dc07
nkomonen <nkomonen@amazon.com>
2023-11-06 14:14:20 -0500

    fix: delay codewhisperer node resolve in initial load
    Problem:
    
    A CodeWhispererNode is resolved immediately on module load
    and this causes issues since its dependencies may have not
    been loaded yet during the extension activation process.
    
    Solution:
    
    Use a getter instead so that we only create the CodeWhispererNode
    instance when we need it, and all its dependencies should have
    been loaded by then.
    
    Signed-off-by: nkomonen <nkomonen@amazon.com>

--- src/test/codewhisperer/explorer/codewhispererNode.test.ts
+++ src/test/codewhisperer/explorer/codewhispererNode.test.ts
@@ -6,12 +6,17 @@
 import vscode from 'vscode'
 import assert from 'assert'
 import sinon from 'sinon'
-import { codewhispererNode } from '../../../codewhisperer/explorer/codewhispererNode'
+import { CodeWhispererNode, getCodewhispererNode } from '../../../codewhisperer/explorer/codewhispererNode'
 import { AuthUtil } from '../../../codewhisperer/util/authUtil'
 
 describe('codewhispererNode', function () {
     let isConnectionValid: sinon.SinonStub
     let isConnected: sinon.SinonStub
+    let codewhispererNode: CodeWhispererNode
+
+    before(function () {
+        codewhispererNode = getCodewhispererNode()
+    })
 
     beforeEach(function () {
         isConnectionValid = sinon.stub(AuthUtil.instance, 'isConnectionValid')
