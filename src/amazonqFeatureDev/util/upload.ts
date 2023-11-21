1cedd91a6dd1c5e2adb080692fed5ceda24eff06
Joao Salles <jvsalles@amazon.de>
2023-11-21 12:46:37 +0100

    Weaverbird: Send ChecksumSha256 header when uploading a file

--- src/amazonqFeatureDev/util/upload.ts
+++ src/amazonqFeatureDev/util/upload.ts
@@ -13,13 +13,15 @@ import { featureName } from '../constants'
  *
  * uses a presigned url and files checksum to transfer data to s3 through http.
  */
-export async function uploadCode(url: string, buffer: Buffer, kmsKeyArn?: string) {
+export async function uploadCode(url: string, buffer: Buffer, checksumSha256: string, kmsKeyArn?: string) {
     try {
         await got(url, {
             method: 'PUT',
             body: buffer,
             headers: {
                 'Content-Type': 'application/zip',
+                'Content-Length': String(buffer.length),
+                'x-amz-checksum-sha256': checksumSha256,
                 ...(kmsKeyArn && {
                     'x-amz-server-side-encryption-aws-kms-key-id': kmsKeyArn,
                     'x-amz-server-side-encryption': 'aws:kms',
