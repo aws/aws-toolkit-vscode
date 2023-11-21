b9e31dfbca54c1d3f9de69d57ccdcc4193f18476
Bryce Ito <bryceito@amazon.com>
2023-11-21 11:27:41 -0800

    Revert "codewhisperer/featureDev: Switch to the public api and deprec… (#1438)
    Revert "codewhisperer/featureDev: Switch to the public api and deprecate region selector (#1437)"
    
    This reverts commit 46c1f49713b4c3c59921ede00a9dddf3fcf699d4.

--- src/codewhisperer/models/constants.ts
+++ src/codewhisperer/models/constants.ts
@@ -7,7 +7,9 @@
  * SDK Client
  */
 
-export const endpoint = 'https://codewhisperer.us-east-1.amazonaws.com/'
+// export const endpoint = 'https://codewhisperer.us-east-1.amazonaws.com/'
+
+export const endpoint = 'https://rts.gamma-us-east-1.codewhisperer.ai.aws.dev/' // gamma IAD
 
 export const region = 'us-east-1'
 
