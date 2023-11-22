d6f8d8d93da858cb4b86f775d5eb93a16c441674
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2023-11-22 13:26:35 -0500

    weaverbird: user can say "yes" to move from approach to codegen
    Problem:
    - Right now users have to explicitly click the write code button but we prompt the user with "Would you like me to generate a suggestion for this? You will be able to review a file diff before inserting code in your project."
    
    Solution:
    - Allow the user to also say yes,ya and all the case insensitive variations of those

--- src/amazonqFeatureDev/controllers/chat/controller.ts
+++ src/amazonqFeatureDev/controllers/chat/controller.ts
@@ -164,6 +164,10 @@ export class FeatureDevController {
                 return
             }
 
+            if (session.state.phase === 'Approach' && /yes|ya|y|yeah|ok/i.test(message.message)) {
+                return this.writeCodeClicked(message)
+            }
+
             switch (session.state.phase) {
                 case 'Init':
                 case 'Approach':
