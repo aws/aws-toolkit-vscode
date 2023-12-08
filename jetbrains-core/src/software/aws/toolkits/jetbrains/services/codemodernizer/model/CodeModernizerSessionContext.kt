314db55cdf40df73004536cd055f0e2327c37a24
joratp <64931968+joratp@users.noreply.github.com>
2023-12-08 11:57:24 -0800

    added the addParentsPom to the maven command (#4029)
    * added the addParentsPom to the maven command
    
    * added the changelog
    
    * Updated the change log message to be shorter per request.
    
    * Updated the chnage log to include amazon q transform
    
    ---------
    
    Co-authored-by: Parsa Jorat <parsjora@amazon.com>

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codemodernizer/model/CodeModernizerSessionContext.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codemodernizer/model/CodeModernizerSessionContext.kt
@@ -141,7 +141,8 @@ data class CodeModernizerSessionContext(
                 "dependency:copy-dependencies",
                 "-DoutputDirectory=$destinationDir",
                 "-Dmdep.useRepositoryLayout=true",
-                "-Dmdep.copyPom=true"
+                "-Dmdep.copyPom=true",
+                "-Dmdep.addParentPoms=true"
             )
                 .withWorkDirectory(sourceFolder)
                 .withRedirectErrorStream(true)
