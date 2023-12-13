db59be96e1ff6ab6036e46784ac3d45cf31847fe
Richard Li <742829+rli@users.noreply.github.com>
2023-12-13 15:34:18 -0800

    Don't show CodeWhisperer status widget in remote env (#4056)

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/status/CodeWhispererStatusBarWidgetFactory.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/status/CodeWhispererStatusBarWidgetFactory.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.impl.status.widget.StatusBarEditorBasedWidgetFactory
 import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.isCodeWhispererEnabled
+import software.aws.toolkits.jetbrains.utils.isRunningOnRemoteBackend
 import software.aws.toolkits.resources.message
 
 class CodeWhispererStatusBarWidgetFactory : StatusBarEditorBasedWidgetFactory() {
@@ -17,7 +18,7 @@ class CodeWhispererStatusBarWidgetFactory : StatusBarEditorBasedWidgetFactory() 
     override fun getDisplayName(): String = message("codewhisperer.statusbar.display_name")
 
     override fun isAvailable(project: Project): Boolean =
-        isCodeWhispererEnabled(project)
+        !isRunningOnRemoteBackend() && isCodeWhispererEnabled(project)
 
     override fun createWidget(project: Project): StatusBarWidget = CodeWhispererStatusBarWidget(project)
 
