f3cf826d2e3d24904fe6e83d7f91f7dad626bd34
David Hasani <davhasan@amazon.com>
2023-12-16 21:14:24 -0800

    fix: fix unit test

--- src/test/codewhisperer/commands/transformByQ.test.ts
+++ src/test/codewhisperer/commands/transformByQ.test.ts
@@ -87,7 +87,7 @@ describe('transformByQ', function () {
         )
     })
 
-    it.only('WHEN validateProjectSelection called on project with pom.xml but no class files THEN throws error', async function () {
+    it('WHEN validateProjectSelection called on project with pom.xml but no class files THEN throws error', async function () {
         const folder = await createTestWorkspaceFolder()
         const dummyPomPath = path.join(folder.uri.fsPath, 'pom.xml')
         toFile('', dummyPomPath)
