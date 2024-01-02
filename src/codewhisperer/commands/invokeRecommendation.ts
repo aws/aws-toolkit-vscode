8b732fba1236528e8cb896db2836370f4b19b95b
Maxim Hayes <hayemaxi@amazon.com>
2024-01-02 15:11:31 -0800

    refactor(auth): don't display switch connections/add new connections in Q/CW ellipses menus
    Problem: Ellpises menus for Q/CW will display "Switch Connections", implying that the connection can be switched between
    other IdC/Builder ID connections on the fly. This isn't supported currently. Also, 'Add new connection' is displayed when already
    signed in, implying that you can add these new connections to swap to.
    
    Soluton: Remove 'Switch Connections' menu item, and only show 'Add new connection' when not signed in.
    
    Also, rename the global context variables to to be more descriptive/not be in all caps.

--- src/codewhisperer/commands/invokeRecommendation.ts
+++ src/codewhisperer/commands/invokeRecommendation.ts
@@ -57,7 +57,7 @@ export async function invokeRecommendation(
     }
 
     if (isCloud9('any')) {
-        // C9 manual trigger key alt/option + C is ALWAYS enabled because the VSC version C9 is on doesn't support setContextKey which is used for CODEWHISPERER_ENABLED
+        // C9 manual trigger key alt/option + C is ALWAYS enabled because the VSC version C9 is on doesn't support setContextKey which is used for aws.codewhisperer.connected
         // therefore we need a connection check if there is ANY connection(regardless of the connection's state) connected to CodeWhisperer on C9
         if (!AuthUtil.instance.isConnected()) {
             return
