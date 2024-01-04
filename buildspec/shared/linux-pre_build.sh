9ef497cb14f4c8fdca42532f2d933e308c14b1cc
Justin M. Keyes <jmkeyes@amazon.com>
2024-01-04 13:58:26 -0800

    ci: report "npm WARN deprecated"
    Problem:
    Deprecated dependencies may not be noticed for a long time.
    
        npm WARN deprecated querystring@0.2.0: The querystring API is considered Legacy. new code should use the URLSearchParams API instead.
        npm WARN deprecated winston@3.7.1: Please use version 3.6.0 or >3.7.1 when available due to https://github.com/winstonjs/winston/issues/2103
    
    Solution:
    Collect these warnings when running CI.
    TODO: fail if new warnings are found.

--- buildspec/shared/linux-pre_build.sh
+++ buildspec/shared/linux-pre_build.sh
@@ -3,6 +3,12 @@
 # Common code for "pre_build" phase of linux codebuild CI job.
 
 set -e
+set -o pipefail
+
+_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Include common functions.
+. "${_SCRIPT_DIR}/common.sh"
 
 # If present, log into CodeArtifact. Provides a fallback in case NPM is down.
 # Should only affect tests run through Toolkits-hosted CodeBuild.
@@ -14,5 +20,7 @@ if [ "$TOOLKITS_CODEARTIFACT_DOMAIN" ] && [ "$TOOLKITS_CODEARTIFACT_REPO" ] && [
     fi
 fi
 
-# TODO: do this in the "install" phase?
-npm ci
+# TODO: move this to the "install" phase?
+npm 2>&1 ci | run_and_report 'npm WARN deprecated' 'Deprecated dependencies must be updated.'
+# TODO: fail the CI job
+# || { echo ''; exit 1; }
