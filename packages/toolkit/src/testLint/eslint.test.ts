e483b28c921d23ba7c29cfcd264cddbf578d36c0
Maxim Hayes <hayemaxi@amazon.com>
2024-01-29 10:49:49 -0500

    fix linting, add header to gitignore-parser.d.ts

--- packages/toolkit/src/testLint/eslint.test.ts
+++ packages/toolkit/src/testLint/eslint.test.ts
@@ -10,9 +10,12 @@ describe('eslint', function () {
     this.timeout(180_000)
 
     it('passes eslint', function () {
-        const result = runCmd(['./node_modules/.bin/eslint', '-c', '.eslintrc.js', '--ext', '.ts', '.'], {
-            throws: false,
-        })
+        const result = runCmd(
+            ['./node_modules/.bin/eslint', '-c', '.eslintrc.js', '--ext', '.ts', 'packages', 'plugins'],
+            {
+                throws: false,
+            }
+        )
         assert.strictEqual(result.status, 0, result.stdout.toString())
     })
 })
--- packages/toolkit/types/gitignore-parser.d.ts
+++ packages/toolkit/types/gitignore-parser.d.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 declare module '@gerhobbelt/gitignore-parser' {
     export interface GitIgnoreAcceptor {
         accepts(filePath: string)
