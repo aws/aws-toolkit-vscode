0df6dec4740bc69ade733362611a2795646b2f60
Maxim Hayes <hayemaxi@amazon.com>
2024-02-01 10:33:48 -0500

    add cleanup after getServiceEnvVarConfig test

--- src/test/shared/vscode/env.test.ts
+++ src/test/shared/vscode/env.test.ts
@@ -7,13 +7,25 @@ import assert from 'assert'
 import { getServiceEnvVarConfig } from '../../../shared/vscode/env'
 
 describe('getServiceEnvVarConfig', function () {
+    const envVars: string[] = []
+
+    afterEach(() => {
+        envVars.forEach(v => delete process.env[v])
+        envVars.length = 0
+    })
+
+    function addEnvVar(k: string, v: string) {
+        process.env[k] = v
+        envVars.push(k)
+    }
+
     it('gets service config', async function () {
         const service = 'codecatalyst'
         const configToEnvMap = {
             region: '__CODECATALYST_REGION',
             endpoint: '__CODECATALYST_ENDPOINT',
         }
-        process.env['__CODECATALYST_ENDPOINT'] = 'test.endpoint'
+        addEnvVar('__CODECATALYST_ENDPOINT', 'test.endpoint')
 
         const expectedConfig = {
             endpoint: 'test.endpoint',
