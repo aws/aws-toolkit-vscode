f4428616bc0282589631e4043bc45cd2e3b53ded
aws-toolkit-automation <>
2024-02-12 11:55:48 -0500

    update .vscodeignore, remove nls

--- packages/amazonq/src/extension.ts
+++ packages/amazonq/src/extension.ts
@@ -3,14 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as nls from 'vscode-nls'
 import * as vscode from 'vscode'
 
-let localize: nls.LocalizeFunc
-
 export async function activate(context: vscode.ExtensionContext) {
-    localize = nls.loadMessageBundle()
-
     void vscode.window.showInformationMessage(
         'Amazon Q + CodeWhisperer: This extension is under development and offers no features at this time.'
     )
