b8fb3b9199b54c50eb44fda098fce0762fa40164
Jacob Largent <largjaco@amazon.com>
2024-02-22 13:54:37 -0800

    fix(appcomposer): add null checks to parsed JSON files

--- packages/toolkit/src/shared/cloudformation/cloudformation.ts
+++ packages/toolkit/src/shared/cloudformation/cloudformation.ts
@@ -439,9 +439,9 @@ export async function tryLoad(
 
     // Check if the template is a SAM template, using the same heuristic as the cfn-lint team:
     // https://github.com/aws-cloudformation/aws-cfn-lint-visual-studio-code/blob/629de0bac4f36cfc6534e409a6f6766a2240992f/client/src/yaml-support/yaml-schema.ts#L39-L51
-    if (rv.template.AWSTemplateFormatVersion || rv.template.Resources) {
+    if (rv.template?.AWSTemplateFormatVersion || rv.template?.Resources) {
         rv.kind =
-            rv.template.Transform && rv.template.Transform.toString().startsWith('AWS::Serverless') ? 'sam' : 'cfn'
+            rv.template?.Transform && rv.template.Transform.toString().startsWith('AWS::Serverless') ? 'sam' : 'cfn'
 
         return rv
     }
