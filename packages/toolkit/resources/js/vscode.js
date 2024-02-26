0d33dbc251ef8ac3d945897f11dac4f863812f8e
Maxim Hayes <hayemaxi@amazon.com>
2024-02-26 17:35:28 -0500

    delete resources, package.nls.json

--- packages/toolkit/resources/js/vscode.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// This injects the VSC webview API into frontend components
-// eslint-disable-next-line no-undef
-const vscode = acquireVsCodeApi()
