c5533b7f54d8b4ca76577c604be01ec46e962952
dhruvigajjar <dhruvi25461@gmail.com>
2024-03-01 16:39:19 -0800

    formated

--- packages/core/src/shared/telemetry/util.ts
+++ packages/core/src/shared/telemetry/util.ts
@@ -92,7 +92,7 @@ export async function getUserAgent(
     return pairs.join(' ')
 }
 
-type EnvType = 'cloud9' | 'cloud9-codecatalyst' | 'codecatalyst' | 'local' | 'ec2' | 'sagemaker' | 'test' | 'wsl' |'unknown' 
+type EnvType = 'cloud9' | 'cloud9-codecatalyst' | 'codecatalyst' | 'local' | 'ec2' | 'sagemaker' | 'test' | 'wsl' |'unknown'
 
 export function getComputeEnvType(): EnvType {
     if (isCloud9('classic')) {
