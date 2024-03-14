690406cd5dc7d3202542303ec04151f34a93fca2
Shao-Hua Lo <xshaohua@amazon.com>
2024-03-13 22:39:22 -0700

    rename editorGutter -> activeState controller

--- src/codewhisperer/service/serviceContainer.ts
+++ src/codewhisperer/service/serviceContainer.ts
@@ -4,7 +4,7 @@
  */
 
 import { AuthUtil } from '../util/authUtil'
-import { EditorGutterController } from '../views/annotations/editorGutterController'
+import { activeStateController } from '../views/annotations/activeStateController'
 import { LineAnnotationController } from '../views/annotations/lineAnnotationController'
 import { LineTracker } from '../views/annotations/lineTracker'
 
@@ -26,12 +26,12 @@ export class Container {
 
     readonly _lineTracker: LineTracker
     readonly _lineAnnotationController: LineAnnotationController
-    readonly _editorGutterController: EditorGutterController
+    readonly _editorGutterController: activeStateController
 
     constructor(readonly auth: AuthUtil) {
         this._lineTracker = new LineTracker()
         this._lineAnnotationController = new LineAnnotationController(this)
-        this._editorGutterController = new EditorGutterController(this)
+        this._editorGutterController = new activeStateController(this)
     }
 
     ready() {
