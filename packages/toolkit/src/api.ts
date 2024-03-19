2a294b8e9d9722c57aaf2f22ac35488319425790
Lei Gao <97199248+leigaol@users.noreply.github.com>
2024-03-19 10:09:14 -0700

    Update packages/toolkit/src/api.ts
    Co-authored-by: Maxim Hayes <149123719+hayemaxi@users.noreply.github.com>

--- packages/toolkit/src/api.ts
+++ packages/toolkit/src/api.ts
@@ -4,7 +4,7 @@
  */
 
 import { Auth, Connection } from 'aws-core-vscode/auth'
-/*!
+/**
  * Exposing listConnections API for other extension to read or re-use
  * the available connections in aws toolkit.
  */
