0031b58c80c5071e65eed7e1ccf911b5ac493636
Lei Gao <leigaol@amazon.com>
2024-03-19 13:37:08 -0700

    add connection api

--- packages/toolkit/src/api.ts
+++ packages/toolkit/src/api.ts
@@ -4,12 +4,36 @@
  */
 
 import { Auth, Connection } from 'aws-core-vscode/auth'
+
+export interface AwsToolkitSsoConnection {
+    readonly id: string
+    readonly label: string
+    readonly type: string
+    readonly ssoRegion: string
+    readonly startUrl: string
+    readonly scopes?: string[]
+}
+
 /**
  * Exposing listConnections API for other extension to read or re-use
  * the available connections in aws toolkit.
  */
 export const awsToolkitApi = {
-    async listConnections(): Promise<Connection[]> {
-        return Auth.instance.listConnections()
+    async listSsoConnections(): Promise<AwsToolkitSsoConnection[]> {
+        const connections = await Auth.instance.listConnections()
+        const exposedConnections: AwsToolkitSsoConnection[] = []
+        connections.forEach((x: Connection) => {
+            if ('ssoRegion' in x) {
+                exposedConnections.push({
+                    id: x.id,
+                    label: x.label,
+                    type: x.type,
+                    ssoRegion: x.ssoRegion,
+                    startUrl: x.startUrl,
+                    scopes: x.scopes,
+                })
+            }
+        })
+        return exposedConnections
     },
 }
