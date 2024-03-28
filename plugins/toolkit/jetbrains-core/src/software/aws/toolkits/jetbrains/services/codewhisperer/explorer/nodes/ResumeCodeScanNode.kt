f96f5b334bd88e6af6467f7f5cae3b3e69123b73
Ashish Reddy Podduturi <ashishrp@amazon.com>
2024-03-27 23:14:03 -0700

    Correcting Resume Auto Code Scans Node imports.

--- plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/nodes/ResumeCodeScanNode.kt
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/nodes/ResumeCodeScanNode.kt
@@ -3,9 +3,10 @@
 
 package software.aws.toolkits.jetbrains.services.codewhisperer.explorer.nodes
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
-import software.aws.toolkits.jetbrains.services.codewhisperer.codescan.CodeWhispererCodeScanManager
 import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager
+import software.aws.toolkits.resources.message
 import java.awt.event.MouseEvent
 
 class ResumeCodeScanNode(nodeProject: Project) : CodeWhispererActionNode(
@@ -19,4 +20,3 @@ class ResumeCodeScanNode(nodeProject: Project) : CodeWhispererActionNode(
         CodeWhispererExplorerActionManager.getInstance().SetAutoCodeScan(project, true)
     }
 }
-
