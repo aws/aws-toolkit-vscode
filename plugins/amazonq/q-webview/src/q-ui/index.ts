c6efdb3c00b26d4926a8bef24c7b45c321ebc52a
Will Lo <96078566+Will-ShaoHua@users.noreply.github.com>
2024-03-29 16:57:13 -0700

    webview-ui: add vuex dependency (#1849)

--- plugins/amazonq/q-webview/src/q-ui/index.ts
+++ plugins/amazonq/q-webview/src/q-ui/index.ts
@@ -3,6 +3,23 @@
 
 // eslint-disable-next-line header/header
 import { createApp } from 'vue'
+import {createStore, Store} from 'vuex'
 import HelloWorld from './components/root.vue'
+
+export interface State {}
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $store: Store<State>
+    }
+}
+
 const app = createApp(HelloWorld)
-app.mount('#app')
+const store = createStore<State>({
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {},
+})
+app.use(store).mount('#app')
