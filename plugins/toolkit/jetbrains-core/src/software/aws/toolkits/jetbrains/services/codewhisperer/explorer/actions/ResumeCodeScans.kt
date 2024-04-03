a95fee2ee926fab997b9a39bb2f80ca38d7f4fa5
Ashish Reddy Podduturi <ashishrp@amazon.com>
2024-04-03 15:19:10 -0700

    Adding trigger for resume code scans.

--- plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/actions/ResumeCodeScans.kt
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/actions/ResumeCodeScans.kt
@@ -6,7 +6,9 @@ package software.aws.toolkits.jetbrains.services.codewhisperer.explorer.actions
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
+import software.aws.toolkits.jetbrains.services.codewhisperer.codescan.CodeWhispererCodeScanManager
 import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager
+import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants
 import software.aws.toolkits.resources.message
 
 class ResumeCodeScans : DumbAwareAction(
@@ -17,5 +19,8 @@ class ResumeCodeScans : DumbAwareAction(
         val project = e.project ?: return
 
         CodeWhispererExplorerActionManager.getInstance().setAutoCodeScan(project, true)
+
+        //  Run Proactive Code File Scan once toggle is enabled
+        CodeWhispererCodeScanManager.getInstance(project).debouncedCodeScan(CodeWhispererConstants.SecurityScanType.FILE)
     }
 }
