46f2a8fefeb84f5d7cc1f7277f5635bb9e17c711
Diana Abitova <144136687+dianaabv16@users.noreply.github.com>
2024-04-04 21:09:38 +0200

    test(Amazon Q FeatureDev): insertChanges #4594
    Problem
    No tests for #4556
    
    Solution:
    Add a test.
    
    TODO: rework the tests to actually test the real codepaths instead of over-using stubs/mocks.

--- packages/core/src/test/amazonqFeatureDev/session/session.test.ts
+++ packages/core/src/test/amazonqFeatureDev/session/session.test.ts
@@ -3,19 +3,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as vscode from 'vscode'
+import * as assert from 'assert'
+
 import sinon from 'sinon'
-import { createMessenger, createSession } from '../utils'
-import { Session } from '../../../amazonqFeatureDev/session/session'
+
+import {
+    ControllerSetup,
+    createController,
+    createMessenger,
+    createSession,
+    generateVirtualMemoryUri,
+    sessionRegisterProvider,
+    sessionWriteFile,
+} from '../utils'
+import { CurrentWsFolders } from '../../../amazonqFeatureDev/types'
+import { CodeGenState } from '../../../amazonqFeatureDev/session/sessionState'
+import { FeatureDevClient } from '../../../amazonqFeatureDev/client/featureDev'
+import path from 'path'
 import { assertTelemetry } from '../../testUtil'
+import { Messenger } from '../../../amazonqFeatureDev/controllers/chat/messenger/messenger'
+import { FileSystemCommon } from '../../../srcShared/fs'
 
 describe('session', () => {
     const conversationID = '12345'
+    let messenger: Messenger
 
-    let session: Session
-
-    beforeEach(async () => {
-        const messenger = createMessenger()
-        session = await createSession({ messenger, conversationID })
+    beforeEach(() => {
+        messenger = createMessenger()
     })
 
     afterEach(() => {
@@ -24,11 +39,85 @@ describe('session', () => {
 
     describe('preloader', () => {
         it('emits start chat telemetry', async () => {
+            const session = await createSession({ messenger, conversationID })
+
             await session.preloader('implement twosum in typescript')
 
             assertTelemetry('amazonq_startConversationInvoke', {
                 amazonqConversationId: conversationID,
             })
+        })
+    })
+    describe('insertChanges', async () => {
+        afterEach(() => {
+            sinon.restore()
+        })
+
+        let workspaceFolderUriFsPath: string
+        const notRejectedFileName = 'notRejectedFile.js'
+        const notRejectedFileContent = 'notrejectedFileContent'
+        let uri: vscode.Uri
+        let encodedContent: Uint8Array
+
+        async function createCodeGenState() {
+            const controllerSetup: ControllerSetup = await createController()
+
+            const uploadID = '789'
+            const tabID = '123'
+            const testApproach = 'test-approach'
+            const workspaceFolders = [controllerSetup.workspaceFolder] as CurrentWsFolders
+            workspaceFolderUriFsPath = controllerSetup.workspaceFolder.uri.fsPath
+            uri = generateVirtualMemoryUri(uploadID, notRejectedFileName)
+
+            const testConfig = {
+                conversationId: conversationID,
+                proxyClient: {} as unknown as FeatureDevClient,
+                sourceRoots: [''],
+                uploadId: uploadID,
+                workspaceFolders,
+            }
+
+            const codeGenState = new CodeGenState(
+                testConfig,
+                testApproach,
+                [
+                    {
+                        zipFilePath: notRejectedFileName,
+                        relativePath: notRejectedFileName,
+                        fileContent: notRejectedFileContent,
+                        rejected: false,
+                        virtualMemoryUri: uri,
+                        workspaceFolder: controllerSetup.workspaceFolder,
+                    },
+                    {
+                        zipFilePath: 'rejectedFile.js',
+                        relativePath: 'rejectedFile.js',
+                        fileContent: 'rejectedFileContent',
+                        rejected: true,
+                        virtualMemoryUri: generateVirtualMemoryUri(uploadID, 'rejectedFile.js'),
+                        workspaceFolder: controllerSetup.workspaceFolder,
+                    },
+                ],
+                [],
+                [],
+                tabID,
+                0
+            )
+            const session = await createSession({ messenger, sessionState: codeGenState, conversationID })
+            encodedContent = new TextEncoder().encode(notRejectedFileContent)
+            await sessionRegisterProvider(session, uri, encodedContent)
+            return session
+        }
+        it('only insert non rejected files', async () => {
+            const fsSpyWriteFile = sinon.spy(FileSystemCommon.instance, 'writeFile')
+            const session = await createCodeGenState()
+            await sessionWriteFile(session, uri, encodedContent)
+            await session.insertChanges()
+
+            const absolutePath = path.join(workspaceFolderUriFsPath, notRejectedFileName)
+
+            assert.ok(fsSpyWriteFile.calledOnce)
+            assert.ok(fsSpyWriteFile.calledWith(absolutePath, notRejectedFileContent))
         })
     })
 })
