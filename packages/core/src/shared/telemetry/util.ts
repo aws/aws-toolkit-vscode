73e5584e081574b12b093fe1fa988f3d705d27d9
Lei Gao <leigaol@amazon.com>
2024-04-15 15:24:42 -0700

    UA

--- packages/core/src/shared/telemetry/util.ts
+++ packages/core/src/shared/telemetry/util.ts
@@ -16,7 +16,6 @@ import { Result } from './telemetry.gen'
 import { MetricDatum } from './clienttelemetry'
 import { isValidationExemptMetric } from './exemptMetrics'
 import { isCloud9, isSageMaker } from '../../shared/extensionUtilities'
-import { VSCODE_EXTENSION_ID } from '../utilities'
 
 const legacySettingsTelemetryValueDisable = 'Disable'
 const legacySettingsTelemetryValueEnable = 'Enable'
@@ -78,10 +77,7 @@ export async function getUserAgent(
     opt?: { includePlatform?: boolean; includeClientId?: boolean },
     globalState = globals.context.globalState
 ): Promise<string> {
-    const pairs =
-        globals.context.extension.id === VSCODE_EXTENSION_ID.amazonq
-            ? [`AmazonQ-For-VSCode/${extensionVersion}`]
-            : [`AWS-Toolkit-For-VSCode/${extensionVersion}`]
+    const pairs = [`AWS-Toolkit-For-VSCode/${extensionVersion}`]
 
     if (opt?.includePlatform) {
         pairs.push(platformPair())
