9f013e652bcce6603d52a8fcc0d8179d65fb501b
Santiago Martin <sannicm@amazon.de>
2024-04-17 11:08:21 +0200

    fix(amazonqFeatureDev): race condition on session storage

--- packages/core/src/amazonqFeatureDev/storages/chatSession.ts
+++ packages/core/src/amazonqFeatureDev/storages/chatSession.ts
@@ -3,11 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import AsyncLock from 'async-lock'
 import { Messenger } from '../controllers/chat/messenger/messenger'
 import { Session } from '../session/session'
 import { createSessionConfig } from '../session/sessionConfigFactory'
-
+import { getLogger } from '../../shared/logger'
 export class ChatSessionStorage {
+    private lock = new AsyncLock()
+
     private sessions: Map<string, Session> = new Map()
 
     constructor(private readonly messenger: Messenger) {}
@@ -16,16 +19,20 @@ export class ChatSessionStorage {
         const sessionConfig = await createSessionConfig()
         const session = new Session(sessionConfig, this.messenger, tabID)
         this.sessions.set(tabID, session)
+        getLogger().info(`after createSession: ${JSON.stringify(Object.fromEntries(this.sessions))}`)
         return session
     }
 
     public async getSession(tabID: string): Promise<Session> {
-        const sessionFromStorage = this.sessions.get(tabID)
-        if (sessionFromStorage === undefined) {
-            // If a session doesn't already exist just create it
-            return this.createSession(tabID)
-        }
-        return sessionFromStorage
+        return this.lock.acquire(tabID, async () => {
+            const sessionFromStorage = this.sessions.get(tabID)
+            getLogger().info(`sessionFromStorage: ${JSON.stringify(Object.fromEntries(this.sessions))}`)
+            if (sessionFromStorage === undefined) {
+                // If a session doesn't already exist just create it
+                return this.createSession(tabID)
+            }
+            return sessionFromStorage
+        })
     }
 
     // Find all sessions that are currently waiting to be authenticated
