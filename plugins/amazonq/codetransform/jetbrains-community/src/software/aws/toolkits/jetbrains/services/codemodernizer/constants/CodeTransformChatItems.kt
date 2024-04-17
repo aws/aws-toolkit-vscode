414e9eb57baed67a201c24434e22ecd4a2687e1d
David Hasani <60020664+dhasani23@users.noreply.github.com>
2024-04-17 16:33:50 -0700

    fix(amazonq): string updates (#4282)

--- plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/constants/CodeTransformChatItems.kt
+++ plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/constants/CodeTransformChatItems.kt
@@ -189,7 +189,7 @@ fun buildCompileLocalFailedChatContent() = CodeTransformChatMessageContent(
     message = "${message(
         "codemodernizer.chat.message.local_build_failed"
     )}\n\n${message(
-        "codemodernizer.chat.message.doc_link_prefix",
+        "codemodernizer.chat.message.validation.error.more_info",
         CODE_TRANSFORM_TROUBLESHOOT_DOC
     )}",
 )
@@ -223,7 +223,7 @@ fun buildTransformResultChatContent(result: CodeModernizerJobCompletedResult): C
             "${message(
                 "codemodernizer.chat.message.result.zip_too_large"
             )}\n\n${message(
-                "codemodernizer.chat.message.doc_link_prefix",
+                "codemodernizer.chat.message.validation.error.more_info",
                 CODE_TRANSFORM_TROUBLESHOOT_DOC
             )}"
         }
