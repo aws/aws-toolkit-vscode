f6bef3986cfbe9283a2610905fcddb9fdad26f3d
Joshua Pinkney <jpink@amazon.com>
2024-04-23 09:05:16 -0400

    fix: copy auth page to amazon q extension
    Problem:
    - Amazon q extension doesn't have the resources for the auth page
    
    Solution:
    - Copy them in

--- packages/amazonq/scripts/build/copyFiles.ts
+++ packages/amazonq/scripts/build/copyFiles.ts
@@ -32,6 +32,10 @@ const tasks: CopyTask[] = [
 
     { target: path.join('../core', 'resources'), destination: path.join('..', 'resources') },
     { target: path.join('../core', 'package.nls.json'), destination: path.join('..', 'package.nls.json') },
+    {
+        target: '../core/src/auth/sso/vue',
+        destination: 'src/auth/sso/vue',
+    },
 
     // Vue
     {
