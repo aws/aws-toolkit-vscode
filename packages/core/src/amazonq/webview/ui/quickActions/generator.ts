35822e74df2149d65badac504b3cfe4ec1c942ba
Dogus Atasoy <dogusata@amazon.com>
2024-04-24 13:40:03 +0200

    updated quick action styles and grouping

--- packages/core/src/amazonq/webview/ui/quickActions/generator.ts
+++ packages/core/src/amazonq/webview/ui/quickActions/generator.ts
@@ -22,33 +22,28 @@ export class QuickActionGenerator {
 
     public generateForTab(tabType: TabType): QuickActionCommandGroup[] {
         const quickActionCommands = [
-            ...(this.isFeatureDevEnabled
-                ? [
-                      {
-                          groupName: 'Application Development',
-                          commands: [
+            {
+                commands: [
+                    ...(this.isFeatureDevEnabled
+                        ? [
                               {
                                   command: '/dev',
                                   placeholder: 'Briefly describe a task or issue',
                                   description:
                                       'Use all project files as context for code suggestions (increases latency).',
                               },
-                          ],
-                      },
-                  ]
-                : []),
-            ...(this.isGumbyEnabled
-                ? [
-                      {
-                          commands: [
+                          ]
+                        : []),
+                    ...(this.isGumbyEnabled
+                        ? [
                               {
                                   command: '/transform',
                                   description: 'Transform your Java 8 or 11 Maven project to Java 17',
                               },
-                          ],
-                      },
-                  ]
-                : []),
+                          ]
+                        : []),
+                ],
+            },
             {
                 commands: [
                     {
@@ -90,7 +85,6 @@ export class QuickActionGenerator {
 
         return quickActionCommands.map(commandGroup => {
             return {
-                groupName: commandGroup.groupName,
                 commands: commandGroup.commands.map((commandItem: QuickActionCommand) => {
                     const commandNotAvailable = commandUnavailability[tabType].unavailableItems.includes(
                         commandItem.command
