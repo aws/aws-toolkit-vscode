4b001a98177301bbf1cac2c8e96c2161d0dfb68b
Lei Gao <leigaol@amazon.com>
2024-04-26 12:41:19 -0700

    remove timer

--- packages/core/src/shared/telemetry/util.ts
+++ packages/core/src/shared/telemetry/util.ts
@@ -16,8 +16,9 @@ import { Result } from './telemetry.gen'
 import { MetricDatum } from './clienttelemetry'
 import { isValidationExemptMetric } from './exemptMetrics'
 import { isCloud9, isSageMaker } from '../../shared/extensionUtilities'
-import { VSCODE_EXTENSION_ID } from '../utilities'
+import { isExtensionInstalled, VSCODE_EXTENSION_ID } from '../utilities'
 import { randomUUID } from '../../common/crypto'
+import { activateExtension } from '../utilities/vsCodeUtils'
 const legacySettingsTelemetryValueDisable = 'Disable'
 const legacySettingsTelemetryValueEnable = 'Enable'
 
@@ -179,10 +180,13 @@ export async function setupTelemetryId(extensionContext: vscode.ExtensionContext
                     getLogger().debug(`telemetry: Store telemetry client id to env ${currentClientId}`)
                     process.env[telemetryClientIdEnvKey] = currentClientId
                     // notify amazon q to use this stored client id
-                    // if amazon q activates first.
-                    setTimeout(() => {
-                        vscode.commands.executeCommand('aws.amazonq.setupTelemetryId')
-                    }, 3000)
+                    // if amazon q activates first. Do not block on activate amazon q
+                    if (isExtensionInstalled(VSCODE_EXTENSION_ID.amazonq)) {
+                        void activateExtension(VSCODE_EXTENSION_ID.amazonq).then(async () => {
+                            getLogger().debug(`telemetry: notifying Amazon Q to adopt client id ${currentClientId}`)
+                            await vscode.commands.executeCommand('aws.amazonq.setupTelemetryId')
+                        })
+                    }
                 } else if (extensionContext.extension.id === VSCODE_EXTENSION_ID.amazonq) {
                     getLogger().debug(`telemetry: Set telemetry client id to ${storedClientId}`)
                     await globals.context.globalState.update(telemetryClientIdGlobalStatekey, storedClientId)
