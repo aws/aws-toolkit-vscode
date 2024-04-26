aab5abedaa6a960c08dbfd68e0a2b517b07cf1c9
Richard Li <742829+rli@users.noreply.github.com>
2024-04-25 17:23:20 -0700

    Add/update sandbox run configurations for plugin variants (#4378)

---
+++ sandbox-all/build.gradle.kts
@@ -0,0 +1,30 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import software.aws.toolkits.gradle.intellij.IdeFlavor
+import software.aws.toolkits.gradle.intellij.toolkitIntelliJ
+
+plugins {
+    id("toolkit-intellij-plugin")
+    id("org.jetbrains.intellij")
+}
+
+toolkitIntelliJ.apply {
+    val runIdeVariant = providers.gradleProperty("runIdeVariant")
+    ideFlavor.set(IdeFlavor.values().firstOrNull { it.name == runIdeVariant.orNull } ?: IdeFlavor.IC)
+}
+
+intellij {
+    version.set(toolkitIntelliJ.version())
+    localPath.set(toolkitIntelliJ.localPath())
+    plugins.set(
+        listOf(
+            project(":plugin-core"),
+            project(":plugin-amazonq"),
+            project(":plugin-toolkit:intellij-standalone"),
+        )
+    )
+
+    updateSinceUntilBuild.set(false)
+    instrumentCode.set(false)
+}
