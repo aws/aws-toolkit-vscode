8aa9199cb2f2cfdd6a2a2173a396de564b7ba947
laileni <laileni@amazon.com>
2024-05-09 12:18:40 -0700

    Try running the build again

--- packages/core/src/codewhisperer/models/errors.ts
+++ packages/core/src/codewhisperer/models/errors.ts
@@ -2,7 +2,6 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import { ToolkitError } from '../../shared/errors'
 import {
     DefaultCodeScanErrorMessage,
