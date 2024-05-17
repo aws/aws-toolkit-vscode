fe0f6860bbb5303941d727756505dd3b86c0a412
Maxim Hayes <149123719+hayemaxi@users.noreply.github.com>
2024-05-17 15:13:05 -0400

    auth: emit user scopes in auth_userState (#5019)

--- packages/core/src/extensionShared.ts
+++ packages/core/src/extensionShared.ts
@@ -46,6 +46,7 @@ import { ExtStartUpSources } from './shared/telemetry/util'
 import { ExtensionUse, getAuthFormIdsFromConnection } from './auth/utils'
 import { Auth } from './auth'
 import { AuthFormId } from './auth/ui/vue/authForms/types'
+import { isSsoConnection } from './auth/connection'
 
 // In web mode everything must be in a single file, so things like the endpoints file will not be available.
 // The following imports the endpoints file, which causes webpack to bundle it in the final output file
@@ -256,6 +257,7 @@ export async function emitUserState() {
 
         let authStatus: AuthStatus = 'notConnected'
         const enabledConnections: Set<AuthFormId> = new Set()
+        const enabledScopes: Set<string> = new Set()
         if (Auth.instance.hasConnections) {
             authStatus = 'expired'
             ;(await Auth.instance.listConnections()).forEach(conn => {
@@ -265,11 +267,15 @@ export async function emitUserState() {
                 }
 
                 getAuthFormIdsFromConnection(conn).forEach(id => enabledConnections.add(id))
+                if (isSsoConnection(conn)) {
+                    conn.scopes?.forEach(s => enabledScopes.add(s))
+                }
             })
         }
         telemetry.record({
             authStatus,
             authEnabledConnections: [...enabledConnections].join(','),
+            authScopes: [...enabledScopes].join(','),
         })
     })
 }
