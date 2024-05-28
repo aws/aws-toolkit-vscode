67d4ec33440dfa649359d78135f2c9b9bd2aef99
Maxim Hayes <hayemaxi@amazon.com>
2024-05-28 17:58:11 -0400

    lint: child_process
    - Add eslint rule to prevent insecure usage of child_process functions
      - 3P package, security-node/detect-child-process
    - Add custom lint rule to prevent usage of "string forms" of child_process functions, e.g. `exec`, `execSync`. Use `execFile`, `execFileSync` instead.

--- plugins/eslint-plugin-aws-toolkits/index.ts
+++ plugins/eslint-plugin-aws-toolkits/index.ts
@@ -6,11 +6,13 @@
 import NoOnlyInTests from './lib/rules/no-only-in-tests'
 import NoAwaitOnVscodeMsg from './lib/rules/no-await-on-vscode-msg'
 import NoIncorrectOnceUsage from './lib/rules/no-incorrect-once-usage'
+import NoStringExecForChildProcess from './lib/rules/no-string-exec-for-child-process'
 
 const rules = {
     'no-only-in-tests': NoOnlyInTests,
     'no-await-on-vscode-msg': NoAwaitOnVscodeMsg,
     'no-incorrect-once-usage': NoIncorrectOnceUsage,
+    'no-string-exec-for-child-process': NoStringExecForChildProcess,
 }
 
 export { rules }
