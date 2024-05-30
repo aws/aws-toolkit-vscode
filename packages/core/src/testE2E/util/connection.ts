c5386c170b7a1c8f62e89d9eceeb2deccf906b58
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2024-05-30 13:50:15 -0400

    tests: cleanup amazon q integ test utils (#5079)
    Problem:
    - Almost identical code between amazon q utils and codewhisperer utils
    
    Solution:
    - unify them

--- packages/core/src/testE2E/util/connection.ts
+++ packages/core/src/testE2E/util/connection.ts
@@ -7,10 +7,10 @@ import { isValidAmazonQConnection } from '../../codewhisperer/util/authUtil'
 import { Auth } from '../../auth/auth'
 
 /*
-In order to run codewhisperer integration tests user must:
+In order to run codewhisperer or gumby integration tests user must:
    
     1) run using VSC launch config.
-    2) have a valid codewhisperer connection.
+    2) have a valid codewhisperer/amazon q connection.
 
 Test cases will skip if the above criteria are not met.
 If user has an expired connection they must reauthenticate prior to running tests.
@@ -20,7 +20,6 @@ async function getValidConnection() {
     return (await Auth.instance.listConnections()).find(isValidAmazonQConnection)
 }
 
-//Returns true if a valid connection is found and set, false if not
 export async function setValidConnection() {
     const conn = await getValidConnection()
     let validConnection: boolean
@@ -30,12 +29,12 @@ export async function setValidConnection() {
         await Auth.instance.useConnection(conn)
     } else {
         validConnection = false
-        console.log('No valid auth connection, will skip codewhisperer integration test cases')
+        console.log(`No valid auth connection, will skip Amazon Q integration test cases`)
     }
     return validConnection
 }
 
-export function skiptTestIfNoValidConn(validConnection: boolean, ctx: Mocha.Context) {
+export function skipTestIfNoValidConn(validConnection: boolean, ctx: Mocha.Context) {
     if (!validConnection && ctx.currentTest) {
         ctx.currentTest.title += ` (skipped - no valid connection)`
         ctx.currentTest.skip()
