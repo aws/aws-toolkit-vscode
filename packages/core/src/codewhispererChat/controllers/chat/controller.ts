7ccf87e1dd68d80fcfe4dc41cd5bd8afba1654e1
Lei Gao <leigaol@amazon.com>
2024-06-03 11:00:41 -0700

    add space and \n

--- packages/core/src/codewhispererChat/controllers/chat/controller.ts
+++ packages/core/src/codewhispererChat/controllers/chat/controller.ts
@@ -564,7 +564,7 @@ export class ChatController {
                     const c = await Search.instance.query(triggerPayload.message)
                     if (c) {
                         getLogger().info(`Relevant code ${c.content}`)
-                        triggerPayload.message += `Here are some relevant code ${c?.content} in file ${c.fileName}. Use it only if my questions is relevant to this code. `
+                        triggerPayload.message += ` \nHere are some relevant code ${c?.content} in file ${c.fileName}. Use it only if my questions is relevant to this code. `
                         triggerPayload.hasProjectLevelContext = true
                     } else {
                         getLogger().info(`No Relevant code`)
