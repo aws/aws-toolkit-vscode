eb08f1cd51f510d328b35e0de7a24fe654eab9cf
Joshua Pinkney <jpink@amazon.com>
2024-06-05 13:28:05 -0400

    test patching auth

--- packages/core/src/testInteg/globalSetup.test.ts
+++ packages/core/src/testInteg/globalSetup.test.ts
@@ -11,11 +11,15 @@ import { getLogger } from '../shared/logger'
 import { WinstonToolkitLogger } from '../shared/logger/winstonToolkitLogger'
 import { mapTestErrors, normalizeError, patchObject, setRunnableTimeout } from '../test/setupUtil'
 import { getTestWindow, resetTestWindow } from '../test/shared/vscode/window'
+import * as sinon from 'sinon'
+import * as tokenProvider from '../auth/sso/ssoAccessTokenProvider'
+import { DeviceFlowAuthorization } from '../auth/sso/ssoAccessTokenProvider'
 
 // ASSUMPTION: Tests are not run concurrently
 
 let windowPatch: vscode.Disposable
 const maxTestDuration = 300_000
+const globalSandbox = sinon.createSandbox()
 
 export async function mochaGlobalSetup(extensionId: string) {
     return async function (this: Mocha.Runner) {
@@ -30,6 +34,9 @@ export async function mochaGlobalSetup(extensionId: string) {
 
         // Set up a listener for proxying login requests
         patchWindow()
+
+        // always use device code authorization
+        patchAuth()
 
         // Needed for getLogger().
         await vscode.extensions.getExtension(extensionId)?.activate()
@@ -51,10 +58,25 @@ export const mochaHooks = {
     afterEach(this: Mocha.Context) {
         patchWindow()
     },
+    after(this: Mocha.Context) {
+        globalSandbox.restore()
+    },
 }
 
 function patchWindow() {
     windowPatch?.dispose()
     resetTestWindow()
     windowPatch = patchObject(vscode, 'window', getTestWindow())
+}
+
+/**
+ * We have to fall back to device flow authorization until:
+ * 1. We can find a way to automatically press the "trust external url" popup
+ * 2. We support the authorization grant flow in the auth lambda
+ * otherwise we get: "Error: DialogService: refused to show dialog in tests. Contents: Do you want Code to open the external website?"
+ */
+function patchAuth() {
+    globalSandbox.stub(tokenProvider.SsoAccessTokenProvider, 'create').callsFake((profile, cache, oidc) => {
+        return new DeviceFlowAuthorization(profile, cache, oidc)
+    })
 }
