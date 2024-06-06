75d0d159c8597559763cb97a61a9e8e36768336c
Bijin Abraham Idicula <104742052+bijinai@users.noreply.github.com>
2024-06-07 07:17:47 +1000

    feat(ThreatComposer): edit, view Threat Models #5109
    Problem
    Threat models, represented as *.tc.json files, are rendered using the default JSON editor
    
    Solution
    Build a custom editor in VSCode to render *.tc.json files as a Threat Model, similar to how it would be displayed in a browser

---
+++ packages/core/src/threatComposer/messageHandlers/addFileWatchMessageHandler.ts
@@ -0,0 +1,69 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Command, FileChangedMessage, MessageType, WebviewContext } from '../types'
+import vscode from 'vscode'
+import { fsCommon } from '../../srcShared/fs'
+
+/**
+ * Function to add a watcher on the file that was opened. The watcher will notify Threat Composer
+ * view when a change occurs to the file externally.
+ * If the change occurs due to
+ *    a user save action, file change is ignored.
+ *    an auto save that is persisted as AutoSave is turned on in VSCode settings, update file state.
+ *    an external file change,
+ *       if a local, unsaved change exist in Threat Composer, the file change is ignored.
+ *       if no unsaved local changes exist, the view is notified of the external file change.
+ * @param context: The Webview Context that contain the details of the file and the webview
+ */
+export function addFileWatchMessageHandler(context: WebviewContext) {
+    const filePath = context.defaultTemplatePath
+    const fileName = context.defaultTemplateName
+
+    context.disposables.push(
+        vscode.workspace.onDidChangeTextDocument(async e => {
+            const fileContents = await fsCommon.readFileAsString(filePath)
+
+            if (fileContents !== context.fileStates[filePath].fileContents) {
+                if (fileContents === context.autoSaveFileState[filePath].fileContents) {
+                    context.fileStates[filePath] = { fileContents: fileContents }
+                } else if (
+                    context.fileStates[filePath].fileContents === context.autoSaveFileState[filePath].fileContents
+                ) {
+                    console.log('DocumentChanged')
+                    await broadcastFileChange(fileName, filePath, fileContents, context.panel)
+                    context.fileStates[filePath] = { fileContents: fileContents }
+                    context.autoSaveFileState[filePath] = { fileContents: fileContents }
+                } else {
+                    console.error('Document Changed externally before local changes are saved')
+                }
+            }
+        })
+    )
+}
+
+/**
+ * Helper Function to broadcast the file change to the Threat Composer view
+ * @param fileName: Name of the file that was changed
+ * @param filePath: The path to the file
+ * @param fileContents: The updated file contents
+ * @param panel: the panel which contains the webview to be notified.
+ */
+export async function broadcastFileChange(
+    fileName: string,
+    filePath: string,
+    fileContents: string,
+    panel: vscode.WebviewPanel
+) {
+    const fileChangedMessage: FileChangedMessage = {
+        messageType: MessageType.BROADCAST,
+        command: Command.FILE_CHANGED,
+        fileName: fileName,
+        fileContents: fileContents,
+        filePath: filePath,
+    }
+
+    await panel.webview.postMessage(fileChangedMessage)
+}
---
+++ packages/core/src/threatComposer/types.ts
@@ -0,0 +1,144 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import * as vscode from 'vscode'
+
+export type WebviewContext = {
+    panel: vscode.WebviewPanel
+    textDocument: vscode.TextDocument
+    disposables: vscode.Disposable[]
+    workSpacePath: string
+    defaultTemplatePath: string
+    defaultTemplateName: string
+    fileStates: Record<string, FileWatchInfo>
+    autoSaveFileState: Record<string, FileWatchInfo>
+    loaderNotification: undefined | LoaderNotification
+    fileId: string
+}
+
+export type LoaderNotification = {
+    progress: vscode.Progress<{
+        message?: string | undefined
+        increment?: number | undefined
+    }>
+    cancellationToken: vscode.CancellationToken
+    promiseResolve: any
+}
+
+export type FileWatchInfo = {
+    fileContents: string
+}
+
+export interface Message {
+    command: Command
+    messageType: MessageType
+}
+
+export enum Command {
+    AUTO_SAVE_FILE = 'AUTO_SAVE_FILE',
+    SAVE_FILE = 'SAVE_FILE',
+    LOG = 'LOG',
+    EMIT_TELEMETRY = 'EMIT_TELEMETRY',
+    OPEN_FEEDBACK = 'OPEN_FEEDBACK',
+    INIT = 'INIT',
+    RELOAD = 'RELOAD',
+    LOAD_STAGE = 'LOAD_STAGE',
+
+    FILE_CHANGED = 'FILE_CHANGED',
+    THEME_CHANGED = 'THEME_CHANGED',
+    OVERWRITE_FILE = 'OVERWRITE_FILE',
+}
+
+export enum MessageType {
+    REQUEST = 'REQUEST',
+    RESPONSE = 'RESPONSE',
+    BROADCAST = 'BROADCAST',
+}
+
+enum TelemetryType {
+    GENERATE_CLICKED = 'GENERATE_CLICKED',
+    REGENERATE_CLICKED = 'REGENERATE_CLICKED',
+    GENERATE_ACCEPTED = 'GENERATE_ACCEPTED',
+    GENERATE_REJECTED = 'GENERATE_REJECTED',
+    INVALID_GENERATION = 'INVALID_GENERATION',
+    POST_PROCESS = 'POST_PROCESS',
+    CUSTOMER_READY = 'CUSTOMER_READY',
+    FPS = 'FPS',
+    ADD_RESOURCE = 'ADD_RESOURCE',
+    ADD_CONNECTION = 'ADD_CONNECTION',
+    OPEN_WFS = 'OPEN_WFS',
+    CLOSE_WFS = 'CLOSE_WFS',
+}
+
+export enum SaveCompleteSubType {
+    SAVED = 'SAVED',
+    SAVE_SKIPPED_SAME_CONTENT = 'SAVE_SKIPPED_SAME_CONTENT',
+    SAVE_SKIPPED_SAME_JSON = 'SAVE_SKIPPED_SAME_JSON',
+    SAVE_FAILED = 'SAVE_FAILED',
+}
+
+export interface SaveFileResponseMessage extends Message {
+    filePath: string
+    isSuccess: boolean
+    saveCompleteSubType: SaveCompleteSubType
+    failureReason?: string
+}
+
+export interface FileChangedMessage extends Message {
+    fileName: string
+    fileContents: string
+    filePath: string
+}
+
+export interface ThemeChangedMessage extends Message {
+    newTheme: string
+}
+
+export interface SaveFileRequestMessage extends Message {
+    fileContents: string
+}
+
+export interface AddFileWatchRequestMessage extends Message {
+    eventId: string
+    fileName: string
+}
+
+export interface NotifyUserRequestMessage extends Message {
+    eventId?: string
+    notification: string
+    notificationType: 'INFO' | 'WARNING' | 'ERROR'
+}
+
+export interface LogMessage extends Message {
+    eventId?: string
+    logMessage: string
+    logType: 'INFO' | 'WARNING' | 'ERROR'
+    showNotification: boolean
+    notificationType: 'INVALID_JSON' | undefined
+}
+
+export interface EmitTelemetryMessage extends Message {
+    eventId?: string
+    eventType: TelemetryType
+    metadata?: string
+}
+
+export interface LoadStageMessage extends Message {
+    loadStage: 'API_LOADED' | 'RENDER_COMPLETE'
+}
+
+export interface ReferenceDetails {
+    title: string | undefined
+    url: string | undefined
+    snippet?: string
+}
+
+export interface GenerateResourceResponseMessage extends Message {
+    chatResponse: string
+    references: ReferenceDetails[]
+    metadata: object
+    isSuccess: boolean
+    errorMessage?: string
+    traceId: string
+}
