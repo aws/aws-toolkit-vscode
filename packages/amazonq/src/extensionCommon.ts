b851ee7c467abe92fcf46eb833bc4e573eb9745e
eugene yevhen andruszczenko <andruseu@amazon.com>
2024-06-10 15:21:43 -0400

    feature: reduce complexity of extensionCommon activation

--- packages/amazonq/src/extensionCommon.ts
+++ packages/amazonq/src/extensionCommon.ts
@@ -42,105 +42,117 @@ export const amazonQContextPrefix = 'amazonq'
 export async function activateAmazonQCommon(context: vscode.ExtensionContext, isWeb: boolean) {
     initialize(context, isWeb)
     await initializeComputeRegion()
+    globals.contextPrefix = 'amazonq.'
 
-    globals.contextPrefix = 'amazonq.' //todo: disconnect from above line
-
-    // Avoid activation if older toolkit is installed
-    // Amazon Q is only compatible with AWS Toolkit >= 3.0.0
-    // Or AWS Toolkit with a development version. Example: 2.19.0-3413gv
-    const toolkit = vscode.extensions.getExtension(VSCODE_EXTENSION_ID.awstoolkit)
-    if (toolkit) {
-        const toolkitVersion = semver.coerce(toolkit.packageJSON.version)
-        // XXX: can't use `SemVer.prerelease` because Toolkit "prerelease" (git sha) is not a valid
-        // semver prerelease: it may start with a number.
-        const isDevVersion = toolkit.packageJSON.version.toString().includes('-')
-        if (toolkitVersion && toolkitVersion.major < 3 && !isDevVersion) {
-            await vscode.commands
-                .executeCommand('workbench.extensions.installExtension', VSCODE_EXTENSION_ID.awstoolkit)
-                .then(
-                    () =>
-                        vscode.window
-                            .showInformationMessage(
-                                `The Amazon Q extension is incompatible with AWS Toolkit ${
-                                    toolkitVersion as any
-                                } and older. Your AWS Toolkit was updated to version 3.0 or later.`,
-                                'Reload Now'
-                            )
-                            .then(async resp => {
-                                if (resp === 'Reload Now') {
-                                    await vscode.commands.executeCommand('workbench.action.reloadWindow')
-                                }
-                            }),
-                    reason => {
-                        getLogger().error('workbench.extensions.installExtension failed: %O', reason)
-                    }
-                )
+    try {
+        if (await isIncompatibleToolkitInstalled()) {
             return
         }
+
+        await setupGlobals(context)
+        await setupLogging(context)
+        await activateTelemetry(context, globals.awsContext, Settings.instance, 'Amazon Q For VS Code')
+        await initializeAuth(globals.loginManager)
+        await activateCodeWhisperer({ extensionContext: context } as ExtContext)
+        registerGenericCommands(context, amazonQContextPrefix)
+        registerCommands(context)
+        await hideAmazonQTree()
+        await reloadWebviews()
+        await enableAutoSuggestions()
+        await handleFirstUse()
+        await recordTelemetry()
+    } catch (error) {
+        getLogger().error('Error during activation: %O', error)
+    }
+}
+
+async function isIncompatibleToolkitInstalled() {
+    const toolkit = vscode.extensions.getExtension(VSCODE_EXTENSION_ID.awstoolkit)
+    if (!toolkit) {
+        return false
     }
 
+    const toolkitVersion = semver.coerce(toolkit.packageJSON.version)
+    const isDevVersion = toolkit.packageJSON.version.toString().includes('-')
+
+    if (toolkitVersion && toolkitVersion.major < 3 && !isDevVersion) {
+        await vscode.commands
+            .executeCommand('workbench.extensions.installExtension', VSCODE_EXTENSION_ID.awstoolkit)
+            .then(
+                () => promptReloadWindow(toolkitVersion),
+                reason => getLogger().error('workbench.extensions.installExtension failed: %O', reason)
+            )
+        return true
+    }
+    return false
+}
+
+async function promptReloadWindow(toolkitVersion: any) {
+    void vscode.window
+        .showInformationMessage(
+            `The Amazon Q extension is incompatible with AWS Toolkit ${toolkitVersion} and older. Your AWS Toolkit was updated to version 3.0 or later.`,
+            'Reload Now'
+        )
+        .then(async resp => {
+            if (resp === 'Reload Now') {
+                await vscode.commands.executeCommand('workbench.action.reloadWindow')
+            }
+        })
+}
+
+async function setupGlobals(context: vscode.ExtensionContext) {
     globals.machineId = await getMachineId()
     globals.awsContext = new DefaultAwsContext()
     globals.sdkClientBuilder = new DefaultAWSClientBuilder(globals.awsContext)
     globals.manifestPaths.endpoints = context.asAbsolutePath(join('resources', 'endpoints.json'))
     globals.regionProvider = RegionProvider.fromEndpointsProvider(makeEndpointsProvider())
+    globals.loginManager = new LoginManager(globals.awsContext, new CredentialsStore())
+}
 
+async function setupLogging(context: vscode.ExtensionContext) {
     const qOutputChannel = vscode.window.createOutputChannel('Amazon Q', { log: true })
     const qLogChannel = vscode.window.createOutputChannel('Amazon Q Logs', { log: true })
     await activateLogger(context, amazonQContextPrefix, qOutputChannel, qLogChannel)
     globals.outputChannel = qOutputChannel
     globals.logOutputChannel = qLogChannel
-    globals.loginManager = new LoginManager(globals.awsContext, new CredentialsStore())
+}
 
-    await activateTelemetry(context, globals.awsContext, Settings.instance, 'Amazon Q For VS Code')
-
-    await initializeAuth(globals.loginManager)
-
-    const extContext = {
-        extensionContext: context,
-    }
-    await activateCodeWhisperer(extContext as ExtContext)
-
-    // Generic extension commands
-    registerGenericCommands(context, amazonQContextPrefix)
-
-    // Amazon Q specific commands
-    registerCommands(context)
-
-    // Hide the Amazon Q tree in toolkit explorer
+async function hideAmazonQTree() {
     await vscode.commands.executeCommand('setContext', amazonQDismissedKey, true)
+}
 
-    // reload webviews
+async function reloadWebviews() {
     await vscode.commands.executeCommand('workbench.action.webview.reloadWebviewAction')
+}
 
-    // enable auto suggestions on activation
+async function enableAutoSuggestions() {
     await CodeSuggestionsState.instance.setSuggestionsEnabled(true)
+}
 
+async function handleFirstUse() {
     if (AuthUtils.ExtensionUse.instance.isFirstUse()) {
         CommonAuthWebview.authSource = ExtStartUpSources.firstStartUp
         await vscode.commands.executeCommand('workbench.view.extension.amazonq')
     }
+}
 
+async function recordTelemetry() {
     await telemetry.auth_userState.run(async () => {
         telemetry.record({ passive: true })
 
         const firstUse = AuthUtils.ExtensionUse.instance.isFirstUse()
         const wasUpdated = AuthUtils.ExtensionUse.instance.wasUpdated()
 
-        if (firstUse) {
-            telemetry.record({ source: ExtStartUpSources.firstStartUp })
-        } else if (wasUpdated) {
-            telemetry.record({ source: ExtStartUpSources.update })
-        } else {
-            telemetry.record({ source: ExtStartUpSources.reload })
-        }
+        telemetry.record({
+            source: firstUse
+                ? ExtStartUpSources.firstStartUp
+                : wasUpdated
+                ? ExtStartUpSources.update
+                : ExtStartUpSources.reload,
+        })
 
         const { authStatus, authEnabledConnections, authScopes } = await getAuthStatus()
-        telemetry.record({
-            authStatus,
-            authEnabledConnections,
-            authScopes,
-        })
+        telemetry.record({ authStatus, authEnabledConnections, authScopes })
     })
 }
 
