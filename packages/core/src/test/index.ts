93b24653fa13d7d2e581363eb777a0351c407b0c
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2024-06-13 12:07:08 -0400

    techdebt: remove almost all of the tryRegister commands      (#5128)
    * techdebt: remove almost all of the tryRegister commands
    
    Problem:
    - tryRegister is a hack to allow settings that get activated in the amazonq extension to run in core
    
    Solution:
    - setup the amazonq project to run unit tests
    - Add "test" script to amazonq package.json
    - Move amazonq, feature dev, gumby, codewhisperer, codewhispererChat unit tests from core to amazonq. This requires us to export any required dependencies from their respective source folders
    - remove tryRegister in as many places as possible. See "Other notes" for the reason why we couldn't get rid of them all
    
    Other notes:
    - A few test files need to override vscode api in order to function correctly. Those test files have been left in core because the overriding wasn't working from a seperate package. tryRegister remains for a few of those files that are left behind and still need it
    - A fallback for timer was needed when moving tests from core -> amazonq. It's unclear why these changes forced that fallback

--- packages/core/src/test/index.ts
+++ packages/core/src/test/index.ts
@@ -11,3 +11,23 @@ export function run(): Promise<void> {
 
 export { registerAuthHook, using } from './setupUtil'
 export { runTests } from './testRunner'
+export { MockDocument } from './fake/fakeDocument'
+export { FakeMemento, FakeExtensionContext } from './fakeExtensionContext'
+export { Stub, stub } from './utilities/stubber'
+export { getTestWindow } from './shared/vscode/window'
+export { SeverityLevel } from './shared/vscode/message'
+export { getTestLogger } from './globalSetup.test'
+export { testCommand } from './shared/vscode/testUtils'
+export { FakeAwsContext } from './utilities/fakeAwsContext'
+export * from './codewhisperer/testUtil'
+export * from './credentials/testUtil'
+export * from './testUtil'
+export * from './amazonqFeatureDev/utils'
+
+import request from '../common/request'
+import { stub } from 'sinon'
+
+// Returns a stubbed fetch for other tests.
+export function getFetchStubWithResponse(response: Partial<Response>) {
+    return stub(request, 'fetch').returns({ response: new Promise((res, _) => res(response)) } as any)
+}
