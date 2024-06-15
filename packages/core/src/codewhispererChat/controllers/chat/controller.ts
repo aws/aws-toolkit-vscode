2e127996afb97f4b1bc4b8375d0f1f0a19d748a5
Lei Gao <leigaol@amazon.com>
2024-06-15 14:14:30 -0700

    minimize changes

--- packages/core/src/codewhispererChat/controllers/chat/controller.ts
+++ packages/core/src/codewhispererChat/controllers/chat/controller.ts
@@ -2,6 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import { Event as VSCodeEvent, Uri } from 'vscode'
 import { EditorContextExtractor } from '../../editor/context/extractor'
 import { ChatSessionStorage } from '../../storages/chatSession'
