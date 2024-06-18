f3c7b4c43685710caf2ded9de6e1f4173b07b211
Mark Cooke <mark.cooke@improving.com>
2024-06-18 15:13:10 -0500

    Add tests for DefaultDocumentDBClient

---
+++ packages/core/src/test/shared/clients/defaultDocdbClient.test.ts
@@ -0,0 +1,49 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+import sinon, { SinonStub } from 'sinon'
+import { ToolkitError } from '../../../shared/errors'
+import { DefaultDocumentDBClient, DocumentDBClient } from '../../../shared/clients/docdbClient'
+
+describe('DefaultDocumentDBClient', function () {
+    const region = 'us-west-2'
+
+    let sdkStub: SinonStub
+
+    beforeEach(() => {
+        sdkStub = sinon.stub().resolves({
+            send: sinon.stub().resolves({
+                DBClusters: [],
+            }),
+        })
+    })
+
+    function createClient({ regionCode = region }: { regionCode?: string } = {}): DocumentDBClient {
+        const client = new DefaultDocumentDBClient(regionCode)
+        client.getClient = sdkStub
+        return client
+    }
+
+    describe('listClusters', function () {
+        it('gets a list of clusters', async function () {
+            const client = createClient()
+            const result = await client.listClusters()
+
+            assert.ok(result)
+            assert.equal(0, result.length)
+            assert(sdkStub.calledOnce)
+        })
+
+        it('throws an Error on failure', async function () {
+            const client = createClient()
+            client.getClient = sinon.stub().resolves({
+                send: sinon.stub().throws(),
+            })
+
+            await assert.rejects(async () => await client.listClusters())
+        })
+    })
+})
