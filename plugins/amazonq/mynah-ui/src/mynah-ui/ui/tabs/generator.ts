837c8ee4a10243c356ae53cc5761760bf9ada67d
chengoramazon <143654391+chengoramazon@users.noreply.github.com>
2024-06-20 14:35:19 -0400

    fix(featureDev): update welcome message (#4603)
    Problem:
    - Current: Welcome to feature development. ...
    
    Solution:
    - Update: Hi, I'm the Amazon Q Developer Agent for software development. ...

--- plugins/amazonq/mynah-ui/src/mynah-ui/ui/tabs/generator.ts
+++ plugins/amazonq/mynah-ui/src/mynah-ui/ui/tabs/generator.ts
@@ -45,7 +45,7 @@ export class TabDataGenerator {
         ],
         [
             'featuredev',
-            `Welcome to feature development.
+            `Hi, I'm the Amazon Q Developer Agent for software development.
 
 I can generate code to implement new functionality across your workspace. We'll start by discussing an implementation plan, and then we can review and regenerate code based on your feedback. 
 
