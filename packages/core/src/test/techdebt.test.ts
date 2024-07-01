93396ccf07732ab5fc1e60822d6097fdcb98513d
Justin M. Keyes <jmkeyes@amazon.com>
2024-07-01 13:28:22 -0700

    tests: snooze tech debt reminder

--- packages/core/src/test/techdebt.test.ts
+++ packages/core/src/test/techdebt.test.ts
@@ -41,7 +41,7 @@ describe('tech debt', function () {
     })
 
     it('remove missing Amazon Q scopes edge case handling', async function () {
-        fixByDate('2024-06-30', 'Remove the edge case code from the commit that this test is a part of.')
+        fixByDate('2024-07-30', 'Remove the edge case code from the commit that this test is a part of.')
     })
 
     it('remove separate sessions login edge cases', async function () {
