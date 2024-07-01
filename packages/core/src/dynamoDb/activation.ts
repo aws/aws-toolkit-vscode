71b5372503a956edf2ae874c000be28d236d785f
Lokesh Dogga <lodogga@amazon.com>
2024-07-01 15:21:19 -0700

    Added dynamodb option in command palette

--- packages/core/src/dynamoDb/activation.ts
+++ packages/core/src/dynamoDb/activation.ts
@@ -4,12 +4,34 @@
  */
 
 import * as vscode from 'vscode'
+import { Commands } from '../shared/vscode/commands2'
+import { searchDynamoDbTables } from './commands/searchDynamoDbTables'
+import { DynamoDbTableNode } from './explorer/dynamoDbTableNode'
+import { DynamoDbInstanceNode } from './explorer/dynamoDbInstanceNode'
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     context.subscriptions.push(
         vscode.commands.registerCommand('aws.dynamoDb.openRemoteConnection', async () => {
             await openRemoteConnection()
         })
+    )
+
+    context.subscriptions.push(
+        Commands.register(
+            'aws.dynamoDb.searchDynamoDbTables',
+            async (node: DynamoDbTableNode | DynamoDbInstanceNode) => {
+                const dynamoDbtableInfo =
+                    node instanceof DynamoDbTableNode
+                        ? { regionName: node.regionCode, groupName: node.regionCode! }
+                        : undefined
+                const source = node
+                    ? dynamoDbtableInfo
+                        ? 'ExplorerDynamoDbTableNode'
+                        : 'ExplorerServiceNode'
+                    : 'Command'
+                await searchDynamoDbTables(source, dynamoDbtableInfo)
+            }
+        )
     )
 }
 
