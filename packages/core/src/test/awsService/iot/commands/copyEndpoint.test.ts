31c07e69ab68657b901a077d5423864ffb55e101
Nikolas Komonen <nkomonen@amazon.com>
2024-07-01 19:18:16 -0400

    refactor: iot -> awsService/
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

---
+++ packages/core/src/test/awsService/iot/commands/copyEndpoint.test.ts
@@ -0,0 +1,46 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+import * as sinon from 'sinon'
+import * as vscode from 'vscode'
+import { copyEndpointCommand } from '../../../../awsService/iot/commands/copyEndpoint'
+import { IotNode } from '../../../../awsService/iot/explorer/iotNodes'
+import { IotClient } from '../../../../shared/clients/iotClient'
+import { getTestWindow } from '../../../shared/vscode/window'
+import { FakeClipboard } from '../../../shared/vscode/fakeEnv'
+
+describe('copyEndpointCommand', function () {
+    beforeEach(function () {
+        const fakeClipboard = new FakeClipboard()
+        sinon.stub(vscode.env, 'clipboard').value(fakeClipboard)
+    })
+
+    let iot: IotClient
+    let node: IotNode
+
+    beforeEach(function () {
+        iot = {} as any as IotClient
+        node = new IotNode(iot)
+    })
+
+    it('copies endpoint to clipboard', async function () {
+        const endpointStub = sinon.stub().resolves('endpoint')
+        iot.getEndpoint = endpointStub
+
+        await copyEndpointCommand(node)
+
+        assert.strictEqual(await vscode.env.clipboard.readText(), 'endpoint')
+    })
+
+    it('shows an error message when retrieval fails', async function () {
+        const endpointStub = sinon.stub().rejects(new Error('Expected failure'))
+        iot.getEndpoint = endpointStub
+
+        await copyEndpointCommand(node)
+
+        getTestWindow().getFirstMessage().assertError('Failed to retrieve endpoint')
+    })
+})
---
+++ packages/core/src/test/awsService/iot/explorer/iotPolicyNode.test.ts
@@ -0,0 +1,58 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+import { IotClient, IotPolicy } from '../../../../shared/clients/iotClient'
+import { Iot } from 'aws-sdk'
+import { AWSTreeNodeBase } from '../../../../shared/treeview/nodes/awsTreeNodeBase'
+import { asyncGenerator } from '../../../../shared/utilities/collectionUtils'
+import { IotPolicyWithVersionsNode } from '../../../../awsService/iot/explorer/iotPolicyNode'
+import { IotPolicyVersionNode } from '../../../../awsService/iot/explorer/iotPolicyVersionNode'
+import { IotPolicyFolderNode } from '../../../../awsService/iot/explorer/iotPolicyFolderNode'
+import { TestSettings } from '../../../utilities/testSettingsConfiguration'
+import sinon from 'sinon'
+
+describe('IotPolicyNode', function () {
+    let iot: IotClient
+    let config: TestSettings
+    const policyName = 'policy'
+    const expectedPolicy: IotPolicy = { name: policyName, arn: 'arn' }
+    const policyVersion: Iot.PolicyVersion = { versionId: 'V1', isDefaultVersion: true }
+
+    function assertPolicyVersionNode(
+        node: AWSTreeNodeBase,
+        expectedPolicy: IotPolicy,
+        expectedVersion: Iot.PolicyVersion
+    ): void {
+        assert.ok(node instanceof IotPolicyVersionNode, `Node ${node} should be a Policy Version Node`)
+        assert.deepStrictEqual((node as IotPolicyVersionNode).version, expectedVersion)
+        assert.deepStrictEqual((node as IotPolicyVersionNode).policy, expectedPolicy)
+    }
+
+    beforeEach(function () {
+        iot = {} as any as IotClient
+        config = new TestSettings()
+    })
+
+    describe('getChildren', function () {
+        it('gets children', async function () {
+            const versions = [{ versionId: 'V1', isDefaultVersion: true }]
+            const stub = sinon.stub().returns(asyncGenerator<Iot.PolicyVersion>(versions))
+            iot.listPolicyVersions = stub
+            const node = new IotPolicyWithVersionsNode(
+                expectedPolicy,
+                {} as IotPolicyFolderNode,
+                iot,
+                undefined,
+                config
+            )
+            const [policyVersionNode, ...otherNodes] = await node.getChildren()
+
+            assert(stub.calledOnceWithExactly({ policyName }))
+            assertPolicyVersionNode(policyVersionNode, expectedPolicy, policyVersion)
+            assert.strictEqual(otherNodes.length, 0)
+        })
+    })
+})
