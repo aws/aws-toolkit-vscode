198d2c9946adfd2a0f0c4ff567a8da2a985c72e6
Nikolas Komonen <118216176+nkomonen-amazon@users.noreply.github.com>
2024-07-03 10:41:44 -0400

    refactor: Move aws services in to `src/awsService/` (#5253)
    * refactor: start moving services to `awsService`
    
    Problem:
    
    We have multiple aws services that each have their own folder in the root.
    This adds more folder to have to search through in the top level
    
    Solution:
    
    Create an `awsService` folder and start the move of the existing folders
    in to them. This commit moves CDK for now, and in future commits we will move
    the rest.
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: move cloudWatchLogs in to awsService
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: ec2 in to awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: ecr -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: apiGateway -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: ecs -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: iot -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: redshift -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: s3 -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: apprunner -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    * refactor: accessanalyzer -> awsService/
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>
    
    ---------
    
    Signed-off-by: Nikolas Komonen <nkomonen@amazon.com>

---
+++ packages/core/src/awsService/apprunner/commands/createService.ts
@@ -0,0 +1,41 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { telemetry } from '../../../shared/telemetry/telemetry'
+import { AppRunnerServiceSource, Result } from '../../../shared/telemetry/telemetry'
+import { AppRunnerNode } from '../explorer/apprunnerNode'
+import { CreateAppRunnerServiceWizard } from '../wizards/apprunnerCreateServiceWizard'
+
+export async function createAppRunnerService(node: AppRunnerNode): Promise<void> {
+    let telemetryResult: Result = 'Failed'
+    let source: AppRunnerServiceSource | undefined = undefined
+
+    try {
+        const wizard = new CreateAppRunnerServiceWizard(node.regionCode)
+        const result = await wizard.run()
+        if (result === undefined) {
+            telemetryResult = 'Cancelled'
+            return
+        }
+
+        await node.createService(result)
+        source =
+            result.SourceConfiguration.CodeRepository !== undefined
+                ? 'repository'
+                : result.SourceConfiguration.ImageRepository !== undefined
+                  ? result.SourceConfiguration.ImageRepository.ImageRepositoryType === 'ECR_PUBLIC'
+                      ? 'ecrPublic'
+                      : 'ecr'
+                  : undefined
+        telemetryResult = 'Succeeded'
+    } finally {
+        telemetry.apprunner_createService.emit({
+            result: telemetryResult,
+            // If we cancel there is no source type (so this should be optional)
+            appRunnerServiceSource: source as any,
+            passive: false,
+        })
+    }
+}
---
+++ packages/core/src/test/awsService/cdk/explorer/constructNode.test.ts
@@ -0,0 +1,207 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+import * as path from 'path'
+import * as vscode from 'vscode'
+import * as treeUtils from '../treeTestUtils'
+import { ConstructNode } from '../../../../awsService/cdk/explorer/nodes/constructNode'
+import { PropertyNode } from '../../../../awsService/cdk/explorer/nodes/propertyNode'
+import { ConstructTreeEntity } from '../../../../awsService/cdk/explorer/tree/types'
+import { isStateMachine } from '../../../../awsService/cdk/explorer/nodes/constructNode'
+import { CdkAppLocation } from '../../../../awsService/cdk/explorer/cdkProject'
+import { getIcon } from '../../../../shared/icons'
+
+describe('ConstructNode', function () {
+    const label = 'MyConstruct'
+    const constructTreePath = 'Path/To/MyConstruct'
+    const cdkJsonPath = path.join('/', 'the', 'road', 'to', 'cdk.json')
+    const location: CdkAppLocation = {
+        cdkJsonUri: vscode.Uri.file(cdkJsonPath),
+        treeUri: vscode.Uri.file(path.join(cdkJsonPath, '..', 'cdk.out', 'tree.json')),
+    }
+
+    it('initializes label, tooltip, and icon', async function () {
+        const testNode = generateTestNode(label).getTreeItem()
+
+        assert.strictEqual(testNode.label, label)
+        assert.strictEqual(testNode.tooltip, constructTreePath)
+        assert.strictEqual(testNode.iconPath, getIcon('aws-cdk-logo'))
+        assert.strictEqual(testNode.collapsibleState, vscode.TreeItemCollapsibleState.Collapsed)
+    })
+
+    it('returns a uri with a fragment pointing to the resource', async () => {
+        const testNode = generateTestNode(label)
+        const resourceUri = testNode.resource.location
+
+        assert.strictEqual(resourceUri.path, location.treeUri.path)
+        assert.strictEqual(resourceUri.fragment, testNode.resource.construct.path)
+    })
+
+    it('initializes icon paths for CDK constructs', async function () {
+        const testNode = new ConstructNode(location, {
+            ...treeUtils.generateConstructTreeEntity('', constructTreePath),
+            attributes: treeUtils.generateAttributes(),
+        }).getTreeItem()
+
+        assert.strictEqual(testNode.iconPath, getIcon('aws-cloudformation-stack'))
+    })
+
+    it('returns no child nodes if construct does not have any', async function () {
+        const testNode = new ConstructNode(location, treeUtils.generateConstructTreeEntity(label, constructTreePath))
+
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 0, 'Unexpected child nodes')
+    })
+
+    it('child node has no collapsible state if it has no children or attributes', async function () {
+        const treeEntity: ConstructTreeEntity = {
+            id: label,
+            path: constructTreePath,
+            children: treeUtils.generateTreeChildResource(),
+        }
+        const testNode = new ConstructNode(location, treeEntity)
+
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 1)
+        assert.strictEqual((await childNodes[0].getTreeItem()).collapsibleState, vscode.TreeItemCollapsibleState.None)
+    })
+
+    it('child node is collapsed if construct has child with attributes', async function () {
+        const childWithAttributes = treeUtils.generateTreeChildResource()
+        childWithAttributes.Resource.attributes = treeUtils.generateAttributes()
+
+        const treeEntity: ConstructTreeEntity = {
+            id: label,
+            path: constructTreePath,
+            children: childWithAttributes,
+        }
+
+        const testNode = new ConstructNode(location, treeEntity)
+
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 1, 'Expected child node with attributes to be collapsed')
+        assert.strictEqual(
+            (await childNodes[0].getTreeItem()).collapsibleState,
+            vscode.TreeItemCollapsibleState.Collapsed
+        )
+    })
+
+    it('returns child node of PropertyNode when construct has props', async function () {
+        const treeEntity: ConstructTreeEntity = {
+            id: label,
+            path: constructTreePath,
+            attributes: treeUtils.generateAttributes(),
+        }
+
+        const testNode = new ConstructNode(location, treeEntity)
+
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 1)
+        assert.strictEqual(childNodes[0] instanceof PropertyNode, true, 'Expected child node to be a PropertyNode')
+    })
+
+    it('returns child nodes of PropertyNode and ConstructNode when construct has props and children', async function () {
+        const treeWithChildrenAndProps: ConstructTreeEntity = {
+            id: label,
+            path: constructTreePath,
+            children: { child: treeUtils.generateConstructTreeEntity(label, constructTreePath) },
+            attributes: treeUtils.generateAttributes(),
+        }
+
+        const testNode = new ConstructNode(location, treeWithChildrenAndProps)
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 2)
+        assert.strictEqual(childNodes[0] instanceof PropertyNode, true, 'Expected child node to be a PropertyNode')
+        assert.strictEqual(childNodes[1] instanceof ConstructNode, true, 'Expected child node to be a ConstructNode')
+    })
+
+    it('returns child node if a construct has grandchildren', async function () {
+        const testNode = new ConstructNode(
+            location,
+            treeUtils.generateConstructTreeEntity(label, constructTreePath, true)
+        )
+
+        const childNodes = testNode.getChildren()
+        assert.strictEqual(childNodes.length, 1)
+        assert.strictEqual(childNodes[0] instanceof ConstructNode, true, 'Expected child node to be a ConstructNode')
+    })
+
+    function generateTestNode(displayLabel: string): ConstructNode {
+        return new ConstructNode(location, treeUtils.generateConstructTreeEntity(displayLabel, constructTreePath))
+    }
+})
+
+describe('Check if ConstructNode is a state machine', function () {
+    it('returns true when tree node contains a node with id === "Resource" and type === "StateMachine"', async function () {
+        const construct: ConstructTreeEntity = {
+            id: 'StateMachine',
+            path: 'aws-stepfunctions-integ/StateMachine',
+            children: {
+                Resource: {
+                    id: 'Resource',
+                    path: 'aws-stepfunctions-integ/StateMachine/Resource',
+                    attributes: {
+                        'aws:cdk:cloudformation:type': 'AWS::StepFunctions::StateMachine',
+                    },
+                },
+            },
+        }
+
+        assert.ok(isStateMachine(construct))
+    })
+
+    it('returns true when tree node contains a node with id !== "Resource" and type === "StateMachine"', async function () {
+        const construct: ConstructTreeEntity = {
+            id: 'StateMachine',
+            path: 'aws-stepfunctions-integ/StateMachine',
+            children: {
+                Other: {
+                    id: 'Other',
+                    path: 'aws-stepfunctions-integ/StateMachine/Resource',
+                    attributes: {
+                        'aws:cdk:cloudformation:type': 'AWS::StepFunctions::StateMachine',
+                    },
+                },
+            },
+        }
+
+        assert.strictEqual(isStateMachine(construct), false)
+    })
+
+    it('returns false when tree node contains a node with id !== "Resource" and type !== "StateMachine"', async function () {
+        const construct: ConstructTreeEntity = {
+            id: 'StateMachine',
+            path: 'aws-stepfunctions-integ/LambdaFunction',
+            children: {
+                Other: {
+                    id: 'Other',
+                    path: 'aws-stepfunctions-integ/LambdaFunction/Resource',
+                    attributes: {
+                        'aws:cdk:cloudformation:type': 'AWS::StepFunctions::LambdaFunction',
+                    },
+                },
+            },
+        }
+        assert.strictEqual(isStateMachine(construct), false)
+    })
+
+    it('returns false when tree node contains a node with id === "Resource" and type !== "StateMachine"', async function () {
+        const construct: ConstructTreeEntity = {
+            id: 'StateMachine',
+            path: 'aws-stepfunctions-integ/LambdaFunction',
+            children: {
+                Resource: {
+                    id: 'Resource',
+                    path: 'aws-stepfunctions-integ/LambdaFunction/Resource',
+                    attributes: {
+                        'aws:cdk:cloudformation:type': 'AWS::StepFunctions::LambdaFunction',
+                    },
+                },
+            },
+        }
+        assert.strictEqual(isStateMachine(construct), false)
+    })
+})
