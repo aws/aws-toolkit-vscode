5676bd6f7aae18abcfe221726764de291b24834f
Lokesh Dogga <lodogga@amazon.com>
2024-07-10 16:45:03 -0700

    Add copy ARN for DynamoDB Tables

--- packages/core/src/dynamoDb/commands/sortDynamoDbTables.ts
+++ packages/core/src/dynamoDb/commands/sortDynamoDbTables.ts
@@ -3,8 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { copyToClipboard } from '../../shared/utilities/messages'
+import { DynamoDbTableNode } from '../explorer/dynamoDbTableNode'
+import { DynamoDbClient } from '../../shared/clients/dynamoDbClient'
 import { DynamoDbInstanceNode } from '../explorer/dynamoDbInstanceNode'
 
 export async function sortTablesByCreatedTime(node: DynamoDbInstanceNode) {
     console.log('I am trying to sort the tables by name.')
+}
+
+export async function copyDynamoDbArn(node: DynamoDbTableNode) {
+    const response = await new DynamoDbClient(node.regionCode).getTableInformation({ TableName: node.dynamoDbtable })
+    if (response.TableArn !== undefined) {
+        await copyToClipboard(response.TableArn, 'ARN')
+    }
 }
