3445a520bdc9c75bb083cbd6a38ab2bc6e007b6e
Lokesh Dogga <lodogga@amazon.com>
2024-07-11 11:58:10 -0700

    Addressing code review comments

--- packages/core/src/dynamoDb/commands/viewDynamoDbTable.ts
+++ packages/core/src/dynamoDb/commands/viewDynamoDbTable.ts
@@ -3,23 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as vscode from 'vscode'
-import { CancellationError } from '../../shared/utilities/timeoutUtils'
+import { getLogger } from '../../shared'
 import { DynamoDbTableNode } from '../explorer/dynamoDbTableNode'
 
-export async function prepareDocument(uri: vscode.Uri) {
-    try {
-        // Gets the data: calls filterLogEventsFromUri().
-        const doc = await vscode.workspace.openTextDocument(uri)
-        await vscode.window.showTextDocument(doc, { preview: false })
-        await vscode.languages.setTextDocumentLanguage(doc, 'log')
-    } catch (err) {
-        if (CancellationError.isUserCancelled(err)) {
-            throw err
-        }
-    }
-}
-
 export async function scanTable(node: DynamoDbTableNode) {
-    console.log('Yes, Table selected')
+    getLogger().debug('Yes, Table selected')
 }
