2e6877b47c4ce01cc702f2784e579a4df219a46b
Richard Li <742829+rli@users.noreply.github.com>
2024-07-16 15:17:46 -0700

    Mark EOL for GW-241 and Platform 232 (#4682)

--- plugins/toolkit/jetbrains-gateway/src/software/aws/toolkits/jetbrains/gateway/GatewayDeprecationNotice.kt
+++ plugins/toolkit/jetbrains-gateway/src/software/aws/toolkits/jetbrains/gateway/GatewayDeprecationNotice.kt
@@ -30,7 +30,7 @@ class GatewayDeprecationNotice : AppLifecycleListener {
     }
 
     companion object {
-        const val MIN_VERSION = 241
-        const val MIN_VERSION_HUMAN = "2024.1"
+        const val MIN_VERSION = 242
+        const val MIN_VERSION_HUMAN = "2024.2"
     }
 }
