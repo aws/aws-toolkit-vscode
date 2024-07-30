62238939beafdbf72925f7d09b5ae2f2a270960e
Maxim Hayes <hayemaxi@amazon.com>
2024-07-30 16:46:57 -0400

    fix(tests): remove Q 3 -> 5 scope migration tech debt
    Cleans up remainder of 9525cdf

--- packages/core/src/test/techdebt.test.ts
+++ packages/core/src/test/techdebt.test.ts
@@ -40,10 +40,6 @@ describe('tech debt', function () {
         )
     })
 
-    it('remove missing Amazon Q scopes edge case handling', async function () {
-        fixByDate('2024-07-30', 'Remove the edge case code from the commit that this test is a part of.')
-    })
-
     it('remove separate sessions login edge cases', async function () {
         // src/auth/auth.ts:SessionSeparationPrompt
         // forgetConnection() function and calls
