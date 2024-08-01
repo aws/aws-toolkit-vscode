43edf22a1b3cccca3bbacb61920eb84ad9d7f3b9
Richard Li <742829+rli@users.noreply.github.com>
2024-08-01 10:56:50 -0700

    Fix toolkit incorrectly including Gateway plugin.xml (#4752)

--- plugins/toolkit/jetbrains-gateway/build.gradle.kts
+++ plugins/toolkit/jetbrains-gateway/build.gradle.kts
@@ -66,8 +66,6 @@ listOf(
 
 dependencies {
     intellijPlatform {
-        pluginModule(project(":plugin-toolkit:jetbrains-core", "gatewayArtifacts"))
-
         pluginVerifier()
 
         testFramework(TestFrameworkType.Bundled)
@@ -75,6 +73,7 @@ dependencies {
 
     // link against :j-c: and rely on :intellij-standalone:composeJar to pull in :j-c:instrumentedJar, but gateway variant when from :jetbrains-gateway
     compileOnly(project(":plugin-toolkit:jetbrains-core"))
+    gatewayOnlyRuntimeOnly(project(":plugin-toolkit:jetbrains-core", "gatewayArtifacts"))
     // delete when fully split
     gatewayOnlyRuntimeOnly(project(":plugin-core:core"))
     gatewayOnlyRuntimeOnly(project(":plugin-core:jetbrains-community"))
@@ -139,6 +138,10 @@ val gatewayResourcesDir = tasks.register<Sync>("gatewayResourcesDir") {
 
 artifacts {
     add(gatewayResources.name, gatewayResourcesDir)
+}
+
+tasks.prepareJarSearchableOptions {
+    enabled = false
 }
 
 tasks.jar {
