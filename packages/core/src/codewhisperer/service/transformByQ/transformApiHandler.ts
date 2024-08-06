1463f285b85fa5146222cdfa550ed0b73b2de243
David Hasani <davhasan@amazon.com>
2024-08-05 18:52:13 -0700

    search table instead of using [0]

--- packages/core/src/codewhisperer/service/transformByQ/transformApiHandler.ts
+++ packages/core/src/codewhisperer/service/transformByQ/transformApiHandler.ts
@@ -641,7 +641,9 @@ export async function getTransformationPlan(jobId: string) {
 
         let plan = `<style>table {border: 1px solid #424750;}</style>\n\n<a id="top"></a><br><p style="font-size: 24px;"><img src="${logoIcon}" style="margin-right: 15px; vertical-align: middle;"></img><b>${CodeWhispererConstants.planTitle}</b></p><br>`
         const authType = await getAuthType()
-        const linesOfCode = jobStatistics[0].value
+        const linesOfCode = jobStatistics.find(
+            (stat: { name: string; value: number }) => stat.name === 'linesOfCode'
+        ).value
         if (authType === 'iamIdentityCenter' && linesOfCode > 100000) {
             plan += `<p>${linesOfCode} lines of code submitted for transformation, maximum charge of this transformation is $${(linesOfCode * 0.003).toFixed(2)} (this charge applies only after the free limit in your organization's subscriptions is exhausted). To prevent the charge, you can stop the job before the transformation completes.</p>`
         }
