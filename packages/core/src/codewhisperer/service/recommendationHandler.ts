e8b520051578dc92f94eae2f549a5c4c93f94e49
Lei Gao <97199248+leigaol@users.noreply.github.com>
2024-08-13 14:14:21 -0700

    Update packages/core/src/codewhisperer/service/recommendationHandler.ts
    Co-authored-by: Maxim Hayes <149123719+hayemaxi@users.noreply.github.com>

--- packages/core/src/codewhisperer/service/recommendationHandler.ts
+++ packages/core/src/codewhisperer/service/recommendationHandler.ts
@@ -657,7 +657,6 @@ export class RecommendationHandler {
         }
         if (this.isSuggestionVisible()) {
             // do not force refresh the tooltip to avoid suggestion "flashing"
-            // await this.showRecommendation(0, false)
             return
         }
         if (
