719128406d0fa0cbc52ef33a9800afe0f0a90ecb
Lei Gao <leigaol@amazon.com>
2024-08-16 09:38:33 -0700

    use util function

--- packages/core/src/test/amazonq/common/contentController.test.ts
+++ packages/core/src/test/amazonq/common/contentController.test.ts
@@ -1,30 +1,20 @@
 import * as vscode from 'vscode'
-import * as fs from 'fs-extra'
-import * as path from 'path'
 import assert from 'assert'
 import { EditorContentController } from '../../../amazonq/commons/controllers/contentController'
-import { makeTemporaryToolkitFolder } from '../../../shared'
+import { openATextEditorWithText } from '../../testUtil'
 
 describe('contentController', () => {
     let controller: EditorContentController
-    let tempFolder: string
 
     beforeEach(async () => {
         controller = new EditorContentController()
-        // Make a temp folder for all these tests
-        tempFolder = await makeTemporaryToolkitFolder()
     })
 
-    afterEach(async function () {
-        await fs.remove(tempFolder)
-    })
+    afterEach(async function () {})
 
     describe('insertTextAtCursorPosition', () => {
         it('insert code when left hand size has no non empty character', async () => {
-            const programFile = path.join(tempFolder, 'program.python')
-            await fs.writeFile(programFile, 'def hello_world():\n    ')
-            const doc = await vscode.workspace.openTextDocument(programFile)
-            const editor = await vscode.window.showTextDocument(doc)
+            const editor = await openATextEditorWithText('def hello_world():\n    ', 'test.py')
             if (editor) {
                 const pos = new vscode.Position(1, 4)
                 editor.selection = new vscode.Selection(pos, pos)
@@ -38,10 +28,7 @@ describe('contentController', () => {
         })
 
         it('insert code when left hand size has non empty character', async () => {
-            const programFile = path.join(tempFolder, 'program.python')
-            await fs.writeFile(programFile, 'def hello_world():\n    ')
-            const doc = await vscode.workspace.openTextDocument(programFile)
-            const editor = await vscode.window.showTextDocument(doc)
+            const editor = await openATextEditorWithText('def hello_world():\n    ', 'test.py')
             if (editor) {
                 const pos = new vscode.Position(0, 4)
                 editor.selection = new vscode.Selection(pos, pos)
