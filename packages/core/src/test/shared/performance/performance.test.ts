16ef42cbb6c34ee74499d8fe500de04f8c10dc5b
Joshua Pinkney <jpink@amazon.com>
2024-08-19 10:42:37 -0400

    performance: Create a starter set of tools for measuring the CPU/memory impact of codei
    Problem:
    - We are sometimes running into problems where functions are using a ton of memory/cpu but we have no way to figure that out ahead of time without manually debugging/customer reports
    
    Solution:
    - Create a starter set of tools that allows us to measure the impact of different parts of code. These tools should be used as needed and surround heavy computational parts of the code i.e. collecting all files in the workspace, reading them in and putting them in a zip

---
+++ packages/core/src/test/shared/performance/performance.test.ts
@@ -0,0 +1,34 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as assert from 'assert'
+import * as sinon from 'sinon'
+import { PerformanceTracker } from '../../../shared/performance/performance'
+import { stubPerformance } from '../../utilities/performance'
+
+describe('performance tooling', () => {
+    let sandbox: sinon.SinonSandbox
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(() => {
+        sandbox.restore()
+    })
+
+    describe('PerformanceTracker', () => {
+        it('gets performance metrics', () => {
+            const { expectedCpuUsage, expectedHeapTotal, expectedTotalSeconds } = stubPerformance(sandbox)
+            const perf = new PerformanceTracker('foo')
+            perf.start()
+            const metrics = perf.stop()
+
+            assert.deepStrictEqual(metrics?.cpuUsage, expectedCpuUsage)
+            assert.deepStrictEqual(metrics?.heapTotal, expectedHeapTotal)
+            assert.deepStrictEqual(metrics?.duration, expectedTotalSeconds)
+        })
+    })
+})
