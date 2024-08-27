1f8351e809a743a35a2bb9a5c9c05a40b873490e
Joshua Pinkney <jpink@amazon.com>
2024-08-27 15:33:22 -0400

    ci(workflows): Remove adding changelog/test coverage comments; use logs instead

--- .github/workflows/utils.js
+++ .github/workflows/utils.js
@@ -25,22 +25,6 @@ function parsePRTitle(title) {
     }
 }
 
-/**
- * Create a comment on a PR if one does not already exist
- */
-async function dedupComment({ github, pullRequestId, owner, repo, comments, message }) {
-    if (comments.data.some((comment) => comment.body.includes(message))) {
-        return
-    }
-
-    await github.rest.issues.createComment({
-        issue_number: pullRequestId,
-        owner,
-        repo,
-        body: message,
-    })
-}
-
 /*
  * Check if path is included in at least one of the filename paths
  */
