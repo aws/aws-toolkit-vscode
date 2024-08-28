10bc58e8e39e173ea13c48889d1db3b9d67136ad
Joshua Pinkney <jpink@amazon.com>
2024-08-28 19:35:14 -0400

    telemetry(amazonq): Add webview load metric for amazonq
    Problem:
    - We have no detailed information on how long the amazonq webview took for first meaningful paint.
    
    Solution:-
    - The onready event gets fired when "UI is initialized and rendered". See https://github.com/aws/mynah-ui/blob/main/docs/PROPERTIES.md#onready

--- packages/core/src/amazonq/webview/messages/messageDispatcher.ts
+++ packages/core/src/amazonq/webview/messages/messageDispatcher.ts
@@ -8,12 +8,31 @@ import { MessagePublisher } from '../../messages/messagePublisher'
 import { MessageListener } from '../../messages/messageListener'
 import { TabType } from '../ui/storages/tabsStorage'
 import { getLogger } from '../../../shared/logger'
+import { amazonqMark } from '../../../shared/performance/marks'
+import { telemetry } from '../../../shared/telemetry'
 
 export function dispatchWebViewMessagesToApps(
     webview: Webview,
     webViewToAppsMessagePublishers: Map<TabType, MessagePublisher<any>>
 ) {
     webview.onDidReceiveMessage((msg) => {
+        if (msg.command === 'ui-is-ready') {
+            /**
+             * ui-is-ready isn't associated to any tab so just record the telemetry event and continue.
+             * This would be equivalent of the duration between "user clicked open q" and "ui has become available"
+             * NOTE: Amazon Q UI is only loaded ONCE. The state is saved between each hide/show of the webview.
+             */
+
+            telemetry.webview_load.emit({
+                webviewName: 'amazonq',
+                duration: performance.measure(amazonqMark.uiReady, amazonqMark.open).duration,
+                result: 'Succeeded',
+            })
+            performance.clearMarks(amazonqMark.uiReady)
+            performance.clearMarks(amazonqMark.open)
+            return
+        }
+
         const appMessagePublisher = webViewToAppsMessagePublishers.get(msg.tabType)
         if (appMessagePublisher === undefined) {
             return
