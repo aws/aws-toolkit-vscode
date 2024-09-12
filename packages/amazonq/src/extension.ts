f2591bc794d91820c3529e4d7ce535591b112073
Vikash Agrawal <vikashagrawal1990@gmail.com>
2024-09-12 09:20:12 -0700

    feat(amazonq): handle extension uninstallation (#5410)
    Setup Uninstall Handler for extensions.
    
    - [x] Telemetry and Logger needs to be verified. 
    
    ## License
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- packages/amazonq/src/extension.ts
+++ packages/amazonq/src/extension.ts
@@ -39,6 +39,7 @@ import {
     messages,
     placeholder,
     setContext,
+    setupUninstallHandler,
 } from 'aws-core-vscode/shared'
 import { ExtStartUpSources, telemetry } from 'aws-core-vscode/telemetry'
 import { VSCODE_EXTENSION_ID } from 'aws-core-vscode/utils'
@@ -128,6 +129,9 @@ export async function activateAmazonQCommon(context: vscode.ExtensionContext, is
 
     // Amazon Q specific commands
     registerCommands(context)
+
+    // Handle Amazon Q Extension un-installation.
+    setupUninstallHandler(VSCODE_EXTENSION_ID.amazonq, context)
 
     // Hide the Amazon Q tree in toolkit explorer
     await setContext('aws.toolkit.amazonq.dismissed', true)
