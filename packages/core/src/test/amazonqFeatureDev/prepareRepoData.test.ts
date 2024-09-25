4ea87f7a35db3c11d5aa2226f9511bd3f52fae79
hkobew <hkobew@amazon.com>
2024-09-25 13:31:09 -0400

    adjust thresholds, remove utility function

--- packages/core/src/test/amazonqFeatureDev/prepareRepoData.test.ts
+++ packages/core/src/test/amazonqFeatureDev/prepareRepoData.test.ts
@@ -4,7 +4,7 @@
  */
 import assert from 'assert'
 import { WorkspaceFolder } from 'vscode'
-import { getEqualOptions, performanceTest } from '../../shared/performance/performance'
+import { performanceTest } from '../../shared/performance/performance'
 import { createTestWorkspace } from '../testUtil'
 import { prepareRepoData, TelemetryHelper } from '../../amazonqFeatureDev'
 import { AmazonqCreateUpload, getRandomString, Metric } from '../../shared'
@@ -22,45 +22,88 @@ describe('prepareRepoDataPerformanceTest', function () {
         assert.strictEqual(result.zipFileChecksum.length, 44)
     }
 
-    performanceTest(getEqualOptions(10, 80, 8, 0.7), 'handles many files', function () {
-        const telemetry = new TelemetryHelper()
-        let workspace: WorkspaceFolder
-        let result: resultType
-        return {
-            setup: async () => {
-                workspace = await createTestWorkspace(1000, {
-                    fileNamePrefix: 'file',
-                    fileContent: '0123456789',
-                    fileNameSuffix: '.md',
-                })
+    performanceTest(
+        {
+            testRuns: 10,
+            linux: {
+                userCpuUsage: 80,
+                heapTotal: 8,
+                duration: 1.0,
             },
-            execute: async () => {
-                result = await prepareRepoData([workspace.uri.fsPath], [workspace], telemetry, {
-                    record: () => {},
-                } as unknown as Metric<AmazonqCreateUpload>)
+            darwin: {
+                userCpuUsage: 80,
+                heapTotal: 8,
+                duration: 1.0,
             },
-            verify: async () => verifyResult(result, telemetry, 10000),
+            win32: {
+                userCpuUsage: 80,
+                heapTotal: 8,
+                duration: 3,
+            },
+        },
+        'handles many files',
+        function () {
+            const telemetry = new TelemetryHelper()
+            let workspace: WorkspaceFolder
+            let result: resultType
+            return {
+                setup: async () => {
+                    workspace = await createTestWorkspace(500, {
+                        fileNamePrefix: 'file',
+                        fileContent: '0123456789',
+                        fileNameSuffix: '.md',
+                    })
+                },
+                execute: async () => {
+                    result = await prepareRepoData([workspace.uri.fsPath], [workspace], telemetry, {
+                        record: () => {},
+                    } as unknown as Metric<AmazonqCreateUpload>)
+                },
+                verify: async () => verifyResult(result, telemetry, 5000),
+            }
         }
-    })
+    )
 
-    performanceTest(getEqualOptions(10, 30, 1, 0.1), 'handles large files', function () {
-        const telemetry = new TelemetryHelper()
-        let result: resultType
-        let workspace: WorkspaceFolder
-        return {
-            setup: async () => {
-                workspace = await createTestWorkspace(10, {
-                    fileNamePrefix: 'file',
-                    fileContent: getRandomString(1000),
-                    fileNameSuffix: '.md',
-                })
+    performanceTest(
+        {
+            testRuns: 10,
+            linux: {
+                userCpuUsage: 30,
+                heapTotal: 1,
+                duration: 0.1,
             },
-            execute: async () => {
-                result = await prepareRepoData([workspace.uri.fsPath], [workspace], telemetry, {
-                    record: () => {},
-                } as unknown as Metric<AmazonqCreateUpload>)
+            darwin: {
+                userCpuUsage: 30,
+                heapTotal: 1,
+                duration: 0.1,
             },
-            verify: async () => verifyResult(result, telemetry, 10000),
+            win32: {
+                userCpuUsage: 30,
+                heapTotal: 1,
+                duration: 0.1,
+            },
+        },
+
+        'handles large files',
+        function () {
+            const telemetry = new TelemetryHelper()
+            let result: resultType
+            let workspace: WorkspaceFolder
+            return {
+                setup: async () => {
+                    workspace = await createTestWorkspace(10, {
+                        fileNamePrefix: 'file',
+                        fileContent: getRandomString(1000),
+                        fileNameSuffix: '.md',
+                    })
+                },
+                execute: async () => {
+                    result = await prepareRepoData([workspace.uri.fsPath], [workspace], telemetry, {
+                        record: () => {},
+                    } as unknown as Metric<AmazonqCreateUpload>)
+                },
+                verify: async () => verifyResult(result, telemetry, 10000),
+            }
         }
-    })
+    )
 })
