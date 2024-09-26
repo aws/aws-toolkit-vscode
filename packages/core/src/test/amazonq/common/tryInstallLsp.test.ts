0905fa9ebd4c3a8dfecd971d9f7c9f5902a8d6c9
hkobew <hkobew@amazon.com>
2024-09-26 14:15:22 -0400

    implement test, remove fs-extra

---
+++ packages/core/src/test/amazonq/common/tryInstallLsp.test.ts
@@ -0,0 +1,81 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import assert from 'assert'
+import { performanceTest } from '../../../shared/performance/performance'
+import { LspController } from '../../../amazonq'
+import { fs, globals } from '../../../shared'
+import sinon from 'sinon'
+import { Content } from 'aws-sdk/clients/codecommit'
+import { createTestWorkspace } from '../../testUtil'
+import AdmZip from 'adm-zip'
+import path from 'path'
+
+// fakeFileContent is matched to fakeQServerContent based on hash.
+const fakeHash = '4eb2865c8f40a322aa04e17d8d83bdaa605d6f1cb363af615240a5442a010e0aef66e21bcf4c88f20fabff06efe8a214'
+
+const fakeQServerContent = {
+    filename: 'qserver-fake.zip',
+    url: 'https://aws-language-servers/fake.zip',
+    hashes: [`sha384:${fakeHash}`],
+    bytes: 93610849,
+    serverVersion: '1.1.1',
+}
+
+const fakeNodeContent = {
+    filename: 'fake-file',
+    url: 'https://aws-language-servers.fake-file',
+    hashes: [`sha384:${fakeHash}`],
+    bytes: 94144448,
+    serverVersion: '1.1.1',
+}
+
+/**
+ * Creates a fake zip with some files in it.
+ * @param filepath where to write the zip to.
+ * @param _content unused parameter, for compatability with real function.
+ */
+const fakeDownload = async (filepath: string, _content: Content) => {
+    const dummyFilesPath = (
+        await createTestWorkspace(100, {
+            fileNamePrefix: 'fakeFile',
+            fileContent: 'emptyContent',
+            workspaceName: 'workspace',
+        })
+    ).uri.fsPath
+    await fs.writeFile(path.join(dummyFilesPath, 'qserver'), 'this value shouldnt matter')
+    const zip = new AdmZip()
+    zip.addLocalFolder(dummyFilesPath)
+    zip.writeZip(filepath)
+}
+
+describe('tryInstallLsp performance test', function () {
+    afterEach(function () {
+        sinon.restore()
+    })
+
+    performanceTest({}, 'sets up', function () {
+        return {
+            setup: async () => {
+                // Avoid making HTTP request or mocking giant manifest, stub what we need directly from request.
+                sinon.stub(LspController.prototype, 'fetchManifest')
+                // Directly feed the runtime specifications.
+                sinon.stub(LspController.prototype, 'getQserverFromManifest').returns(fakeQServerContent)
+                sinon.stub(LspController.prototype, 'getNodeRuntimeFromManifest').returns(fakeNodeContent)
+                // avoid fetch call.
+                sinon.stub(LspController.prototype, '_download').callsFake(fakeDownload)
+                // Hard code the hash since we are creating files on the spot, whose hashes can't be predicted.
+                sinon.stub(LspController.prototype, 'getFileSha384').resolves(fakeHash)
+                // Don't allow tryInstallLsp to move runtimes out of temporary folder.
+                sinon.stub(fs, 'rename')
+            },
+            execute: async () => {
+                return await LspController.instance.tryInstallLsp(globals.context)
+            },
+            verify: async (setup: any, result: boolean) => {
+                assert.ok(result)
+            },
+        }
+    })
+})
