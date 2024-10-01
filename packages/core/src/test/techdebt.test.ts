971d6e2faeff1e9bce597f3d721c8dce2817abc7
hkobew <hkobew@amazon.com>
2024-10-01 13:57:13 -0400

    remove c9 specific find file

--- packages/core/src/test/techdebt.test.ts
+++ packages/core/src/test/techdebt.test.ts
@@ -18,11 +18,7 @@ describe('tech debt', function () {
 
     it('vscode minimum version', async function () {
         const minVscode = env.getMinVscodeVersion()
-
-        assert.ok(
-            semver.lt(minVscode, '1.75.0'),
-            'remove filesystemUtilities.findFile(), use vscode.workspace.findFiles() instead (after Cloud9 VFS fixes bug)'
-        )
+        assert.ok(semver.lt(minVscode, '1.83.0'))
 
         assert.ok(
             semver.lt(minVscode, '1.75.0'),
