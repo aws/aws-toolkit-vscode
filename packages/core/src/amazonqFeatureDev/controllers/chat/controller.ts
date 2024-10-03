0743620ac8118d17a2f2711f636e6a95c3fb687b
Thiago Verney <tverney@amazon.com>
2024-10-03 12:18:01 -0400

    fix(amazonqFeatureDev): disable input

--- packages/core/src/amazonqFeatureDev/controllers/chat/controller.ts
+++ packages/core/src/amazonqFeatureDev/controllers/chat/controller.ts
@@ -723,11 +723,12 @@ export class FeatureDevController {
             type: 'answer-part',
             tabID: message.tabID,
         })
-        this.messenger.sendChatInputEnabled(message.tabID, false)
         this.messenger.sendUpdatePlaceholder(
             message.tabID,
             i18n('AWS.amazonq.featureDev.pillText.stoppingCodeGeneration')
         )
+        this.messenger.sendChatInputEnabled(message.tabID, false)
+
         const session = await this.sessionStorage.getSession(message.tabID)
         if (session.state?.tokenSource) {
             session.state?.tokenSource?.cancel()
