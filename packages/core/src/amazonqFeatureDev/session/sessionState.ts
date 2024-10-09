d697f4434f0a12d49e0b6f5214abf2e0cb544a64
Thiago Verney <tverney@amazon.com>
2024-10-09 18:25:30 -0400

    telemetry(amazonq dev): track empty path file scenario #5625
    ## Problem
    When customer use workspaces to generate files, RTS and LLM cannot
    decide which folder to deliver the generated file. We're for now
    tracking how many times we reach for accountability and prioritization.
    
    ## Solution
    Add telemetry to count and save the conversationIds that reach this
    specific workspace usage.
    
    PS: This is only included in VS Code since the Workspaces capability is only used here.

--- packages/core/src/amazonqFeatureDev/session/sessionState.ts
+++ packages/core/src/amazonqFeatureDev/session/sessionState.ts
@@ -61,7 +61,8 @@ function registerNewFiles(
     fs: VirtualFileSystem,
     newFileContents: NewFileZipContents[],
     uploadId: string,
-    workspaceFolders: CurrentWsFolders
+    workspaceFolders: CurrentWsFolders,
+    conversationId: string
 ): NewFileInfo[] {
     const result: NewFileInfo[] = []
     const workspaceFolderPrefixes = getWorkspaceFoldersByPrefixes(workspaceFolders)
@@ -81,6 +82,12 @@ function registerNewFiles(
                       Object.values(workspaceFolderPrefixes).find((val) => val.index === 0)?.name ?? ''
                   ]
         if (folder === undefined) {
+            telemetry.toolkit_trackScenario.emit({
+                count: 1,
+                amazonqConversationId: conversationId,
+                credentialStartUrl: AuthUtil.instance.startUrl,
+                scenario: 'wsOrphanedDocuments',
+            })
             getLogger().error(`No workspace folder found for file: ${zipFilePath} and prefix: ${prefix}`)
             continue
         }
@@ -174,7 +181,13 @@ abstract class CodeGenBase {
                 case CodeGenerationStatus.COMPLETE: {
                     const { newFileContents, deletedFiles, references } =
                         await this.config.proxyClient.exportResultArchive(this.conversationId)
-                    const newFileInfo = registerNewFiles(fs, newFileContents, this.uploadId, workspaceFolders)
+                    const newFileInfo = registerNewFiles(
+                        fs,
+                        newFileContents,
+                        this.uploadId,
+                        workspaceFolders,
+                        this.conversationId
+                    )
                     telemetry.setNumberOfFilesGenerated(newFileInfo.length)
 
                     return {
@@ -378,7 +391,13 @@ export class MockCodeGenState implements SessionState {
                 zipFilePath: f.zipFilePath,
                 fileContent: f.fileContent,
             }))
-            this.filePaths = registerNewFiles(action.fs, newFileContents, this.uploadId, this.config.workspaceFolders)
+            this.filePaths = registerNewFiles(
+                action.fs,
+                newFileContents,
+                this.uploadId,
+                this.config.workspaceFolders,
+                this.conversationId
+            )
             this.deletedFiles = [
                 {
                     zipFilePath: 'src/this-file-should-be-deleted.ts',
