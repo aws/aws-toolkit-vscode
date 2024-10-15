ef5163434e9377b3e2d66260e05fa452d5cc17ce
Keegan Irby <keegani@amazon.com>
2024-10-15 14:26:18 -0700

    Change implementation of LiveTailRegistry to standard map

--- packages/core/src/test/awsService/cloudWatchLogs/registry/liveTailRegistry.test.ts
+++ packages/core/src/test/awsService/cloudWatchLogs/registry/liveTailRegistry.test.ts
@@ -4,53 +4,9 @@
  */
 import * as vscode from 'vscode'
 import assert from 'assert'
-import {
-    LiveTailSession,
-    LiveTailSessionConfiguration,
-} from '../../../../awsService/cloudWatchLogs/registry/liveTailSession'
-import {
-    createLiveTailURIFromArgs,
-    LiveTailSessionRegistry,
-} from '../../../../awsService/cloudWatchLogs/registry/liveTailSessionRegistry'
+import { LiveTailSessionConfiguration } from '../../../../awsService/cloudWatchLogs/registry/liveTailSession'
+import { createLiveTailURIFromArgs } from '../../../../awsService/cloudWatchLogs/registry/liveTailSessionRegistry'
 import { CLOUDWATCH_LOGS_LIVETAIL_SCHEME } from '../../../../shared/constants'
-
-/**
- * Exposes protected methods so we can test them
- */
-class TestLiveTailSessionRegistry extends LiveTailSessionRegistry {
-    constructor() {
-        super()
-    }
-
-    override hash(uri: vscode.Uri): string {
-        return super.hash(uri)
-    }
-
-    override get default(): LiveTailSession {
-        return super.default
-    }
-}
-
-describe('LiveTailRegistry', async function () {
-    const session = new LiveTailSession({
-        logGroupName: 'test-log-group',
-        region: 'test-region',
-    })
-
-    let liveTailSessionRegistry: TestLiveTailSessionRegistry
-
-    beforeEach(function () {
-        liveTailSessionRegistry = new TestLiveTailSessionRegistry()
-    })
-
-    it('hash()', function () {
-        assert.deepStrictEqual(liveTailSessionRegistry.hash(session.uri), session.uri.toString())
-    })
-
-    it('default()', function () {
-        assert.throws(() => liveTailSessionRegistry.default)
-    })
-})
 
 describe('LiveTailSession URI', async function () {
     const testLogGroupName = 'test-log-group'
