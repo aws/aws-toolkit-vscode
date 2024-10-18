bb1c3b93bba524f18859d5c4af01ba821aa34089
hkobew <hkobew@amazon.com>
2024-10-18 13:07:24 -0400

    add tests for executable file

--- packages/core/src/test/shared/utilities/testUtils.test.ts
+++ packages/core/src/test/shared/utilities/testUtils.test.ts
@@ -3,7 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import assert from 'assert'
-import { envWithNewPath, readEnv, setEnv } from '../../testUtil'
+import { createExecutableFile, createTestWorkspaceFolder, envWithNewPath, readEnv, setEnv } from '../../testUtil'
+import path from 'path'
+import { ChildProcess } from '../../../shared/utilities/processUtils'
+import { fs } from '../../../shared'
 
 describe('envWithNewPath', function () {
     it('gives current path with new PATH', function () {
@@ -21,5 +24,20 @@ describe('writeEnv', function () {
 
         setEnv(originalEnv)
         assert.deepStrictEqual(readEnv(), originalEnv)
+    })
+})
+
+describe('createExecutableFile', function () {
+    it('creates a file that can be executes in a child process', async function () {
+        const tempDir = await createTestWorkspaceFolder()
+        const filePath = path.join(tempDir.uri.fsPath, 'exec')
+        await createExecutableFile(filePath, '')
+
+        const proc = new ChildProcess(filePath)
+        const result = await proc.run()
+        assert.ok(result)
+        assert.ok(result.exitCode === 0)
+
+        await fs.delete(tempDir.uri, { force: true, recursive: true })
     })
 })
