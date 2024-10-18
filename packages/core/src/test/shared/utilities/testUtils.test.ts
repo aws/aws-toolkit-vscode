ba9fa6fbf22dcadc6b0bab148f5328e8d16bc0c1
hkobew <hkobew@amazon.com>
2024-10-18 16:36:39 -0400

    replace manual childprocess with tryRun

--- packages/core/src/test/shared/utilities/testUtils.test.ts
+++ packages/core/src/test/shared/utilities/testUtils.test.ts
@@ -5,9 +5,9 @@
 import assert from 'assert'
 import { createExecutableFile, createTestWorkspaceFolder, envWithNewPath, readEnv, setEnv } from '../../testUtil'
 import path from 'path'
-import { ChildProcess } from '../../../shared/utilities/processUtils'
 import { fs } from '../../../shared'
 import { isWin } from '../../../shared/vscode/env'
+import { tryRun } from '../../../shared/utilities/pathFind'
 
 describe('envWithNewPath', function () {
     it('gives current path with new PATH', function () {
@@ -29,16 +29,13 @@ describe('writeEnv', function () {
 })
 
 describe('createExecutableFile', function () {
-    it('creates a file that can be executes in a child process', async function () {
+    it('creates a file that can be executed', async function () {
         const tempDir = await createTestWorkspaceFolder()
         const filePath = path.join(tempDir.uri.fsPath, `exec${isWin() ? '.cmd' : ''}`)
         await createExecutableFile(filePath, '')
 
-        const proc = new ChildProcess(filePath)
-        const result = await proc.run()
+        const result = await tryRun(filePath, [], 'yes')
         assert.ok(result)
-        assert.ok(result.exitCode === 0)
-
         await fs.delete(tempDir.uri, { force: true, recursive: true })
     })
 })
