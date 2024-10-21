593538a9816386ad975b581fc372b7b8565b4ac8
hkobew <hkobew@amazon.com>
2024-10-21 15:46:35 -0400

    clear dir inbetween each test

--- packages/core/src/test/globalSetup.test.ts
+++ packages/core/src/test/globalSetup.test.ts
@@ -103,6 +103,7 @@ export const mochaHooks = {
         TelemetryDebounceInfo.instance.clear()
         // mochaGlobalSetup() set this to a fake, so it's safe to clear it here.
         await globals.globalState.clear()
+        await fs.delete(globals.context.globalStorageUri.fsPath, { recursive: true, force: true })
 
         await testUtil.closeAllEditors()
     },
