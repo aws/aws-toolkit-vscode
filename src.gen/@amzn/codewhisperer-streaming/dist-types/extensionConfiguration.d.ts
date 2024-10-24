1bb97f3e733fa98a6fcc64d77e8338582958c1eb
Ahmed Ali <a.husseinzaki@gmail.com>
2024-10-24 12:03:36 -0700

    Remove build artifacts from code change

--- src.gen/@amzn/codewhisperer-streaming/dist-types/extensionConfiguration.d.ts
+++
@@ -1,8 +0,0 @@
-import { AwsRegionExtensionConfiguration } from "@aws-sdk/types";
-import { HttpHandlerExtensionConfiguration } from "@smithy/protocol-http";
-import { DefaultExtensionConfiguration } from "@smithy/types";
-/**
- * @internal
- */
-export interface CodeWhispererStreamingExtensionConfiguration extends HttpHandlerExtensionConfiguration, DefaultExtensionConfiguration, AwsRegionExtensionConfiguration {
-}
