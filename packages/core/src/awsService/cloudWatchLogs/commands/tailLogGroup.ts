e3ee3f7556b03f6c939aab3cda0c7608590a59ef
Keegan Irby <keegani@amazon.com>
2024-10-24 11:04:46 -0700

    Support autoscrolling

--- packages/core/src/awsService/cloudWatchLogs/commands/tailLogGroup.ts
+++ packages/core/src/awsService/cloudWatchLogs/commands/tailLogGroup.ts
@@ -77,7 +77,11 @@ async function handleSessionStream(
                 formatLogEvent(logEvent)
             )
             if (formattedLogEvents.length !== 0) {
+                //Determine should scroll before adding new lines to doc because adding large
+                //amount of new lines can push bottom of file out of view before scrolling.
+                const editorsToScroll = getTextEditorsToScroll(document)
                 await updateTextDocumentWithNewLogEvents(formattedLogEvents, document, session.maxLines)
+                editorsToScroll.forEach(scrollTextEditorToBottom)
             }
         }
     }
@@ -97,6 +101,24 @@ function formatLogEvent(logEvent: LiveTailSessionLogEvent): string {
         line = line.concat('\n')
     }
     return line
+}
+
+//Auto scroll visible LiveTail session editors if the end-of-file is in view.
+//This allows for newly added log events to stay in view.
+function getTextEditorsToScroll(document: vscode.TextDocument): vscode.TextEditor[] {
+    return vscode.window.visibleTextEditors.filter((editor) => {
+        const isEditorForSession = editor.document === document
+        if (!isEditorForSession) {
+            return false
+        }
+        return editor.visibleRanges[0].contains(new vscode.Position(document.lineCount - 1, 0))
+    })
+}
+
+function scrollTextEditorToBottom(editor: vscode.TextEditor) {
+    const topPosition = new vscode.Position(Math.max(editor.document.lineCount - 2, 0), 0)
+    const bottomPosition = new vscode.Position(Math.max(editor.document.lineCount - 2, 0), 0)
+    editor.revealRange(new vscode.Range(topPosition, bottomPosition), vscode.TextEditorRevealType.Default)
 }
 
 async function updateTextDocumentWithNewLogEvents(
