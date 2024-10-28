ef1f17407157956aab3749e5f313554fc89f4f7a
Will Lo <xshaohua@amazon.com>
2024-10-28 14:12:59 -0700

    update constant

--- packages/core/src/codewhisperer/models/constants.ts
+++ packages/core/src/codewhisperer/models/constants.ts
@@ -712,7 +712,7 @@ export const crossFileContextConfig = {
     numberOfChunkToFetch: 60,
     topK: 3,
     numberOfLinesEachChunk: 50,
-    maximumTotalLength: 10240,
+    maximumTotalLength: 20480,
 }
 
 export const utgConfig = {
