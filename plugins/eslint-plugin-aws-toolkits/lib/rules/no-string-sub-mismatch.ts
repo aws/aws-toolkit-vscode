aaad8adf2ce95df10673569481bfa749110af9e9
hkobew <hkobew@amazon.com>
2024-10-29 16:01:36 -0400

    implement rule

--- plugins/eslint-plugin-aws-toolkits/lib/rules/no-string-sub-mismatch.ts
+++ plugins/eslint-plugin-aws-toolkits/lib/rules/no-string-sub-mismatch.ts
@@ -2,7 +2,24 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import { ESLintUtils, TSESTree } from '@typescript-eslint/utils'
+import { AST_NODE_TYPES, ESLintUtils, TSESTree } from '@typescript-eslint/utils'
+import { isLoggerCall } from './no-json-stringify-in-log'
+import { Rule } from 'eslint'
+
+/**
+ * reuse solution done in logger itself: https://github.com/winstonjs/winston/blob/195e55c7e7fc58914ae4967ea7b832c9e0ced930/lib/winston/logger.js#L27
+ */
+function countSubTokens(literalNode: TSESTree.StringLiteral) {
+    const formatRegExp = /%[scdjifoO%]/g
+    return literalNode.value.match(formatRegExp)?.length || 0
+}
+/**
+ * Form the error message using templates or actual values.
+ * Allows us to avoid copy pasting message into test file.
+ */
+export function formErrorMsg(substitutionTokens: string | number, numArgs: string | number): string {
+    return `String substitution has ${substitutionTokens} substitution tokens, but ${numArgs} arguments.`
+}
 
 export default ESLintUtils.RuleCreator.withoutDocs({
     meta: {
@@ -10,13 +27,36 @@ export default ESLintUtils.RuleCreator.withoutDocs({
             description: 'ensure string substitution args and templates match',
             recommended: 'recommended',
         },
-        messages: {},
+        messages: {
+            errMsg: formErrorMsg('{{ substitutionTokens }}', '{{ args }}'),
+        },
         type: 'problem',
         fixable: 'code',
         schema: [],
     },
     defaultOptions: [],
     create(context) {
-        return {}
+        return {
+            CallExpression(node: TSESTree.CallExpression) {
+                if (
+                    isLoggerCall(node) &&
+                    node.arguments[0].type === AST_NODE_TYPES.Literal &&
+                    typeof node.arguments[0].value === 'string'
+                ) {
+                    const numSubTokens = countSubTokens(node.arguments[0])
+                    const numExtraArgs = node.arguments.length - 1
+                    if (numSubTokens !== numExtraArgs) {
+                        return context.report({
+                            node: node,
+                            data: {
+                                substitutionTokens: numSubTokens,
+                                args: numExtraArgs,
+                            },
+                            messageId: 'errMsg',
+                        })
+                    }
+                }
+            },
+        }
     },
-})
+}) as unknown as Rule.RuleModule
---
+++ plugins/eslint-plugin-aws-toolkits/test/rules/no-string-sub-mismatch.test.ts
@@ -0,0 +1,32 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { rules } from '../../index'
+import { formErrorMsg } from '../../lib/rules/no-string-sub-mismatch'
+import { getRuleTester } from '../testUtil'
+
+getRuleTester().run('no-string-sub-mismatch', rules['no-string-sub-mismatch'], {
+    valid: [
+        'getLogger().debug("this is a string %s and a number %d", "s", 2)',
+        'getLogger().debug("this is a number %d", 2)',
+        'getLogger().debug("this has no substitutions")',
+        'getLogger().debug("1 %s 2 %d 3 %O 4 %o 5 %s", arg1, arg2, arg3, arg4, arg5)',
+        'getLogger().debug("not real a sub-token %z")',
+    ],
+    invalid: [
+        {
+            code: 'getLogger().debug("this is a string %s and a number %d", "s")',
+            errors: [formErrorMsg(2, 1)],
+        },
+        {
+            code: 'getLogger().debug("this is a string %s a string %s a string %s")',
+            errors: [formErrorMsg(3, 0)],
+        },
+        {
+            code: 'getLogger().debug("this is a string", err)',
+            errors: [formErrorMsg(0, 1)],
+        },
+    ],
+})
