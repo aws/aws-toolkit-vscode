6af05e49025fa6dfdd4267cf004129c454b008fb
Ahmed Ali <a.husseinzaki@gmail.com>
2024-10-28 23:23:14 -0700

    Guardrail inline chat as noop for sagemaker

--- packages/amazonq/src/inlineChat/app.ts
+++ packages/amazonq/src/inlineChat/app.ts
@@ -5,13 +5,8 @@
 import * as vscode from 'vscode'
 import { InlineChatController } from '../inlineChat/controller/inlineChatController'
 import { registerInlineCommands } from '../inlineChat/command/registerInlineCommands'
-import { isSageMaker } from 'aws-core-vscode/shared'
-import { AuthUtil } from 'aws-core-vscode/codewhisperer'
-import { isIamConnection } from 'aws-core-vscode/auth'
 
 export function init(context: vscode.ExtensionContext) {
-    if (!(isSageMaker() && isIamConnection(AuthUtil.instance.conn))) {
-        const inlineChatController = new InlineChatController(context)
-        registerInlineCommands(context, inlineChatController)
-    }
+    const inlineChatController = new InlineChatController(context)
+    registerInlineCommands(context, inlineChatController)
 }
