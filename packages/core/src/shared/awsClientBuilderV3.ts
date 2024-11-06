8a0c77c6438166a334adbc61ef8778f032852535
hkobew <hkobew@amazon.com>
2024-11-06 10:09:49 -0500

    work towards changing type system

--- packages/core/src/shared/awsClientBuilderV3.ts
+++ packages/core/src/shared/awsClientBuilderV3.ts
@@ -5,26 +5,32 @@
 
 import { CredentialsShim } from '../auth/deprecated/loginManager'
 import { AwsContext } from './awsContext'
-import { AwsCredentialIdentityProvider, Client } from '@smithy/types'
+import { AwsCredentialIdentityProvider } from '@smithy/types'
+// import { SmithyResolvedConfiguration } from '@aws-sdk/smithy-client'
+import { Client as IClient } from '@smithy/types'
 import { getUserAgent } from './telemetry/util'
 import { DevSettings } from './settings'
 import { Provider, UserAgent } from '@aws-sdk/types'
 
-export type AwsClient = Client<any, any, any>
+//type AwsClientConfig = SmithyResolvedConfiguration<any> & Partial<AwsClientOptions>
+
+// export type AwsClient<O extends AwsClientConfig> = Client<any, any, any, O> | IClient<any, any, O>
+export type AwsClient = IClient<any, any, any>
+
 export interface AwsClientOptions {
-    credentials?: AwsCredentialIdentityProvider
-    region?: string | Provider<string>
-    customUserAgent?: UserAgent
+    credentials: AwsCredentialIdentityProvider
+    region: string | Provider<string>
+    customUserAgent: UserAgent
 }
 
 export interface AWSClientBuilderV3 {
-    createAwsService<T extends AwsClient>(
-        type: new (o: AwsClientOptions) => T,
-        options?: AwsClientOptions,
+    createAwsService<C extends AwsClient>(
+        type: new (o: AwsClientOptions) => C,
+        options?: Partial<AwsClientOptions>,
         region?: string,
         userAgent?: string,
         settings?: DevSettings
-    ): Promise<T>
+    ): Promise<C>
 }
 
 export class DefaultAWSClientBuilderV3 implements AWSClientBuilderV3 {
@@ -38,15 +44,15 @@ export class DefaultAWSClientBuilderV3 implements AWSClientBuilderV3 {
         return shim
     }
 
-    public async createAwsService<T extends AwsClient>(
-        type: new (o: Partial<AwsClientOptions>) => T,
-        options?: AwsClientOptions,
+    public async createAwsService<C extends AwsClient>(
+        type: new (o: AwsClientOptions) => C,
+        options?: Partial<AwsClientOptions>,
         region?: string,
         userAgent?: string,
         settings?: DevSettings
-    ): Promise<T> {
-        const opt = { ...options }
+    ): Promise<C> {
         const shim = this.getShim()
+        const opt = (options ?? {}) as AwsClientOptions
 
         if (!opt.region && region) {
             opt.region = region
