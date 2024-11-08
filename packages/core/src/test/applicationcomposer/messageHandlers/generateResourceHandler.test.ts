601013ec5fc7a0917f97168b4d9b8ff63f31a2b2
hkobew <hkobew@amazon.com>
2024-11-08 11:46:07 -0500

    try retry x 10

--- packages/core/src/test/applicationcomposer/messageHandlers/generateResourceHandler.test.ts
+++ packages/core/src/test/applicationcomposer/messageHandlers/generateResourceHandler.test.ts
@@ -16,6 +16,7 @@ describe.only('generateResourceHandler', function () {
     })
     for (const _ of Array.from({ length: 1000 }, (i) => i)) {
         it('amazon q is not installed', async () => {
+            this.retries(10)
             const panel = await createTemplate()
             const postMessageSpy = sinon.spy(panel.webview, 'postMessage')
             const context = await createWebviewContext({
