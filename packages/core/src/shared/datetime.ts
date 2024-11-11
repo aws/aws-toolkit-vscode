fbb7b2c4ab2f5d9db8b37ff2e180f0a61d8e470f
nkomonen-amazon <nkomonen@amazon.com>
2024-11-11 12:26:46 -0500

    add constants for millisecond amounts
    Signed-off-by: nkomonen-amazon <nkomonen@amazon.com>

---
+++ packages/core/src/shared/datetime.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// constants for working with milliseconds
+export const oneSecond = 1000
+export const oneMinute = oneSecond * 60
+export const oneHour = oneMinute * 60
+export const oneDay = oneHour * 24
