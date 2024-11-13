874743822c248c45206da011586e6f51187165be
Keegan Irby <keegani@amazon.com>
2024-11-13 14:05:43 -0800

    Change convertToTimeString import

--- packages/core/src/awsService/cloudWatchLogs/registry/liveTailSession.ts
+++ packages/core/src/awsService/cloudWatchLogs/registry/liveTailSession.ts
@@ -11,9 +11,10 @@ import {
 } from '@aws-sdk/client-cloudwatch-logs'
 import { LogStreamFilterResponse } from '../wizard/liveTailLogStreamSubmenu'
 import { CloudWatchLogsSettings, uriToKey } from '../cloudWatchLogsUtils'
-import { convertToTimeString, getLogger, globals, Settings, ToolkitError } from '../../../shared'
+import { getLogger, globals, Settings, ToolkitError } from '../../../shared'
 import { createLiveTailURIFromArgs } from './liveTailSessionRegistry'
 import { getUserAgent } from '../../../shared/telemetry/util'
+import { convertToTimeString } from '../../../shared/datetime'
 
 export type LiveTailSessionConfiguration = {
     logGroupArn: string
