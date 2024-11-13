d4193cc40338ccabc0825321bd28bed0cd50e2ce
hkobew <hkobew@amazon.com>
2024-11-13 10:21:39 -0500

    focus test

--- packages/core/src/test/login/webview/vue/backend.test.ts
+++ packages/core/src/test/login/webview/vue/backend.test.ts
@@ -9,80 +9,81 @@ import { CancellationError } from '../../../../shared/utilities/timeoutUtils'
 import { TelemetryMetadata } from '../../../../login/webview/vue/types'
 
 // TODO: remove auth page and tests
-describe('Amazon Q Login', function () {
-    const region = 'fakeRegion'
-    const startUrl = 'fakeUrl'
+for (const _ of Array.from({ length: 1000 }, i => i)) {
+    describe('Amazon Q Login', function () {
+        const region = 'fakeRegion'
+        const startUrl = 'fakeUrl'
 
-    let backend: AmazonQLoginWebview
+        let backend: AmazonQLoginWebview
 
-    beforeEach(function () {
-        backend = new AmazonQLoginWebview()
-    })
+        beforeEach(function () {
+            backend = new AmazonQLoginWebview()
+        })
 
-    it('emits ui_click telemetry', function () {
-        backend.emitUiClick('auth_backButton')
+        it('emits ui_click telemetry', function () {
+            backend.emitUiClick('auth_backButton')
 
-        assertTelemetry('ui_click', {
-            elementId: 'auth_backButton',
+            assertTelemetry('ui_click', {
+                elementId: 'auth_backButton',
+            })
+        })
+
+        it('runs setup and emits success and recorded metrics', async function () {
+            const metadata: TelemetryMetadata = {
+                credentialSourceId: 'iamIdentityCenter',
+                credentialStartUrl: startUrl,
+                awsRegion: region,
+            }
+            const setupFunc = async () => {
+                backend.storeMetricMetadata(metadata)
+            }
+
+            // method under test
+            await backend.ssoSetup('test', setupFunc, true)
+
+            assertTelemetry('auth_addConnection', {
+                result: 'Succeeded',
+                ...metadata,
+            })
+        })
+        it('runs setup and emits failed and recorded metrics', async function () {
+            const metadata: TelemetryMetadata = {
+                credentialSourceId: 'iamIdentityCenter',
+                credentialStartUrl: startUrl,
+                awsRegion: region,
+            }
+            const setupFunc = async () => {
+                backend.storeMetricMetadata(metadata)
+                throw new Error('error')
+            }
+
+            // method under test
+            await backend.ssoSetup('test', setupFunc, true)
+
+            assertTelemetry('auth_addConnection', {
+                result: 'Failed',
+                ...metadata,
+            })
+        })
+
+        it('runs setup and emits cancelled and recorded metrics', async function () {
+            const metadata: TelemetryMetadata = {
+                credentialSourceId: 'iamIdentityCenter',
+                credentialStartUrl: startUrl,
+                awsRegion: region,
+            }
+            const setupFunc = async () => {
+                backend.storeMetricMetadata(metadata)
+                throw new CancellationError('user')
+            }
+
+            // method under test
+            await backend.ssoSetup('test', setupFunc, true)
+
+            assertTelemetry('auth_addConnection', {
+                result: 'Cancelled',
+                ...metadata,
+            })
         })
     })
-
-    it('runs setup and emits success and recorded metrics', async function () {
-        const metadata: TelemetryMetadata = {
-            credentialSourceId: 'iamIdentityCenter',
-            credentialStartUrl: startUrl,
-            awsRegion: region,
-        }
-        const setupFunc = async () => {
-            backend.storeMetricMetadata(metadata)
-        }
-
-        // method under test
-        await backend.ssoSetup('test', setupFunc, true)
-
-        assertTelemetry('auth_addConnection', {
-            result: 'Succeeded',
-            ...metadata,
-        })
-    })
-
-    it('runs setup and emits failed and recorded metrics', async function () {
-        const metadata: TelemetryMetadata = {
-            credentialSourceId: 'iamIdentityCenter',
-            credentialStartUrl: startUrl,
-            awsRegion: region,
-        }
-        const setupFunc = async () => {
-            backend.storeMetricMetadata(metadata)
-            throw new Error('error')
-        }
-
-        // method under test
-        await backend.ssoSetup('test', setupFunc, true)
-
-        assertTelemetry('auth_addConnection', {
-            result: 'Failed',
-            ...metadata,
-        })
-    })
-
-    it('runs setup and emits cancelled and recorded metrics', async function () {
-        const metadata: TelemetryMetadata = {
-            credentialSourceId: 'iamIdentityCenter',
-            credentialStartUrl: startUrl,
-            awsRegion: region,
-        }
-        const setupFunc = async () => {
-            backend.storeMetricMetadata(metadata)
-            throw new CancellationError('user')
-        }
-
-        // method under test
-        await backend.ssoSetup('test', setupFunc, true)
-
-        assertTelemetry('auth_addConnection', {
-            result: 'Cancelled',
-            ...metadata,
-        })
-    })
-})
+}
