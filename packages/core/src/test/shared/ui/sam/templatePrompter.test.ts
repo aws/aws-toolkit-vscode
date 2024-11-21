4d9644d7f72ecb5ba472e067a1167e2086cf7d98
vicheey <vichym@amazon.com>
2024-11-21 16:56:07 -0500

    fix(lambda): correct prompter url button for sam deploy sync build commands (#6081)
    ## Problem
    We have been using components from sync.ts, where all prompters created
    for other command still use samSyncUrl.
    
    ## Solution
    Use correct url link for respective command in all prompter button. 
    ---
    
    
    License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/core/src/test/shared/ui/sam/templatePrompter.test.ts
+++ packages/core/src/test/shared/ui/sam/templatePrompter.test.ts
@@ -13,6 +13,7 @@ import { WatchedItem } from '../../../../shared/fs/watchedFiles'
 import * as SamUtilsModule from '../../../../shared/sam/utils'
 import { createTemplatePrompter } from '../../../../shared/ui/sam/templatePrompter'
 import { assertEqualPaths } from '../../../testUtil'
+import { samDeployUrl } from '../../../../shared/constants'
 
 describe('createTemplatePrompter', () => {
     let registry: CloudFormationTemplateRegistry
@@ -41,7 +42,7 @@ describe('createTemplatePrompter', () => {
         const workspaceFolder = vscode.workspace.workspaceFolders?.[0]
         assert.ok(workspaceFolder)
 
-        const prompter = createTemplatePrompter(registry, mementoRootKey)
+        const prompter = createTemplatePrompter(registry, mementoRootKey, samDeployUrl)
 
         assert.strictEqual(prompter.quickPick.items.length, 2)
         assertEqualPaths(prompter.quickPick.items[0].label, '/path/to/template1.yaml')
