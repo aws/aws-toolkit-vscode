8e72b6c701f8c8826dc337e584d7f89e61a5eb09
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2024-12-04 15:28:39 -0500

    feat(amazonq): setting toggle for using the amazonq language server (#6142)
    ## Problem
    We want to experiment with the amazon q language server
    
    ## Solution
    Set up the structure for the eventual amazon q language srever

---
+++ packages/amazonq/src/lsp/activation.ts
@@ -0,0 +1,12 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import vscode from 'vscode'
+
+export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
+    /**
+     * download install and run the language server
+     */
+}
