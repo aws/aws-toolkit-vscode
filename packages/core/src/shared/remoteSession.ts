4c38aab31ebf420c6423a61b953c607f53fa8b6d
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2024-12-05 10:24:14 -0500

    feat(ec2): dry run connection script to surface errors earlier. (#6037)
    ## Problem
    Follow up to
    https://github.com/aws/aws-toolkit-vscode/pull/6018#discussion_r1844284580
    
    ## Solution
    - Run `ssh` within the same env as it will be run on real connection. 
    - Log any resulting errors, and inform user where the process failed. 
    - Also part of this PR is moving some functions to `remoteSession.ts`
    that are general enough to be there.
    
    Error msg:
    <img width="1518" alt="Screenshot 2024-11-15 at 5 53 51 PM"
    src="https://github.com/user-attachments/assets/e8e56887-b792-43e3-9d94-73d0e5246766">
    
    
    ---
    
    <!--- REMINDER: Ensure that your PR meets the guidelines in
    CONTRIBUTING.md -->
    
    License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.
    
    ---------
    
    Co-authored-by: Justin M. Keyes <jmkeyes@amazon.com>

--- packages/core/src/shared/remoteSession.ts
+++ packages/core/src/shared/remoteSession.ts
@@ -77,7 +77,7 @@ interface DependencyPaths {
     readonly ssh: string
 }
 
-type EnvProvider = () => Promise<NodeJS.ProcessEnv>
+export type EnvProvider = () => Promise<NodeJS.ProcessEnv>
 
 export interface VscodeRemoteConnection {
     readonly sshPath: string
@@ -250,4 +250,24 @@ export async function getDeniedSsmActions(client: IamClient, roleArn: string): P
     })
 
     return deniedActions
+}
+
+/**
+ * Creates a new {@link ChildProcess} class bound to a specific remote environment. All instances of this
+ * derived class will have SSM session information injected as environment variables as-needed.
+ */
+export function createBoundProcess(envProvider: EnvProvider): typeof ChildProcess {
+    type Run = ChildProcess['run']
+    return class SessionBoundProcess extends ChildProcess {
+        public override async run(...args: Parameters<Run>): ReturnType<Run> {
+            const options = args[0]
+            const envVars = await envProvider()
+            const spawnOptions = {
+                ...options?.spawnOptions,
+                env: { ...envVars, ...options?.spawnOptions?.env },
+            }
+
+            return super.run({ ...options, spawnOptions })
+        }
+    }
 }
