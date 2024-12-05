5d6512f458cad105178671c0e76f59424fc71ba7
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2024-12-05 14:23:52 -0500

    feat(amazonq): Use codewhisperer language server for completions (#6158)
    ## Problem
    - vscode doesn't have the codewhisperer lsp for completion requests
    
    ## Solution
    - use the codewhisperer lsp when `"aws.experiments": {
        "amazonqLSP": true
      },`
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    
    License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/amazonq/src/lsp/activation.ts
+++ packages/amazonq/src/lsp/activation.ts
@@ -4,18 +4,13 @@
  */
 
 import vscode from 'vscode'
+import path from 'path'
 import { AmazonQLSPDownloader } from './download'
+import { startLanguageServer } from './client'
 
 export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
     const serverPath = ctx.asAbsolutePath('resources/qdeveloperserver')
     const clientPath = ctx.asAbsolutePath('resources/qdeveloperclient')
     await new AmazonQLSPDownloader(serverPath, clientPath).tryInstallLsp()
-
-    /**
-     * at this point the language server should be installed and available
-     * at serverPath and mynah ui should be available and serveable at
-     * clientPath
-     *
-     * TODO: actually hook up the language server
-     */
+    await startLanguageServer(ctx, path.join(serverPath, 'aws-lsp-codewhisperer.js'))
 }
---
+++ packages/amazonq/src/lsp/client.ts
@@ -0,0 +1,110 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import vscode, { env, version } from 'vscode'
+import * as nls from 'vscode-nls'
+import * as cp from 'child_process'
+import * as crypto from 'crypto'
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient'
+import { registerInlineCompletion } from '../inline/completion'
+import { AmazonQLspAuth, notificationTypes, writeEncryptionInit } from './auth'
+import { AuthUtil } from 'aws-core-vscode/codewhisperer'
+import { ConnectionMetadata } from '@aws/language-server-runtimes/protocol'
+import { registerChat } from '../chat/activation'
+
+const localize = nls.loadMessageBundle()
+
+export function startLanguageServer(extensionContext: vscode.ExtensionContext, serverPath: string) {
+    const toDispose = extensionContext.subscriptions
+
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+    const debugOptions = {
+        execArgv: [
+            '--nolazy',
+            '--preserve-symlinks',
+            '--stdio',
+            '--pre-init-encryption',
+            '--set-credentials-encryption-key',
+        ],
+    }
+
+    // If the extension is launch in debug mode the debug server options are use
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run: { module: serverPath, transport: TransportKind.ipc },
+        debug: { module: serverPath, transport: TransportKind.ipc, options: debugOptions },
+    }
+
+    const child = cp.spawn('node', [serverPath, ...debugOptions.execArgv])
+    writeEncryptionInit(child.stdin)
+
+    serverOptions = () => Promise.resolve(child)
+
+    const documentSelector = [{ scheme: 'file', language: '*' }]
+
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for json documents
+        documentSelector,
+        initializationOptions: {
+            aws: {
+                clientInfo: {
+                    name: env.appName,
+                    version: version,
+                    extension: {
+                        name: `AWS IDE Extensions for VSCode`, // TODO change this to C9/Amazon
+                        version: '0.0.1',
+                    },
+                    clientId: crypto.randomUUID(),
+                },
+                awsClientCapabilities: {
+                    window: {
+                        notifications: true,
+                    },
+                },
+            },
+            credentials: {
+                providesBearerToken: true,
+            },
+        },
+    }
+
+    const client = new LanguageClient(
+        'amazonq',
+        localize('amazonq.server.name', 'Amazon Q Language Server'),
+        serverOptions,
+        clientOptions
+    )
+
+    const disposable = client.start()
+    toDispose.push(disposable)
+
+    const auth = new AmazonQLspAuth(client)
+
+    return client.onReady().then(async () => {
+        await auth.init()
+        registerInlineCompletion(client)
+        registerChat(client)
+
+        // Request handler for when the server wants to know about the clients auth connnection
+        client.onRequest<ConnectionMetadata, Error>(notificationTypes.getConnectionMetadata.method, () => {
+            return {
+                sso: {
+                    startUrl: AuthUtil.instance.auth.startUrl,
+                },
+            }
+        })
+
+        toDispose.push(
+            AuthUtil.instance.auth.onDidChangeActiveConnection(async () => {
+                await auth.init()
+            }),
+            AuthUtil.instance.auth.onDidDeleteConnection(async () => {
+                client.sendNotification(notificationTypes.deleteBearerToken.method)
+            })
+        )
+    })
+}
