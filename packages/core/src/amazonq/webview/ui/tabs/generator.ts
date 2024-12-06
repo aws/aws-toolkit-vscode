f601d898737a2d8a808f8878c5372b8a3d7ba025
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2024-12-06 07:01:43 -0500

    feat(amazonq): Add acknowledgement button for disclaimer (#6151)
    ## Problem
    - disclaimer is too wordy
    
    ## Solution
    - go back to the old disclaimer and add an ackowledgement button

--- packages/core/src/amazonq/webview/ui/tabs/generator.ts
+++ packages/core/src/amazonq/webview/ui/tabs/generator.ts
@@ -47,7 +47,7 @@ export class TabDataGenerator {
         const tabData: MynahUIDataModel = {
             tabTitle: taskName ?? TabTypeDataMap[tabType].title,
             promptInputInfo:
-                'Amazon Q Developer uses generative AI. You may need to verify responses. See the [AWS Responsible AI Policy](https://aws.amazon.com/machine-learning/responsible-ai/policy/). Amazon Q Developer processes data across all US Regions. See [here](https://docs.aws.amazon.com/amazonq/latest/qdeveloper-ug/cross-region-inference.html) for more info. Amazon Q may retain chats to provide and maintain the service.',
+                'Amazon Q Developer uses generative AI. You may need to verify responses. See the [AWS Responsible AI Policy](https://aws.amazon.com/machine-learning/responsible-ai/policy/).',
             quickActionCommands: this.quickActionsGenerator.generateForTab(tabType),
             promptInputPlaceholder: TabTypeDataMap[tabType].placeholder,
             contextCommands: TabTypeDataMap[tabType].contextCommands,
