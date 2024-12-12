003941ecb93a023e22cc9b71a4b8d3375e87d592
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2024-12-12 13:39:39 -0500

    feat(ec2): enable EC2 experiment #6222
    ## Problem
    EC2 is currently hidden behind dev mode. 
    
    ## Solution
    Make it an experimental feature that customers can enable. 
    Also, remove some old experiments from settings page description.

--- packages/core/src/test/shared/settings.test.ts
+++ packages/core/src/test/shared/settings.test.ts
@@ -521,17 +521,17 @@ describe('Experiments', function () {
     // The `Experiments` class is basically an immutable form of `PromptSettings`
 
     it('returns false when the setting is missing', async function () {
-        assert.strictEqual(await sut.isExperimentEnabled('jsonResourceModification'), false)
+        assert.strictEqual(sut.isExperimentEnabled('jsonResourceModification'), false)
     })
 
     it('returns false for invalid types', async function () {
         await sut.update('jsonResourceModification', 'definitely a boolean' as unknown as boolean)
-        assert.strictEqual(await sut.isExperimentEnabled('jsonResourceModification'), false)
+        assert.strictEqual(sut.isExperimentEnabled('jsonResourceModification'), false)
     })
 
     it('returns true when the flag is set', async function () {
         await sut.update('jsonResourceModification', true)
-        assert.strictEqual(await sut.isExperimentEnabled('jsonResourceModification'), true)
+        assert.strictEqual(sut.isExperimentEnabled('jsonResourceModification'), true)
     })
 
     it('fires events from nested settings', async function () {
