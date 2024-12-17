0b2aed904f5e9a5278d8d6f46a14942fdb3d5b2d
Josh Pinkney <jpink@amazon.com>
2024-12-17 08:44:32 -0500

    test(core): merge all package test reports
    Problem:
    - CI test reporting only captures the last package's results
    - Running `npm run testE2E` only preserves toolkit's report.xml, losing other package results
    
    Solution:
    - Generate individual report.xml files per subproject
    - Consolidate all existing package reports into root .test-reports/report.xml

---
+++ scripts/mergeReports.ts
@@ -0,0 +1,55 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as fs from 'fs'
+import * as path from 'path'
+import * as xml2js from 'xml2js'
+
+/**
+ * Merge all of the packages/ test reports and test logs into a single directory
+ */
+async function mergeReports() {
+    const packagesDir = path.join(__dirname, '..', 'packages')
+
+    // Get all packages/* directories
+    const packageDirs = fs.readdirSync(packagesDir).map((dir) => path.join(packagesDir, dir))
+
+    // Find report.xml files in .test-reports subdirectories
+    const testReports = packageDirs
+        .map((dir) => path.join(dir, '.test-reports', 'report.xml'))
+        .filter((file) => fs.existsSync(file))
+
+    let mergedReport = {
+        testsuites: {
+            testsuite: [],
+        },
+    }
+
+    // Collect all test reports into a single merged test report object
+    for (const file of testReports) {
+        const content = fs.readFileSync(file)
+        const result: { testsuites: { testsuite: [] } } = await xml2js.parseStringPromise(content)
+        if (result.testsuites && result.testsuites.testsuite) {
+            mergedReport.testsuites.testsuite.push(...result.testsuites.testsuite)
+        }
+    }
+
+    const builder = new xml2js.Builder()
+    const xml = builder.buildObject(mergedReport)
+
+    /**
+     * Create the new test reports directory and write the test report
+     */
+    const reportsDir = path.join(__dirname, '..', '.test-reports')
+
+    // Create reports directory if it doesn't exist
+    if (!fs.existsSync(reportsDir)) {
+        fs.mkdirSync(reportsDir, { recursive: true })
+    }
+
+    fs.writeFileSync(path.join(reportsDir, 'report.xml'), xml)
+}
+
+mergeReports()
