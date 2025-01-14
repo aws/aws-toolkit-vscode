272f7532e92d9f65c084d5e91cd0a5de79b72ea3
nkomonen-amazon <nkomonen@amazon.com>
2025-01-14 16:27:27 -0500

    fix(sso): Sign in did not allow custom start url
    Problem:
    
    A user reported that a non-standard start url is technically
    valid. This is because it can redirect to the underlying valid
    start url that matches the pattern: https://xxxxxxxx.awsapps.com/start
    
    Solution:
    
    Allow any URL, but warn users if they are using a non-standard one.
    We will show a yellow warning message in this case.
    The red error message is still shown when the input does not match a
    URL in general.
    
    Signed-off-by: nkomonen-amazon <nkomonen@amazon.com>

--- packages/core/src/auth/sso/constants.ts
+++ packages/core/src/auth/sso/constants.ts
@@ -11,8 +11,15 @@
 export const builderIdStartUrl = 'https://view.awsapps.com/start'
 export const internalStartUrl = 'https://amzn.awsapps.com/start'
 
+/**
+ * Doc: https://docs.aws.amazon.com/singlesignon/latest/userguide/howtochangeURL.html
+ */
 export const ssoUrlFormatRegex =
     /^(https?:\/\/(.+)\.awsapps\.com\/start|https?:\/\/identitycenter\.amazonaws\.com\/ssoins-[\da-zA-Z]{16})\/?$/
 
+/**
+ * It is possible for a start url to be a completely custom url that redirects to something that matches the format
+ * below, so this message is only a warning.
+ */
 export const ssoUrlFormatMessage =
-    'URLs must start with http:// or https://. Example: https://d-xxxxxxxxxx.awsapps.com/start'
+    'URL possibly invalid. It typically follows the pattern: https://xxxxxxxxxx.awsapps.com/start'
--- packages/core/src/login/webview/vue/login.vue
+++ packages/core/src/login/webview/vue/login.vue
@@ -193,6 +193,7 @@
                     @keydown.enter="handleContinueClick()"
                 />
                 <h4 class="start-url-error">{{ startUrlError }}</h4>
+                <h4 class="start-url-warning">{{ startUrlWarning }}</h4>
                 <div class="title topMargin">Region</div>
                 <div class="hint">AWS Region that hosts identity directory</div>
                 <select
@@ -340,6 +341,7 @@ export default defineComponent({
             stage: 'START' as Stage,
             regions: [] as Region[],
             startUrlError: '',
+            startUrlWarning: '',
             selectedRegion: 'us-east-1',
             startUrl: '',
             app: this.app,
@@ -365,7 +367,7 @@ export default defineComponent({
 
         // Pre-select the first available login option
         await this.preselectLoginOption()
-        this.handleUrlInput() // validate the default startUrl
+        await this.handleUrlInput() // validate the default startUrl
     },
     methods: {
         toggleItemSelection(itemId: number) {
@@ -475,17 +477,47 @@ export default defineComponent({
                 this.stage = 'CONNECTED'
             }
         },
-        handleUrlInput() {
-            if (this.startUrl && !ssoUrlFormatRegex.test(this.startUrl)) {
-                this.startUrlError = ssoUrlFormatMessage
-            } else if (this.startUrl && this.existingStartUrls.some((url) => url === this.startUrl)) {
-                this.startUrlError =
-                    'A connection for this start URL already exists. Sign out before creating a new one.'
-            } else {
-                this.startUrlError = ''
+        async handleUrlInput() {
+            const messages = await resolveStartUrlMessages(this.startUrl, this.existingStartUrls)
+            this.startUrlError = messages.error
+            this.startUrlWarning = messages.warning
+
+            if (!messages.error && !messages.warning) {
                 void client.storeMetricMetadata({
                     credentialStartUrl: this.startUrl,
                 })
+            }
+
+            async function resolveStartUrlMessages(
+                startUrl: string | undefined,
+                existingStartUrls: string[]
+            ): Promise<{ warning: string; error: string }> {
+                // No URL
+                if (!startUrl) {
+                    return { error: '', warning: '' }
+                }
+
+                // Validate URL format
+                if (!ssoUrlFormatRegex.test(startUrl)) {
+                    console.log('Before Validate')
+                    if (await client.validateUrl(startUrl)) {
+                        console.log('After Validate')
+                        return { error: '', warning: ssoUrlFormatMessage }
+                    } else {
+                        return { error: 'URL format invalid. Must follow eg: https://xxxxx.com/yyyy', warning: '' }
+                    }
+                }
+
+                // Ensure that URL does not exist yet
+                if (existingStartUrls.some((url) => url === startUrl)) {
+                    return {
+                        error: 'A connection for this start URL already exists. Sign out before creating a new one.',
+                        warning: '',
+                    }
+                }
+
+                // URL is valid
+                return { error: '', warning: '' }
             }
         },
         handleRegionInput(event: any) {
@@ -741,6 +773,10 @@ body.vscode-high-contrast:not(body.vscode-high-contrast-light) .regionSelect {
 }
 .start-url-error {
     color: #ff0000;
+    font-size: var(--font-size-sm);
+}
+.start-url-warning {
+    color: #dfe216;
     font-size: var(--font-size-sm);
 }
 #logo {
