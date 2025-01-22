f0d05215eac2fffe922dca689077fc651c96c370
Zhuowen Chen <siakmun@amazon.com>
2025-01-22 11:07:44 -0800

    startCodeGeneration change

--- packages/core/src/amazonqDoc/session/sessionState.ts
+++ packages/core/src/amazonqDoc/session/sessionState.ts
@@ -92,14 +92,14 @@ export class DocCodeGenState extends BaseCodeGenState {
     }
 
     protected async startCodeGeneration(action: SessionStateAction, codeGenerationId: string): Promise<void> {
-         if (!action.tokenSource?.token.isCancellationRequested) {
-        ;(action.messenger as DocMessenger).sendDocProgress(
-            this.tabID,
-            DocGenerationStep.SUMMARIZING_FILES,
-            0,
-            action.mode as Mode
-        )
-     }
+        if (!action.tokenSource?.token.isCancellationRequested) {
+            ;(action.messenger as DocMessenger).sendDocProgress(
+                this.tabID,
+                DocGenerationStep.SUMMARIZING_FILES,
+                0,
+                action.mode as Mode
+            )
+        }
 
         await this.config.proxyClient.startCodeGeneration(
             this.config.conversationId,
