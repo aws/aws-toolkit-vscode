604d2e1717e57f9884789c3565e0f2d3fc5ff572
hkobew <hkobew@amazon.com>
2025-01-22 10:28:09 -0500

    increase test coverage for cases when < 2 versions

--- packages/core/src/test/amazonq/lsp/workspaceinstaller.test.ts
+++ packages/core/src/test/amazonq/lsp/workspaceinstaller.test.ts
@@ -16,26 +16,33 @@ async function fakeInstallVersion(version: string, installationDir: string): Pro
     await fs.writeFile(path.join(versionDir, 'file.txt'), 'content')
 }
 
+async function fakeInstallVersions(versions: string[], installationDir: string): Promise<void> {
+    for (const v of versions) {
+        await fakeInstallVersion(v, installationDir)
+    }
+}
+
 describe('workspaceInstaller', function () {
     describe('cleanUp', function () {
         let installationDir: Uri
-        let lspVersions: string[]
 
         before(async function () {
             installationDir = (await createTestWorkspaceFolder()).uri
-            lspVersions = ['1.0.0', '1.0.1', '1.1.1', '2.1.1']
         })
 
-        beforeEach(async function () {
-            for (const v of lspVersions) {
-                await fakeInstallVersion(v, installationDir.fsPath)
+        afterEach(async function () {
+            const files = await fs.readdir(installationDir.fsPath)
+            for (const [name, _type] of files) {
+                await fs.delete(path.join(installationDir.fsPath, name), { force: true, recursive: true })
             }
         })
 
         after(async function () {
             await fs.delete(installationDir, { force: true, recursive: true })
         })
+
         it('keeps two newest versions', async function () {
+            await fakeInstallVersions(['1.0.0', '1.0.1', '1.1.1', '2.1.1'], installationDir.fsPath)
             const wsr = new WorkspaceLSPResolver()
             await wsr.cleanUp([], installationDir.fsPath)
 
@@ -46,6 +53,7 @@ describe('workspaceInstaller', function () {
         })
 
         it('deletes delisted versions', async function () {
+            await fakeInstallVersions(['1.0.0', '1.0.1', '1.1.1', '2.1.1'], installationDir.fsPath)
             const wsr = new WorkspaceLSPResolver()
             await wsr.cleanUp([{ serverVersion: '1.1.1', isDelisted: true, targets: [] }], installationDir.fsPath)
 
@@ -56,6 +64,7 @@ describe('workspaceInstaller', function () {
         })
 
         it('handles case where less than 2 versions are not delisted', async function () {
+            await fakeInstallVersions(['1.0.0', '1.0.1', '1.1.1', '2.1.1'], installationDir.fsPath)
             const wsr = new WorkspaceLSPResolver()
             await wsr.cleanUp(
                 [
@@ -69,6 +78,24 @@ describe('workspaceInstaller', function () {
             const result = (await fs.readdir(installationDir.fsPath)).map(([filename, _filetype], _index) => filename)
             assert.strictEqual(result.length, 1)
             assert.ok(result.includes('1.0.1'))
+        })
+
+        it('handles case where less than 2 versions exist', async function () {
+            await fakeInstallVersions(['1.0.0'], installationDir.fsPath)
+            const wsr = new WorkspaceLSPResolver()
+            await wsr.cleanUp([], installationDir.fsPath)
+
+            const result = (await fs.readdir(installationDir.fsPath)).map(([filename, _filetype], _index) => filename)
+            assert.strictEqual(result.length, 1)
+        })
+
+        it('does not install delisted version when no other option exists', async function () {
+            await fakeInstallVersions(['1.0.0'], installationDir.fsPath)
+            const wsr = new WorkspaceLSPResolver()
+            await wsr.cleanUp([{ serverVersion: '1.0.0', isDelisted: true, targets: [] }], installationDir.fsPath)
+
+            const result = (await fs.readdir(installationDir.fsPath)).map(([filename, _filetype], _index) => filename)
+            assert.strictEqual(result.length, 0)
         })
     })
 })
