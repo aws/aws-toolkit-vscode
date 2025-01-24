45901e7dbda1dddb29b352e7690eb839fee61711
hkobew <hkobew@amazon.com>
2025-01-24 17:53:53 -0500

    test: add tests for lsp resolver (WIP)

---
+++ packages/core/src/test/shared/lsp/lspResolver.test.ts
@@ -0,0 +1,9 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from 'assert'
+import sinon from 'sinon'
+
+describe('lspResolver', function () {})
