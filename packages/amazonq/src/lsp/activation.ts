5284763155cfe49f1a1561166bee08938f0bb5d7
hkobew <hkobew@amazon.com>
2025-01-24 13:18:48 -0500

    telemtry: implement telemetry for amazonQLSP

--- packages/amazonq/src/lsp/activation.ts
+++ packages/amazonq/src/lsp/activation.ts
@@ -7,11 +7,14 @@ import vscode from 'vscode'
 import { startLanguageServer } from './client'
 import { AmazonQLSPResolver } from './lspInstaller'
 import { ToolkitError } from 'aws-core-vscode/shared'
+import { lspSetupStage } from 'aws-core-vscode/amazonq'
 
 export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
     try {
-        const installResult = await new AmazonQLSPResolver().resolve()
-        await startLanguageServer(ctx, installResult.resourcePaths)
+        await lspSetupStage('all', async () => {
+            const installResult = await new AmazonQLSPResolver().resolve()
+            await lspSetupStage('launch', async () => await startLanguageServer(ctx, installResult.resourcePaths))
+        })
     } catch (err) {
         const e = err as ToolkitError
         void vscode.window.showInformationMessage(`Unable to launch amazonq language server: ${e.message}`)
