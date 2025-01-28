b4b7d1716fb307ba4311fb060448c4588332759d
Josh Pinkney <jpink@amazon.com>
2025-01-28 12:23:09 -0500

    fix(amazonq): Register invokeInlineCompletion and rejectCodeSuggestion with lsp implementation

--- packages/amazonq/src/lsp/activation.ts
+++ packages/amazonq/src/lsp/activation.ts
@@ -6,12 +6,20 @@
 import vscode from 'vscode'
 import { startLanguageServer } from './client'
 import { AmazonQLSPResolver } from './lspInstaller'
-import { ToolkitError } from 'aws-core-vscode/shared'
+import { Commands, ToolkitError } from 'aws-core-vscode/shared'
 
 export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
     try {
         const installResult = await new AmazonQLSPResolver().resolve()
         await startLanguageServer(ctx, installResult.resourcePaths)
+        ctx.subscriptions.push(
+            Commands.register({ id: 'aws.amazonq.invokeInlineCompletion', autoconnect: true }, async () => {
+                await vscode.commands.executeCommand('editor.action.inlineSuggest.trigger')
+            }),
+            Commands.declare('aws.amazonq.rejectCodeSuggestion', () => async () => {
+                await vscode.commands.executeCommand('editor.action.inlineSuggest.hide')
+            }).register()
+        )
     } catch (err) {
         const e = err as ToolkitError
         void vscode.window.showInformationMessage(`Unable to launch amazonq language server: ${e.message}`)
