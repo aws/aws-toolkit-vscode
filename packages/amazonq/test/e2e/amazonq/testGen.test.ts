76b167fe87736514e220bae0477a2babb95bed3f
Jacob Chung <chungjac@amazon.com>
2025-02-04 11:18:39 -0800

    add waitUntil in beforeEach for valid languages

--- packages/amazonq/test/e2e/amazonq/testGen.test.ts
+++ packages/amazonq/test/e2e/amazonq/testGen.test.ts
@@ -163,7 +163,7 @@ describe('Amazon Q Test Generation', function () {
         for (const { language, filePath } of testFiles) {
             describe(`${language} file`, () => {
                 beforeEach(async () => {
-                    await setupTestDocument(filePath, language)
+                    await waitUntil(async () => setupTestDocument(filePath, language), {})
 
                     tab.addChatMessage({ command: '/test' })
                     await tab.waitForChatFinishesLoading()
