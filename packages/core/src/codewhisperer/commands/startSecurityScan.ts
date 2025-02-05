69db397da4324b97b758af724553a72d5032a961
Tai Lai <ctlai95@gmail.com>
2025-02-05 05:11:31 -0800

    fix(amazonq): auto-review doesn't populate treeview (#6502)
    ## Problem
    
    Issues detected by auto-review feature are not populating the code
    issues treeview list unless you run a manual `/review` first.
    
    <img width="1594" alt="Screenshot 2025-02-04 at 3 00 08 PM"
    src="https://github.com/user-attachments/assets/4b817fad-a17e-4ce6-8374-fb81634f18b1"
    />
    
    This issue is happening because `vscode.window.createTreeView` is hidden
    inside `SecurityIssuesTree` class which only gets invoked when you call
    `SecurityIssuesTree.instance.focus()`. Since we don't want to focus the
    tree view for auto-reviews, the treeview itself is never created.
    
    ## Solution
    
    Move the treeview creation to activation so that it always gets created
    when the extension is activated
    
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/core/src/codewhisperer/commands/startSecurityScan.ts
+++ packages/core/src/codewhisperer/commands/startSecurityScan.ts
@@ -47,7 +47,7 @@ import {
     MaximumProjectScanReachedError,
     SecurityScanError,
 } from '../models/errors'
-import { SecurityIssuesTree } from '../service/securityIssueTreeViewProvider'
+import { SecurityIssueTreeViewProvider } from '../service/securityIssueTreeViewProvider'
 import { ChatSessionManager } from '../../amazonqScan/chat/storages/chatSession'
 import { TelemetryHelper } from '../util/telemetryHelper'
 
@@ -385,13 +385,6 @@ export function showSecurityScanResults(
     totalIssues: number
 ) {
     initSecurityScanRender(securityRecommendationCollection, context, editor, scope)
-    if (
-        totalIssues > 0 &&
-        (scope === CodeWhispererConstants.CodeAnalysisScope.PROJECT ||
-            scope === CodeWhispererConstants.CodeAnalysisScope.FILE_ON_DEMAND)
-    ) {
-        SecurityIssuesTree.instance.focus()
-    }
 
     if (scope === CodeWhispererConstants.CodeAnalysisScope.PROJECT) {
         populateCodeScanLogStream(zipMetadata.scannedFiles)
@@ -431,7 +424,7 @@ export function showScanResultsInChat(
 
     initSecurityScanRender(securityRecommendationCollection, context, editor, scope)
     if (totalIssues > 0) {
-        SecurityIssuesTree.instance.focus()
+        SecurityIssueTreeViewProvider.focus()
     }
 
     populateCodeScanLogStream(zipMetadata.scannedFiles)
@@ -531,7 +524,7 @@ function showScanCompletedNotification(total: number, scannedFiles: Set<string>)
     const items = [CodeWhispererConstants.showScannedFilesMessage]
     void vscode.window.showInformationMessage(`Code Review Completed`, ...items).then((value) => {
         if (total > 0 && value === CodeWhispererConstants.showScannedFilesMessage) {
-            SecurityIssuesTree.instance.focus()
+            SecurityIssueTreeViewProvider.focus()
         }
     })
 }
