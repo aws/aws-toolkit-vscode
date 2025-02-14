b861bc95d6d82f1d0433dae93dfef1fb733416d3
chungjac <chungjac@amazon.com>
2025-02-13 17:20:20 -0800

    DefaultAmazonQAppInitContext

--- packages/amazonq/test/e2e/amazonq/testGen.test.ts
+++ packages/amazonq/test/e2e/amazonq/testGen.test.ts
@@ -8,7 +8,7 @@ import vscode from 'vscode'
 import { qTestingFramework } from './framework/framework'
 import sinon from 'sinon'
 import { Messenger } from './framework/messenger'
-import { FollowUpTypes } from 'aws-core-vscode/amazonq'
+import { DefaultAmazonQAppInitContext, FollowUpTypes } from 'aws-core-vscode/amazonq'
 import { registerAuthHook, using, TestFolder, closeAllEditors } from 'aws-core-vscode/test'
 import { loginToIdC } from './utils/setup'
 import { waitUntil, workspaceUtils } from 'aws-core-vscode/shared'
@@ -75,6 +75,10 @@ describe('Amazon Q Test Generation', function () {
     })
 
     beforeEach(async () => {
+        await waitUntil(
+            async () => DefaultAmazonQAppInitContext.instance.getAppsToWebViewMessagePublisher().setUiReady(),
+            {}
+        )
         registerAuthHook('amazonq-test-account')
         framework = new qTestingFramework('testgen', true, [])
         tab = framework.createTab()
