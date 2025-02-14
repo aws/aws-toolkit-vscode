70fc869d369160841a180a3cd4ce760a6dd8898e
Josh Pinkney <jpink@amazon.com>
2025-02-14 10:24:57 -0500

    fixup

--- packages/amazonq/test/e2e/amazonq/framework/framework.ts
+++ packages/amazonq/test/e2e/amazonq/framework/framework.ts
@@ -79,8 +79,6 @@ export class qTestingFramework {
                 await ui.messageReceiver(event)
             })
         )
-
-        DefaultAmazonQAppInitContext.instance.getAppsToWebViewMessagePublisher().setUiReady()
     }
 
     /**
