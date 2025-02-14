b8bec08ca877f6ae0a195cc4764f09bce9efc535
Josh Pinkney <jpink@amazon.com>
2025-02-14 11:56:11 -0500

    config(amazonq): Add override settings for workspace context lsp

--- packages/amazonq/src/lsp/config.ts
+++ packages/amazonq/src/lsp/config.ts
@@ -4,25 +4,19 @@
  */
 
 import { DevSettings, getServiceEnvVarConfig } from 'aws-core-vscode/shared'
+import { LspConfig } from 'aws-core-vscode/amazonq'
 
-export interface AmazonQLspConfig {
-    manifestUrl: string
-    supportedVersions: string
-    id: string
-    locationOverride?: string
-}
-
-export const defaultAmazonQLspConfig: AmazonQLspConfig = {
+export const defaultAmazonQLspConfig: LspConfig = {
     manifestUrl: 'https://aws-toolkit-language-servers.amazonaws.com/codewhisperer/0/manifest.json',
     supportedVersions: '^3.1.1',
     id: 'AmazonQ', // used for identification in global storage/local disk location. Do not change.
     locationOverride: undefined,
 }
 
-export function getAmazonQLspConfig(): AmazonQLspConfig {
+export function getAmazonQLspConfig(): LspConfig {
     return {
         ...defaultAmazonQLspConfig,
-        ...(DevSettings.instance.getServiceConfig('amazonqLsp', {}) as AmazonQLspConfig),
+        ...(DevSettings.instance.getServiceConfig('amazonqLsp', {}) as LspConfig),
         ...getServiceEnvVarConfig('amazonqLsp', Object.keys(defaultAmazonQLspConfig)),
     }
 }
--- packages/amazonq/test/unit/amazonq/lsp/config.test.ts
+++ packages/amazonq/test/unit/amazonq/lsp/config.test.ts
@@ -7,73 +7,104 @@ import assert from 'assert'
 import { DevSettings } from 'aws-core-vscode/shared'
 import sinon from 'sinon'
 import { defaultAmazonQLspConfig, getAmazonQLspConfig } from '../../../../src/lsp/config'
+import { LspConfig, getAmazonQWorkspaceLspConfig, defaultAmazonQWorkspaceLspConfig } from 'aws-core-vscode/amazonq'
 
-describe('getAmazonQLspConfig', () => {
-    let sandbox: sinon.SinonSandbox
-    let serviceConfigStub: sinon.SinonStub
-    const settingConfig = {
-        manifestUrl: 'https://custom.url/manifest.json',
-        supportedVersions: '4.0.0',
-        id: 'AmazonQSetting',
-        locationOverride: '/custom/path',
-    }
-
-    beforeEach(() => {
-        sandbox = sinon.createSandbox()
-
-        serviceConfigStub = sandbox.stub()
-        sandbox.stub(DevSettings, 'instance').get(() => ({
-            getServiceConfig: serviceConfigStub,
-        }))
-    })
-
-    afterEach(() => {
-        sandbox.restore()
-    })
-
-    it('uses default config', () => {
-        serviceConfigStub.returns({})
-        const config = getAmazonQLspConfig()
-        assert.deepStrictEqual(config, defaultAmazonQLspConfig)
-    })
-
-    it('overrides location', () => {
-        const locationOverride = '/custom/path/to/lsp'
-        serviceConfigStub.returns({ locationOverride })
-
-        const config = getAmazonQLspConfig()
-        assert.deepStrictEqual(config, {
-            ...defaultAmazonQLspConfig,
-            locationOverride,
-        })
-    })
-
-    it('overrides default settings', () => {
-        serviceConfigStub.returns(settingConfig)
-
-        const config = getAmazonQLspConfig()
-        assert.deepStrictEqual(config, settingConfig)
-    })
-
-    it('environment variable takes precedence over settings', () => {
-        const envConfig = {
-            manifestUrl: 'https://another-custom.url/manifest.json',
-            supportedVersions: '5.1.1',
-            id: 'AmazonQEnv',
-            locationOverride: '/some/new/custom/path',
+for (const [name, config, defaultConfig, setEnv, resetEnv] of [
+    [
+        'getAmazonQLspConfig',
+        getAmazonQLspConfig,
+        defaultAmazonQLspConfig,
+        (envConfig: LspConfig) => {
+            process.env.__AMAZONQLSP_MANIFEST_URL = envConfig.manifestUrl
+            process.env.__AMAZONQLSP_SUPPORTED_VERSIONS = envConfig.supportedVersions
+            process.env.__AMAZONQLSP_ID = envConfig.id
+            process.env.__AMAZONQLSP_LOCATION_OVERRIDE = envConfig.locationOverride
+        },
+        () => {
+            delete process.env.__AMAZONQLSP_MANIFEST_URL
+            delete process.env.__AMAZONQLSP_SUPPORTED_VERSIONS
+            delete process.env.__AMAZONQLSP_ID
+            delete process.env.__AMAZONQLSP_LOCATION_OVERRIDE
+        },
+    ],
+    [
+        'getAmazonQWorkspaceLspConfig',
+        getAmazonQWorkspaceLspConfig,
+        defaultAmazonQWorkspaceLspConfig,
+        (envConfig: LspConfig) => {
+            process.env.__AMAZONQWORKSPACELSP_MANIFEST_URL = envConfig.manifestUrl
+            process.env.__AMAZONQWORKSPACELSP_SUPPORTED_VERSIONS = envConfig.supportedVersions
+            process.env.__AMAZONQWORKSPACELSP_ID = envConfig.id
+            process.env.__AMAZONQWORKSPACELSP_LOCATION_OVERRIDE = envConfig.locationOverride
+        },
+        () => {
+            delete process.env.__AMAZONQWORKSPACELSP_MANIFEST_URL
+            delete process.env.__AMAZONQWORKSPACELSP_SUPPORTED_VERSIONS
+            delete process.env.__AMAZONQWORKSPACELSP_ID
+            delete process.env.__AMAZONQWORKSPACELSP_LOCATION_OVERRIDE
+        },
+    ],
+] as const) {
+    describe(name, () => {
+        let sandbox: sinon.SinonSandbox
+        let serviceConfigStub: sinon.SinonStub
+        const settingConfig: LspConfig = {
+            manifestUrl: 'https://custom.url/manifest.json',
+            supportedVersions: '4.0.0',
+            id: 'AmazonQSetting',
+            locationOverride: '/custom/path',
         }
 
-        process.env.__AMAZONQLSP_MANIFEST_URL = envConfig.manifestUrl
-        process.env.__AMAZONQLSP_SUPPORTED_VERSIONS = envConfig.supportedVersions
-        process.env.__AMAZONQLSP_ID = envConfig.id
-        process.env.__AMAZONQLSP_LOCATION_OVERRIDE = envConfig.locationOverride
+        beforeEach(() => {
+            sandbox = sinon.createSandbox()
 
-        serviceConfigStub.returns(settingConfig)
+            serviceConfigStub = sandbox.stub()
+            sandbox.stub(DevSettings, 'instance').get(() => ({
+                getServiceConfig: serviceConfigStub,
+            }))
+        })
 
-        const config = getAmazonQLspConfig()
-        assert.deepStrictEqual(config, {
-            ...defaultAmazonQLspConfig,
-            ...envConfig,
+        afterEach(() => {
+            sandbox.restore()
+            resetEnv()
+        })
+
+        it('uses default config', () => {
+            serviceConfigStub.returns({})
+            assert.deepStrictEqual(config(), defaultConfig)
+        })
+
+        it('overrides location', () => {
+            const locationOverride = '/custom/path/to/lsp'
+            serviceConfigStub.returns({ locationOverride })
+
+            assert.deepStrictEqual(config(), {
+                ...defaultConfig,
+                locationOverride,
+            })
+        })
+
+        it('overrides default settings', () => {
+            serviceConfigStub.returns(settingConfig)
+
+            assert.deepStrictEqual(config(), settingConfig)
+        })
+
+        it('environment variable takes precedence over settings', () => {
+            const envConfig: LspConfig = {
+                manifestUrl: 'https://another-custom.url/manifest.json',
+                supportedVersions: '5.1.1',
+                id: 'AmazonQEnv',
+                locationOverride: '/some/new/custom/path',
+            }
+
+            setEnv(envConfig)
+            serviceConfigStub.returns(settingConfig)
+
+            assert.deepStrictEqual(config(), {
+                ...defaultAmazonQLspConfig,
+                ...envConfig,
+            })
         })
     })
-})
+}
