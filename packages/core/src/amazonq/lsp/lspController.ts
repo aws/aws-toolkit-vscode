6ef5f35ce1020928bd3212fd0dcbfe5365a09ae9
Lei Gao <leigaol@amazon.com>
2025-02-18 15:42:28 -0800

    update lsp version

--- packages/core/src/amazonq/lsp/lspController.ts
+++ packages/core/src/amazonq/lsp/lspController.ts
@@ -61,9 +61,9 @@ export interface Manifest {
         targets: Target[]
     }[]
 }
-const manifestUrl = 'https://ducvaeoffl85c.cloudfront.net/manifest-0.1.37.json'
+const manifestUrl = 'https://ducvaeoffl85c.cloudfront.net/manifest-0.1.38.json'
 // this LSP client in Q extension is only going to work with these LSP server versions
-const supportedLspServerVersions = ['0.1.37']
+const supportedLspServerVersions = ['0.1.38']
 
 const nodeBinName = process.platform === 'win32' ? 'node.exe' : 'node'
 
