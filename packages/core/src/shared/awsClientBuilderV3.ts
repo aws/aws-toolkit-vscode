739eede298213b1b812495eebef61961dfb171e8
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2025-02-25 14:52:18 -0500

    feat(sdkv3): enable persistent connections by forcing keep alive header.  (#6664)
    ## Problem
    Inspecting the logs, and http requests themselves, it appears that the
    sdkv3 clients are somehow adding a `connection: close` header to the
    http requests. This is not done by the SDK (according to SDK team), and
    inspecting the headers throughout the SDK packages at the recommended
    points suggests this is being overwritten somewhere else.
    
    Creating a new vscode extension with this code snippet also fails, but a
    new project without vscode succeeds. This suggests its VS Code.
    
    I believe this is caused by
    https://github.com/microsoft/vscode/issues/173861, which was resolved,
    but is now reopened. The comments and techdebt tests suggested this is
    no longer an issue, but it now is again due to fix being reverted.
    
    ## Solution
    - Remove outdated notes/test that suggest this is fixed in newer
    versions of VS Code.
    - add e2e test verifying expected `keepAlive` behavior. 
    - Add custom "build" step middleware that adds the keepAlive to the HTTP
    request, unless explicitly told not to.
    - Improve some typing in the middleware by making use of type
    assertions.
    
    ### Notes
    Related work done to completely overwrite the proxyAgent in vscode to
    fix this done here:
    https://github.com/aws/aws-toolkit-vscode-staging/pull/1214. However,
    this code is outdated and doesn't work anymore (its also unused). There
    is a functioning implementation of this logic at
    https://github.com/sourcegraph/cody/blob/62d73f78c432036d1f99bc9631ed534cc2ed846b/vscode/src/net/net.patch.ts.
    
    However, adding the `keepAlive` header manually appears to be working
    based on the e2e test. My understanding is that if the client was
    closing the connection in between each request, it must send a FIN
    message to the server to let it know according to TCP. The server would
    then close its side of the connection as well, but we know from the e2e
    test this isn't happening since it maintains the same connection across
    two requests. Therefore, the client must also be keeping its connection
    open, making use of persistent connections on both sides.
    
    ### Experimental Results
    Redoing the experiment of timing repeated requests when caching,
    not-caching, and caching with the change in this PR (connection
    persistence):
    <img width="485" alt="image"
    src="https://github.com/user-attachments/assets/786130ea-6e6c-4029-8e1d-df5cc89edb66"
    />
    Note: experiment separate into 3 trails of 10 requests to limit the
    effect of throttling.
    
    Therefore, this change is saving almost 40 ms per request!
    
    ## Future Work
    Delete the dead code at
    https://github.com/aws/aws-toolkit-vscode-staging/pull/1214.
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/core/src/shared/awsClientBuilderV3.ts
+++ packages/core/src/shared/awsClientBuilderV3.ts
@@ -5,7 +5,7 @@
 
 import { CredentialsShim } from '../auth/deprecated/loginManager'
 import { AwsContext } from './awsContext'
-import { AwsCredentialIdentityProvider, RetryStrategyV2 } from '@smithy/types'
+import { AwsCredentialIdentityProvider, Logger, RetryStrategyV2 } from '@smithy/types'
 import { getUserAgent } from './telemetry/util'
 import { DevSettings } from './settings'
 import {
@@ -64,6 +64,7 @@ interface AwsClientOptions {
     apiVersion: string
     endpoint: string
     retryStrategy: RetryStrategy | RetryStrategyV2
+    logger: Logger
 }
 
 export class AWSClientBuilderV3 {
@@ -82,6 +83,7 @@ export class AWSClientBuilderV3 {
         options?: Partial<AwsClientOptions>,
         region?: string,
         userAgent: boolean = true,
+        keepAlive: boolean = true,
         settings?: DevSettings
     ): Promise<C> {
         const shim = this.getShim()
@@ -112,6 +114,10 @@ export class AWSClientBuilderV3 {
         service.middlewareStack.add(telemetryMiddleware, { step: 'deserialize' })
         service.middlewareStack.add(loggingMiddleware, { step: 'finalizeRequest' })
         service.middlewareStack.add(getEndpointMiddleware(settings), { step: 'build' })
+
+        if (keepAlive) {
+            service.middlewareStack.add(keepAliveMiddleware, { step: 'build' })
+        }
         return service
     }
 }
@@ -154,6 +160,9 @@ function getEndpointMiddleware(settings: DevSettings = DevSettings.instance): Bu
         overwriteEndpoint(next, context, settings, args)
 }
 
+const keepAliveMiddleware: BuildMiddleware<any, any> = (next: BuildHandler<any, any>) => async (args: any) =>
+    addKeepAliveHeader(next, args)
+
 export async function emitOnRequest(next: DeserializeHandler<any, any>, context: HandlerExecutionContext, args: any) {
     if (!HttpResponse.isInstance(args.request)) {
         return next(args)
@@ -175,8 +184,9 @@ export async function emitOnRequest(next: DeserializeHandler<any, any>, context:
 }
 
 export async function logOnRequest(next: FinalizeHandler<any, any>, args: any) {
+    const request = args.request
     if (HttpRequest.isInstance(args.request)) {
-        const { hostname, path } = args.request
+        const { hostname, path } = request
         // TODO: omit credentials / sensitive info from the logs.
         const input = partialClone(args.input, 3)
         getLogger().debug(`API Request (%s %s): %O`, hostname, path, input)
@@ -190,14 +200,30 @@ export function overwriteEndpoint(
     settings: DevSettings,
     args: any
 ) {
-    if (HttpRequest.isInstance(args.request)) {
-        const serviceId = getServiceId(context as object)
+    const request = args.request
+    if (HttpRequest.isInstance(request)) {
+        const serviceId = getServiceId(context)
         const endpoint = serviceId ? settings.get('endpoints', {})[serviceId] : undefined
         if (endpoint) {
             const url = new URL(endpoint)
-            Object.assign(args.request, selectFrom(url, 'hostname', 'port', 'protocol', 'pathname'))
-            args.request.path = args.request.pathname
+            Object.assign(request, selectFrom(url, 'hostname', 'port', 'protocol', 'pathname'))
+            request.path = (request as typeof request & { pathname: string }).pathname
         }
+    }
+    return next(args)
+}
+
+/**
+ * Overwrite agents behavior and add the keepAliveHeader. This is needed due to
+ * https://github.com/microsoft/vscode/issues/173861.
+ * @param next
+ * @param args
+ * @returns
+ */
+export function addKeepAliveHeader(next: BuildHandler<any, any>, args: any) {
+    const request = args.request
+    if (HttpRequest.isInstance(request)) {
+        request.headers['Connection'] = 'keep-alive'
     }
     return next(args)
 }
---
+++ packages/core/src/testE2E/shared/awsClientBuilderV3.test.ts
@@ -0,0 +1,86 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import http from 'http'
+import assert from 'assert'
+import { SSMClient, DescribeSessionsCommand } from '@aws-sdk/client-ssm'
+import { globals } from '../../shared'
+import { Socket } from 'net'
+
+async function closeSocket(socket: Socket) {
+    return new Promise((resolve, reject) => {
+        socket.end(() => {
+            resolve(true)
+        })
+    })
+}
+
+describe('AWSClientBuilderV3', function () {
+    const port = 3000
+    let server: http.Server
+    let requests: http.IncomingMessage[]
+    let connections: Socket[]
+
+    before(function () {
+        server = http.createServer({ keepAlive: true }, (req, rsp) => {
+            rsp.writeHead(200, { 'Content-Type': 'application/json' })
+            rsp.end(JSON.stringify({ message: 'success' }))
+        })
+        server.listen(port, () => {})
+        server.on('request', (req) => {
+            requests.push(req)
+        })
+        server.on('connection', (connection) => {
+            connections.push(connection)
+        })
+        connections = []
+    })
+
+    beforeEach(async function () {
+        requests = []
+        await Promise.all(connections.map((c) => closeSocket(c)))
+        connections = []
+    })
+
+    after(function () {
+        server.close()
+    })
+
+    it('reuses existing HTTP connections by default', async function () {
+        const client = await globals.sdkClientBuilderV3.createAwsService(SSMClient, {
+            region: 'us-east-1',
+            endpoint: `http://localhost:${port}`,
+        })
+        assert.strictEqual(connections.length, 0)
+        await client.send(new DescribeSessionsCommand({ State: 'Active' }))
+        assert.strictEqual(connections.length, 1)
+        await client.send(new DescribeSessionsCommand({ State: 'Active' }))
+        assert.strictEqual(connections.length, 1)
+
+        assert.strictEqual(requests[0].headers.connection, 'keep-alive')
+        assert.strictEqual(requests[1].headers.connection, 'keep-alive')
+    })
+
+    it('does not reuse HTTP connections if told not to', async function () {
+        const client = await globals.sdkClientBuilderV3.createAwsService(
+            SSMClient,
+            {
+                region: 'us-east-1',
+                endpoint: `http://localhost:${port}`,
+            },
+            'us-east-1',
+            true,
+            false
+        )
+        assert.strictEqual(connections.length, 0, 'no connections before requesting')
+        await client.send(new DescribeSessionsCommand({ State: 'Active' }))
+        assert.strictEqual(connections.length, 1, 'one connection after first request')
+        await client.send(new DescribeSessionsCommand({ State: 'Active' }))
+        assert.strictEqual(connections.length, 2, 'two connections after both requests')
+
+        assert.strictEqual(requests[0].headers.connection, 'close')
+        assert.strictEqual(requests[1].headers.connection, 'close')
+    })
+})
