8a34c79228db3dae288e6d1723f70b421deed5d8
Tom Zu <138054255+tomcat323@users.noreply.github.com>
2025-03-12 12:06:40 -0400

    feat(amazonq): manifest deprecation message with optional don't show (#6604)
    ## Problem
    If the language server manifest is deprecated we just show it in the
    logs. However, users may not be explicitly aware that they will need to
    upgrade the extension to get a different manifest.
    
    Also according to the design specsheet, user should have the option to
    mute the message for good once seen.
    
    ## Solution
    
    - Added a message window with `don't show` and `ok` buttons for
    deprecated LSP manifest.
    - Use AmazonQ suppressed prompts for `don't show` tracking
    - Added a re-enable feature so that we can re-enable that setting if
    manifest gets updated.
    
    ## Note
    
    Also added language server name in logs, helpful to track different
    instances
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.
    
    ---------
    
    Co-authored-by: tomzu <tomzu@amazon.com>

--- packages/core/src/shared/lsp/manifestResolver.ts
+++ packages/core/src/shared/lsp/manifestResolver.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as vscode from 'vscode'
 import { getLogger } from '../logger/logger'
 import { ToolkitError } from '../errors'
 import { Timeout } from '../utilities/timeoutUtils'
@@ -10,6 +11,8 @@ import globals from '../extensionGlobals'
 import { Manifest } from './types'
 import { StageResolver, tryStageResolvers } from './utils/setupStage'
 import { HttpResourceFetcher } from '../resourcefetcher/httpResourceFetcher'
+import * as localizedText from '../localizedText'
+import { AmazonQPromptSettings, amazonQPrompts } from '../settings'
 
 const logger = getLogger('lsp')
 
@@ -60,27 +63,29 @@ export class ManifestResolver {
         }).getNewETagContent(this.getEtag())
 
         if (!resp.content) {
-            throw new ToolkitError('New content was not downloaded; fallback to the locally stored manifest')
+            throw new ToolkitError(
+                `New content was not downloaded; fallback to the locally stored "${this.lsName}" manifest`
+            )
         }
 
         const manifest = this.parseManifest(resp.content)
         await this.saveManifest(resp.eTag, resp.content)
-        this.checkDeprecation(manifest)
+        await this.checkDeprecation(manifest)
         manifest.location = 'remote'
         return manifest
     }
 
     private async getLocalManifest(): Promise<Manifest> {
-        logger.info('Failed to download latest LSP manifest. Falling back to local manifest.')
+        logger.info(`Failed to download latest "${this.lsName}" manifest. Falling back to local manifest.`)
         const storage = this.getStorage()
         const manifestData = storage[this.lsName]
 
         if (!manifestData?.content) {
-            throw new ToolkitError('Failed to download LSP manifest and no local manifest found.')
+            throw new ToolkitError(`Failed to download "${this.lsName}" manifest and no local manifest found.`)
         }
 
         const manifest = this.parseManifest(manifestData.content)
-        this.checkDeprecation(manifest)
+        await this.checkDeprecation(manifest)
         manifest.location = 'cache'
         return manifest
     }
@@ -90,14 +95,45 @@ export class ManifestResolver {
             return JSON.parse(content) as Manifest
         } catch (error) {
             throw new ToolkitError(
-                `Failed to parse manifest: ${error instanceof Error ? error.message : 'Unknown error'}`
+                `Failed to parse "${this.lsName}" manifest: ${error instanceof Error ? error.message : 'Unknown error'}`
             )
         }
     }
 
-    private checkDeprecation(manifest: Manifest): void {
-        if (manifest.isManifestDeprecated) {
-            logger.info('This LSP manifest is deprecated. No future updates will be available.')
+    /**
+     * Check if the current manifest is deprecated.
+     * If yes and user hasn't muted this notification, shows a toast message with two buttons:
+     * - OK: close and do nothing
+     * - Don't Show Again: Update suppressed prompt setting so the deprecation message is never shown for this manifest.
+     * @param manifest
+     */
+    private async checkDeprecation(manifest: Manifest): Promise<void> {
+        const prompts = AmazonQPromptSettings.instance
+        const lspId = `${this.lsName}LspManifestMessage` as keyof typeof amazonQPrompts
+
+        // Sanity check, if the lsName is changed then we also need to update the prompt keys in settings-amazonq.gen
+        if (!(lspId in amazonQPrompts)) {
+            logger.error(`LSP ID "${lspId}" not found in amazonQPrompts.`)
+            return
+        }
+
+        if (!manifest.isManifestDeprecated) {
+            // In case we got an new url, make sure the prompt is re-enabled for active manifests
+            await prompts.enablePrompt(lspId)
+            return
+        }
+
+        const deprecationMessage = `"${this.lsName}" manifest is deprecated. No future updates will be available.`
+        logger.info(deprecationMessage)
+
+        if (prompts.isPromptEnabled(lspId)) {
+            void vscode.window
+                .showInformationMessage(deprecationMessage, localizedText.ok, localizedText.dontShow)
+                .then(async (button) => {
+                    if (button === localizedText.dontShow) {
+                        await prompts.disablePrompt(lspId)
+                    }
+                })
         }
     }
 
