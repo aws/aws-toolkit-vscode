86df670c7e30a74640dad9e33ad0026861cd3474
Lei Gao <leigaol@amazon.com>
2025-03-25 10:59:30 -0700

    rc8

--- packages/amazonq/src/lsp/config.ts
+++ packages/amazonq/src/lsp/config.ts
@@ -8,7 +8,7 @@ import { LspConfig } from 'aws-core-vscode/amazonq'
 
 export const defaultAmazonQLspConfig: LspConfig = {
     manifestUrl: 'https://aws-language-servers-gamma.amazonaws.com/remoteWorkspaceContext/0/manifest.json',
-    supportedVersions: '0.1.2-rc.5',
+    supportedVersions: '0.1.2-rc.8',
     id: 'AmazonQ', // used for identification in global storage/local disk location. Do not change.
     path: undefined,
 }
