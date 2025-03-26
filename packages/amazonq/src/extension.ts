c7cb3c9f830b99ab4dfd21fa06fb4f3ca2de7fab
Josh Pinkney <jpink@amazon.com>
2025-03-26 12:04:43 -0400

    refactor(amazonq): expose lsp inline completion as an experiment

--- packages/amazonq/src/extension.ts
+++ packages/amazonq/src/extension.ts
@@ -120,7 +120,9 @@ export async function activateAmazonQCommon(context: vscode.ExtensionContext, is
     await activateCodeWhisperer(extContext as ExtContext)
     if (Experiments.instance.get('amazonqLSP', false)) {
         await activateAmazonqLsp(context)
-    } else {
+    }
+
+    if (!Experiments.instance.get('amazonqLSPInline', false)) {
         await activateInlineCompletion()
     }
 
@@ -157,7 +159,7 @@ export async function activateAmazonQCommon(context: vscode.ExtensionContext, is
 
     context.subscriptions.push(
         Experiments.instance.onDidChange(async (event) => {
-            if (event.key === 'amazonqLSP' || event.key === 'amazonqChatLSP') {
+            if (event.key === 'amazonqLSP' || event.key === 'amazonqChatLSP' || event.key === 'amazonqLSPInline') {
                 await vscode.window
                     .showInformationMessage(
                         'Amazon Q LSP setting has changed. Reload VS Code for the changes to take effect.',
