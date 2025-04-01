dc9b623d58f396a7f175f2beec14e31cda0dd715
Josh Pinkney <103940141+jpinkney-aws@users.noreply.github.com>
2025-04-01 08:54:18 -0400

    refactor(amazonq): expose lsp inline completion as an experiment (#6858)
    ## Problem
    - We previously thought inline completion was going to be launched first
    so we didn't end up putting it behind a feature flag
    
    ## Solution
    - Add inline completion behind a feature flag
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/amazonq/src/lsp/activation.ts
+++ packages/amazonq/src/lsp/activation.ts
@@ -6,7 +6,7 @@
 import vscode from 'vscode'
 import { startLanguageServer } from './client'
 import { AmazonQLspInstaller } from './lspInstaller'
-import { Commands, lspSetupStage, ToolkitError } from 'aws-core-vscode/shared'
+import { lspSetupStage, ToolkitError } from 'aws-core-vscode/shared'
 
 export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
     try {
@@ -14,14 +14,6 @@ export async function activate(ctx: vscode.ExtensionContext): Promise<void> {
             const installResult = await new AmazonQLspInstaller().resolve()
             await lspSetupStage('launch', async () => await startLanguageServer(ctx, installResult.resourcePaths))
         })
-        ctx.subscriptions.push(
-            Commands.register({ id: 'aws.amazonq.invokeInlineCompletion', autoconnect: true }, async () => {
-                await vscode.commands.executeCommand('editor.action.inlineSuggest.trigger')
-            }),
-            vscode.workspace.onDidCloseTextDocument(async () => {
-                await vscode.commands.executeCommand('aws.amazonq.rejectCodeSuggestion')
-            })
-        )
     } catch (err) {
         const e = err as ToolkitError
         void vscode.window.showInformationMessage(`Unable to launch amazonq language server: ${e.message}`)
