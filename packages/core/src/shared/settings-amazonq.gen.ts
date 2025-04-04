847cbffb91afd8da22e85c919496152d64c8af34
Lei Gao <leigaol@amazon.com>
2025-04-04 11:19:12 -0700

    remove settings

--- packages/core/src/shared/settings-amazonq.gen.ts
+++ packages/core/src/shared/settings-amazonq.gen.ts
@@ -26,7 +26,6 @@ export const amazonqSettings = {
     "amazonQ.allowFeatureDevelopmentToRunCodeAndTests": {},
     "amazonQ.importRecommendationForInlineCodeSuggestions": {},
     "amazonQ.shareContentWithAWS": {},
-    "amazonQ.workspaceContext": {},
     "amazonQ.workspaceIndex": {},
     "amazonQ.workspaceIndexWorkerThreads": {},
     "amazonQ.workspaceIndexUseGPU": {},
