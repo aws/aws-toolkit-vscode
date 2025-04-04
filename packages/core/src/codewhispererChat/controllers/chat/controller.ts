368f65a6751a96f64498c8221b8e97c091313a09
Tai Lai <tlaiamz@amazon.com>
2025-04-03 18:45:25 -0700

    fix(chat): request fails if character limit exceeded

--- packages/core/src/codewhispererChat/controllers/chat/controller.ts
+++ packages/core/src/codewhispererChat/controllers/chat/controller.ts
@@ -88,7 +88,7 @@ import {
 } from '../../constants'
 import { ChatSession } from '../../clients/chat/v0/chat'
 import { amazonQTabSuffix } from '../../../shared/constants'
-import { OutputKind } from '../../tools/toolShared'
+import { maxToolOutputCharacterLength, OutputKind } from '../../tools/toolShared'
 import { ToolUtils, Tool, ToolType } from '../../tools/toolUtils'
 import { ChatStream } from '../../tools/chatStream'
 import { ChatHistoryStorage } from '../../storages/chatHistoryStorage'
@@ -667,6 +667,11 @@ export class ChatController {
                             requiresAcceptance: false,
                         })
                         const output = await ToolUtils.invoke(tool, chatStream)
+                        if (output.output.content.length > maxToolOutputCharacterLength) {
+                            throw Error(
+                                `Tool output exceeds maximum character limit of ${maxToolOutputCharacterLength}`
+                            )
+                        }
 
                         toolResults.push({
                             content: [
