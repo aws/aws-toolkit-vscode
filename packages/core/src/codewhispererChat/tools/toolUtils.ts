2e8156fe892a757d3e89fe0e3018092ac1138047
Tai Lai <tlaiamz@amazon.com>
2025-04-05 11:59:15 -0700

    revert toolUtils change

--- packages/core/src/codewhispererChat/tools/toolUtils.ts
+++ packages/core/src/codewhispererChat/tools/toolUtils.ts
@@ -42,7 +42,7 @@ export class ToolUtils {
             case ToolType.FsRead:
                 return { requiresAcceptance: false }
             case ToolType.FsWrite:
-                return { requiresAcceptance: false }
+                return { requiresAcceptance: true }
             case ToolType.ExecuteBash:
                 return tool.tool.requiresAcceptance()
             case ToolType.ListDirectory:
