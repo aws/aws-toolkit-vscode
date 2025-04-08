43d3ca240a0054842b86657549a093ffe9c26ed8
Avi Alpert <aalpert@amazon.com>
2025-04-08 12:58:05 -0400

    feat(amazonq): Add @Code context for PHP, Ruby, Scala, Shell, and Swift projects

--- packages/core/src/codewhispererChat/controllers/chat/controller.ts
+++ packages/core/src/codewhispererChat/controllers/chat/controller.ts
@@ -577,13 +577,7 @@ export class ChatController {
                         id: contextCommandItem.id,
                         icon: 'folder' as MynahIconsType,
                     })
-                }
-                // TODO: Remove the limit of 25k once the performance issue of mynahUI in webview is fixed.
-                else if (
-                    contextCommandItem.symbol &&
-                    symbolsCmd.children &&
-                    symbolsCmd.children[0].commands.length < 25_000
-                ) {
+                } else if (contextCommandItem.symbol && symbolsCmd.children) {
                     symbolsCmd.children?.[0].commands.push({
                         command: contextCommandItem.symbol.name,
                         description: `${contextCommandItem.symbol.kind}, ${path.join(wsFolderName, contextCommandItem.relativePath)}, L${contextCommandItem.symbol.range.start.line}-${contextCommandItem.symbol.range.end.line}`,
