7ae18ea23b2b68c58ae2dc7b77b4a743e1bf87cb
opieter-aws <opieter@amazon.com>
2025-04-16 12:57:48 -0400

    Update packages/core/src/auth/index.ts
    Co-authored-by: Justin M. Keyes <jmkeyes@amazon.com>

--- packages/core/src/auth/index.ts
+++ packages/core/src/auth/index.ts
@@ -22,6 +22,6 @@ export {
 export { Auth } from './auth'
 export { CredentialsStore } from './credentials/store'
 export { LoginManager } from './deprecated/loginManager'
-export { builderIdStartUrl, builderIdRegion } from './sso/constants'
+export * as constants from './sso/constants'
 export * as authUtils from './utils'
 export * as auth2 from './auth2'
