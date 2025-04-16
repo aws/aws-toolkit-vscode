756c286c8728b5c5434971ecd58a96a1c56a992b
Josh Pinkney <jpink@amazon.com>
2025-04-16 19:59:46 -0400

    feat(amazonq): use qAgenticChatServer alpha manifest

--- packages/amazonq/src/lsp/config.ts
+++ packages/amazonq/src/lsp/config.ts
@@ -11,8 +11,8 @@ export interface ExtendedAmazonQLSPConfig extends LspConfig {
 }
 
 export const defaultAmazonQLspConfig: ExtendedAmazonQLSPConfig = {
-    manifestUrl: 'https://d3akiidp1wvqyg.cloudfront.net/codewhisperer/0/manifest.json',
-    supportedVersions: '^3.1.1',
+    manifestUrl: 'https://d3akiidp1wvqyg.cloudfront.net/qAgenticChatServer/0/manifest.json', // TODO swap this back
+    supportedVersions: '*', // TODO swap this back
     id: 'AmazonQ', // used across IDEs for identifying global storage/local disk locations. Do not change.
     suppressPromptPrefix: 'amazonQ',
     path: undefined,
