86c2f2a2cc09d60b53c5820f8e17b037755d5742
Lei Gao <97199248+leigaol@users.noreply.github.com>
2025-04-17 17:55:06 -0700

    fix(amazonq): no starting LSP in AL2 (#7096)
    ## Problem
    
    LSP cannot run in AL2 due to glibc compatibility.
    
    ## Solution
    
    Do not start LSP in AL2. 
    
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/amazonq/src/extension.ts
+++ packages/amazonq/src/extension.ts
@@ -43,6 +43,7 @@ import { registerCommands } from './commands'
 import { focusAmazonQPanel } from 'aws-core-vscode/codewhispererChat'
 import { activate as activateAmazonqLsp } from './lsp/activation'
 import { activate as activateInlineCompletion } from './app/inline/activation'
+import { isAmazonInternalOs } from 'aws-core-vscode/shared'
 
 export const amazonQContextPrefix = 'amazonq'
 
@@ -119,7 +120,10 @@ export async function activateAmazonQCommon(context: vscode.ExtensionContext, is
     }
     // This contains every lsp agnostic things (auth, security scan, code scan)
     await activateCodeWhisperer(extContext as ExtContext)
-    if (Experiments.instance.get('amazonqLSP', false) || Auth.instance.isInternalAmazonUser()) {
+    if (
+        (Experiments.instance.get('amazonqLSP', false) || Auth.instance.isInternalAmazonUser()) &&
+        !isAmazonInternalOs()
+    ) {
         // start the Amazon Q LSP for internal users first
         await activateAmazonqLsp(context)
     }
