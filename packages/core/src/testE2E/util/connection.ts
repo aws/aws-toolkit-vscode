edb38026b72c8437537e547cb928328543fd00b0
opieter-aws <opieter@amazon.com>
2025-04-21 12:22:05 -0400

    Remove obsolete testing function

--- packages/core/src/testE2E/util/connection.ts
+++ packages/core/src/testE2E/util/connection.ts
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AuthUtil } from '../../codewhisperer'
-
 /*
 In order to run codewhisperer or gumby integration tests user must:
    
@@ -14,10 +12,6 @@ In order to run codewhisperer or gumby integration tests user must:
 Test cases will skip if the above criteria are not met.
 If user has an expired connection they must reauthenticate prior to running tests.
 */
-
-export async function setValidConnection() {
-    return AuthUtil.instance.isConnected()
-}
 
 export function skipTestIfNoValidConn(validConnection: boolean, ctx: Mocha.Context) {
     if (!validConnection && ctx.currentTest) {
