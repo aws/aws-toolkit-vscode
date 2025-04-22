1b107ab6ef9b30c2860575fd3f8d62fa1ad3650d
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2025-04-22 08:55:23 -0400

    feat(amazonq): add handling for button clicks (#7125)
    ## Problem
    With https://github.com/aws/language-servers/pull/1037, the chat client
    will forward the button click event to the host client (VSC in this
    case). We are then responsible for making a request to the LSP with this
    button click.
    
    ## Solution
    - route the button click to the LSP. 
    - log any failed button click actions. 
    
    ## Verification
    See https://github.com/aws/language-servers/pull/1037, for a demo of the
    e2e button hookup working.
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/amazonq/src/lsp/chat/messages.ts
+++ packages/amazonq/src/lsp/chat/messages.ts
@@ -46,6 +46,8 @@ import {
     LINK_CLICK_NOTIFICATION_METHOD,
     LinkClickParams,
     INFO_LINK_CLICK_NOTIFICATION_METHOD,
+    buttonClickRequestType,
+    ButtonClickResult,
     CancellationTokenSource,
 } from '@aws/language-server-runtimes/protocol'
 import { v4 as uuidv4 } from 'uuid'
@@ -304,6 +306,18 @@ export function registerMessageListeners(
                     languageClient.sendNotification(followUpClickNotificationType.method, message.params)
                 }
                 break
+            case buttonClickRequestType.method: {
+                const buttonResult = await languageClient.sendRequest<ButtonClickResult>(
+                    buttonClickRequestType.method,
+                    message.params
+                )
+                if (!buttonResult.success) {
+                    languageClient.error(
+                        `[VSCode Client] Failed to execute action associated with button with reason: ${buttonResult.failureReason}`
+                    )
+                }
+                break
+            }
             default:
                 if (isServerEvent(message.command)) {
                     languageClient.sendNotification(message.command, message.params)
@@ -505,7 +519,6 @@ async function handleCompleteResult<T>(
 ) {
     const decryptedMessage =
         typeof result === 'string' && encryptionKey ? await decodeRequest(result, encryptionKey) : result
-
     void provider.webview?.postMessage({
         command: chatRequestType.method,
         params: decryptedMessage,
