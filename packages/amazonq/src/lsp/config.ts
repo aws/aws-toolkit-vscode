fd48864ee7e568431990a1e708e4ecb42849dcde
Josh Pinkney <jpink@amazon.com>
2025-04-24 20:02:34 -0400

    consume beta manifest

--- packages/amazonq/src/lsp/config.ts
+++ packages/amazonq/src/lsp/config.ts
@@ -11,7 +11,8 @@ export interface ExtendedAmazonQLSPConfig extends LspConfig {
 }
 
 export const defaultAmazonQLspConfig: ExtendedAmazonQLSPConfig = {
-    manifestUrl: 'https://d3akiidp1wvqyg.cloudfront.net/qAgenticChatServer/0/manifest.json', // TODO swap this back
+    manifestUrl:
+        'https://raw.githubusercontent.com/aws/aws-toolkit-vscode/refs/heads/agentic-chat-beta/qAgenticServerManifest.json', // TODO swap this back
     supportedVersions: '*', // TODO swap this back
     id: 'AmazonQ', // used across IDEs for identifying global storage/local disk locations. Do not change.
     suppressPromptPrefix: 'amazonQ',
