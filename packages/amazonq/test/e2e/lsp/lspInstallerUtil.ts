079bb52baf1598b62729fd2f21152ba15f56aacb
Josh Pinkney <jpink@amazon.com>
2025-05-08 22:59:50 -0400

    refactor(amazonq): refactor base lsp config

--- packages/amazonq/test/e2e/lsp/lspInstallerUtil.ts
+++ packages/amazonq/test/e2e/lsp/lspInstallerUtil.ts
@@ -18,7 +18,6 @@ import {
 } from 'aws-core-vscode/shared'
 import * as semver from 'semver'
 import { assertTelemetry } from 'aws-core-vscode/test'
-import { LspConfig } from 'aws-core-vscode/amazonq'
 import { LanguageServerSetup } from 'aws-core-vscode/telemetry'
 
 function createVersion(version: string, contents: TargetContent[]) {
@@ -44,8 +43,8 @@ export function createLspInstallerTests({
     resetEnv,
 }: {
     suiteName: string
-    lspConfig: LspConfig
-    createInstaller: (lspConfig?: LspConfig) => BaseLspInstaller.BaseLspInstaller
+    lspConfig: BaseLspInstaller.LspConfig
+    createInstaller: (lspConfig?: BaseLspInstaller.LspConfig) => BaseLspInstaller.BaseLspInstaller
     targetContents: TargetContent[]
     setEnv: (path: string) => void
     resetEnv: () => void
