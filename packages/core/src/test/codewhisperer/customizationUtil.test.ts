f3a449d8f977b380ea17e47fbc205855c8f36ac6
opieter-aws <opieter@amazon.com>
2025-05-20 12:03:11 -0400

    Fix linter

--- packages/core/src/test/codewhisperer/customizationUtil.test.ts
+++ packages/core/src/test/codewhisperer/customizationUtil.test.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import * as sinon from 'sinon'
 import * as assert from 'assert'
 import * as customizationModule from '../../../src/codewhisperer/util/customizationUtil'
