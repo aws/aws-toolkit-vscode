f51c22fea8ef69eefcd999a8e83c32948899a48c
Will Lo <xshaohua@amazon.com>
2025-06-06 06:34:30 -0700

    test

--- packages/amazonq/src/app/inline/completion.ts
+++ packages/amazonq/src/app/inline/completion.ts
@@ -219,6 +219,9 @@ export class AmazonQInlineCompletionItemProvider implements InlineCompletionItem
             // TODO: comment this out for now as it's slow, will take ~200ms each trigger, need to investigate more
             // tell the tutorial that completions has been triggered
             // await this.inlineTutorialAnnotation.triggered(context.triggerKind)
+            // TODO: remove this line as otherwise it wont compile
+            this.inlineTutorialAnnotation
+
             TelemetryHelper.instance.setInvokeSuggestionStartTime()
             TelemetryHelper.instance.setTriggerType(context.triggerKind)
 
