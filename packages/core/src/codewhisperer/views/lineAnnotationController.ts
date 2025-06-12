9bab14ce3b74db9028dcd05ed26ff7d11dccb292
Lei Gao <97199248+leigaol@users.noreply.github.com>
2025-06-12 15:34:25 -0700

    deps(amazonq): remove unused code part 2 for flare inline (#7483)
    ## Problem
    
    
    ## Solution
    
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/core/src/codewhisperer/views/lineAnnotationController.ts
+++ packages/core/src/codewhisperer/views/lineAnnotationController.ts
@@ -9,14 +9,11 @@ import { LineSelection, LinesChangeEvent } from '../tracker/lineTracker'
 import { isTextEditor } from '../../shared/utilities/editorUtilities'
 import { cancellableDebounce } from '../../shared/utilities/functionUtils'
 import { subscribeOnce } from '../../shared/utilities/vsCodeUtils'
-// import { RecommendationService } from '../service/recommendationService'
 import { AnnotationChangeSource, inlinehintKey } from '../models/constants'
 import globals from '../../shared/extensionGlobals'
 import { Container } from '../service/serviceContainer'
 import { telemetry } from '../../shared/telemetry/telemetry'
 import { getLogger } from '../../shared/logger/logger'
-// import { session } from '../util/codeWhispererSession'
-// import { RecommendationHandler } from '../service/recommendationHandler'
 import { runtimeLanguageContext } from '../util/runtimeLanguageContext'
 import { setContext } from '../../shared/vscode/setContext'
 
@@ -73,13 +70,6 @@ export class AutotriggerState implements AnnotationState {
     static acceptedCount = 0
 
     updateState(changeSource: AnnotationChangeSource, force: boolean): AnnotationState | undefined {
-        // if (AutotriggerState.acceptedCount < RecommendationService.instance.acceptedSuggestionCount) {
-        //     return new ManualtriggerState()
-        // } else if (session.recommendations.length > 0 && RecommendationHandler.instance.isSuggestionVisible()) {
-        //     return new PressTabState()
-        // } else {
-        //     return this
-        // }
         return undefined
     }
 
@@ -267,28 +257,6 @@ export class LineAnnotationController implements vscode.Disposable {
             subscribeOnce(this.container.lineTracker.onReady)(async (_) => {
                 await this.onReady()
             }),
-            // RecommendationService.instance.suggestionActionEvent(async (e) => {
-            //     await telemetry.withTraceId(async () => {
-            //         if (!this._isReady) {
-            //             return
-            //         }
-
-            //         if (this._currentState instanceof ManualtriggerState) {
-            //             if (e.triggerType === 'OnDemand' && this._currentState.hasManualTrigger === false) {
-            //                 this._currentState.hasManualTrigger = true
-            //             }
-            //             if (
-            //                 e.response?.recommendationCount !== undefined &&
-            //                 e.response?.recommendationCount > 0 &&
-            //                 this._currentState.hasValidResponse === false
-            //             ) {
-            //                 this._currentState.hasValidResponse = true
-            //             }
-            //         }
-
-            //         await this.refresh(e.editor, 'codewhisperer')
-            //     }, TelemetryHelper.instance.traceId)
-            // }),
             this.container.lineTracker.onDidChangeActiveLines(async (e) => {
                 await this.onActiveLinesChanged(e)
             }),
@@ -300,17 +268,6 @@ export class LineAnnotationController implements vscode.Disposable {
             this.container.auth.secondaryAuth.onDidChangeActiveConnection(async () => {
                 await this.refresh(vscode.window.activeTextEditor, 'editor')
             })
-            // Commands.register('aws.amazonq.dismissTutorial', async () => {
-            //     const editor = vscode.window.activeTextEditor
-            //     if (editor) {
-            //         this.clear()
-            //         try {
-            //             telemetry.ui_click.emit({ elementId: `dismiss_${this._currentState.id}` })
-            //         } catch (_) {}
-            //         await this.dismissTutorial()
-            //         getLogger().debug(`codewhisperer: user dismiss tutorial.`)
-            //     }
-            // })
         )
     }
 
