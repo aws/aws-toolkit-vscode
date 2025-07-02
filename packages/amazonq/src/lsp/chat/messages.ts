daa3ffe7bfa0b72c43544f0a92e4530fe390bffb
Nitish Kumar Singh <nkrsingh@amazon.com>
2025-07-01 22:11:25 -0700

    Updated explain code issue prompt

--- packages/amazonq/src/lsp/chat/messages.ts
+++ packages/amazonq/src/lsp/chat/messages.ts
@@ -286,17 +286,6 @@ export function registerMessageListeners(
                 if (editor) {
                     chatParams.cursorState = getCursorState(editor.selections)
                     chatParams.textDocument = { uri: editor.document.uri.toString() }
-                    // chatParams.findingsPath = path.join(
-                    //     __dirname,
-                    //     '..',
-                    //     '..',
-                    //     '..',
-                    //     '..',
-                    //     '..',
-                    //     '..',
-                    //     'findings',
-                    //     `SecurityIssues-${SecurityIssueProvider.instance.id}.json`
-                    // )
                 }
 
                 const chatRequest = await encryptRequest<ChatParams>(chatParams, encryptionKey)
