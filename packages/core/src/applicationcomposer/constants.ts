fbdefdebfd49c1f17c8948766c03fd52a9628290
Thomas Ruggeri <trug@amazon.com>
2025-07-08 15:38:55 -0700

    feat(appcomposer): Update CFN snippet source

---
+++ packages/core/src/applicationcomposer/constants.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const isLocalDev = false
+const localhost = 'http://127.0.0.1:3000'
+const cdn = 'https://ide-toolkits.app-composer.aws.dev'
+
+export { isLocalDev, localhost, cdn }
