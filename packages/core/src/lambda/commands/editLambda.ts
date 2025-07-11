aws/aws-toolkit-vscode

commit d81c0a68cd14c4ac1ef521d9f3823247ba39325e

feat(lambda): Merging Feature/lambda console2 ide to staging branch. (#7601)
## Notes:
- Releasing https://github.com/aws/aws-toolkit-vscode-staging/pull/2144


---

- Treat all work as PUBLIC. Private `feature/x` branches will not be
squash-merged at release time.
- Your code changes must meet the guidelines in
[CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
- License: I confirm that my contribution is made under the terms of the
Apache 2.0 license.

Co-authored-by: aws-toolkit-automation <43144436+aws-toolkit-automation@users.noreply.github.com>
Co-authored-by: Roger Zhang <roger.zhang.cs@gmail.com>
Co-authored-by: Reed Hamilton <49345456+rhamilt@users.noreply.github.com>
Co-authored-by: Jacob Chung <chungjac@amazon.com>

packages/core/src/lambda/commands/editLambda.ts
@@ -1,0 +1,244 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import * as vscode from 'vscode'
+import * as nls from 'vscode-nls'
+import { LambdaFunctionNode } from '../explorer/lambdaFunctionNode'
+import { downloadLambdaInLocation, openLambdaFile } from './downloadLambda'
+import { LambdaFunction, runUploadDirectory } from './uploadLambda'
+import {
+    compareCodeSha,
+    getFunctionInfo,
+    getLambdaDetails,
+    getTempLocation,
+    lambdaEdits,
+    lambdaTempPath,
+    setFunctionInfo,
+} from '../utils'
+import { showConfirmationMessage } from '../../shared/utilities/messages'
+import fs from '../../shared/fs/fs'
+import globals from '../../shared/extensionGlobals'
+import { LambdaFunctionNodeDecorationProvider } from '../explorer/lambdaFunctionNodeDecorationProvider'
+import path from 'path'
+import { telemetry } from '../../shared/telemetry/telemetry'
+import { ToolkitError } from '../../shared/errors'
+
+const localize = nls.loadMessageBundle()
+
+let lastPromptTime = Date.now() - 5000
+
+export function watchForUpdates(lambda: LambdaFunction, projectUri: vscode.Uri): void {
+    const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(projectUri, '*'))
+    const startTime = globals.clock.Date.now()
+
+    watcher.onDidChange(async (fileUri) => {
+        await promptForSync(lambda, projectUri, fileUri)
+    })
+
+    watcher.onDidCreate(async (fileUri) => {
+        // When the code is downloaded and the watcher is set, this will immediately trigger the onDidCreate
+        // To avoid this, we must check that the file was actually created AFTER the watcher was created
+        if ((await fs.stat(fileUri.fsPath)).ctime < startTime) {
+            return
+        }
+        await promptForSync(lambda, projectUri, fileUri)
+    })
+
+    watcher.onDidDelete(async (fileUri) => {
+        // We don't want to sync if the whole directory has been deleted
+        if (fileUri.fsPath !== projectUri.fsPath) {
+            await promptForSync(lambda, projectUri, fileUri)
+        }
+    })
+}
+
+// Creating this function for testing, can't mock the vscode.window in the tests
+export async function promptDeploy() {
+    const confirmItem = localize('AWS.lambda.upload.sync', 'Deploy')
+    const cancelItem = localize('AWS.lambda.upload.noSync', 'No, thanks')
+    const response = await vscode.window.showInformationMessage(
+        localize('AWS.lambda.upload.confirmSync', 'Would you like to deploy these changes to the cloud?'),
+        confirmItem,
+        cancelItem
+    )
+    return response === confirmItem
+}
+
+export async function promptForSync(lambda: LambdaFunction, projectUri: vscode.Uri, fileUri: vscode.Uri) {
+    if (!(await fs.existsDir(projectUri.fsPath)) || globals.clock.Date.now() - lastPromptTime < 5000) {
+        return
+    }
+
+    await setFunctionInfo(lambda, {
+        undeployed: true,
+    })
+
+    await LambdaFunctionNodeDecorationProvider.getInstance().addBadge(
+        fileUri,
+        vscode.Uri.from({ scheme: 'lambda', path: `${lambda.region}/${lambda.name}` })
+    )
+
+    lastPromptTime = globals.clock.Date.now()
+    if (await promptDeploy()) {
+        await deployFromTemp(lambda, projectUri)
+    }
+}
+
+async function confirmOutdatedChanges(prompt: string): Promise<boolean> {
+    return await showConfirmationMessage({
+        prompt,
+        confirm: localize('AWS.lambda.upload.overwrite', 'Overwrite'),
+        cancel: localize('AWS.lambda.upload.noOverwrite', 'Cancel'),
+    })
+}
+
+export async function deployFromTemp(lambda: LambdaFunction, projectUri: vscode.Uri) {
+    return telemetry.lambda_quickDeploy.run(async () => {
+        const prompt = localize(
+            'AWS.lambda.upload.confirmOutdatedSync',
+            'There are changes to your Function in the cloud after you created this local copy, overwrite anyway?'
+        )
+
+        const isShaDifferent = !(await compareCodeSha(lambda))
+        const overwriteChanges = isShaDifferent ? await confirmOutdatedChanges(prompt) : true
+
+        if (overwriteChanges) {
+            // Reset the lastPrompt time because we don't want to retrigger the watcher flow
+            lastPromptTime = globals.clock.Date.now()
+            await vscode.workspace.saveAll()
+            try {
+                await runUploadDirectory(lambda, 'zip', projectUri)
+            } catch {
+                throw new ToolkitError('Failed to deploy Lambda function', { code: 'deployFailure' })
+            }
+            await setFunctionInfo(lambda, {
+                lastDeployed: globals.clock.Date.now(),
+                undeployed: false,
+            })
+            await LambdaFunctionNodeDecorationProvider.getInstance().removeBadge(
+                projectUri,
+                vscode.Uri.from({ scheme: 'lambda', path: `${lambda.region}/${lambda.name}` })
+            )
+            if (isShaDifferent) {
+                telemetry.record({ action: 'overwriteChanges' })
+            }
+        } else {
+            telemetry.record({ action: 'cancelOverwrite' })
+        }
+    })
+}
+
+export async function editLambdaCommand(functionNode: LambdaFunctionNode) {
+    const region = functionNode.regionCode
+    const functionName = functionNode.configuration.FunctionName!
+    return await editLambda({ name: functionName, region, configuration: functionNode.configuration })
+}
+
+export async function editLambda(lambda: LambdaFunction, onActivation?: boolean) {
+    return await telemetry.lambda_quickEditFunction.run(async () => {
+        telemetry.record({ source: onActivation ? 'workspace' : 'explorer' })
+        const { name, region, configuration } = lambda
+        const downloadLocation = getTempLocation(lambda.name, lambda.region)
+        const downloadLocationName = vscode.workspace.asRelativePath(downloadLocation, true)
+
+        // We don't want to do anything if the folder already exists as a workspace folder, it means it's already being edited
+        if (
+            vscode.workspace.workspaceFolders?.some((folder) => folder.uri.fsPath === downloadLocation && !onActivation)
+        ) {
+            return downloadLocation
+        }
+
+        const prompt = localize(
+            'AWS.lambda.download.confirmOutdatedSync',
+            'There are changes to your Function in the cloud since you last edited locally, do you want to overwrite your local changes?'
+        )
+
+        // We want to overwrite changes in the following cases:
+        // 1. There is no code sha locally (getCodeShaLocal returns falsy)
+        // 2. There is a code sha locally, it does not match the one remotely, and the user confirms they want to overwrite it
+        const localExists = !!(await getFunctionInfo(lambda, 'sha'))
+        // This record tells us if they're attempting to edit a function they've edited before
+        telemetry.record({ action: localExists ? 'existingEdit' : 'newEdit' })
+
+        const overwriteChanges =
+            !localExists || (!(await compareCodeSha(lambda)) ? await confirmOutdatedChanges(prompt) : false)
+
+        if (overwriteChanges) {
+            try {
+                // Clear directory contents instead of deleting to avoid Windows EBUSY errors
+                if (await fs.existsDir(downloadLocation)) {
+                    const entries = await fs.readdir(downloadLocation)
+                    await Promise.all(
+                        entries.map((entry) =>
+                            fs.delete(path.join(downloadLocation, entry[0]), { recursive: true, force: true })
+                        )
+                    )
+                } else {
+                    await fs.mkdir(downloadLocation)
+                }
+
+                await downloadLambdaInLocation(lambda, downloadLocationName, downloadLocation)
+
+                // Watching for updates, then setting info, then removing the badges must be done in this order
+                // This is because the files creating can throw the watcher, which sometimes leads to changes being marked as undeployed
+                watchForUpdates(lambda, vscode.Uri.file(downloadLocation))
+                await setFunctionInfo(lambda, {
+                    lastDeployed: globals.clock.Date.now(),
+                    undeployed: false,
+                    sha: lambda.configuration!.CodeSha256,
+                })
+                await LambdaFunctionNodeDecorationProvider.getInstance().removeBadge(
+                    vscode.Uri.file(downloadLocation),
+                    vscode.Uri.from({ scheme: 'lambda', path: `${lambda.region}/${lambda.name}` })
+                )
+            } catch {
+                throw new ToolkitError('Failed to download Lambda function', { code: 'failedDownload' })
+            }
+        } else {
+            const lambdaLocation = path.join(downloadLocation, getLambdaDetails(lambda.configuration!).fileName)
+            await openLambdaFile(lambdaLocation)
+            watchForUpdates(lambda, vscode.Uri.file(downloadLocation))
+        }
+
+        const newEdit = { location: downloadLocationName, region, functionName: name, configuration }
+        lambdaEdits.push(newEdit)
+
+        return downloadLocation
+    })
+}
+
+export async function openLambdaFolderForEdit(name: string, region: string) {
+    const downloadLocation = getTempLocation(name, region)
+
+    if (
+        vscode.workspace.workspaceFolders?.some((workspaceFolder) =>
+            workspaceFolder.uri.fsPath.toLowerCase().startsWith(downloadLocation.toLowerCase())
+        )
+    ) {
+        // If the folder already exists in the workspace, show that folder
+        await vscode.commands.executeCommand('workbench.action.focusSideBar')
+        await vscode.commands.executeCommand('workbench.view.explorer')
+    } else {
+        await fs.mkdir(downloadLocation)
+
+        await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(downloadLocation), {
+            newWindow: true,
+            noRecentEntry: true,
+        })
+    }
+}
+
+export async function getReadme(): Promise<string> {
+    const readmeSource = 'resources/markdown/lambdaEdit.md'
+    const readmeDestination = path.join(lambdaTempPath, 'README.md')
+    const readmeContent = await fs.readFileText(globals.context.asAbsolutePath(readmeSource))
+    await fs.writeFile(readmeDestination, readmeContent)
+
+    // Put cloud deploy icon in the readme
+    const createStackIconSource = 'resources/icons/aws/lambda/create-stack-light.svg'
+    const createStackIconDestination = path.join(lambdaTempPath, 'create-stack.svg')
+    await fs.copy(globals.context.asAbsolutePath(createStackIconSource), createStackIconDestination)
+
+    return readmeDestination
+}

commit 9cf9bdd3f66cb15fa43d8d65a652f99db6d93103

fix(lambda): Revert update to function upload directory selection wizard (#7624)
fixes #7618

## Problem
Introduced in #7601 is a bug where choosing to upload a Lambda function
from a directory would not allow the user to actually select the
directory. In the directory selection window, the button to select is
disabled. This was an inadvertent change when adding new functionality.

## Solution
Revert the breaking change. In addition to switching `canSelectFolders`
to `true` and `canSelectFiles` to `false`, I have reverted the other
changes in the upload flow that were added in the original PR. This is
because these changes added another flow to the upload modal that
doesn't fit with all the other added functionality, and just increases
the complexity of the code without much benefit. Because this was the
only place that used the `lambdaEdits` array from the `utils`, I removed
that as well.

---

- Treat all work as PUBLIC. Private `feature/x` branches will not be
squash-merged at release time.
- Your code changes must meet the guidelines in
[CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
- License: I confirm that my contribution is made under the terms of the
Apache 2.0 license.

packages/core/src/lambda/commands/editLambda.ts
@@ -12,7 +12,6 @@ import {
     getFunctionInfo,
     getLambdaDetails,
     getTempLocation,
-    lambdaEdits,
     lambdaTempPath,
     setFunctionInfo,
 } from '../utils'
@@ -138,7 +137,6 @@ export async function editLambdaCommand(functionNode: LambdaFunctionNode) {
 export async function editLambda(lambda: LambdaFunction, onActivation?: boolean) {
     return await telemetry.lambda_quickEditFunction.run(async () => {
         telemetry.record({ source: onActivation ? 'workspace' : 'explorer' })
-        const { name, region, configuration } = lambda
         const downloadLocation = getTempLocation(lambda.name, lambda.region)
         const downloadLocationName = vscode.workspace.asRelativePath(downloadLocation, true)
 
@@ -200,9 +198,6 @@ export async function editLambda(lambda: LambdaFunction, onActivation?: boolean)
             await openLambdaFile(lambdaLocation)
             watchForUpdates(lambda, vscode.Uri.file(downloadLocation))
         }
-
-        const newEdit = { location: downloadLocationName, region, functionName: name, configuration }
-        lambdaEdits.push(newEdit)
 
         return downloadLocation
     })

packages/core/src/lambda/commands/uploadLambda.ts
@@ -19,14 +19,13 @@ import { SamCliBuildInvocation } from '../../shared/sam/cli/samCliBuild'
 import { getSamCliContext } from '../../shared/sam/cli/samCliContext'
 import { SamTemplateGenerator } from '../../shared/templates/sam/samTemplateGenerator'
 import { addCodiconToString } from '../../shared/utilities/textUtilities'
-import { getLambdaEditFromNameRegion, getLambdaDetails, listLambdaFunctions } from '../utils'
+import { getLambdaDetails, listLambdaFunctions } from '../utils'
 import { getIdeProperties } from '../../shared/extensionUtilities'
 import { createQuickPick, DataQuickPickItem } from '../../shared/ui/pickerPrompter'
 import { createCommonButtons } from '../../shared/ui/buttons'
 import { StepEstimator, Wizard, WIZARD_BACK } from '../../shared/wizards/wizard'
 import { createSingleFileDialog } from '../../shared/ui/common/openDialog'
 import { Prompter, PromptResult } from '../../shared/ui/prompter'
-import { SkipPrompter } from '../../shared/ui/common/skipPrompter'
 import { ToolkitError } from '../../shared/errors'
 import { FunctionConfiguration } from 'aws-sdk/clients/lambda'
 import globals from '../../shared/extensionGlobals'
@@ -104,13 +103,6 @@ export async function uploadLambdaCommand(lambdaArg?: LambdaFunction, path?: vsc
         } else if (response.uploadType === 'directory' && response.directoryBuildType) {
             result = (await runUploadDirectory(lambda, response.directoryBuildType, response.targetUri)) ?? result
             result = 'Succeeded'
-        } else if (response.uploadType === 'edit') {
-            const functionPath = getLambdaEditFromNameRegion(lambda.name, lambda.region)?.location
-            if (!functionPath) {
-                throw new ToolkitError('Function had a local copy before, but not anymore')
-            } else {
-                await runUploadDirectory(lambda, 'zip', vscode.Uri.file(functionPath))
-            }
         }
         // TODO(sijaden): potentially allow the wizard to easily support tagged-union states
     } catch (err) {
@@ -139,8 +131,8 @@ export async function uploadLambdaCommand(lambdaArg?: LambdaFunction, path?: vsc
 /**
  * Selects the type of file to upload (zip/dir) and proceeds with the rest of the workflow.
  */
-function createUploadTypePrompter(lambda?: LambdaFunction) {
-    const items: DataQuickPickItem<'edit' | 'zip' | 'directory'>[] = [
+function createUploadTypePrompter() {
+    const items: DataQuickPickItem<'zip' | 'directory'>[] = [
         {
             label: addCodiconToString('file-zip', localize('AWS.generic.filetype.zipfile', 'ZIP Archive')),
             data: 'zip',
@@ -150,17 +142,6 @@ function createUploadTypePrompter(lambda?: LambdaFunction) {
             data: 'directory',
         },
     ]
-
-    if (lambda !== undefined) {
-        const { region, name: functionName } = lambda
-        const lambdaEdit = getLambdaEditFromNameRegion(functionName, region)
-        if (lambdaEdit) {
-            items.unshift({
-                label: addCodiconToString('edit', localize('AWS.generic.filetype.edit', 'Local edit')),
-                data: 'edit',
-            })
-        }
-    }
 
     return createQuickPick(items, {
         title: localize('AWS.lambda.upload.title', 'Select Upload Type'),
@@ -215,7 +196,7 @@ function createConfirmDeploymentPrompter(lambda: LambdaFunction) {
 }
 
 export interface UploadLambdaWizardState {
-    readonly uploadType: 'edit' | 'zip' | 'directory'
+    readonly uploadType: 'zip' | 'directory'
     readonly targetUri: vscode.Uri
     readonly directoryBuildType: 'zip' | 'sam'
     readonly confirmedDeploy: boolean
@@ -234,23 +215,23 @@ export class UploadLambdaWizard extends Wizard<UploadLambdaWizardState> {
                 this.form.targetUri.setDefault(this.invokePath)
             }
         } else {
-            this.form.uploadType.bindPrompter(() => createUploadTypePrompter(this.lambda))
-            this.form.targetUri.bindPrompter(
-                ({ uploadType }) => {
-                    if (uploadType === 'directory') {
-                        return createSingleFileDialog({
-                            canSelectFolders: false,
-                            canSelectFiles: true,
-                            filters: {
-                                'ZIP archive': ['zip'],
-                            },
-                        })
-                    } else {
-                        return new SkipPrompter()
-                    }
-                },
-                { showWhen: ({ uploadType }) => uploadType !== 'edit' }
-            )
+            this.form.uploadType.bindPrompter(() => createUploadTypePrompter())
+            this.form.targetUri.bindPrompter(({ uploadType }) => {
+                if (uploadType === 'directory') {
+                    return createSingleFileDialog({
+                        canSelectFolders: true,
+                        canSelectFiles: false,
+                    })
+                } else {
+                    return createSingleFileDialog({
+                        canSelectFolders: false,
+                        canSelectFiles: true,
+                        filters: {
+                            'ZIP archive': ['zip'],
+                        },
+                    })
+                }
+            })
         }
 
         this.form.lambda.name.bindPrompter((state) => {
@@ -277,12 +258,7 @@ export class UploadLambdaWizard extends Wizard<UploadLambdaWizardState> {
             this.form.directoryBuildType.setDefault('zip')
         }
 
-        this.form.confirmedDeploy.bindPrompter(
-            (state) => {
-                return createConfirmDeploymentPrompter(state.lambda!)
-            },
-            { showWhen: ({ uploadType }) => uploadType !== 'edit' }
-        )
+        this.form.confirmedDeploy.bindPrompter((state) => createConfirmDeploymentPrompter(state.lambda!))
 
         return this
     }

packages/core/src/lambda/utils.ts
@@ -202,23 +202,3 @@ export function getTempRegionLocation(region: string) {
 export function getTempLocation(functionName: string, region: string) {
     return path.join(getTempRegionLocation(region), functionName)
 }
-
-type LambdaEdit = {
-    location: string
-    functionName: string
-    region: string
-    configuration?: Lambda.FunctionConfiguration
-}
-
-// Array to keep the list of functions that are being edited.
-export const lambdaEdits: LambdaEdit[] = []
-
-// Given a particular function and region, it returns the full LambdaEdit object
-export function getLambdaEditFromNameRegion(name: string, functionRegion: string) {
-    return lambdaEdits.find(({ functionName, region }) => functionName === name && region === functionRegion)
-}
-
-// Given a particular localPath, it returns the full LambdaEdit object
-export function getLambdaEditFromLocation(functionLocation: string) {
-    return lambdaEdits.find(({ location }) => location === functionLocation)
-}

packages/core/src/test/lambda/commands/editLambda.test.ts
@@ -75,7 +75,6 @@ describe('editLambda', function () {
         sinon.replace(require('../../../lambda/commands/editLambda'), 'promptDeploy', promptDeployStub)
 
         // Other stubs
-        sinon.stub(utils, 'lambdaEdits').value([])
         sinon.stub(utils, 'getLambdaDetails').returns({ fileName: 'index.js', functionName: 'test-function' })
         sinon.stub(fs, 'readdir').resolves([])
         sinon.stub(fs, 'delete').resolves()

packages/core/src/test/lambda/utils.test.ts
@@ -12,9 +12,6 @@ import {
     getFunctionInfo,
     setFunctionInfo,
     compareCodeSha,
-    lambdaEdits,
-    getLambdaEditFromNameRegion,
-    getLambdaEditFromLocation,
 } from '../../lambda/utils'
 import { LambdaFunction } from '../../lambda/commands/uploadLambda'
 import { DefaultLambdaClient } from '../../shared/clients/lambdaClient'
@@ -185,50 +182,6 @@ describe('lambda utils', function () {
 
             const result = await compareCodeSha(mockLambda)
             assert.strictEqual(result, false)
-        })
-    })
-
-    describe('lambdaEdits array functions', function () {
-        beforeEach(function () {
-            lambdaEdits.length = 0
-            lambdaEdits.push(
-                {
-                    location: '/tmp/func1',
-                    functionName: 'func1',
-                    region: 'us-east-1',
-                },
-                {
-                    location: '/tmp/func2',
-                    functionName: 'func2',
-                    region: 'us-west-2',
-                }
-            )
-        })
-
-        describe('getLambdaEditFromNameRegion', function () {
-            it('finds edit by name and region', function () {
-                const result = getLambdaEditFromNameRegion('func1', 'us-east-1')
-                assert.strictEqual(result?.functionName, 'func1')
-                assert.strictEqual(result?.region, 'us-east-1')
-            })
-
-            it('returns undefined when not found', function () {
-                const result = getLambdaEditFromNameRegion('nonexistent', 'us-east-1')
-                assert.strictEqual(result, undefined)
-            })
-        })
-
-        describe('getLambdaEditFromLocation', function () {
-            it('finds edit by location', function () {
-                const result = getLambdaEditFromLocation('/tmp/func2')
-                assert.strictEqual(result?.functionName, 'func2')
-                assert.strictEqual(result?.location, '/tmp/func2')
-            })
-
-            it('returns undefined when not found', function () {
-                const result = getLambdaEditFromLocation('/tmp/nonexistent')
-                assert.strictEqual(result, undefined)
-            })
         })
     })
 })