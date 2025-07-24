ac7cdc687ada29d85b414129cb86cb31e9d380d5
laura-codess <121297288+laura-codess@users.noreply.github.com>
2025-07-24 11:40:55 -0400

    some initial implementation, and a gitignore fix, as well as a testFileFolder

--- packages/amazonq/test/e2e_new/amazonq/helpers/rulesHelper.ts
+++ packages/amazonq/test/e2e_new/amazonq/helpers/rulesHelper.ts
@@ -3,23 +3,56 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { WebviewView, By } from 'vscode-extension-tester'
+import { waitForElement } from '../utils/generalUtils'
 
 /**
- * Clicks the tools to get to the MCP server overlay
+ * Clicks the Rules button in the top bar
  * @param webviewView The WebviewView instance
- * @returns Promise<boolean> True if tools button was found and clicked, false otherwise
  */
 export async function clickRulesButton(webviewView: WebviewView): Promise<void> {
-    const buttons = await webviewView.findElements(
-        By.css('.mynah-button.mynah-button-secondary.fill-state-always.status-clear.mynah-ui-clickable-item')
-    )
-    for (const button of buttons) {
-        const span = await button.findElement(By.css('span'))
-        const text = await span.getText()
-        if (text === 'Rules') {
-            await button.click()
-            return
-        }
-    }
-    throw new Error('Rules button not found')
+    const wrapper = await webviewView.findElement(By.css('.mynah-chat-prompt-wrapper'))
+    const topBar = await wrapper.findElement(By.css('.mynah-prompt-input-top-bar'))
+    console.log('THIS WORKS 1')
+    const buttons = await topBar.findElement(By.css('[data-testid="prompt-input-top-bar-button"]'))
+    console.log('THIS WORKS 2')
+    const button = await buttons.findElement(By.css('*'))
+    console.log('THIS WORKS 3')
+    await button.click()
+}
+
+/**
+ * Creates a new rule with the specified name
+ * @param webviewView The WebviewView instance
+ * @param ruleName The name of the rule to create (defaults to "testRule")
+ */
+export async function createRule(webviewView: WebviewView, ruleName: string = 'testRule'): Promise<void> {
+    const overlay = await waitForElement(webviewView, By.css('[data-testid="prompt-input-top-bar-action-overlay"]'))
+    const create = overlay.findElement(By.css('[data-testid="prompt-input-quick-pick-item"]'))
+    await create.click()
+    const anotheroverlay = await waitForElement(webviewView, By.css('[data-testid="sheet-wrapper"]'))
+    const input = await anotheroverlay.findElement(By.css('[data-testid="chat-item-form-item-text-input"]'))
+    await input.sendKeys(ruleName)
+    await clickCreateRule(webviewView)
+}
+
+/**
+ * Clicks the Create button in the rule creation dialog
+ * @param webviewView The WebviewView instance
+ */
+export async function clickCreateRule(webviewView: WebviewView): Promise<void> {
+    const anotheroverlay = await waitForElement(webviewView, By.css('[data-testid="sheet-wrapper"]'))
+    const buttonsContainer = await anotheroverlay.findElement(By.css('[data-testid="chat-item-buttons-wrapper"]'))
+    const button = await buttonsContainer.findElements(By.css('[data-testid="chat-item-action-button"]'))
+    await button[1].click()
+}
+
+/**
+ * Clicks the Cancel button in the rule creation dialog
+ * @param webviewView The WebviewView instance
+ */
+export async function clickCancelRule(webviewView: WebviewView): Promise<void> {
+    const anotheroverlay = await waitForElement(webviewView, By.css('[data-testid="sheet-wrapper"]'))
+    const buttonsContainer = await anotheroverlay.findElement(By.css('[data-testid="chat-item-buttons-wrapper"]'))
+    const button = await buttonsContainer.findElements(By.css('[data-testid="chat-item-action-button"]'))
+    await button[0].click()
 }
--- packages/amazonq/test/e2e_new/amazonq/tests/rules.test.ts
+++ packages/amazonq/test/e2e_new/amazonq/tests/rules.test.ts
@@ -3,41 +3,55 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import '../utils/setup'
-import { WebviewView } from 'vscode-extension-tester'
+import {
+    WebviewView,
+    VSBrowser,
+    DefaultTreeSection,
+    ActivityBar,
+    SideBarView,
+    ViewContent,
+    Workbench,
+} from 'vscode-extension-tester'
 import { closeAllTabs } from '../utils/cleanupUtils'
 import { testContext } from '../utils/testContext'
-import { clickRulesButton } from '../helpers/rulesHelper'
-import { sleep } from '../utils/generalUtils'
+import * as path from 'path'
+import { clickRulesButton, createRule } from '../helpers/rulesHelper'
 
 describe('Amazon Q Rules Functionality', function () {
     // this timeout is the general timeout for the entire test suite
     this.timeout(150000)
     let webviewView: WebviewView
+    let tree: DefaultTreeSection
+    let content: ViewContent
+    let workbench: Workbench
 
     before(async function () {
+        // switch out of the webview (we assume that the last test was a webview test)
         webviewView = testContext.webviewView
+        await webviewView.switchBack()
+
+        // in order to access rules you must have at least 1 folder
+        await VSBrowser.instance.openResources(path.join('..', 'utils', 'resources', 'testFolder'))
+        ;(await new ActivityBar().getViewControl('Explorer'))?.openView()
+        const view = new SideBarView()
+        content = view.getContent()
+        tree = (await content.getSection('testFolder')) as DefaultTreeSection
+        await tree.openItem('test-folder')
+
+        // once the folder is opened, we switch back to the amazonQ webview
+        workbench = testContext.workbench
+        await workbench.executeCommand('Amazon Q: Open Chat')
+        webviewView = new WebviewView()
+        await webviewView.switchToFrame()
+        testContext.webviewView = webviewView
     })
 
     after(async function () {
         await closeAllTabs(webviewView)
     })
 
-    afterEach(async () => {})
-
-    it('Rules Test', async () => {
-        /**
-         * TODO abstractions
-         *
-         * click the rules button DONE
-         * list all the possible rules (disappearing overlay again UGH)
-         * check and uncheck a rule
-         * click create new rule
-         * enter rule name
-         * click create rule
-         * click cancel rule
-         */
+    it('Rules Option Test', async () => {
         await clickRulesButton(webviewView)
-
-        await sleep(5000)
+        await createRule(webviewView)
     })
 })
