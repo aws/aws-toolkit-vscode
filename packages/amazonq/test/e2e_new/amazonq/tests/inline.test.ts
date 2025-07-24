81b4e681daec1f20ce9cda5f31773512492adfe2
laura-codess <121297288+laura-codess@users.noreply.github.com>
2025-07-24 15:35:00 -0400

    inline completion test done

--- packages/amazonq/test/e2e_new/amazonq/tests/inline.test.ts
+++ packages/amazonq/test/e2e_new/amazonq/tests/inline.test.ts
@@ -5,12 +5,7 @@
 import '../utils/setup'
 import { Workbench, EditorView, InputBox, TextEditor, WebviewView, Key } from 'vscode-extension-tester'
 import { testContext } from '../utils/testContext'
-import { sleep, expect } from '../utils/generalUtils'
-
-function pressShortcut(element: any, key: string, platform: 'mac' | 'windows', modifier: 'cmd' | 'alt' = 'cmd') {
-    const modifierKey = modifier === 'cmd' ? (platform === 'mac' ? Key.META : Key.CONTROL) : Key.ALT
-    return element.sendKeys(modifierKey, key)
-}
+import { sleep, expect, pressKey, createNewTextFile } from '../utils/generalUtils'
 
 describe('Amazon Q Inline Completion / Chat Functionality', function () {
     // this timeout is the general timeout for the entire test suite
@@ -25,48 +20,33 @@ describe('Amazon Q Inline Completion / Chat Functionality', function () {
         await webviewView.switchBack()
         workbench = testContext.workbench
 
-        await workbench.executeCommand('Create: New File...')
-        await (await InputBox.create()).selectQuickPick('Text File')
-        await sleep(1000)
         editorView = new EditorView()
         testContext.editorView = editorView
-        textEditor = (await editorView.openEditor('Untitled-1')) as TextEditor
-        // or if the file we want is currently opened we can simply do
-        // editor = new TextEditor();
+
+        textEditor = await createNewTextFile(workbench, editorView)
     })
 
-    after(async function () {
-        // cleanup, delete the file contents and close the editor
-        await textEditor.clearText()
-        await editorView.closeAllEditors()
-
-        // after, in order to not affect the other tests, we must switch back to the webview
-        await webviewView.switchToFrame()
-    })
+    // after(async function () {
+    //     await editorView.closeAllEditors()
+    // })
 
     it('Inline Test', async () => {
-        // the file is currently empty, lets write something in it
-        // note the coordinates are (1, 1) for the beginning of the file
-        await textEditor.typeTextAt(1, 1, 'hello')
-
-        // now we can check if the text is correct
+        await textEditor.typeTextAt(1, 1, 'Select Me')
         const text = await textEditor.getText()
-        expect(text).equals('hello')
+        expect(text).equals('Select Me')
+        await textEditor.clearText()
 
+        await workbench.executeCommand('Amazon Q: Inline Chat')
+        const input = new InputBox()
+        await input.sendKeys('Write a simple sentece')
+        await input.sendKeys(Key.ENTER)
+        await sleep(5000)
+        const driver = textEditor.getDriver()
+        await pressKey(driver, 'Enter')
+        await sleep(3000)
+        await pressKey(driver, 'Tab')
         // we can also replace all the text with whatever we want
-        await textEditor.setText(`line1\nline2\nline3`)
-        // assert how many lines there are now
-        expect(await textEditor.getNumberOfLines()).equals(3)
-
-        // get text at the line with given number
-        const line = await textEditor.getTextAtLine(2)
-        expect(line).include('line2')
-
-        // get the line number of a search string
-        const lineNum = await textEditor.getLineOfText('3')
-        expect(lineNum).equals(3)
-
-        // the editor should be dirty since we haven't saved yet
-        expect(await textEditor.isDirty()).is.true
+        await sleep(3000)
+        // await textEditor.clear()
     })
 })
