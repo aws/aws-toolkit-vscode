12b77630712c7eece0f78e35c92ebe002a2c2d3f
Suraj Reddy <surajrdy@amazon.com>
2025-08-11 15:42:39 -0400

    feature: add inline shortcut test

--- packages/amazonq/test/e2e_new/amazonq/tests/inline.test.ts
+++ packages/amazonq/test/e2e_new/amazonq/tests/inline.test.ts
@@ -5,7 +5,7 @@
 import '../utils/setup'
 import { Workbench, EditorView, InputBox, TextEditor, WebviewView, Key } from 'vscode-extension-tester'
 import { testContext } from '../utils/testContext'
-import { createNewTextFile, writeToTextEditor, waitForInlineGeneration } from '../utils/generalUtils'
+import { createNewTextFile, writeToTextEditor, waitForInlineGeneration, pressShortcut } from '../utils/generalUtils'
 import assert from 'assert'
 
 describe('Amazon Q Inline Completion / Chat Functionality', function () {
@@ -16,7 +16,7 @@ describe('Amazon Q Inline Completion / Chat Functionality', function () {
     let textEditor: TextEditor
     let webviewView: WebviewView
 
-    before(async function () {
+    beforeEach(async function () {
         webviewView = testContext.webviewView
         await webviewView.switchBack()
         workbench = testContext.workbench
@@ -24,17 +24,12 @@ describe('Amazon Q Inline Completion / Chat Functionality', function () {
         testContext.editorView = editorView
         textEditor = await createNewTextFile(workbench, editorView)
     })
-    after(async function () {
+    afterEach(async function () {
         // Switch back to Webview Iframe when dealing with external webviews from Amazon Q.
         await editorView.closeAllEditors()
         await webviewView.switchToFrame()
     })
-    it('Inline Test Shortcut', async () => {
-        await writeToTextEditor(textEditor, 'def factorial(n):')
-        const text = await textEditor.getText()
-        assert.equal(text, 'def factorial(n): ')
-        await textEditor.clearText()
-
+    it('Inline Test Generate', async () => {
         const textBefore = await textEditor.getText()
         await workbench.executeCommand('Amazon Q: Inline Chat')
         const input = new InputBox()
@@ -45,6 +40,19 @@ describe('Amazon Q Inline Completion / Chat Functionality', function () {
 
         const textAfter = await textEditor.getText()
         assert(textAfter.length > textBefore.length, 'Amazon Q generated code')
+        await textEditor.clearText()
+    })
+    it('Inline Keybind Shortcut', async () => {
+        await writeToTextEditor(textEditor, 'def fibonacci(n):')
+        await textEditor.selectText('def fibonacci(n):')
+
+        const driver = webviewView.getDriver()
+        await pressShortcut(driver, Key.COMMAND, 'i')
+        const input = new InputBox()
+        await input.sendKeys('Generate the fibonacci sequence through recursion')
+        await input.sendKeys(Key.ENTER)
+        await waitForInlineGeneration(textEditor)
+        //Clean Up Text
         await textEditor.clearText()
     })
 })
