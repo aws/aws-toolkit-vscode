9d0c51205cb5d8421b11a8f435dc0d261acac808
Will Lo <xshaohua@amazon.com>
2025-08-14 14:09:53 -0700

    linter

--- packages/amazonq/src/lsp/client.ts
+++ packages/amazonq/src/lsp/client.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import vscode, { version } from 'vscode'
+import vscode from 'vscode'
 import * as nls from 'vscode-nls'
 import { LanguageClient, LanguageClientOptions, RequestType, State } from 'vscode-languageclient'
 import { InlineCompletionManager } from '../app/inline/completion'
