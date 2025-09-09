77676b329957c24b6a2f4769774ecda1128ef01b
andrewyuq <89420755+andrewyuq@users.noreply.github.com>
2025-09-09 11:45:18 -0700

    feat(amazonq): enable pre flare experiment (#7999)
    ## Problem
    
    
    ## Solution
    
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/amazonq/src/lsp/client.ts
+++ packages/amazonq/src/lsp/client.ts
@@ -21,6 +21,7 @@ import {
 import {
     AuthUtil,
     CodeWhispererSettings,
+    FeatureConfigProvider,
     getSelectedCustomization,
     TelemetryHelper,
     vsCodeState,
@@ -339,12 +340,14 @@ async function onLanguageServerReady(
     // tutorial for inline chat
     const inlineChatTutorialAnnotation = new InlineChatTutorialAnnotation(inlineTutorialAnnotation)
 
-    const enableInlineRollback = true
+    const enableInlineRollback = FeatureConfigProvider.instance.getPreFlareRollbackGroup() === 'treatment'
     if (enableInlineRollback) {
         // use VSC inline
+        getLogger().info('Entering preflare logic')
         await activateInline(client)
     } else {
         // use language server for inline completion
+        getLogger().info('Entering postflare logic')
         const inlineManager = new InlineCompletionManager(client, sessionManager, lineTracker, inlineTutorialAnnotation)
         inlineManager.registerInlineCompletion()
         toDispose.push(
