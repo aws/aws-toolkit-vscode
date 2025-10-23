7c1d4800bfbd2140f099930332e412e8adb17704
Will Lo <xshaohua@amazon.com>
2025-10-23 16:46:40 -0700

    fix: 123

--- packages/amazonq/test/e2e/inline/inline.test.ts
+++ packages/amazonq/test/e2e/inline/inline.test.ts
@@ -14,7 +14,7 @@ import {
     toTextEditor,
     using,
 } from 'aws-core-vscode/test'
-import { RecommendationHandler, RecommendationService, session } from 'aws-core-vscode/codewhisperer'
+import { RecommendationService } from 'aws-core-vscode/codewhisperer'
 import { Commands, globals, sleep, waitUntil, collectionUtil } from 'aws-core-vscode/shared'
 import { loginToIdC } from '../amazonq/utils/setup'
 
@@ -69,31 +69,6 @@ describe('Amazon Q Inline', async function () {
         await toTextEditor(textContents, fileName, tempFolder, {
             selection: new vscode.Range(new vscode.Position(1, 4), new vscode.Position(1, 4)),
         })
-    }
-
-    async function waitForRecommendations() {
-        const suggestionShown = await waitUntil(async () => session.getSuggestionState(0) === 'Showed', waitOptions)
-        if (!suggestionShown) {
-            throw new Error(`Suggestion did not show. Suggestion States: ${JSON.stringify(session.suggestionStates)}`)
-        }
-        const suggestionVisible = await waitUntil(
-            async () => RecommendationHandler.instance.isSuggestionVisible(),
-            waitOptions
-        )
-        if (!suggestionVisible) {
-            throw new Error(
-                `Suggestions failed to become visible. Suggestion States: ${JSON.stringify(session.suggestionStates)}`
-            )
-        }
-        console.table({
-            'suggestions states': JSON.stringify(session.suggestionStates),
-            'valid recommendation': RecommendationHandler.instance.isValidResponse(),
-            'recommendation service status': RecommendationService.instance.isRunning,
-            recommendations: session.recommendations,
-        })
-        if (!RecommendationHandler.instance.isValidResponse()) {
-            throw new Error('Did not find a valid response')
-        }
     }
 
     /**
