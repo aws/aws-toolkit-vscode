6a7eed33f91fe62df8daab9d4aecbad47bc9e4cd
Zeeshan Ahmed <zeemed@amazon.com>
2025-12-12 18:05:45 -0500

    fix lint

--- packages/core/src/test/awsService/cloudformation/consoleLinksUtils.test.ts
+++ packages/core/src/test/awsService/cloudformation/consoleLinksUtils.test.ts
@@ -4,7 +4,11 @@
  */
 
 import * as assert from 'assert'
-import { arnToConsoleUrl, arnToConsoleTabUrl, operationIdToConsoleUrl } from '../../../awsService/cloudformation/consoleLinksUtils'
+import {
+    arnToConsoleUrl,
+    arnToConsoleTabUrl,
+    operationIdToConsoleUrl,
+} from '../../../awsService/cloudformation/consoleLinksUtils'
 
 describe('consoleLinksUtils', () => {
     const testArn = 'arn:aws:cloudformation:us-west-2:123456789012:stack/test-stack/abc-123'
@@ -12,24 +16,36 @@ describe('consoleLinksUtils', () => {
     describe('arnToConsoleUrl', () => {
         it('should generate correct console URL', () => {
             const url = arnToConsoleUrl(testArn)
-            assert.strictEqual(url, 'https://console.aws.amazon.com/go/view?arn=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123')
+            assert.strictEqual(
+                url,
+                'https://console.aws.amazon.com/go/view?arn=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123'
+            )
         })
     })
 
     describe('arnToConsoleTabUrl', () => {
         it('should generate correct events tab URL', () => {
             const url = arnToConsoleTabUrl(testArn, 'events')
-            assert.strictEqual(url, 'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/events?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123')
+            assert.strictEqual(
+                url,
+                'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/events?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123'
+            )
         })
 
         it('should generate correct resources tab URL', () => {
             const url = arnToConsoleTabUrl(testArn, 'resources')
-            assert.strictEqual(url, 'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/resources?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123')
+            assert.strictEqual(
+                url,
+                'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/resources?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123'
+            )
         })
 
         it('should generate correct outputs tab URL', () => {
             const url = arnToConsoleTabUrl(testArn, 'outputs')
-            assert.strictEqual(url, 'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/outputs?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123')
+            assert.strictEqual(
+                url,
+                'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/outputs?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123'
+            )
         })
     })
 
@@ -37,7 +53,10 @@ describe('consoleLinksUtils', () => {
         it('should generate correct operation details URL', () => {
             const operationId = '056a1310-6307-466a-a167-2cbbd353b29f'
             const url = operationIdToConsoleUrl(testArn, operationId)
-            assert.strictEqual(url, 'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/operations/info?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123&operationId=056a1310-6307-466a-a167-2cbbd353b29f')
+            assert.strictEqual(
+                url,
+                'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/operations/info?stackId=arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack%2Ftest-stack%2Fabc-123&operationId=056a1310-6307-466a-a167-2cbbd353b29f'
+            )
         })
 
         it('should handle different regions', () => {
