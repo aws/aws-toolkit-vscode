3ae0bc78d4e01d3bc3efb54b36aa7e8e68fb3795
Will Lo <xshaohua@amazon.com>
2026-01-06 13:02:44 -0800

    linter

--- packages/amazonq/src/lsp/client.ts
+++ packages/amazonq/src/lsp/client.ts
@@ -22,7 +22,7 @@ import {
 import {
     AuthUtil,
     CodeWhispererSettings,
-    FeatureConfigProvider,
+    // FeatureConfigProvider,
     getSelectedCustomization,
     TelemetryHelper,
     vsCodeState,
