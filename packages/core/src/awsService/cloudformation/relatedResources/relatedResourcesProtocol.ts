02cd1c78b82ddab14829d78e1ca18e8d38d86d13
gemammercado <gemamrcd@amazon.com>
2026-03-19 16:23:24 -0400

    feat(cloudformation): add parent resource selection for related resources

--- packages/core/src/awsService/cloudformation/relatedResources/relatedResourcesProtocol.ts
+++ packages/core/src/awsService/cloudformation/relatedResources/relatedResourcesProtocol.ts
@@ -11,10 +11,16 @@ export type GetRelatedResourceTypesParams = {
     parentResourceType: string
 }
 
+export type AuthoredResource = {
+    logicalId: string
+    type: string
+}
+
 export type InsertRelatedResourcesParams = {
     templateUri: string
     relatedResourceTypes: string[]
     parentResourceType: string
+    parentLogicalId?: string
 }
 
 export interface RelatedResourcesCodeAction extends CodeAction {
@@ -24,7 +30,7 @@ export interface RelatedResourcesCodeAction extends CodeAction {
     }
 }
 
-export const GetAuthoredResourceTypesRequest = new RequestType<TemplateUri, string[], void>(
+export const GetAuthoredResourceTypesRequest = new RequestType<TemplateUri, AuthoredResource[], void>(
     'aws/cfn/template/resources/authored'
 )
 
